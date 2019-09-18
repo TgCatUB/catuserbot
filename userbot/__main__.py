5b335beac53ea4dae4ece0478316bb1b7c46af7a
SnapDragon <maliktahir0702@gmail.com>
2019-09-18 23:36:56 +0200

    Create __main__.py

---
+++ userbot/__main__.py
@@ -0,0 +1,18 @@
+from userbot import bot
+from sys import argv
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+
+try:
+    bot.start()
+except PhoneNumberInvalidError:
+    print("Phone Number you added was incorrect. Make sure to use your country code with your code")
+    exit(1)
+
+from userbot.plugins import *
+
+print("Yay your userbot is officially working.")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
