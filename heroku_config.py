4a7f7c5b901c5a1bc1b245dbda7bb227683d29fe
SnapDragon <maliktahir0702@gmail.com>
2019-09-18 23:43:54 +0200

    Create heroku_config.py

---
+++ heroku_config.py
@@ -0,0 +1,14 @@
+import os
+
+class Var(object):
+    APP_ID = int(os.environ.get("APP_ID", 6))
+    # 6 is a placeholder
+    API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    DB_URI = os.environ.get("DATABASE_URL", None)
+    LOGGER = True
+    # Here for later purposes
+
+class Development(Var):
+    LOGGER = True
+    # Here for later purposes
