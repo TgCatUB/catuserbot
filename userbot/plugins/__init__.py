60fc1156529d3cbbc4597244cd9cedbcdc02c336
SnapDragon <maliktahir0702@gmail.com>
2019-09-19 00:18:19 +0200

    Create __init__.py

---
+++ userbot/plugins/__init__.py
@@ -0,0 +1,15 @@
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3] for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+LOGS.info("Modules to load: %s", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
