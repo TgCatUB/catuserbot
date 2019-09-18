58acd9adc5f3c62f0e568113f460adfa5d885189
SnapDragon <maliktahir0702@gmail.com>
2019-09-19 01:28:08 +0200

    Create utils.py

---
+++ userbot/utils.py
@@ -0,0 +1,21 @@
+from userbot import bot
+from telethon import events
+
+def bot.command(**args):
+    pattern = args.get["pattern"]
+    allow_edited_updates = args.get('allow_edited_updates', False)
+
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+
+    if "allow_edited_updates" in args:
+        del args['allow_edited_updates']
+
+    def decorator(func):
+        if not disable_edited:
+            bot.add_event_handler(func, events.MessageEdited(**args))
+        bot.add_event_handler(func, events.NewMessage(**args))
+
+        return func
+
+    return decorator
