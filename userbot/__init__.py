b704b72132e98a92d1aeec447f867df8f7519c27
SnapDragon <maliktahir0702@gmail.com>
2019-09-19 17:38:04 +0200

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,16 +5,7 @@ from pathlib import Path
 from telethon.sessions import StringSession
 from telethon import TelegramClient
 
-ENV = bool(os.environ.get("ENV", False))
-if ENV:
-    from heroku_config import Var
-else:
-    if os.path.exists("config.py"):
-        from local_config import Development as Var
-    else:
-        logging.warning("No local_config.py Found!")
-        logging.info("Please run the command, again, after creating local_config.py similar to README.md")
-        sys.exit(1)
+from config import Config
 
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
