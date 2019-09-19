176b0835964b6686b07f08f905f7d95ffd22f3cd
SnapDragon <maliktahir0702@gmail.com>
2019-09-19 10:52:46 +0200

    Update cmd_list.py

--- userbot/plugins/cmd_list.py
+++ userbot/plugins/cmd_list.py
@@ -5,8 +5,8 @@ from userbot import CMD_LIST
 @command(pattern="^.cmd")
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-    string = ""
-    for i in CMD_LIST:
-        string += "ℹ️ `" + str(i)
-        string += "`\n"
-    await event.reply(string)
+        string = ""
+        for i in CMD_LIST:
+            string += "ℹ️ `" + str(i)
+            string += "`\n"
+        await event.reply(string)
