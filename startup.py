8663b2836b404f99455f7f5a8b381312bcf2ee68
SnapDragon <maliktahir0702@gmail.com>
2019-09-19 10:32:12 +0200

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,21 +1,3 @@
-import logging
-import os
-import sys
-from pathlib import Path
-from telethon.sessions import StringSession
-from telethon import TelegramClient
-
-ENV = bool(os.environ.get("ENV", False))
-if ENV:
-    from heroku_config import Var
-else:
-    if os.path.exists("config.py"):
-        from local_config import Development as Var
-    else:
-        logging.warning("No local_config.py Found!")
-        logging.info("Please run the command, again, after creating local_config.py similar to README.md")
-        sys.exit(1)
-
 import userbot.__init__
 import userbot.__main__
 
