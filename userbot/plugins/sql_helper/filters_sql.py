2d944be88e93d8ac80ca4ac895cb7f41b95c1ec6
SnapDragon <maliktahir0702@gmail.com>
2019-09-20 14:43:54 +0200

    Update filters_sql.py

--- userbot/plugins/sql_helper/filters_sql.py
+++ userbot/plugins/sql_helper/filters_sql.py
@@ -1,40 +1,30 @@
-try:
-    from userbot.modules.sql_helper import SESSION, BASE
-except ImportError:
-    raise AttributeError
 from sqlalchemy import Column, UnicodeText, LargeBinary, Numeric, String
+from sql_helpers import SESSION, BASE
 
 
 class Filters(BASE):
     __tablename__ = "filters"
     chat_id = Column(String(14), primary_key=True)
-    keyword = Column(UnicodeText, primary_key=True, nullable=False)
-    reply = Column(UnicodeText, nullable=False)
+    keyword = Column(UnicodeText, primary_key=True)
+    reply = Column(UnicodeText)
     snip_type = Column(Numeric)
     media_id = Column(UnicodeText)
     media_access_hash = Column(UnicodeText)
     media_file_reference = Column(LargeBinary)
 
-    def __init__(self,
-                 chat_id,
-                 keyword,
-                 reply,
-                 snip_type,
-                 media_id=None,
-                 media_access_hash=None,
-                 media_file_reference=None):
-        self.chat_id = str(chat_id)  # ensure string
+    def __init__(
+        self,
+        chat_id,
+        keyword, reply, snip_type,
+        media_id=None, media_access_hash=None, media_file_reference=None
+    ):
+        self.chat_id = chat_id
         self.keyword = keyword
         self.reply = reply
         self.snip_type = snip_type
         self.media_id = media_id
         self.media_access_hash = media_access_hash
         self.media_file_reference = media_file_reference
-
-    def __eq__(self, other):
-        return bool(
-            isinstance(other, Filters) and self.chat_id == other.chat_id
-            and self.keyword == other.keyword)
 
 
 Filters.__table__.create(checkfirst=True)
@@ -43,51 +33,45 @@ Filters.__table__.create(checkfirst=True)
 def get_filter(chat_id, keyword):
     try:
         return SESSION.query(Filters).get((str(chat_id), keyword))
+    except:
+        return None
     finally:
         SESSION.close()
 
 
-def get_filters(chat_id):
+def get_all_filters(chat_id):
     try:
-        return SESSION.query(Filters).filter(
-            Filters.chat_id == str(chat_id)).all()
+        return SESSION.query(Filters).filter(Filters.chat_id == str(chat_id)).all()
+    except:
+        return None
     finally:
         SESSION.close()
 
 
-def add_filter(chat_id, keyword, reply, snip_type, media_id, media_access_hash,
-               media_file_reference):
-    to_check = get_filter(chat_id, keyword)
-    if not to_check:
-        adder = Filters(str(chat_id), keyword, reply, snip_type, media_id,
-                        media_access_hash, media_file_reference)
-        SESSION.add(adder)
-        SESSION.commit()
-        return True
+def add_filter(chat_id, keyword, reply, snip_type, media_id, media_access_hash, media_file_reference):
+    adder = SESSION.query(Filters).get((str(chat_id), keyword))
+    if adder:
+        adder.reply = reply
+        adder.snip_type = snip_type
+        adder.media_id = media_id
+        adder.media_access_hash = media_access_hash
+        adder.media_file_reference = media_file_reference
     else:
-        rem = SESSION.query(Filters).get((str(chat_id), keyword))
-        SESSION.delete(rem)
-        SESSION.commit()
-        adder = Filters(str(chat_id), keyword, reply, snip_type, media_id,
+        adder = Filters(chat_id, keyword, reply, snip_type, media_id,
                         media_access_hash, media_file_reference)
-        SESSION.add(adder)
-        SESSION.commit()
-        return False
+    SESSION.add(adder)
+    SESSION.commit()
 
 
 def remove_filter(chat_id, keyword):
-    to_check = get_filter(chat_id, keyword)
-    if not to_check:
-        return False
-    else:
-        rem = SESSION.query(Filters).get((str(chat_id), keyword))
-        SESSION.delete(rem)
+    saved_filter = SESSION.query(Filters).get((str(chat_id), keyword))
+    if saved_filter:
+        SESSION.delete(saved_filter)
         SESSION.commit()
-        return True
 
 
-def rm_all_filters(chat_id):
-    filters = SESSION.query(Filters).filter(Filters.chat_id == str(chat_id))
-    if filters:
-        filters.delete()
+def remove_all_filters(chat_id):
+    saved_filter = SESSION.query(Filters).filter(Filters.chat_id == str(chat_id))
+    if saved_filter:
+        saved_filter.delete()
         SESSION.commit()
