211f16cc651436bc49e1fdcd1084d5b2d3126956
SnapDragon <maliktahir0702@gmail.com>
2019-09-20 15:13:56 +0200

    Update sed.py

--- userbot/plugins/sed.py
+++ userbot/plugins/sed.py
@@ -1,101 +1,112 @@
+from collections import defaultdict, deque
 import re
-from sre_constants import error as sre_err
-from asyncio import sleep
+
+import regex
+from telethon import events, utils
+from telethon.tl import types, functions
 from userbot.utils import command
+from userbot import bot
 
-DELIMITERS = ("/", ":", "|", "_")
+HEADER = "「sed」\n"
+KNOWN_RE_BOTS = re.compile(
+    r"(regex|moku|BananaButler_|rgx|l4mR)bot",
+    flags=re.IGNORECASE
+)
+
+# Heavily based on
+# https://github.com/SijmenSchoon/regexbot/blob/master/regexbot.py
+
+last_msgs = defaultdict(lambda: deque(maxlen=10))
 
 
-async def separate_sed(sed_string):
-    """ Separate sed arguments. """
+def doit(chat_id, match, original):
+    fr = match.group(1)
+    to = match.group(2)
+    to = to.replace('\\/', '/')
+    try:
+        fl = match.group(3)
+        if fl is None:
+            fl = ''
+        fl = fl[1:]
+    except IndexError:
+        fl = ''
 
-    if len(sed_string) < 2:
+    # Build Python regex flags
+    count = 1
+    flags = 0
+    for f in fl:
+        if f == 'i':
+            flags |= regex.IGNORECASE
+        elif f == 'g':
+            count = 0
+        else:
+            return None, f"Unknown flag: {f}"
+
+    def actually_doit(original):
+        try:
+            s = original.message
+            if s.startswith(HEADER):
+                s = s[len(HEADER):]
+            s, i = regex.subn(fr, to, s, count=count, flags=flags)
+            if i > 0:
+                return original, s
+        except Exception as e:
+            return None, f"u dun goofed m8: {str(e)}"
+        return None, None
+
+    if original is not None:
+        return actually_doit(original)
+    # Try matching the last few messages
+    for original in last_msgs[chat_id]:
+        m, s = actually_doit(original)
+        if s is not None:
+            return m, s
+    return None, None
+
+
+async def group_has_sedbot(group):
+    if isinstance(group, types.InputPeerChannel):
+        full = await borg(functions.channels.GetFullChannelRequest(group))
+    elif isinstance(group, types.InputPeerChat):
+        full = await borg(functions.messages.GetFullChatRequest(group.chat_id))
+    else:
+        return False
+
+    return any(KNOWN_RE_BOTS.match(x.username or '') for x in full.users)
+
+
+@command
+async def on_message(event):
+    last_msgs[event.chat_id].appendleft(event.message)
+
+@command(allow_edited_update=False)
+async def on_edit(event):
+    for m in last_msgs[event.chat_id]:
+        if m.id == event.id:
+            m.raw_text = event.raw_text
+            break
+
+@command(
+    pattern=re.compile(r"^s/((?:\\/|[^/])+)/((?:\\/|[^/])*)(/.*)?"), outgoing=True)
+async def on_regex(event):
+    if event.fwd_from:
+        return
+    if not event.is_private and\
+            await group_has_sedbot(await event.get_input_chat()):
+        # await event.edit("This group has a sed bot. Ignoring this message!")
         return
 
-    if (len(sed_string) >= 2 and sed_string[2] in DELIMITERS
-            and sed_string.count(sed_string[2]) >= 2):
-        delim = sed_string[2]
-        start = counter = 3
-        while counter < len(sed_string):
-            if sed_string[counter] == "\\":
-                counter += 1
+    chat_id = utils.get_peer_id(await event.get_input_chat())
 
-            elif sed_string[counter] == delim:
-                replace = sed_string[start:counter]
-                counter += 1
-                start = counter
-                break
+    m, s = doit(chat_id, event.pattern_match, await event.get_reply_message())
 
-            counter += 1
+    if m is not None:
+        s = f"{HEADER}{s}"
+        out = await bot.send_message(
+            await event.get_input_chat(), s, reply_to=m.id
+        )
+        last_msgs[chat_id].appendleft(out)
+    elif s is not None:
+        await event.edit(s)
 
-        else:
-            return None
-
-        while counter < len(sed_string):
-            if (sed_string[counter] == "\\" and counter + 1 < len(sed_string)
-                    and sed_string[counter + 1] == delim):
-                sed_string = sed_string[:counter] + sed_string[counter + 1:]
-
-            elif sed_string[counter] == delim:
-                replace_with = sed_string[start:counter]
-                counter += 1
-                break
-
-            counter += 1
-        else:
-            return replace, sed_string[start:], ""
-
-        flags = ""
-        if counter < len(sed_string):
-            flags = sed_string[counter:]
-        return replace, replace_with, flags.lower()
-    return None
-
-
-@command(outgoing=True, pattern="^.s")
-async def sed(command):
-    if not command.text[0].isalpha() and command.text[0] not in ("/", "#", "@",
-                                                                 "!"):
-        """ For sed command, use sed on Telegram. """
-        sed_result = await separate_sed(command.text)
-        textx = await command.get_reply_message()
-        if sed_result:
-            if textx:
-                to_fix = textx.text
-            else:
-                await command.edit(
-                    "`Master, I don't have brains. Well you too don't I guess.`"
-                )
-                return
-
-            repl, repl_with, flags = sed_result
-
-            if not repl:
-                await command.edit(
-                    "`Master, I don't have brains. Well you too don't I guess.`"
-                )
-                return
-
-            try:
-                check = re.match(repl, to_fix, flags=re.IGNORECASE)
-                if check and check.group(0).lower() == to_fix.lower():
-                    await command.edit("`Boi!, that's a reply. Don't use sed`")
-                    return
-
-                if "i" in flags and "g" in flags:
-                    text = re.sub(repl, repl_with, to_fix, flags=re.I).strip()
-                elif "i" in flags:
-                    text = re.sub(repl, repl_with, to_fix, count=1,
-                                  flags=re.I).strip()
-                elif "g" in flags:
-                    text = re.sub(repl, repl_with, to_fix).strip()
-                else:
-                    text = re.sub(repl, repl_with, to_fix, count=1).strip()
-            except sre_err:
-                await command.edit("B O I! [Learn Regex](https://regexone.com)"
-                                   )
-                return
-            if text:
-                await command.edit(f"Did you mean? \n\n{text}")
-                await sleep(3)
-                await command.edit(text)
+    raise events.StopPropagation
