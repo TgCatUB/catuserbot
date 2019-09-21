ee0f2db422744b79623f03624f032f674d108da0
SnapDragon <maliktahir0702@gmail.com>
2019-09-22 00:46:25 +0200

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,14 +1,12 @@
 from userbot import bot
 from sys import argv
 import sys
-import importlib
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 import os
 from config import Config
 from userbot.utils import command, load_module
 from userbot import BAN_PLUG
 import userbot.utils
-from pathlib import Path
 import logging
 logging.basicConfig(level=logging.WARNING)
 
@@ -23,7 +21,6 @@ path = 'userbot/plugins/*.py'
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
-        path = Path(f.name)
         shortname = path.stem
         load_module(shortname.replace(".py", ""))
         print("Successfully (re)imported {}".format(f.name.replace("userbot/plugins/", "")))
