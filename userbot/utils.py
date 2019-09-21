ef8675e1f30fd8e4c239d69244554d75fddfd61a
SnapDragon <maliktahir0702@gmail.com>
2019-09-22 00:41:12 +0200

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -62,6 +62,10 @@ def command(**args):
 
 
 def load_module(shortname):
+    import sys
+    import importlib
+    from pathlib import Path
+    path = Path(f"userbot/plugins/{shortname}.py")
     name = "userbot.plugins.{}".format(shortname)
     spec = importlib.util.spec_from_file_location(name, path)
     mod = importlib.util.module_from_spec(spec)
