7241d4adc4a506f81e4fe91bb184be56232da59a
SnapDragon <maliktahir0702@gmail.com>
2019-09-21 18:54:40 +0200

    Update welcome_sql.py

--- userbot/plugins/sql_helper/welcome_sql.py
+++ userbot/plugins/sql_helper/welcome_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import BigInteger, Boolean, Column, String, UnicodeText
-from sql_helpers import SESSION, BASE
+from userbot.plugins.sql_helper import SESSION, BASE
 
 
 class Welcome(BASE):
