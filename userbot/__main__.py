aba8662eb5cdbfd521acdabf80495ed661890b16
SnapDragon <maliktahir0702@gmail.com>
2019-09-21 22:27:06 +0200

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -6,6 +6,7 @@ from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 import os
 from config import Config
 from userbot.utils import command
+from userbot import BAN_PLUG
 import userbot.utils
 from pathlib import Path
 import logging
@@ -24,19 +25,22 @@ for name in files:
     with open(name) as f:
         path = Path(f.name)
         shortname = path.stem
-        name = "userbot.plugins.{}".format(shortname.replace(".py", ""))
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        mod.bot = bot
-        mod.Config = Config
-        mod.command = command
-        # support for uniborg
-        sys.modules["uniborg.util"] = userbot.utils
-        mod.borg = bot
-        # support for paperplaneextended
-        sys.modules["userbot.events"] = userbot.utils
-        spec.loader.exec_module(mod)
-        print("Successfully imported {}".format(f.name.replace("userbot/plugins/", "")))
+        if not shortname in BAN_PLUG
+            name = "userbot.plugins.{}".format(shortname.replace(".py", ""))
+            spec = importlib.util.spec_from_file_location(name, path)
+            mod = importlib.util.module_from_spec(spec)
+            mod.bot = bot
+            mod.Config = Config
+            mod.command = command
+            # support for uniborg
+            sys.modules["uniborg.util"] = userbot.utils
+            mod.borg = bot
+            # support for paperplaneextended
+            sys.modules["userbot.events"] = userbot.utils
+            spec.loader.exec_module(mod)
+            print("Successfully imported {}".format(f.name.replace("userbot/plugins/", "")))
+        else:
+            print(f"{shortname} is banned.")
 import userbot._core
 os.makedirs(Config.TEMP_DOWNLOAD_DIRECTORY)
 
