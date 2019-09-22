ea7ae2870af8bbd86e65fc7520f70b8048364c7c
SnapDragon <maliktahir0702@gmail.com>
2019-09-22 10:55:33 +0200

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -50,7 +50,7 @@ def command(**args):
             args["from_users"] = list(Config.SUDO_USERS)
             # Mutually exclusive with outgoing (can only set one of either).
             args["incoming"] = True
-        del args["allow_sudo"]
+        del allow_sudo
 
         if "allow_edited_updates" in args:
             del args['allow_edited_updates']
