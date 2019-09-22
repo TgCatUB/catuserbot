8e79e8298af7d0e6f846c810a8b67504295bb92a
SnapDragon <maliktahir0702@gmail.com>
2019-09-22 23:33:11 +0200

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -14,19 +14,6 @@ from datetime import datetime
 
 DELETE_TIMEOUT = 5
 
-class ImportBlocker(object):
-    def __init__(self, *args):
-        self.module_names = args
-
-    def find_module(self, fullname, path=None):
-        if fullname in self.module_names:
-            return self
-        return None
-
-    def load_module(self, mdl):
-        # return an empty namespace
-        return {}
-
 @command(pattern="^.install", outgoing=True)
 async def install(event):
     if event.fwd_from:
