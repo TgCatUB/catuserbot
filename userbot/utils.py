0a2b6859447af4b6b10b7a3f2d42515eaafb84c8
SnapDragon <maliktahir0702@gmail.com>
2019-09-23 22:37:37 +0200

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -92,7 +92,7 @@ def admin_cmd(pattern=None, **args):
     import inspect
     stack = inspect.stack()
     the_method = stack[1][0].f_code
-    print("I was called by {}".format(str(the_method)))
+    print("I was called by {}".format(str(dir(the_method))))
     allow_sudo = args.get("allow_sudo", False)
 
     # get the pattern from the decorator
