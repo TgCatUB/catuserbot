b99128633c665602c14b87e4a6a36305e9dbb9eb
SnapDragon <maliktahir0702@gmail.com>
2019-09-24 13:53:02 +0200

    Update answer.py

--- userbot/plugins/answer.py
+++ userbot/plugins/answer.py
@@ -1,8 +1,42 @@
 from uniborg.util import admin_cmd
+import random
 
-@borg.on(admin_cmd(pattern="answer ?(.*)", outgoing=True))
-async def answer(event):
+INSULT_STRINGS = [ 
+    "`Owww ... Such a stupid idiot.`",
+    "`Don't drink and type.`",
+    "`Command not found. Just like your brain.`",
+    "`Bot rule 544 section 9 prevents me from replying to stupid humans like you.`",
+    "`Sorry, we do not sell brains.`",
+    "`Believe me you are not normal.`",
+    "`I bet your brain feels as good as new, seeing that you never use it.`",
+    "`If I wanted to kill myself I'd climb your ego and jump to your IQ.`",
+    "`You didn't evolve from apes, they evolved from you.`",
+    "`What language are you speaking? Cause it sounds like bullshit.`",
+    "`You are proof that evolution CAN go in reverse.`",
+    "`I would ask you how old you are but I know you can't count that high.`",
+    "`As an outsider, what do you think of the human race?`",
+    "`Ordinarily people live and learn. You just live.`",
+    "`Keep talking, someday you'll say something intelligent!.......(I doubt it though)`",
+    "`Everyone has the right to be stupid but you are abusing the privilege.`",
+    "`I'm sorry I hurt your feelings when I called you stupid. I thought you already knew that.`",
+    "`You should try tasting cyanide.`",
+    "`You should try sleeping forever.`",
+    "`Pick up a gun and shoot yourself.`",
+    "`Try bathing with Hydrochloric Acid instead of water.`",
+    "`Go Green! Stop inhaling Oxygen.`",
+    "`God was searching for you. You should leave to meet him.`",
+    "`You should Volunteer for target in an firing range.`",
+    "`Try playing catch and throw with RDX its fun.`",
+    "`People like you are the reason we have middle fingers.`",
+    "`When your mom dropped you off at the school, she got a ticket for littering.`",
+    "`You’re so ugly that when you cry, the tears roll down the back of your head…just to avoid your face.`",
+    "`If you’re talking behind my back then you’re in a perfect position to kiss my a**!.`",
+]
+
+@borg.on(admin_cmd(pattern="insult"))
+async def _(event):
     if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    await event.answer(message="Nom Nom NOM! Files deleted.\nLast Words were: {}".format(input_str))
+         return
+    bro = random.randint(0, len(INSULT_STRINGS) - 1)    
+    reply_text = INSULT_STRINGS[bro]
+    await event.edit(reply_text)
