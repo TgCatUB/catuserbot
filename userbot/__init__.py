de74e5adb2f6f7a2d8bfe14dc6bb1453dd7926a8
SnapDragon <maliktahir0702@gmail.com>
2019-09-25 10:20:44 +0200

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,15 +5,15 @@ from pathlib import Path
 from telethon.sessions import StringSession
 from telethon import TelegramClient
 
-from config import Var
+from var import Var
 
 os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name), Config.APP_ID, Config.API_HASH)
+    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
 else:
     session_name = "startup"
-    bot = TelegramClient(session_name, Config.APP_ID, Config.API_HASH)
+    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 
 
 CMD_LIST = {}
