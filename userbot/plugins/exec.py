78b962fc1c8ac066062b391d7085cb063802d019
SnapDragon <maliktahir0702@gmail.com>
2019-09-28 00:21:27 +0200

    Update exec.py

--- userbot/plugins/exec.py
+++ userbot/plugins/exec.py
@@ -42,7 +42,7 @@ async def _(event):
     else:
         evaluation = "Success"
 
-    final_output = "**EVAL**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
+    final_output = "**EXEC**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
 
     if len(final_output) > 4096:
         with io.BytesIO(str.encode(final_output)) as out_file:
