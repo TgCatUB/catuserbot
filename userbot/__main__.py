86a4f2e0df09d311b08e0a78423217e3abb177f2
SnapDragon <maliktahir0702@gmail.com>
2019-09-29 19:07:34 +0200

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -3,6 +3,7 @@ from sys import argv
 import sys
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 import os
+from telethon import TelegramClient
 from var import Var
 from userbot.utils import command, load_module
 from userbot import LOAD_PLUG
@@ -34,12 +35,12 @@ print("Yay your userbot is officially working.")
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
-    self.tgbot = None
+    bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
         # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
             "TG_BOT_TOKEN",
-            api_id=api_config.APP_ID,
-            api_hash=api_config.API_HASH
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-    bot.run_until_disconnected(await self.start(bot_token))
+    bot.run_until_disconnected(await bot.start(bot_token))
