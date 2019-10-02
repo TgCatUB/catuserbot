ecdf335f5a287129daf2a4e42323a9af365a2e14
Dark-Princ3 <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-02 16:58:38 +0530

    Update get_admin.py

--- userbot/plugins/get_admin.py
+++ userbot/plugins/get_admin.py
@@ -2,7 +2,7 @@
 Syntax: .get_admin"""
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins, ChannelParticipantAdmin, ChannelParticipantCreator
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
 
 
 @borg.on(admin_cmd("get_ad?(m)in ?(.*)"))
