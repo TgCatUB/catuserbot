da0707fed9014c27a3fccdc732423f49ae9c6058
Dark-Princ3 <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-02 16:57:43 +0530

    Update ff_mpeg.py

--- userbot/plugins/ff_mpeg.py
+++ userbot/plugins/ff_mpeg.py
@@ -7,7 +7,7 @@ import time
 from datetime import datetime
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-from uniborg.util import admin_cmd, progress
+from userbot.utils import admin_cmd, progress
 
 
 FF_MPEG_DOWN_LOAD_MEDIA_PATH = "uniborg.media.ffmpeg"
