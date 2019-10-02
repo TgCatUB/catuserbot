0906e0f7e22a4f1cc060100b8aa73f555cecd29b
Dark-Princ3 <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-02 17:09:08 +0530

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -6,7 +6,7 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
 
 
 @borg.on(admin_cmd("whois ?(.*)"))
