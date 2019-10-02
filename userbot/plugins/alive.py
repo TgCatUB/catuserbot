e6c30bf1b82d3b0d76845099969aab1db183c4f0
Dark-Princ3 <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-02 16:54:57 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -2,7 +2,7 @@
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
 
 
 @borg.on(admin_cmd("alive"))
