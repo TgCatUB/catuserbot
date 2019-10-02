e5ec5b09929bc25cce3028b3a328e873b60e6f5c
Dark-Princ3 <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-02 16:04:36 +0530

    Delete alive.py

--- userbot/plugins/alive.py
+++
@@ -1,5 +0,0 @@
-@command(pattern="^.alive", outgoing=True)
-async def hello_world(event):
-    if event.fwd_from:
-        return
-    await event.edit("**HELLO WORLD**\n\nThe following is controlling me too!\n" + str(event.from_id) + " " + Var.SUDO_USERS)
