5a5eb4cba3289777a5acf1c2aa6d0915942dd9ff
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-05 22:40:39 +0530

    snappy is lub <3

---
+++ userbot/plugins/_helper.py
@@ -0,0 +1,42 @@
+from userbot import CMD_LIST
+
+@command(pattern="^.help ?(.*)")
+async def cmd_list(event):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+        input_str = event.pattern_match.group(1)
+        if tgbotusername is None or input_str == "text":
+            string = ""
+            for i in CMD_LIST:
+                string += "ℹ️ " + i + "\n"
+                for iter_list in CMD_LIST[i]:
+                    string += "    `" + str(iter_list) + "`"
+                    string += "\n"
+                string += "\n"
+            if len(string) > 4095:
+                with io.BytesIO(str.encode(string)) as out_file:
+                    out_file.name = "cmd.txt"
+                    await bot.send_file(
+                        event.chat_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption="**COMMANDS**",
+                        reply_to=reply_to_id
+                    )
+                    await event.delete()
+            else:
+                await event.edit(string)
+        else:
+            help_string = """Userbot Helper
+`Userbot Helper to reveal all the commands`"""
+            results = await bot.inline_query(  # pylint:disable=E0602
+                tgbotusername,
+                help_string
+            )
+            await results[0].click(
+                event.chat_id,
+                reply_to=event.reply_to_msg_id,
+                hide_via=True
+            )
+            await event.delete()
---
+++ userbot/plugins/autopic.py
@@ -0,0 +1,39 @@
+import os
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from pySmartDL import SmartDL
+from telethon.tl import functions
+import asyncio
+import shutil
+
+FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
+
+@command(pattern="^.autopic", outgoing=True)
+async def autopic(event):
+    downloaded_file_name = "userbot/original_pic.png"
+    downloader = SmartDL(Var.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False)
+    downloader.start(blocking=False)
+    photo = "userbot/photo_pfp.png"
+    while not downloader.isFinished():
+        place_holder = None
+    counter = -5
+    while True:
+        shutil.copy(downloaded_file_name, photo)
+        im = Image.open(photo)
+        file_test = im.rotate(counter, expand=False).save(photo, "PNG")
+        current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n ⚡USERBOT TIMEZONE⚡ \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
+        img = Image.open(photo)
+        drawn_text = ImageDraw.Draw(img)
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+        drawn_text.text((50, 250), current_time, font=fnt, fill=(255, 255, 255))
+        img.save(photo)
+        file = await bot.upload_file(photo)  # pylint:disable=E0602
+        try:
+            await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+                file
+            ))
+            os.remove(photo)
+            counter -= 5
+            await asyncio.sleep(65)
+        except:
+            return
