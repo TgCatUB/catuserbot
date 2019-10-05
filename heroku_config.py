1dca14a9aea8b2c3724b1450c76279601775d2cf
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-05 22:37:36 +0530

    Add files via upload

---
+++ heroku_config.py
@@ -0,0 +1,24 @@
+import os
+
+class Var(object):
+    APP_ID = int(os.environ.get("APP_ID", 6))
+    # 6 is a placeholder
+    API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    DB_URI = os.environ.get("DATABASE_URL", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", None)
+    LOGGER = True
+    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
+    GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
+    # Here for later purposes
+    SUDO_USERS = os.environ.get("SUDO_USERS", "719877937")
+    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
+    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
+    DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", None)
+
+class Development(Var):
+    LOGGER = True
+    # Here for later purposes
