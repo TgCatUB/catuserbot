ec1d4db60cb7ab8434cda5fcf74dc57df2ab8fb3
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-08 19:32:54 +0530

    Add files via upload

---
+++ userbot/plugins/decide.py
@@ -0,0 +1,22 @@
+"""Quickly make a decision
+Syntax: .decide"""
+from telethon import events
+import requests
+from userbot.utils import admin_cmd
+
+
+@borg.on(admin_cmd("decide"))
+async def _(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    if event.reply_to_msg_id:
+        message_id = event.reply_to_msg_id
+    r = requests.get("https://yesno.wtf/api").json()
+    await borg.send_message(
+        event.chat_id,
+        r["answer"],
+        reply_to=message_id,
+        file=r["image"]
+    )
+    await event.delete()
