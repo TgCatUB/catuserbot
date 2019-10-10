cc28f2ecb488be7bc9809082a4d86a332fdb8c91
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-10 08:56:30 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -18,6 +18,21 @@ class Var(object):
     TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
     TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
     DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", None)
+    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
+    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
+    AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
+    if AUTH_TOKEN_DATA != None:
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+        t_file = open(TEMP_DOWNLOAD_DIRECTORY+"auth_token.txt","w")
+        t_file.write(AUTH_TOKEN_DATA)
+        t_file.close()
+    PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
+    if PRIVATE_GROUP_ID != None:
+        try:
+            PRIVATE_GROUP_ID = int(PRIVATE_GROUP_ID)
+        except ValueError:
+            raise ValueError("Invalid Private Group ID. Make sure your ID is starts with -100 and make sure that it is only numbers.")
 
 class Development(Var):
     LOGGER = True
