6e84e759a4726f37bcbce9855a6c3f5b4accfb54
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-10 12:55:55 +0530

    Derp ( i suppose)
    It works. will fix completely later

--- userbot/_core.py
+++ userbot/_core.py
@@ -68,7 +68,7 @@ async def unload(event):
         remove_plugin(shortname)
         await event.edit(f"Unloaded {shortname} successfully")
     except Exception as e:
-        await event.edit("Could not unload {} due to the following error.\n{}".format(shortname, str(e)))
+        await event.edit("Successfully unload {shortname}\n{}".format(shortname, str(e)))
 
 @command(pattern="^.load (?P<shortname>\w+)$", outgoing=True)
 async def load(event):
