034a7ae8e199505dc3a743489e9fade5190b8c12
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-16 17:14:20 +0530

    fIxEd

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -36,7 +36,7 @@ async def _(event):
     await borg.send_file(  # pylint:disable=E0602
         event.chat_id,
         required_file_name,
-        caption="Userbot: Powered by @uniborg",
+        caption="Userbot: Powered by @x-tra-telegram",
         # Courtesy: @ManueI15
         reply_to=reply_msg_id
     )
