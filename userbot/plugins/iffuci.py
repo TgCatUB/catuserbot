83a4e31b5af54ba3fedc0eeb4808e24c436b4475
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-10-25 19:03:03 +0530

    Uploaded New Plugin

---
+++ userbot/plugins/iffuci.py
@@ -0,0 +1,55 @@
+"""iffuci.tk pastebin site
+Code written by @loxxi {iffuci}
+Syntax: .iffuci"""
+from telethon import events
+import asyncio
+from datetime import datetime
+import os
+import requests
+from userbot.utils import admin_cmd
+
+
+def progress(current, total):
+    logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
+
+
+@borg.on(admin_cmd(pattern="iffuci ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    input_str = event.pattern_match.group(1)
+    message = "SYNTAX: `.iffuci <long text to include>`"
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.media:
+            downloaded_file_name = await borg.download_media(
+                previous_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
+                progress_callback=progress
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            message = ""
+            for m in m_list:
+                message += m.decode("UTF-8") + "\r\n"
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = "SYNTAX: `.iffuci <long text to include>`"
+    url = "https://www.iffuci.tk/documents"
+    r = requests.post(url, data=message.encode("UTF-8")).json()
+    url = f"https://iffuci.tk/{r['key']}"
+    end = datetime.now()
+    ms = (end - start).seconds
+    if r["isUrl"]:
+        nurl = f"https://iffuci.tk/v/{r['key']}"
+        await event.edit("code is pasted to {} in {} seconds. GoTo Original URL: {}".format(url, ms, nurl))
+    else:
+        await event.edit("code is pasted to {} in {} seconds".format(url, ms))
