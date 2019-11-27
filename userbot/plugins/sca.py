931b3eb873d5493a86142bdb53cab94e6fb6b734
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-11-27 15:04:52 +0530

    Uploaded New Plugin

---
+++ userbot/plugins/sca.py
@@ -0,0 +1,30 @@
+"""Send Chat Actions
+Syntax: .sca <option> <time in sec>
+        sca options: Options for sca 
+
+typing
+contact
+game
+location
+voice
+round
+video
+photo
+document
+cancel"""
+
+import asyncio
+from uniborg.util import admin_cmd
+ 
+ 
+@borg.on(admin_cmd("sca ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.delete()
+    input_str = event.pattern_match.group(1)
+    action = "typing"
+    if input_str:
+        action = input_str
+    async with borg.action(event.chat_id, action):
+        await asyncio.sleep(86400)  # type for 10 seconds
