7a5517dad362ab7d31dd2d0202d1217adf776ad8
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-05 19:57:06 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -45,7 +45,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"@A_Dark_Princ3 ki gufa"
-    packshortname = f"@A_Dark_Princ3_ki_gufa_me{userid}"  # format: Uni_Borg_userid
+    packshortname = f"Anubis_ki_gufa_me{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@UniBorg_Sticker.png"
