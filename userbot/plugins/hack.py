05b784478ad7fbbbed02b13fbf546238d912222d
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-08 19:40:52 +0530

    Update hack.py

--- userbot/plugins/hack.py
+++ userbot/plugins/hack.py
@@ -43,7 +43,7 @@ async def _(event):
             "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
             "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay 69$ To @A_Dark_Princ3 Or send nudes of female Homo Sapiens To Remove This Hack`"
+            "`Targeted Account Hacked...\n\nPay 69$ To` @A_Dark_Princ3 `Or send nudes of female Homo Sapiens To Remove This Hack`"
         ]
 
         for i in animation_ttl:
