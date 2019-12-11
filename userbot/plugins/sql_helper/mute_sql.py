d07e388d1bd3dd7295604194f4344c0caf76372e
SnapDragon <maliktahir0702@gmail.com>
2019-12-11 20:18:32 +0200

    Create mute_sql.py

---
+++ userbot/plugins/sql_helper/mute_sql.py
@@ -0,0 +1,41 @@
+try:
+    from userbot.plugins.sql_helper import SESSION, BASE
+except ImportError:
+    raise Exception("Hello!")
+
+from sqlalchemy import Column, String, UnicodeText
+
+
+class Mute(BASE):
+    __tablename__ = "mute"
+    sender = Column(String(14), primary_key=True)
+    chat_id = Column(String(14), primary_key=True)
+
+    def __init__(self, sender, chat_id):
+        self.sender = str(sender)
+        self.chat_id = str(chat_id)
+
+
+Mute.__table__.create(checkfirst=True)
+
+
+def is_muted():
+    try:
+        return SESSION.query(Mute).all()
+    except:
+        return None
+    finally:
+        SESSION.close()
+
+
+def mute(sender, chat_id):
+    adder = Mute(str(sender), str(chat_id))
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def unmute(sender, chat_id):
+    rem = SESSION.query(Mute).get((str(sender), str(chat_id)))
+    if rem:
+        SESSION.delete(rem)
+        SESSION.commit()
