f258414563259c48133b7c5e7b961a15ad0ca2a5
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-14 20:34:28 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -126,22 +126,22 @@ async def updater(message):
     temp_upstream_remote.fetch(active_branch_name)
     repo.git.reset("--hard", "FETCH_HEAD")
 
-    if var.HEROKU_API_KEY is not None:
+    if Var.HEROKU_API_KEY is not None:
         import heroku3
-        heroku = heroku3.from_key(var.HEROKU_API_KEY)
+        heroku = heroku3.from_key(Var.HEROKU_API_KEY)
         heroku_applications = heroku.apps()
         if len(heroku_applications) >= 1:
-            if var.HEROKU_APP_NAME is not None:
+            if Var.HEROKU_APP_NAME is not None:
                 heroku_app = None
                 for i in heroku_applications:
-                    if i.name == var.HEROKU_APP_NAME:
+                    if i.name == Var.HEROKU_APP_NAME:
                         heroku_app = i
                 if heroku_app is None:
                     await message.edit("Invalid APP Name. Please set the name of your bot in heroku in the var `HEROKU_APP_NAME.`")
                     return
                 heroku_git_url = heroku_app.git_url.replace(
                     "https://",
-                    "https://api:" + var.HEROKU_API_KEY + "@"
+                    "https://api:" + Var.HEROKU_API_KEY + "@"
                 )
                 if "heroku" in repo.remotes:
                     remote = repo.remote("heroku")
