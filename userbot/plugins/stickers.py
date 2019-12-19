9ee0d611800800856a9cefc79e73a8325d618516
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-20 00:11:24 +0530

    Made the command shorter. Changed some strings.

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -29,12 +29,12 @@ from telethon.tl.types import (
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="kangsticker ?(.*)"))
+@borg.on(admin_cmd(pattern="kang ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     if not event.is_reply:
-        await event.edit("Reply to a photo to add to my personal sticker pack.")
+        await event.edit("Reply to a photo to add to my personal sticker pack.NiBbI.")
         return
     reply_message = await event.get_reply_message()
     sticker_emoji = "🔥"
@@ -68,7 +68,7 @@ async def _(event):
             sticker.seek(0)
             uploaded_sticker = await borg.upload_file(sticker, file_name=file_ext_ns_ion)
 
-    await event.edit("Processing this sticker. Please Wait!")
+    await event.edit("Look dat way,it's a gurl!Meanwhile lemme kang this stcker over hehe 0.0")
 
     async with borg.conversation("@Stickers") as bot_conv:
         now = datetime.datetime.now()
@@ -120,7 +120,7 @@ async def _(event):
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/done")
 
-    await event.edit(f"sticker added! Your pack can be found [here](t.me/addstickers/{packshortname})")
+    await event.edit(f"BOOM BOI!sticker added!This sticker has been stolen to [this place](t.me/addstickers/{packshortname})")
 
 
 @borg.on(admin_cmd(pattern="packinfo"))
