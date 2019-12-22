4e41e2a1c8a3eec457f64283e1f537bc2caf30c5
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-22 23:41:52 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,12 +6,12 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check @XtraTgBot"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("`Jinda Hu Sarr ^.^ \nYour bot is running\n\n`"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @A_Dark_Princ3\n`Database Status: Databases functioning normally!\n\n Always with you, my master!\n`"
-                     "[Deploy Now](https://github.com/Dark-Princ3/X-tra-Telegram)"
+                     "[Deploy this userbot Now](https://github.com/Dark-Princ3/X-tra-Telegram)\n"
                      f"Owner: {DEFAULTUSER}")
