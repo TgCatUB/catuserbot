7724fe990063ac5f9a5c5b1f15f92e2608a3bd88
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-22 23:35:12 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,7 +8,7 @@ from userbot.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 
-@borg.on(outgoing=True, pattern="^.alive$")
+@command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("`Jinda Hu Sarr ^.^ \nYour bot is running\n\n`"
