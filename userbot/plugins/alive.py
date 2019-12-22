2db27a694bcc6ea129c882ff1887c05304063a2b
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-22 22:17:17 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -2,14 +2,16 @@
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
+from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 
 @borg.on(admin_cmd("alive"))
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "` Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\nfork by:` @A_Dark_Princ3\n`Database Status: Databases functioning normally!\n\n Always with you, my master! `"
+    mentions = "` Jinda Hu Sarr ^.^ \nYour bot is running\n\nOwner: {DEFAULTUSER}\nTelethon version: 6.9.0\nPython: 3.7.3\nfork by:` @A_Dark_Princ3\n`Database Status: Databases functioning normally!\n\n Always with you, my master! `(https://github.com/Dark-Princ3/X-tra-Telegram)"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
