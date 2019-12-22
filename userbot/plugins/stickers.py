33f411350b4e797d8e11cb0f15bef0975c4d04b3
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-23 02:17:25 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -122,8 +122,8 @@ async def _(event):
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/done")
 
-    await event.edit(f"**BOOM BOI!**\n`Sticker added! This sticker has been stolen to` [this place](t.me/addstickers/{packshortname} )"
-                     f"by {DEFAULTUSER}")
+    await event.edit(f"**BOOM BOI!**\n`Sticker added! This sticker has been stolen to` [this place](t.me/addstickers/{packshortname}))"
+                     f" by {DEFAULTUSER}")
 
 
 @borg.on(admin_cmd(pattern="packinfo"))
