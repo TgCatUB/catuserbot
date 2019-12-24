907557b4d6d34dd716b10acf28a8c08a9e6029a3
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-25 02:57:22 +0530

    Ok Boomer.

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -17,9 +17,9 @@ async def _(event):
     if event.fwd_from:
         return
     # await asyncio.sleep(2)
-    # await event.edit("Restarting [██░] ...\n`.ping` me or `.helpme` to check if I am online")
+    # await event.edit("Restarting [██░] ...\n`.ping` me or `.help` to check if I am online after a lil bit.")
     # await asyncio.sleep(2)
-    # await event.edit("Restarting [███]...\n`.ping` me or `.helpme` to check if I am online")
+    # await event.edit("Restarting [███]...\n`.ping` me or `.help` to check if I am online after a lil bit.")
     # await asyncio.sleep(2)
     await event.edit("Restarted. `.ping` me or `.helpme` to check if I am online")
     await borg.disconnect()
