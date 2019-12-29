571ab7b6155688844c7116205faf8be125fcd49c
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2019-12-29 20:47:21 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -110,9 +110,9 @@ if Var.PRIVATE_GROUP_ID is not None:
         if sender.verified:
             # don't log verified accounts
             return
-        if response.textstartswith("/start")
+        if response.textstartswith("/start"):
             await borg.send_message(chat_id, "Do you choose death?{Y/N}")
-            if response.textstartwith("Y")
+            if response.textstartwith("Y"):
             await event.client(functions.contacts.BlockRequest(chat_id))
             else:
                 await borg.send_message(chat_id, "oh, ok")
