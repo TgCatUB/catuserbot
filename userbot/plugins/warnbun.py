dbdcbd7536f723cf819a36442e50f98e236de70e
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-02-04 10:56:15 +0530

    Purana jayega tabhi to naya aayega #olx

--- userbot/plugins/warnbun.py
+++ userbot/plugins/warnbun.py
@@ -90,29 +90,6 @@ async def _(event):
         await event.reply(mentions)
     await event.delete()
 
-""".admin Plugin for @UniBorg"""
-import asyncio
-from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from uniborg.util import admin_cmd
-
-
-@borg.on(admin_cmd("gbun"))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "`Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By Admin...\nReason: Potential Porn Addict. `"
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f""
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
-
 
 @borg.on(admin_cmd("ocb"))
 async def _(event):
