cfabc920d109c836d82027a9262b97b1fb6e207b
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-19 19:29:36 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Yay your userbot is officially working. Ja gaand mara")
+print("Yay your userbot is officially working.")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
