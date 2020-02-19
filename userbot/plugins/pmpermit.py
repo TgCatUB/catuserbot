623490ca8b00d86c1ce0ee5d538d78357ba70cab
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-20 01:00:33 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -13,7 +13,7 @@ PREV_REPLY_MESSAGE = {}
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No name set yet nibba, check pinned message in** @XtraTgBot"
 USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` **Now GTFO, i'm playing minecraft** "
 USER_BOT_NO_WARN = ("[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id=916234223)\n\n"
-                    "`Hello, this is X-tra-Telegram Security Service.You have found your way here to my master\n,`"
+                    "`Hello, this is sandeep-Telegram Security Service.You have found your way here to my master\n,`"
                     "`నేను ఆన్‌లైన్‌లో లేను. నన్ను స్పామ్ చేయవద్దు.నేను మీకు తరువాత ప్రత్యుత్తరం ఇస్తాను,`\n"
                     "'nenu online lo lenu present online loki vachaka message chastha.'\n"
                     f"{DEFAULTUSER}'s` inbox.\n\n"
