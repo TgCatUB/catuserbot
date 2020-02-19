15563db09d89caaf730995c11ac3d750d971329f
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-20 00:09:51 +0530

    Create globals.py

---
+++ userbot/plugins/sql_helper/globals.py
@@ -0,0 +1,46 @@
+try:
+    from userbot.plugins.sql_helper import SESSION, BASE
+except ImportError:
+    raise AttributeError
+
+from sqlalchemy import Column, String, UnicodeText
+
+
+class Globals(BASE):
+    __tablename__ = "globals"
+    variable = Column(String, primary_key=True, nullable=False)
+    value = Column(UnicodeText, primary_key=True, nullable=False)
+
+    def __init__(self, variable, value):
+        self.variable = str(variable)
+        self.value = value
+
+
+Globals.__table__.create(checkfirst=True)
+
+
+def gvarstatus(variable):
+    try:
+        return SESSION.query(Globals).filter(
+            Globals.variable == str(variable)).first().value
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+
+def addgvar(variable, value):
+    if SESSION.query(Globals).filter(
+            Globals.variable == str(variable)).one_or_none():
+        delgvar(variable)
+    adder = Globals(str(variable), value)
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def delgvar(variable):
+    rem = SESSION.query(Globals).filter(Globals.variable == str(variable))\
+        .delete(synchronize_session="fetch")
+    if rem:
+        SESSION.commit()
+globals.py
