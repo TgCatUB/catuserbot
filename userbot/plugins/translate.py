c69dee84f12def9a51938047c3d2bfca625b2061
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-23 02:11:25 +0530

    Update translate.py

--- userbot/plugins/translate.py
+++ userbot/plugins/translate.py
@@ -8,7 +8,7 @@ from googletrans import Translator
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("tr ?(.*)"))
+@borg.on(admin_cmd("tt ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
