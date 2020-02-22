5c19017201d72e65e699ee984f06ad1307f7e692
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-22 23:27:50 +0530

    Update link.py

--- userbot/plugins/link.py
+++ userbot/plugins/link.py
@@ -14,10 +14,10 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
-from userbot.utils import register
+from userbot.events import register
 
 
-@register(pattern=".link(?: |$)(.*)", outgoing=True)
+@register(pattern=".whois(?: |$)(.*)", outgoing=True)
 async def who(event):
 
     await event.edit(
@@ -129,20 +129,20 @@ async def fetch_info(replied_user, event):
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
 
-   caption = "<b>USER INFO:</b>\n\n"
- #  caption = f"First Name: {first_name}\n"
-#    caption += f"Last Name: {last_name}\n"
-    #caption += f"Username: {username}\n"
-   # caption += f"Data Centre ID: {dc_id}\n"
-  #  caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
- #   caption += f"Is Bot: {is_bot}\n"
-#    caption += f"Is Restricted: {restricted}\n"
-   # caption += f"Is Verified by Telegram: {verified}\n"
-  #  caption += f"ID: <code>{user_id}</code>\n\n"
- #   caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-#    caption += f"Common Chats with this user: {common_chat}\n"
- #   caption += f"Link: "
+    caption = "<b>USER INFO:</b>\n\n"
+    caption += f"First Name: {first_name}\n"
+    caption += f"Last Name: {last_name}\n"
+   """ caption += f"Username: {username}\n"
+    caption += f"Data Centre ID: {dc_id}\n"
+    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
+    caption += f"Is Bot: {is_bot}\n"
+    caption += f"Is Restricted: {restricted}\n"
+    caption += f"Is Verified by Telegram: {verified}\n"
+    caption += f"ID: <code>{user_id}</code>\n\n"
+    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
+    caption += f"Common Chats with this user: {common_chat}\n" """
+    caption += f"Permanent Link To Profile: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
-    return caption
+    return  caption
 
