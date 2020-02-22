3072ca1ecb5afa1f6a723f6f182fcd3d2a59a98d
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-22 17:29:31 +0530

    Update mf.py

--- userbot/plugins/mf.py
+++ userbot/plugins/mf.py
@@ -74,4 +74,4 @@ async def _(event):
     result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
     result = result.stringify()
     logger.info(result)  # pylint:disable=E0602
-    await event.edit("""Telethon UserBot powered by @UniBorg""")
+    await event.edit("""sandy""")
