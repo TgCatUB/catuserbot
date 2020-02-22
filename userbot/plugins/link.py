1ca8662729e58a2f3983f7da419f06f7cfca05d6
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-22 22:33:32 +0530

    Update link.py

--- userbot/plugins/link.py
+++ userbot/plugins/link.py
@@ -14,7 +14,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
-from userbot.events import register
+from userbot.utils import register
 
 
 @register(pattern=".whois(?: |$)(.*)", outgoing=True)
@@ -93,20 +93,56 @@ async def get_user(event):
 
 async def fetch_info(replied_user, event):
     """ Get details from the User object. """
-
+    replied_user_profile_photos = await event.client(
+        GetUserPhotosRequest(user_id=replied_user.user.id,
+                             offset=42,
+                             max_id=0,
+                             limit=80))
+    replied_user_profile_photos_count = "Person needs help with uploading profile picture."
+    try:
+        replied_user_profile_photos_count = replied_user_profile_photos.count
+    except AttributeError as e:
+        pass
     user_id = replied_user.user.id
+    first_name = replied_user.user.first_name
+    last_name = replied_user.user.last_name
+    try:
+        dc_id, location = get_input_location(replied_user.profile_photo)
+    except Exception as e:
+        dc_id = "Couldn't fetch DC ID!"
+        location = str(e)
+    common_chat = replied_user.common_chats_count
+    username = replied_user.user.username
+    user_bio = replied_user.about
+    is_bot = replied_user.user.bot
+    restricted = replied_user.user.restricted
+    verified = replied_user.user.verified
+    photo = await event.client.download_profile_photo(user_id,
+                                                      TEMP_DOWNLOAD_DIRECTORY +
+                                                      str(user_id) + ".jpg",
+                                                      download_big=True)
+    first_name = first_name.replace(
+        "\u2060", "") if first_name else ("This User has no First Name")
+    last_name = last_name.replace(
+        "\u2060", "") if last_name else ("This User has no Last Name")
     username = "@{}".format(username) if username else (
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
 
-
+    caption = "<b>USER INFO:</b>\n\n"
+    caption += f"First Name: {first_name}\n"
+    caption += f"Last Name: {last_name}\n"
+    caption += f"Username: {username}\n"
+    caption += f"Data Centre ID: {dc_id}\n"
+    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
+    caption += f"Is Bot: {is_bot}\n"
+    caption += f"Is Restricted: {restricted}\n"
+    caption += f"Is Verified by Telegram: {verified}\n"
+    caption += f"ID: <code>{user_id}</code>\n\n"
+    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
+    caption += f"Common Chats with this user: {common_chat}\n"
+    caption += f"Permanent Link To Profile: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
-    return caption
+    return photo, caption
 
-
-CMD_HELP.update({
-    "link":
-    ".link <username> or reply to someones text with .whois\
-    \nUsage: Gets info of an user."
-})
