cb29fe819ae4fccd874e4f5b06dec905b9db14a0
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-22 21:57:37 +0530

    Update link.py

--- userbot/plugins/link.py
+++ userbot/plugins/link.py
@@ -17,7 +17,13 @@ async def _(event):
     if replied_user is None:
         await event.edit(str(error_i_a))
         return False
-   
+    replied_user_profile_photos = await borg(GetUserPhotosRequest(
+        user_id=replied_user.user.id,
+        offset=42,
+        max_id=0,
+        limit=80
+    ))
+    replied_user_profile_photos_count = "NaN"
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
     except AttributeError as e:
@@ -27,7 +33,33 @@ async def _(event):
     first_name = html.escape(replied_user.user.first_name)
     # https://stackoverflow.com/a/5072031/4723940
     # some Deleted Accounts do not have first_name
+    if first_name is not None:
+        # some weird people (like me) have more than 4096 characters in their names
+        first_name = first_name.replace("\u2060", "")
     # inspired by https://telegram.dog/afsaI181
+    user_bio = replied_user.about
+    if user_bio is not None:
+        user_bio = html.escape(replied_user.about)
+    common_chats = replied_user.common_chats_count
+    try:
+        dc_id, location = get_input_location(replied_user.profile_photo)
+    except Exception as e:
+        dc_id = "Need a Profile Picture to check **this**"
+        location = str(e)
+    caption = """
+ <a href='tg://user?id={}'>{}</a>
+""".format(
+        user_id,
+        user_id,
+        first_name,
+        user_bio,
+        dc_id,
+        replied_user_profile_photos_count,
+        replied_user.user.restricted,
+        replied_user.user.verified,
+        replied_user.user.bot,
+        common_chats
+    )
     message_id_to_reply = event.message.reply_to_msg_id
     if not message_id_to_reply:
         message_id_to_reply = event.message.id
