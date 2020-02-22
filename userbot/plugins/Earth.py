00c8a0e092ecabbcab80c7faee6f9b00d98cbcdc
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-22 16:19:26 +0530

    Update Earth.py

--- userbot/plugins/Earth.py
+++ userbot/plugins/Earth.py
@@ -6,6 +6,7 @@ import asyncio
 from collections import deque
 
 
+
 @borg.on(events.NewMessage(pattern=r"\.earth", outgoing=True))
 async def _(event):
 	if event.fwd_from:
@@ -16,3 +17,6 @@ async def _(event):
 		await event.edit("".join(deq))
 		deq.rotate(1)
     
+CMD_HELP.update({
+    ".earth"
+})
