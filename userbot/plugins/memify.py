c0964355ed575a88f1f3d61dbd5e64d86126c899
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-22 23:25:39 +0530

    Update memify.py

--- userbot/plugins/memify.py
+++ userbot/plugins/memify.py
@@ -1,4 +1,7 @@
 """Reply to an image/sticker with .mmf` 'text on top' ; 'text on bottom
+base by: @r4v4n4
+created by: @A_Dark_Princ3
+if you change these, you gay.
 """
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -8,9 +11,6 @@ from PIL import Image
 import asyncio
 import time
 from datetime import datetime
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-from pySmartDL import SmartDL
 from telethon.tl.types import DocumentAttributeVideo
 from uniborg.util import progress, humanbytes, time_formatter, admin_cmd
 import datetime
@@ -19,8 +19,6 @@ import math
 import os
 import requests
 import zipfile
-from telethon.errors.rpcerrorlist import StickersetInvalidError
-from telethon.errors import MessageNotModifiedError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from telethon.tl.functions.messages import GetStickerSetRequest
 from telethon.tl.types import (
@@ -51,13 +49,13 @@ async def _(event):
     chat = "@MemeAutobot"
     sender = reply_message.sender
     file_ext_ns_ion = "@memetime.png"
-    file = await borg.download_file(reply_message.media)
     uploaded_gif = None
     if reply_message.sender.bot:
        await event.edit("```Reply to actual users message.```")
        return
     else:
-     await event.edit("```Transfiguration Time! Mwahaha memifying this image! (」ﾟﾛﾟ)｣ ```")
+       await event.edit("```Transfiguration Time! Mwahaha memifying this image! (」ﾟﾛﾟ)｣ ```")
+    file = await borg.download_file(reply_message.media)
     
     async with borg.conversation("@MemeAutobot") as bot_conv:
           try:
@@ -114,7 +112,7 @@ async def _(event):
                 event.chat_id,
                 requires_file_name,
                 supports_streaming=False,
-                caption="Userbot: Powered by @x-tra-telegram",
+                caption="bot",
                 # Courtesy: @A_Dark_Princ3
             )
             await event.delete()
