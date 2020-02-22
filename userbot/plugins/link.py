bc75f751926e4f06b5357262d109bdaeb72aaa4a
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-22 23:33:46 +0530

    Update link.py

--- userbot/plugins/link.py
+++ userbot/plugins/link.py
@@ -132,7 +132,7 @@ async def fetch_info(replied_user, event):
     caption = "<b>USER INFO:</b>\n\n"
     caption += f"First Name: {first_name}\n"
     caption += f"Last Name: {last_name}\n"
-   """ caption += f"Username: {username}\n"
+    caption += f"Username: {username}\n"
     caption += f"Data Centre ID: {dc_id}\n"
     caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
     caption += f"Is Bot: {is_bot}\n"
@@ -140,7 +140,7 @@ async def fetch_info(replied_user, event):
     caption += f"Is Verified by Telegram: {verified}\n"
     caption += f"ID: <code>{user_id}</code>\n\n"
     caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n" """
+    caption += f"Common Chats with this user: {common_chat}\n" 
     caption += f"Permanent Link To Profile: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
