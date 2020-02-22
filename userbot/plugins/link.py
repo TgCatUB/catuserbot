b1681e0df42b83a2e4d6db605d4820b7defd911d
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-22 20:16:34 +0530

    Create link.py

---
+++ userbot/plugins/link.py
@@ -0,0 +1,103 @@
+ # Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+# The entire source code is OSSRPL except 'whois' which is MPL
+# License: MPL and OSSRPL
+""" Userbot module for getiing info about any user on Telegram(including you!). """
+
+import os
+
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
+from userbot.utils import register
+
+
+
+
+@register(pattern=".whois(?: |$)(.*)", outgoing=True)
+async def who(event):
+
+    await event.edit(
+        "`Sit tight while I steal some data from Mark Zuckerburg...`")
+
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+
+    replied_user = await get_user(event)
+
+    try:
+        photo, caption = await fetch_info(replied_user, event)
+    except AttributeError:
+        event.edit("`Could not fetch info of that user.`")
+        return
+
+    message_id_to_reply = event.message.reply_to_msg_id
+
+    if not message_id_to_reply:
+        message_id_to_reply = None
+
+    try:
+        await event.client.send_file(event.chat_id,
+                                     photo,
+                                     caption=caption,
+                                     link_preview=False,
+                                     force_document=False,
+                                     reply_to=message_id_to_reply,
+                                     parse_mode="html")
+
+        if not photo.startswith("http"):
+            os.remove(photo)
+        await event.delete()
+
+    except TypeError:
+        await event.edit(caption, parse_mode="html")
+
+
+async def get_user(event):
+    """ Get the user from argument or replied message. """
+    if event.reply_to_msg_id and not event.pattern_match.group(1):
+        previous_message = await event.get_reply_message()
+        replied_user = await event.client(
+            GetFullUserRequest(previous_message.from_id))
+    else:
+        user = event.pattern_match.group(1)
+
+        if user.isnumeric():
+            user = int(user)
+
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+
+        if event.message.entities is not None:
+            probable_user_mention_entity = event.message.entities[0]
+
+            if isinstance(probable_user_mention_entity,
+                          MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user
+        try:
+            user_object = await event.client.get_entity(user)
+            replied_user = await event.client(
+                GetFullUserRequest(user_object.id))
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+
+    return replied_user
+
+
+    
+    
+    caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
+
+    return caption
+   
+
+
