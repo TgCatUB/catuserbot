33ddebb6fd98714dc90367ae3ed04db7d7f34796
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-02-22 16:31:10 +0530

    Update antivirus2.py

--- userbot/plugins/antivirus2.py
+++ userbot/plugins/antivirus2.py
@@ -4,6 +4,7 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.utils import admin_cmd,register
+from userbot import CMD_HELP
 
 @borg.on(admin_cmd("scan ?(.*)"))
 async def _(event):
