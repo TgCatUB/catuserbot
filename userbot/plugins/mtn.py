b7f3410722d933a00cfacc52dcc3a29992105ea6
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-01 16:56:44 +0530

    Update mtn.py

--- userbot/plugins/mtn.py
+++ userbot/plugins/mtn.py
@@ -10,7 +10,7 @@ from telethon import events
 import asyncio
 from userbot import CMD_HELP 
 
-
+from userbot.utils import admin_cmd
 
 
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
