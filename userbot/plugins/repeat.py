6baa11905fc3cf62c586002d19163fb208fece16
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-01 18:09:19 +0530

    Update repeat.py

--- userbot/plugins/repeat.py
+++ userbot/plugins/repeat.py
@@ -8,5 +8,5 @@ async def _(event):
     message = event.text[10:]
     count = int(event.text[8:10])
     repmessage = message * count
-    await wait([event.respond(repmessage)for i in range(count)])
+    await wait([event.respond(repmessage)])
     await event.delete()
