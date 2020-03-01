3d4e67437ef0669ec3ec96496e2524df682ac930
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-02 01:03:26 +0530

    Update being_logical.py

--- userbot/plugins/being_logical.py
+++ userbot/plugins/being_logical.py
@@ -18,8 +18,8 @@ import random
 from userbot import CMD_HELP
 from userbot.utils import admin_cmd
 
-@borg.on(events.NewMessage(pattern=r"\.belo", outgoing=True))
-
+@borg.on(events.NewMessage(pattern="belo ?(.*)", outgoing=True))
+#@borg.on(admin_cmd(pattern="dump ?(.*)"))
 async def _(event):
 
     if event.fwd_from:
