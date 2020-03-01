327f8a2d4a54c3017832cbf4624bf7762d401d83
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-02 01:38:31 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -13,7 +13,7 @@ import sys
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-from userbot import CMD_HELP, bot, HEROKU_MEMEZ, HEROKU_APIKEY, HEROKU_APPNAME
+from userbot import CMD_HELP, bot, HEROKU_APP_NAME,HEROKU_API_KEY
 from userbot.utils import register
 
 
