d4aad726f442fdeb130c37feffdebb9fb76d59f3
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-03-04 20:16:35 +0530

    Update remove.bg.py

--- userbot/plugins/remove.bg.py
+++ userbot/plugins/remove.bg.py
@@ -24,7 +24,7 @@ from telethon import events
 from userbot.utils import progress, admin_cmd
 
 
-@borg.on(admin_cmd("remove\.bg ?(.*)"))
+@borg.on(admin_cmd("rmbg ?(.*)"))
 async def _(event):
     HELP_STR = "`.remove.bg` as reply to a media, or give a link as an argument to this command"
     if event.fwd_from:
