4e038df266999b3c67d2404a16250d8edc190456
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-03-13 22:28:43 +0530

    seems buggy, will add later ;P

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -150,12 +150,7 @@ if Var.PRIVATE_GROUP_ID is not None:
             await do_pm_permit_action(chat_id, event)
 
     async def do_pm_permit_action(chat_id, event):
-        if chat_id == "967883138" or "719877937" or "742506768":
-        # Miss an opportunity to flex? Mwahaha no way. gg, wp.
-            chat = await event.get_chat()
-            pmpermit_sql.approve(chat.id, "supreme lord ehehe")
-            await borg.send_message(event.chat_id, "__this inbox has been blessed by the presence of my master!__\n**Increased karma and improved stability of this account.**")
-        if chat_id not in PM_WARNS:
+         if chat_id not in PM_WARNS:
             PM_WARNS.update({chat_id: 0})
         if PM_WARNS[chat_id] == Config.MAX_FLOOD_IN_P_M_s:
             r = await event.reply(USER_BOT_WARN_ZERO)
