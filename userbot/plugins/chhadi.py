24cabda9c30a658932aa31ebeda92d7d132def88
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-14 01:24:08 +0530

    Update chhadi.py

--- userbot/plugins/chhadi.py
+++ userbot/plugins/chhadi.py
@@ -3,7 +3,7 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
 
 @borg.on(admin_cmd("dm ?(.*)"))
 async def _(event):
