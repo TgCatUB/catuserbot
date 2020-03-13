0e6da3fbeb3d9d0aec14ec24fac1e5005f9eb2c3
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-14 01:56:50 +0530

    Update eval.py

--- userbot/plugins/eval.py
+++ userbot/plugins/eval.py
@@ -1,196 +1,75 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot module for executing code and terminal commands from Telegram. """
+"""Evaluate Python Code inside Telegram
+Syntax: .eval PythonCode"""
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from telethon import events, errors, functions, types
+import inspect
+import traceback
 import asyncio
-from getpass import getuser
-from os import remove
-from sys import executable
-from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID
-from userbot.utils import register
+import sys
+import io
+from userbot.utils import admin_cmd
 
 
-@register(outgoing=True, pattern="^.eval(?: |$)(.*)")
-async def evaluate(query):
-    """ For .eval command, evaluates the given Python expression. """
-    if query.is_channel and not query.is_group:
-        await query.edit("`Eval isn't permitted on channels`")
+@borg.on(admin_cmd("eval"))
+async def _(event):
+    if event.fwd_from:
         return
+    await event.edit("Processing ...")
+    cmd = event.text.split(" ", maxsplit=1)[1]
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
 
-    if query.pattern_match.group(1):
-        expression = query.pattern_match.group(1)
-    else:
-        await query.edit("``` Give an expression to evaluate. ```")
-        return
-
-    if expression in ("userbot.session", "config.env"):
-        await query.edit("`That's a dangerous operation! Not Permitted!`")
-        return
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
 
     try:
-        evaluation = str(eval(expression))
-        if evaluation:
-            if isinstance(evaluation, str):
-                if len(evaluation) >= 4096:
-                    file = open("output.txt", "w+")
-                    file.write(evaluation)
-                    file.close()
-                    await query.client.send_file(
-                        query.chat_id,
-                        "output.txt",
-                        reply_to=query.id,
-                        caption="`Output too large, sending as file`",
-                    )
-                    remove("output.txt")
-                    return
-                await query.edit("**Query: **\n`"
-                                 f"{expression}"
-                                 "`\n**Result: **\n`"
-                                 f"{evaluation}"
-                                 "`")
-        else:
-            await query.edit("**Query: **\n`"
-                             f"{expression}"
-                             "`\n**Result: **\n`No Result Returned/False`")
-    except Exception as err:
-        await query.edit("**Query: **\n`"
-                         f"{expression}"
-                         "`\n**Exception: **\n"
-                         f"`{err}`")
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
 
-    if BOTLOG:
-        await query.client.send_message(
-            BOTLOG_CHATID,
-            f"Eval query {expression} was executed successfully")
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
 
-
-@register(outgoing=True, pattern=r"^.exec(?: |$)([\s\S]*)")
-async def run(run_q):
-    """ For .exec command, which executes the dynamically created program """
-    code = run_q.pattern_match.group(1)
-
-    if run_q.is_channel and not run_q.is_group:
-        await run_q.edit("`Exec isn't permitted on channels!`")
-        return
-
-    if not code:
-        await run_q.edit("``` At least a variable is required to \
-execute. Use .help exec for an example.```")
-        return
-
-    if code in ("userbot.session", "config.env"):
-        await run_q.edit("`That's a dangerous operation! Not Permitted!`")
-        return
-
-    if len(code.splitlines()) <= 5:
-        codepre = code
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
     else:
-        clines = code.splitlines()
-        codepre = clines[0] + "\n" + clines[1] + "\n" + clines[2] + \
-            "\n" + clines[3] + "..."
+        evaluation = "Success"
 
-    command = "".join(f"\n {l}" for l in code.split("\n.strip()"))
-    process = await asyncio.create_subprocess_exec(
-        executable,
-        '-c',
-        command.strip(),
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await process.communicate()
-    result = str(stdout.decode().strip()) \
-        + str(stderr.decode().strip())
+    final_output = "**EVAL**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
 
-    if result:
-        if len(result) > 4096:
-            file = open("output.txt", "w+")
-            file.write(result)
-            file.close()
-            await run_q.client.send_file(
-                run_q.chat_id,
-                "output.txt",
-                reply_to=run_q.id,
-                caption="`Output too large, sending as file`",
+    if len(final_output) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id
             )
-            remove("output.txt")
-            return
-        await run_q.edit("**Query: **\n`"
-                         f"{codepre}"
-                         "`\n**Result: **\n`"
-                         f"{result}"
-                         "`")
+            await event.delete()
     else:
-        await run_q.edit("**Query: **\n`"
-                         f"{codepre}"
-                         "`\n**Result: **\n`No Result Returned/False`")
-
-    if BOTLOG:
-        await run_q.client.send_message(
-            BOTLOG_CHATID,
-            "Exec query " + codepre + " was executed successfully")
+        await event.edit(final_output)
 
 
-@register(outgoing=True, pattern="^.term(?: |$)(.*)")
-async def terminal_runner(term):
-    """ For .term command, runs bash commands and scripts on your server. """
-    curruser = getuser()
-    command = term.pattern_match.group(1)
-    try:
-        from os import geteuid
-        uid = geteuid()
-    except ImportError:
-        uid = "This ain't it chief!"
-
-    if term.is_channel and not term.is_group:
-        await term.edit("`Term commands aren't permitted on channels!`")
-        return
-
-    if not command:
-        await term.edit("``` Give a command or use .help term for \
-            an example.```")
-        return
-
-    if command in ("userbot.session", "config.env"):
-        await term.edit("`That's a dangerous operation! Not Permitted!`")
-        return
-
-    process = await asyncio.create_subprocess_shell(
-        command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await process.communicate()
-    result = str(stdout.decode().strip()) \
-        + str(stderr.decode().strip())
-
-    if len(result) > 4096:
-        output = open("output.txt", "w+")
-        output.write(result)
-        output.close()
-        await term.client.send_file(
-            term.chat_id,
-            "output.txt",
-            reply_to=term.id,
-            caption="`Output too large, sending as file`",
-        )
-        remove("output.txt")
-        return
-
-    if uid is 0:
-        await term.edit("`" f"{curruser}:~# {command}" f"\n{result}" "`")
-    else:
-        await term.edit("`" f"{curruser}:~$ {command}" f"\n{result}" "`")
-
-    if BOTLOG:
-        await term.client.send_message(
-            BOTLOG_CHATID,
-            "Terminal Command " + command + " was executed sucessfully",
-        )
-
-
-CMD_HELP.update({"eval": ".eval 2 + 3\nUsage: Evalute mini-expressions."})
-CMD_HELP.update(
-    {"exec": ".exec print('hello')\nUsage: Execute small python scripts."})
-CMD_HELP.update(
-    {"term": ".term ls\nUsage: Run bash commands and scripts on your server."})
+async def aexec(code, event):
+    exec(
+        f'async def __aexec(event): ' +
+        ''.join(f'\n {l}' for l in code.split('\n'))
+    )
+    return await locals()['__aexec'](event)
