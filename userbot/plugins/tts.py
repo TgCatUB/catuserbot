1ecc357efc33b5002867a33a70305fab84c41967
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-14 02:43:49 +0530

    Update tts.py

--- userbot/plugins/tts.py
+++ userbot/plugins/tts.py
@@ -11,7 +11,7 @@ from gtts import gTTS
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("speech (.*)"))
+@borg.on(admin_cmd("tts (.*)"))
 async def _(event):
     if event.fwd_from:
         return
