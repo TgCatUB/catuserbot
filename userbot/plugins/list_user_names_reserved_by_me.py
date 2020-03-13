fec9bd8ec340e213bf53df63b7a29895ff340a17
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-14 02:17:38 +0530

    Update list_user_names_reserved_by_me.py

--- userbot/plugins/list_user_names_reserved_by_me.py
+++ userbot/plugins/list_user_names_reserved_by_me.py
@@ -2,15 +2,16 @@
 # (c) Shrimadhav U K
 
 from telethon import events, functions, types
-import asyncio
+from userbot.utils import admin_cmd
 
+from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest
 
-@borg.on(events.NewMessage(pattern=r"\-listmyusernames", outgoing=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    result = await borg(functions.channels.GetAdminedPublicChannelsRequest())
+@borg.on(admin_cmd("listmyusernames"))
+
+async def mine(event):
+    """ For .reserved command, get a list of your reserved usernames. """
+    result = await bot(GetAdminedPublicChannelsRequest())
     output_str = ""
     for channel_obj in result.chats:
-        output_str += f"- {channel_obj.title} @{channel_obj.username} \n"
+        output_str += f"{channel_obj.title}\n@{channel_obj.username}\n\n"
     await event.edit(output_str)
