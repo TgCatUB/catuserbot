4080a2854a380bc96d939c21ce6b1b01557f2819
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-13 23:48:14 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -152,7 +152,7 @@ def admin_cmd(pattern=None, **args):
     args["outgoing"] = True
     # should this command be available for other users?
     if allow_sudo:
-        args["from_users"] = list(Var.SUDO_USERS)
+        args["from_users"] = list(Config.SUDO_USERS)
         # Mutually exclusive with outgoing (can only set one of either).
         args["incoming"] = True
         del args["allow_sudo"]
@@ -233,11 +233,56 @@ def register(**args):
 
 
 def errors_handler(func):
-    async def wrapper(event):
+    async def wrapper(errors):
         try:
-            return await func(event)
-        except Exception:
-            pass
+            await func(errors)
+        except BaseException:
+
+            date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+            new = {
+                'error': str(sys.exc_info()[1]),
+                'date': datetime.datetime.now()
+            }
+
+            text = "**USERBOT CRASH REPORT**\n\n"
+
+            link = "[here](https://t.me/PaperplaneExtendedSupport)"
+            text += "If you wanna you can report it"
+            text += f"- just forward this message {link}.\n"
+            text += "Nothing is logged except the fact of error and date\n"
+
+            ftext = "\nDisclaimer:\nThis file uploaded ONLY here,"
+            ftext += "\nwe logged only fact of error and date,"
+            ftext += "\nwe respect your privacy,"
+            ftext += "\nyou may not report this error if you've"
+            ftext += "\nany confidential data here, no one will see your data\n\n"
+
+            ftext += "--------BEGIN USERBOT TRACEBACK LOG--------"
+            ftext += "\nDate: " + date
+            ftext += "\nGroup ID: " + str(errors.chat_id)
+            ftext += "\nSender ID: " + str(errors.sender_id)
+            ftext += "\n\nEvent Trigger:\n"
+            ftext += str(errors.text)
+            ftext += "\n\nTraceback info:\n"
+            ftext += str(traceback.format_exc())
+            ftext += "\n\nError text:\n"
+            ftext += str(sys.exc_info()[1])
+            ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
+
+            command = "git log --pretty=format:\"%an: %s\" -5"
+
+            ftext += "\n\n\nLast 5 commits:\n"
+
+            process = await asyncio.create_subprocess_shell(
+                command,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE)
+            stdout, stderr = await process.communicate()
+            result = str(stdout.decode().strip()) \
+                + str(stderr.decode().strip())
+
+            ftext += result
+
     return wrapper
 
 async def progress(current, total, event, start, type_of_ps, file_name=None):
