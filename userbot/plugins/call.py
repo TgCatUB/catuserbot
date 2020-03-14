40eae15b21766e6ce5e9cafaf924b4c4b2265705
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-15 01:18:47 +0530

    Update call.py

--- userbot/plugins/call.py
+++ userbot/plugins/call.py
@@ -14,11 +14,11 @@ from telethon import events
 
 import asyncio
 
+from userbot.utils import admin_cmd
 
 
 
-
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(admin_cmd(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
 
