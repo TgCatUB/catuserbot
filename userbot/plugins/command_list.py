86cefd48883a5893bdea0de75c70cac53c9365b1
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-14 15:54:46 +0530

    Create command_list.py

---
+++ userbot/plugins/command_list.py
@@ -0,0 +1,21 @@
+from telethon import events
+import subprocess
+import asyncio
+import time
+from userbot.utils import admin_cmd
+
+
+@command(pattern="^.cmds", outgoing=True)
+async def install(event):
+    if event.fwd_from:
+        return
+    cmd = "ls userbot/plugins"
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    o = stdout.decode()
+    _o = o.split("\n")
+    o = "\n".join(_o)
+    OUTPUT = f"**List of Plugins:**\n{o}\n\n**TIP:** __If you want to know the commands for a plugin, do:-__ \n `.help <plugin name>` **without the < > brackets.**\n__All plugins might not work directly."
+    await event.edit(OUTPUT)
