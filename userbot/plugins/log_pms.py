3cffcf4653f17315721c39ce71c75baf66f2c44b
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-15 03:14:27 +0530

    Update log_pms.py

--- userbot/plugins/log_pms.py
+++ userbot/plugins/log_pms.py
@@ -40,7 +40,7 @@ async def monito_p_m_s(event):
                 print(e) 
 
 
-@borg.on(events.NewMessage(pattern="nolog ?(.*)"))
+@borg.on(admin_cmd(pattern="nolog ?(.*)"))
 async def approve_p_m(event):
     if event.fwd_from:
         return
