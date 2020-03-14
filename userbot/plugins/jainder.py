7748d60cb39ddffa1397a5cfbd0794f71dccec02
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-15 03:12:44 +0530

    Update jainder.py

--- userbot/plugins/jainder.py
+++ userbot/plugins/jainder.py
@@ -4,8 +4,9 @@
 from telethon import events
 import random
 import asyncio
+from userbot.utils import admin_cmd
 
-@borg.on(events.NewMessage(pattern=r"\.jainder(.*)", outgoing=True))
+@borg.on(admin_cmd(pattern=r"\.jainder(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
