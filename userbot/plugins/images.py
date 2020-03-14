19328f489f141c7013047fea818058ac09935eda
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-15 00:00:07 +0530

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -9,7 +9,7 @@ from re import findall
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("img ?(.*)"))
+@register(outgoing=True, pattern="^.img (.*)")
 async def img_sampler(event):
     await event.edit("Processing...")
     query = event.pattern_match.group(1)
@@ -19,9 +19,8 @@ async def img_sampler(event):
         lim = lim.replace("lim=", "")
         query = query.replace("lim=" + lim[0], "")
     except IndexError:
-        lim = 2
+        lim = 3
     response = google_images_download.googleimagesdownload()
-
     # creating list of arguments
     arguments = {
         "keywords": query,
@@ -29,10 +28,10 @@ async def img_sampler(event):
         "format": "jpg",
         "no_directory": "no_directory"
     }
-
     # passing the arguments to the function
     paths = response.download(arguments)
     lst = paths[0][query]
-    await borg.send_file(await borg.get_input_entity(event.chat_id), lst)
+    await event.client.send_file(
+        await event.client.get_input_entity(event.chat_id), lst)
     shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
     await event.delete()
