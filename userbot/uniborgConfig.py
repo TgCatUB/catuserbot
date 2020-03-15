b0db8b212c78696259a017ffc98f6aa26ed0124a
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-16 03:22:28 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -112,7 +112,7 @@ class Config(object):
     VIRUSTOTAL_API_KEY = os.environ.get("VIRUSTOTAL_API_KEY", None)
     #
     # define "spam" in PMs
-    MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 3))
+    MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", None))
     # leave this blank, should be automatically filled for Heroku.com users
     PM_LOGGR_BOT_API_ID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
     if PM_LOGGR_BOT_API_ID:
