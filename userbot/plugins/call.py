3da672564014081ac5da22e90b2cb9b105fc9ff2
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-15 15:08:35 +0530

    Update call.py

--- userbot/plugins/call.py
+++ userbot/plugins/call.py
@@ -18,7 +18,7 @@ from userbot.utils import admin_cmd
 
 
 
-@borg.on(admin_cmd(pattern=r"\.(.*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
 
