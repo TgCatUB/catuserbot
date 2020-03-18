080367243ea3c3231b08cb7a5bd9b37f432ac982
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-03-18 13:05:00 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -28,8 +28,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. Provided by @A_Dark_Princ3 \n [Check out this dope af website](https://www.moddingunited.xyz/) \n
-`Userbot Helper to reveal all the commands`"""
+            help_string = """Userbot Helper.. Provided by @A_Dark_Princ3 \n[Check out this dope af website](https://www.moddingunited.xyz/) \n
+`Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
