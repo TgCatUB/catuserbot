6030add48b92033cdd5065674d09a3a807df7e1e
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-03-18 12:22:53 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -14,17 +14,8 @@ async def cmd_list(event):
                     string += "\n"
                 string += "\n"
             if len(string) > 4095:
-                with io.BytesIO(str.encode(string)) as out_file:
-                    out_file.name = "cmd.txt"
-                    await bot.send_file(
-                        event.chat_id,
-                        out_file,
-                        force_document=True,
-                        allow_cache=False,
-                        caption="**COMMANDS**",
-                        reply_to=reply_to_id
-                    )
-                    await event.delete()
+                await borg.send_message(event.chat_id, "Do .help cmd")
+                await asyncio.sleep(5)
             else:
                 await event.edit(string)
         elif input_str:
