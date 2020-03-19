d17d98edc2a240832716a5ee5319f5316b41fcc2
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-19 21:51:41 +0530

    Update autoname.py

--- userbot/plugins/autoname.py
+++ userbot/plugins/autoname.py
@@ -10,7 +10,7 @@ from userbot import AUTONAME
 
 
 DEL_TIME_OUT = 60
-DEFAULTUSER = str(AUTONAME) if AUTONAME else "mrconfusedbot"
+DEFAULTUSER = str(AUTONAME) if AUTONAME else "π.$"
 
 
 @borg.on(admin_cmd(pattern="autoname"))  # pylint:disable=E0602
@@ -20,7 +20,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d-%m-%y")
         HM = time.strftime("%H:%M")
-        name = f"{HM} {DEFAULTUSER} {DM}"
+        name = f"{DEFAULTUSER} {HM} {DM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
