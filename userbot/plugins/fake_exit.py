b8edfc9a3c35b43f94656f0c46c0f21de710079b
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-21 11:49:48 +0530

    Create fake_exit.py

---
+++ userbot/plugins/fake_exit.py
@@ -0,0 +1,26 @@
+# For @UniBorg
+
+"""fake exit
+\n.fexit"""
+
+
+
+from telethon import events
+from datetime import datetime
+from userbot.utils import admin_cmd
+import importlib.util
+import asyncio
+import random
+import importlib.util
+
+@borg.on(events.NewMessage(outgoing=True, pattern='^\.(f?f)exit'))
+async def timer_blankx(e):
+ txt=e.text[7:] + '\n\n`Processing....` '
+ j=1
+ k=j
+ for j in range(j):
+  await e.edit(txt + str(k))
+  k=k-1
+  await asyncio.sleep(1)
+ if e.pattern_match.group(1) == 'f':
+  await e.edit("`Legend is leaving this chat.....!` `Goodbye aren't forever. It was a pleasant time with you guys..` ")
