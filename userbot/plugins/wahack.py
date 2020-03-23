03a0e37c2c1a6d356eac4663528ec3637be98c3a
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-24 01:48:44 +0530

    Update wahack.py

--- userbot/plugins/wahack.py
+++ userbot/plugins/wahack.py
@@ -1,13 +1,6 @@
 """Emoji
-
-
-
 Available Commands:
-
 .wahack
-
-
-
 by © Thunder God Raiden
 
 """
@@ -15,60 +8,22 @@ by © Thunder God Raiden
 
 
 from telethon import events
-
-
-
 import asyncio
 
 
-
-
-
-
-
-
-
-
-
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
-
-
 async def _(event):
-
-
-
     if event.fwd_from:
-
-
-
         return
-
-
-
     animation_interval = 2
-
-
-
     animation_ttl = range(0, 15)
-
-
-
     input_str = event.pattern_match.group(1)
-
-
-
     if input_str == "wahack":
-
-
-
         await event.edit(input_str)
 
 
 
         animation_chars = [
-
-        
 
             "Looking for WhatsApp databases in targeted person...",
 
@@ -105,11 +60,5 @@ async def _(event):
 
 
         for i in animation_ttl:
-
-
-
             await asyncio.sleep(animation_interval)
-
-
-
             await event.edit(animation_chars[i % 15])
