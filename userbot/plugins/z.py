b56f62b1b801b925ed47c599d4940133fb33231a
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-25 02:35:44 +0530

    Update z.py

--- userbot/plugins/z.py
+++ userbot/plugins/z.py
@@ -1,6 +1,14 @@
 from telethon import events
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd("zen"))
+@borg.on(admin_cmd("zelp"))
 async def handler(event):
-    await event.reply("hey")
+    await event.reply("commands for carbon are \n
+                      '.kar1'
+                      '.kar2'
+                      '.kar3'
+                      '.kar4'
+                      '.karpp'
+                      '.kargb'
+                      '.rgbk2'
+                      ")
