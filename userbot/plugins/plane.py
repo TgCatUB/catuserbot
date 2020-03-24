7af4fda2ad5c2bb374ce33ba75b576658d0857db
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-25 02:20:02 +0530

    Update plane.py

--- userbot/plugins/plane.py
+++ userbot/plugins/plane.py
@@ -6,6 +6,8 @@ import sys
 from userbot import CMD_HELP 
 
 from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern=f"plane", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.plane", outgoing=True))
 async def _(event):
     if event.fwd_from:
