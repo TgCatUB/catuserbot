45006e40121f745e5518968dfd062d71ddf07e5f
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-25 02:32:37 +0530

    Update virus.py

--- userbot/plugins/virus.py
+++ userbot/plugins/virus.py
@@ -12,6 +12,9 @@ import asyncio
 
 
 
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern=f"virus", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
