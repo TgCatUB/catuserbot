fee998fbe7660b6d6a729c924f58368790a342eb
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-25 02:19:15 +0530

    Update padmin.py

--- userbot/plugins/padmin.py
+++ userbot/plugins/padmin.py
@@ -13,6 +13,7 @@ from userbot.utils import admin_cmd
 
 
 
+@borg.on(admin_cmd(pattern=f"padmin", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
