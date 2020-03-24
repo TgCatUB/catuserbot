272afb56f8ad54b8d9a68bf15831e9810a863db8
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-25 02:18:39 +0530

    Update os.py

--- userbot/plugins/os.py
+++ userbot/plugins/os.py
@@ -18,6 +18,7 @@ from userbot import CMD_HELP
 from userbot.utils import admin_cmd
 
 
+@borg.on(admin_cmd(pattern=f"macos", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
@@ -58,6 +59,7 @@ async def _(event):
             await event.edit(animation_chars[i % 11])
 
 
+@borg.on(admin_cmd(pattern=f"windows", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
@@ -99,6 +101,7 @@ async def _(event):
 
 
 
+@borg.on(admin_cmd(pattern=f"linux", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
@@ -139,6 +142,7 @@ async def _(event):
             await event.edit(animation_chars[i % 11])
 
 
+@borg.on(admin_cmd(pattern=f"stock", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
@@ -179,6 +183,7 @@ async def _(event):
             await event.edit(animation_chars[i % 11])
 
 
+@borg.on(admin_cmd(pattern=f"os", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
