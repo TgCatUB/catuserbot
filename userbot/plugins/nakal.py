a32588250eed65bc0a6d7a82f3f645de49052bdf
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-25 02:07:13 +0530

    Update nakal.py

--- userbot/plugins/nakal.py
+++ userbot/plugins/nakal.py
@@ -11,13 +11,12 @@ Available Commands:
 .emoji -_-"""
 
 from telethon import events
-from userbot.utils import admin_cmd
 import asyncio
 
 
+from userbot.utils import admin_cmd
 
-
-
+@borg.on(admin_cmd(pattern=f"nakal", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
