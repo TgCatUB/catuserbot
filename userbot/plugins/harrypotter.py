77d8acdf3de5c5b85cb752af0476047578bd5af6
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-25 01:20:04 +0530

    Update harrypotter.py

--- userbot/plugins/harrypotter.py
+++ userbot/plugins/harrypotter.py
@@ -8,9 +8,10 @@ import asyncio
 import os
 import sys
 import random
+
 from userbot.utils import admin_cmd
 
-
+@borg.on(admin_cmd(pattern=f"hps", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.hps", outgoing=True))
 async def _(event):
     if event.fwd_from:
