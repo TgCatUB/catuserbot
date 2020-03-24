2914b8e1c27ec06d9406749593bc34f9f369597f
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-24 17:28:40 +0530

    Update exec.py

--- userbot/plugins/exec.py
+++ userbot/plugins/exec.py
@@ -1,68 +1,52 @@
-from telethon import events, errors, functions, types
-import inspect
-import traceback
-import asyncio
-import sys
+"""Execute GNU/Linux commands inside Telegram
+Syntax: .exec Code"""
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from telethon import events
+import subprocess
+from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
 import io
+import asyncio
+import time
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd("exec"))
+
+@borg.on(admin_cmd(pattern="exec ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("Processing ...")
-    cmd = event.text.split(" ", maxsplit=1)[1]
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
-    stdout, stderr, exc = None, None, None
-
-    try:
-        await aexec(cmd, event)
-    except Exception:
-        exc = traceback.format_exc()
-
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    e = stderr.decode()
+    if not e:
+        e = "No Error"
+    o = stdout.decode()
+    if not o:
+        o = "**Tip**: \n`If you want to see the results of your code, I suggest printing them to stdout.`"
     else:
-        evaluation = "Success"
-
-    final_output = "**EXEC**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
-
-    if len(final_output) > 4096:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await bot.send_file(
+        _o = o.split("\n")
+        o = "`\n".join(_o)
+    OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n{o}"
+    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "exec.text"
+            await borg.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
                 allow_cache=False,
-                caption=f"**PROCCESSED**: `{cmd}`",
+                caption=cmd,
                 reply_to=reply_to_id
             )
             await event.delete()
-    else:
-        await event.edit(final_output)
-
-
-async def aexec(code, event):
-    exec(
-        f'async def __aexec(event): ' +
-        ''.join(f'\n {l}' for l in code.split('\n'))
-    )
-    return await locals()['__aexec'](event)
+    await event.edit(OUTPUT)
