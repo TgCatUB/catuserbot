d8e0357b8d9f17b8653531feaea8ed887c516a96
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-03-25 02:32:12 +0530

    Update tost.py

--- userbot/plugins/tost.py
+++ userbot/plugins/tost.py
@@ -5,6 +5,9 @@ Available Command:
 from telethon import events
 import asyncio
 
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern=f"tost", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
