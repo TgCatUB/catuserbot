b0adaa037b7631d7e4bdfc5a2ab2589b4baf93bc
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-26 02:20:48 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -10,7 +10,7 @@ PM_WARNS = {}
 PREV_REPLY_MESSAGE = {}
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No name set yet nibba, check pinned message in** @XtraTgBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**@mrconfused** "
 USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth you  has been blocked by my master's userbot.` **Now GTFO, i'm playing minecraft** "
 USER_BOT_NO_WARN = ("[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](https://t.me/indiansongs143)\n\n"
                     "`Hello, this is sandeep's Security Service.You have found your way here to my master,`"
