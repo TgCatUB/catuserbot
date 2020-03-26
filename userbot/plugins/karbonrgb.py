d56b345edd698416721766e1eb3a0439b2471d74
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-26 23:48:38 +0530

    Update karbonrgb.py

--- userbot/plugins/karbonrgb.py
+++ userbot/plugins/karbonrgb.py
@@ -13,7 +13,10 @@ from time import sleep
 import asyncio
 import os
 import random
+from userbot.utils import admin_cmd
 
+
+@borg.on(admin_cmd(pattern=f"kargb", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.kargb", outgoing=True))
 async def carbon_api(e):
  RED = random.randint(0,256)
