232bac5674e7a867b9fea9bd80bf59e994c5c9bb
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-28 00:18:46 +0530

    Update add_watermark.py

--- userbot/plugins/add_watermark.py
+++ userbot/plugins/add_watermark.py
@@ -22,8 +22,8 @@ async def _(event):
     mone = await event.edit("Processing ...")
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    if not os.path.isdir("./DOWNLOADS/watermark/"):
-        os.makedirs("./DOWNLOADS/watermark/")
+    if not os.path.isdir("./cat/watermark/"):
+        os.makedirs("./cat/watermark/")
     if event.reply_to_msg_id:
         start = datetime.now()
         reply_message = await event.get_reply_message()
@@ -44,10 +44,10 @@ async def _(event):
             await mone.edit("Stored the pdf to `{}` in {} seconds.".format(downloaded_file_name, ms))
             watermark(
                 inputpdf=downloaded_file_name,
-                outputpdf='./DOWNLOADS/watermark/' + reply_message.file.name,
+                outputpdf='./cat/watermark/' + reply_message.file.name,
                 watermarkpdf='./bin/watermark.pdf'
             )
-        # filename = sorted(get_lst_of_files('./cat/watermark/' + reply_message.file.name, []))
+        # filename = sorted(get_lst_of_files('./ravana/watermark/' + reply_message.file.name, []))
         #filename = filename + "/"
         await event.edit("Uploading now")
         caption_rts = os.path.basename(watermark_path + reply_message.file.name)
@@ -59,6 +59,60 @@ async def _(event):
                 progress(d, t, event, c_time, "trying to upload")
             )
         )
+        # r=root, d=directories, f = files
+        # for single_file in filename:
+        #     if os.path.exists(single_file):
+        #         # https://stackoverflow.com/a/678242/4723940
+        #         caption_rts = os.path.basename(single_file)
+        #         force_document = False
+        #         supports_streaming = True
+        #         document_attributes = []
+        #         if single_file.endswith((".mp4", ".mp3", ".flac", ".webm")):
+        #             metadata = extractMetadata(createParser(single_file))
+        #             duration = 0
+        #             width = 0
+        #             height = 0
+        #             if metadata.has("duration"):
+        #                 duration = metadata.get('duration').seconds
+        #             if os.path.exists(thumb_image_path):
+        #                 metadata = extractMetadata(createParser(thumb_image_path))
+        #                 if metadata.has("width"):
+        #                     width = metadata.get("width")
+        #                 if metadata.has("height"):
+        #                     height = metadata.get("height")
+        #             document_attributes = [
+        #                 DocumentAttributeVideo(
+        #                     duration=duration,
+        #                     w=width,
+        #                     h=height,
+        #                     round_message=False,
+        #                     supports_streaming=True
+        #                 )
+        #             ]
+        #         try:
+        #             await borg.send_file(
+        #                 event.chat_id,
+        #                 single_file,
+        #                 caption=f"`{caption_rts}`",
+        #                 force_document=force_document,
+        #                 supports_streaming=supports_streaming,
+        #                 allow_cache=False,
+        #                 reply_to=event.message.id,
+        #                 attributes=document_attributes,
+        #                 # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+        #                 #     progress(d, t, event, c_time, "trying to upload")
+        #                 # )
+        #             )
+        #         except Exception as e:
+        #             await borg.send_message(
+        #                 event.chat_id,
+        #                 "{} caused `{}`".format(caption_rts, str(e)),
+        #                 reply_to=event.message.id
+        #             )
+        #             # some media were having some issues
+        #             continue
+        #         os.remove(single_file)
+        # os.remove(downloaded_file_name)
 
 def watermark(inputpdf, outputpdf, watermarkpdf):
     watermark = PdfFileReader(watermarkpdf)
