5470874e356a4c14786740b91a478ebbf98166ba
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-31 00:00:26 +0530

    Create dice.py

---
+++ userbot/plugins/dice.py
@@ -0,0 +1,21 @@
+"""@RollADie
+Syntax: .dice"""
+from telethon.tl.types import InputMediaDice
+from userbot.utils import admin_cmd
+
+
+@borg.on(admin_cmd(pattern="dice ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice())
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice())
+        except:
+            pass
