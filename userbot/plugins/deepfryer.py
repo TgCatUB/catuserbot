ebbf101ab0c0397d1387c10a9920e0dd0b2d7d7c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-31 02:40:20 +0530

    Update deepfryer.py

--- userbot/plugins/deepfryer.py
+++ userbot/plugins/deepfryer.py
@@ -35,7 +35,7 @@ from random import randint, uniform
 from PIL import Image, ImageEnhance, ImageOps
 from telethon.tl.types import DocumentAttributeFilename
 
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
 from telethon import events
 
 
