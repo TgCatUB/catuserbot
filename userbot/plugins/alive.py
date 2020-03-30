a10fff301beac7d1b6ac8954f04318bdfcd17c5d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-30 20:13:44 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,14 +6,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@mrconfused"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**MY BOT IS RUNNING SUCCESFULLY**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.8.2\n"
-                     "`Bot created by:`[Sandeep](https://t.me/mrconfused) \n"
+                     "`Telethon version: 1.11.3\nPython: 3.8.2\n"
+                     "`Bot created by:` [Sandeep](tg://user?id=916234223)\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n"
                      #"[Deploy this userbot Now](https://github.com/sandy1709/userbot)"
