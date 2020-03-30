0bd45eddbe91c36c7184da9cc8987083a4c7b1c4
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-30 13:44:10 +0530

    Update currency.py

--- userbot/plugins/currency.py
+++ userbot/plugins/currency.py
@@ -4,7 +4,7 @@ from telethon import events
 import asyncio
 from datetime import datetime
 import requests
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
 
 
 @borg.on(admin_cmd(pattern="currency (.*)"))
