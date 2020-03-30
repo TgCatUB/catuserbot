bd49cd623b70c16c95c03d5fa4dc18a411c04a06
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-31 02:33:49 +0530

    Update meme.py

--- userbot/plugins/meme.py
+++ userbot/plugins/meme.py
@@ -117,3 +117,52 @@ async def give(event):
     await event.edit(giveVar+giveVar+giveVar+giveVar+giveVar+giveVar+giveVar+giveVar+giveVar+giveVar)
     await asyncio.sleep(sleepValue)        
     
+@borg.on(admin_cmd(pattern="lcry"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1
+    animation_ttl = range(0, 103)
+    await event.edit("crying")
+    animation_chars = [
+
+            ";__",
+            ";___",
+            ";____",
+            ";_____",
+            ";______",
+            ";_______",
+            ";________",
+            ";__________",
+            ";____________",
+            ";______________",
+            ";________________",
+            ";__________________",
+            ";____________________",
+            ";______________________",
+            ";________________________",
+            ";_________________________",
+            ";_________________________",
+            ";________________________",
+            ";_______________________",
+            ";______________________",
+            ";_____________________",
+            ";____________________",
+            ";___________________",
+            ";__________________",
+            ";_________________",
+            ";________________",
+            ";_______________",
+            ";_____________",
+            ";___________",
+            ";_________",
+            ";_______",
+            ";_____",
+            ";____",
+            ";___",
+            ";__",
+            ";You made me `CRY`"
+        ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 35])
