b38fa9eef710bd91127c16babe2c81c0e84f5edc
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-01 02:48:46 +0530

    Update wirelesscharge.py

--- userbot/plugins/wirelesscharge.py
+++ userbot/plugins/wirelesscharge.py
@@ -42,7 +42,7 @@ async def timer_blankx(e):
 
   await asyncio.sleep(1)
 
- if e.pattern_match.group(1) == 'f':
+ if e.pattern_match.group(1) == '100':
 
   await e.edit("`Tesla Wireless Charging (beta) Completed...\nDevice Detected: Nokia 1100 (Space Grey Varient)\nBattery Percentage:` [100%](https://telegra.ph/file/a45aa7450c8eefed599d9.mp4) ", link_preview=True)
 
