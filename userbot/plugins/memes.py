d88b18db268307e54748fc93abc6d014131c2c7f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-31 16:50:56 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -848,13 +848,9 @@ async def vapor(vpr):
 @register(outgoing=True, pattern="^.repo$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Click [here](https://github.com/sandy1709/userbot) to open this lit af repo.")
+        await e.edit("Click [here](https://github.com/sandy1709/catuserbot) to open this lit af repo.")
 			  
 
-@register(outgoing=True, pattern="^.myrepo$")
-async def source(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Click [here](https://github.com/sandy1709/userbot) to open this lit af repo.")
 			  
 @register(outgoing=True, pattern="^.str(?: |$)(.*)")
 async def stretch(stret):
@@ -927,17 +923,7 @@ async def hoi(hello):
         await hello.edit(random.choice(HELLOSTR))
 			  
 
-			  
-@register(outgoing=True, pattern="^.bt$")
-async def bluetext(bte):
-    """ Believe me, you will find this useful. """
-    if not bte.text[0].isalpha() and bte.text[0] not in ("/", "#", "@", "!"):
-        if await bte.get_reply_message():
-            await bte.edit(
-                "`BLUETEXT MUST CLICK.`\n"
-                "`Are you a stupid animal which is attracted to colours?`"
-            )
-			  
+			  			  
 @register(outgoing=True, pattern="^.rape$")
 async def raping (raped):
     """ Dont Rape Too much -_-"""
@@ -982,7 +968,7 @@ async def fuckedd (abusehard):
 
 			  
 			  
-@register(outgoing=True, pattern="^.abuse$")
+@register(outgoing=True, pattern="^.abusehim$")
 async def abusing (abused):
     """ Dont Abuse Too much bsdk -_-"""
     if not abused.text[0].isalpha() and abused.text[0] not in ("/", "#", "@", "!"):
@@ -1186,78 +1172,6 @@ async def let_me_google_that_for_you(lmgtfy_q):
             )
 
 
-@register(outgoing=True, pattern=r".yt_dl (\S*) ?(\S*)")
-async def download_video(v_url):
-    """ For .yt_dl command, download videos from YouTube. """
-    if not v_url.text[0].isalpha() and v_url.text[0] not in ("/", "#", "@", "!"):
-        url = v_url.pattern_match.group(1)
-        quality = v_url.pattern_match.group(2)
-
-        await v_url.edit("**Fetching...**")
-
-        video = YouTube(url)
-
-        if quality:
-            video_stream = video.streams.filter(
-                progressive=True,
-                subtype="mp4",
-                res=quality
-            ).first()
-        else:
-            video_stream = video.streams.filter(
-                progressive=True,
-                subtype="mp4"
-            ).first()
-
-        if video_stream is None:
-            all_streams = video.streams.filter(
-                progressive=True,
-                subtype="mp4"
-            ).all()
-            available_qualities = ""
-
-            for item in all_streams[:-1]:
-                available_qualities += f"{item.resolution}, "
-            available_qualities += all_streams[-1].resolution
-
-            await v_url.edit(
-                "**A stream matching your query wasn't found. Try again with different options.\n**"
-                "**Available Qualities:**\n"
-                f"{available_qualities}"
-            )
-            return
-
-        video_size = video_stream.filesize / 1000000
-
-        if video_size >= 50:
-            await v_url.edit(
-                ("**File larger than 50MB. Sending the link instead.\n**"
-                 f"Get the video [here]({video_stream.url})\n\n"
-                 "**If the video plays instead of downloading, right click(or long press on touchscreen) and "
-                 "press 'Save Video As...'(may depend on the browser) to download the video.**")
-            )
-            return
-
-        await v_url.edit("**Downloading...**")
-
-        video_stream.download(filename=video.title)
-
-        url = f"https://img.youtube.com/vi/{video.video_id}/maxresdefault.jpg"
-        resp = get(url)
-        with open('thumbnail.jpg', 'wb') as file:
-            file.write(resp.content)
-
-        await v_url.edit("**Uploading...**")
-        await v_url.client.send_file(
-            v_url.chat_id,
-            f'{safe_filename(video.title)}.mp4',
-            caption=f"{video.title}",
-            thumb="thumbnail.jpg"
-        )
-
-        os.remove(f"{safe_filename(video.title)}.mp4")
-        os.remove('thumbnail.jpg')
-        await v_url.delete()
 			  
 @register(pattern='.type(?: |$)(.*)')
 async def typewriter(typew):
