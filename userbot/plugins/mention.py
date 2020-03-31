2672785af59c8bbc4fc7283cfcc8563df3c85446
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-31 23:53:50 +0530

    Update mention.py

--- userbot/plugins/mention.py
+++ userbot/plugins/mention.py
@@ -1,23 +1,52 @@
-# For Uniborg
-# (c) @INF1N17Y
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K && @INF1N17Y
 
 from telethon import events
-from userbot.utils import admin_cmd
+from telethon.tl.functions.users import GetFullUserRequest
+import os
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
 
+from userbot import bot
 
-@borg.on(admin_cmd("mention (.*)"))
-async def (event):
-	if event.fwd_from:
+@borg.on(admin_cmd(pattern=r"mention"))
+@bot.on(events.MessageEdited(pattern="^.mention ?(.*)", outgoing=True))
+async def mention(e):
+	if e.fwd_from:
 		return	
-	input_str = event.pattern_match.group(1)
-	if event.reply_to_msg_id:
-		previous_message = await event.get_reply_message()
+	input_str = e.pattern_match.group(1)
+
+	if e.reply_to_msg_id:
+		previous_message = await e.get_reply_message()
 		if previous_message.forward:
-			replied_user = previous_message.forward.from_id
+			replied_user = await bot(GetFullUserRequest(previous_message.forward.from_id))
 		else:
-			replied_user = previous_message.from_id
+			replied_user = await bot(GetFullUserRequest(previous_message.from_id))
 	else:
-		await event.edit("reply To Message")
-	user_id = replied_user
+		if e.message.entities is not None:
+			mention_entity = e.message.entities
+			probable_user_mention_entity = mention_entity[0]
+			if type(probable_user_mention_entity) == MessageEntityMentionName:
+				user_id = probable_user_mention_entity.user_id
+				replied_user = await bot(GetFullUserRequest(user_id))
+		else:
+			try:
+				user_object = await bot.get_entity(input_str)
+				user_id = user_object.id
+				replied_user = await bot(GetFullUserRequest(user_id))
+			except Exception as e:
+				await e.edit(str(e))
+				return None
+
+	user_id = replied_user.user.id
 	caption = """<a href='tg://user?id={}'>{}</a>""".format(user_id, input_str)
-	await event.edit(caption, parse_mode="HTML")
+	await bot.send_message(
+		e.chat_id,
+		caption,
+		parse_mode="HTML",        
+		force_document=False,
+		silent=True
+		)
+	await e.delete()
