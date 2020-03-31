69df89c1680523904b168584d19689764db136d8
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-31 18:04:36 +0530

    Update fping.py

--- userbot/plugins/fping.py
+++ userbot/plugins/fping.py
@@ -4,7 +4,8 @@ Available Commands:
 
 from telethon import events
 import asyncio
-
+from userbot.utils import admin_cmd
+@borg.on(admin_cmd(pattern="fping"))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
