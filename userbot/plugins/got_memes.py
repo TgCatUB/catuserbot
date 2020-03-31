8c3f8897d7589ec80eef193c63e825ebeae2d910
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-03-31 18:49:47 +0530

    Update got_memes.py

--- userbot/plugins/got_memes.py
+++ userbot/plugins/got_memes.py
@@ -19,8 +19,6 @@ import random
 from userbot.utils import admin_cmd
 
 @borg.on(admin_cmd(pattern=f"gotm", allow_sudo=True))
-@borg.on(events.NewMessage(pattern=r"\.gotm", outgoing=True))
-
 async def _(event):
 
     if event.fwd_from:
