7f642162b5726d4c4698628a1df826d113522a37
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-04 01:25:25 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,24 +1,8 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot initialization. """
-
 import os
-
-from sys import version_info
-from logging import basicConfig, getLogger, INFO, DEBUG
-from distutils.util import strtobool as sb
-import asyncio
-
-import pylast
-from pylast import LastFMNetwork, md5
-from pySmartDL import SmartDL
-from dotenv import load_dotenv
-from requests import get
-from telethon import TelegramClient
+import sys
 from telethon.sessions import StringSession
+from telethon import TelegramClient
+
 from var import Var
 
 os.system("pip install --upgrade pip")
@@ -28,153 +12,130 @@ if Var.STRING_SESSION:
 else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
-    
-load_dotenv("config.env")
+
+
 CMD_LIST = {}
 # for later purposes
 CMD_HELP = {}
 INT_PLUG = ""
 LOAD_PLUG = {}
+
 # PaperPlaneExtended Support Vars
 ENV = os.environ.get("ENV", False)
+""" PPE initialization. """
 
+from logging import basicConfig, getLogger, INFO, DEBUG
+from distutils.util import strtobool as sb
+import asyncio
+
+import pylast
+from pySmartDL import SmartDL
+from requests import get
 # Bot Logs setup:
-CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+if bool(ENV):
+    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
 
-if CONSOLE_LOGGER_VERBOSE:
-    basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=DEBUG,
-    )
-else:
-    basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                level=INFO)
-LOGS = getLogger(__name__)
+    if CONSOLE_LOGGER_VERBOSE:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=DEBUG,
+        )
+    else:
+        basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    level=INFO)
+    LOGS = getLogger(__name__)
 
-if version_info[0] < 3 or version_info[1] < 8:
-    LOGS.info("You MUST have a python version of at least 3.8."
-              "Multiple features depend on this. Bot quitting.")
-    quit(1)
+    # Check if the config was edited by using the already used variable.
+    # Basically, its the 'virginity check' for the config file ;)
+    CONFIG_CHECK = os.environ.get(
+        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
 
-# Check if the config was edited by using the already used variable.
-# Basically, its the 'virginity check' for the config file ;)
-CONFIG_CHECK = os.environ.get(
-    "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+    if CONFIG_CHECK:
+        LOGS.info(
+            "Please remove the line mentioned in the first hashtag from the config.env file"
+        )
+        quit(1)
 
-if CONFIG_CHECK:
-    LOGS.info(
-        "Please remove the line mentioned in the first hashtag from the config.env file"
-    )
-    quit(1)
+    # Logging channel/group configuration.
+    BOTLOG_CHATID = os.environ.get("BOTLOG_CHATID", None)
+    try:
+        BOTLOG_CHATID = int(BOTLOG_CHATID)
+    except:
+        pass
 
-# Telegram App KEY and HASH
-API_KEY = os.environ.get("API_KEY", None)
-API_HASH = os.environ.get("API_HASH", None)
+    # Userbot logging feature switch.
+    BOTLOG = sb(os.environ.get("BOTLOG", "False"))
 
+    # Bleep Blop, this is a bot ;)
+    PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
 
-# Userbot Session String
-STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    # Console verbose logging
+    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
 
-# Logging channel/group ID configuration.
-#BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID", None))
+    # SQL Database URI
+    DB_URI = os.environ.get("DATABASE_URL", None)
 
-# Userbot logging feature switch.
-BOTLOG = sb(os.environ.get("BOTLOG", "False"))
-LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", "False"))
+    # OCR API key
+    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
 
-# Bleep Blop, this is a bot ;)
-PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
+    # remove.bg API key
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
 
-# Heroku Credentials for updater.
-HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
-HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    # Chrome Driver and Headless Google Chrome Binaries
+    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
+    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
 
-# Github Credentials for updater and Gitupload.
-GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
-GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
+    # OpenWeatherMap API Key
+    OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
 
-# Custom (forked) repo URL and BRANCH for updater.
-UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL",
-    "https://github.com/MoveAngel/One4uBot.git")
-UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "sql-extended")
+    # Anti Spambot Config
+    ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
 
-# Console verbose logging
-CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+    ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
 
-# SQL Database URI
-DB_URI = os.environ.get("DATABASE_URL", None)
+    # Youtube API key
+    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 
-# OCR API key
-OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+    # Default .alive name
+    ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
+    AUTONAME = os.environ.get("AUTONAME", None)
+    LESS_SPAMMY = os.environ.get("LESS_SPAMMY", True)
+    
+    # Time & Date - Country and Time Zone
+    COUNTRY = str(os.environ.get("COUNTRY", ""))
 
-# remove.bg API key
-REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+    TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
 
-# Chrome Driver and Headless Google Chrome Binaries
-CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
-GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
+    # Clean Welcome
+    CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
 
-# OpenWeatherMap API Key
-OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY", None)
+    # Last.fm Module
+    BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
+    DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
 
-# Genius lyrics  API Token
-GENIUS = os.environ.get("GENIUS_API_TOKEN", None)
+    LASTFM_API = os.environ.get("LASTFM_API", None)
+    LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
+    LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
+    LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
+    LASTFM_PASS = pylast.md5(LASTFM_PASSWORD_PLAIN)
+    if not LASTFM_USERNAME == "None":
+        lastfm = pylast.LastFMNetwork(api_key=LASTFM_API,
+                                      api_secret=LASTFM_SECRET,
+                                      username=LASTFM_USERNAME,
+                                      password_hash=LASTFM_PASS)
+    else:
+        lastfm = None
 
-# Lydia API
-LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
-
-# Quotes API Token
-QUOTES_API_TOKEN = os.environ.get("QUOTES_API_TOKEN", None)
-
-# Anti Spambot Config
-ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
-ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
-
-# Youtube API key
-YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
-
-# Telegraph 
-TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", None)
-
-# Default .alive name
-ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
-AUTONAME = os.environ.get("AUTONAME", None)
-LESS_SPAMMY = os.environ.get("LESS_SPAMMY", True)
-# Time & Date - Country and Time Zone
-COUNTRY = str(os.environ.get("COUNTRY", ""))
-TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
-
-# Clean Welcome
-CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
-
-# Last.fm Module
-BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
-DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
-
-LASTFM_API = os.environ.get("LASTFM_API", None)
-LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
-LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
-LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
-LASTFM_PASS = md5(LASTFM_PASSWORD_PLAIN)
-if LASTFM_API and LASTFM_SECRET and LASTFM_USERNAME and LASTFM_PASS:
-    lastfm = LastFMNetwork(api_key=LASTFM_API,
-                           api_secret=LASTFM_SECRET,
-                           username=LASTFM_USERNAME,
-                           password_hash=LASTFM_PASS)
-else:
-    lastfm = None
-
-# Google Drive Module
-G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
-G_DRIVE_FOLDER_ID = os.environ.get("G_DRIVE_FOLDER_ID", None)
-TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY",
+    # Google Drive Module
+    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
+    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+    G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
+    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY",
                                          "./downloads")
+else:
+    # Put your ppe vars here if you are using local hosting
+    PLACEHOLDER = None
 
 # Setting Up CloudMail.ru and MEGA.nz extractor binaries,
 # and giving them correct perms to work properly.
@@ -182,7 +143,7 @@ if not os.path.exists('bin'):
     os.mkdir('bin')
 
 binaries = {
-    "https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown":
+    "https://raw.githubusercontent.com/yshalsager/megadown/master/megadown":
     "bin/megadown",
     "https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py":
     "bin/cmrudl"
@@ -193,54 +154,13 @@ for binary, path in binaries.items():
     downloader.start()
     os.chmod(path, 0o755)
 
-# 'bot' variable
-if STRING_SESSION:
-    # pylint: disable=invalid-name
-    bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
-else:
-    # pylint: disable=invalid-name
-    bot = TelegramClient("userbot", API_KEY, API_HASH)
-
-
-async def check_botlog_chatid():
-    if not BOTLOG_CHATID and LOGSPAMMER:
-        LOGS.info(
-            "You must set up the BOTLOG_CHATID variable in the config.env or environment variables, for the private error log storage to work."
-        )
-        quit(1)
-
-    elif not BOTLOG_CHATID and BOTLOG:
-        LOGS.info(
-            "You must set up the BOTLOG_CHATID variable in the config.env or environment variables, for the userbot logging feature to work."
-        )
-        quit(1)
-
-    elif not BOTLOG or not LOGSPAMMER:
-        return
-
-    entity = await bot.get_entity(BOTLOG_CHATID)
-    if entity.default_banned_rights.send_messages:
-        LOGS.info(
-            "Your account doesn't have rights to send messages to BOTLOG_CHATID "
-            "group. Check if you typed the Chat ID correctly.")
-        quit(1)
-
-
-with bot:
-    try:
-        bot.loop.run_until_complete(check_botlog_chatid())
-    except:
-        LOGS.info(
-            "BOTLOG_CHATID environment variable isn't a "
-            "valid entity. Check your environment variables/config.env file.")
-        quit(1)
-
 # Global Variables
 COUNT_MSG = 0
 USERS = {}
 COUNT_PM = {}
 LASTMSG = {}
 CMD_HELP = {}
-ZALG_LIST = {}
 ISAFK = False
 AFKREASON = None
+# End of PaperPlaneExtended Support Vars
+
