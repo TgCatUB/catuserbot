6e605cc873f103daa1e79c91acaed3ac1183cf4a
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-05 02:36:07 +0530

    Update eval.py

--- userbot/plugins/eval.py
+++ userbot/plugins/eval.py
@@ -13,9 +13,9 @@ import io
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("eval"))
+@borg.on(admin_cmd(pattern="eval"))
 async def _(event):
-    if event.fwd_from:
+    if event.fwd_from or event.via_bot_id:
         return
     await event.edit("Processing ...")
     cmd = event.text.split(" ", maxsplit=1)[1]
