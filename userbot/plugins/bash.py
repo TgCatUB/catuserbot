e88f8809c0594383c17252803207147270d19419
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-07 00:41:08 +0530

    Update bash.py

--- userbot/plugins/bash.py
+++ userbot/plugins/bash.py
@@ -1,12 +1,18 @@
+"""Execute GNU/Linux commands inside Telegram
+Syntax: .bash Code"""
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from telethon import events
 import subprocess
 from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
 import io
 import asyncio
 import time
-from userbot import CMD_HELP
+from uniborg.util import admin_cmd
 
-@command(pattern="^.bash ?(.*)")
+
+@borg.on(admin_cmd("bash ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -21,25 +27,16 @@ async def _(event):
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
     stdout, stderr = await process.communicate()
-    e = stderr.decode()
-    if not e:
-        e = "No Error"
-    o = stdout.decode()
-    if not o:
-        o = "**Tip**: \n`If you want to see the results of your code, I suggest printing them to stdout.`"
-    else:
-        _o = o.split("\n")
-        o = "`\n".join(_o)
-    OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n{o}"
-    if len(OUTPUT) > 4095:
+    OUTPUT = f"`{stdout.decode()}`"
+    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
-            out_file.name = "exec.text"
-            await bot.send_file(
+            out_file.name = "bash.text"
+            await borg.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
                 allow_cache=False,
-                caption=cmd,
+                caption=OUTPUT,
                 reply_to=reply_to_id
             )
             await event.delete()
