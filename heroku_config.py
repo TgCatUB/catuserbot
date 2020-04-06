14f8f8e8ec9fd2264f866aeb802a4039df457d9e
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-04-06 13:33:26 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -18,7 +18,7 @@ class Var(object):
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
     # Send .get_id in any channel to fill this value.
-    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", None))
+    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", -100))
     TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
     NO_SONGS = bool(os.environ.get("NO_SONGS", False))
     DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", None)
