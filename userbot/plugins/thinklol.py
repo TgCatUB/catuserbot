bdb17a8ac3b3d5a37727145388dc6fcc7f4209bc
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-19 21:39:54 +0530

    Update thinklol.py

--- userbot/plugins/thinklol.py
+++ userbot/plugins/thinklol.py
@@ -7,7 +7,7 @@ from collections import deque
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd(pattern=f"lol", allow_sudo=True))
+@borg.on(admin_cmd(pattern="lol", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
