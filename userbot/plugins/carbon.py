c801d2b45e27390f74a5abd458a057379e459f32
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-04-19 20:37:55 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -79,7 +79,7 @@ async def carbon_api(e):
    command_result = driver.execute("send_command", params)
    driver.find_element_by_xpath('/html/body/div[1]/main/div[2]/div[2]/div[1]/div[1]/div/span[2]').click()
    if skeme != None:
-         k_skeme = driver.find_element_by_xpath('/html/body/div[1]/main/div[3]/div[2]/div[1]/div[1]/div/span[2]/input')
+         k_skeme = driver.find_element_by_xpath('/html/body/div[1]/main/div[2]/div[2]/div[1]/div[1]/div/span[2]/input')
          k_skeme.send_keys(skeme)
          k_skeme.send_keys(Keys.DOWN)
          k_skeme.send_keys(Keys.ENTER)
