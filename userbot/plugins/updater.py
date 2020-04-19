4d6a7ad1c3c0f7402646da91d16fad6d85649560
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-19 18:43:35 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -15,7 +15,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/sandy1709/catuserbot"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/sandy1709/catuserbot/"
 BOT_IS_UP_TO_DATE = "the userbot is up-to-date."
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
