2434ce661daaf958c555ca5867e54d178839985f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-21 19:32:48 +0530

    Update call.py

--- userbot/plugins/call.py
+++ userbot/plugins/call.py
@@ -1,14 +1,7 @@
-"""Emoji
-
+"""call
 Available Commands:
-
-.emoji shrug
-
-.emoji apple
-
-.emoji :/
-
-.emoji -_-"""
+.call
+"""
 
 from telethon import events
 
@@ -19,8 +12,8 @@ from userbot.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
-@borg.on(admin_cmd(pattern=f"call", allow_sudo=True))
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(admin_cmd(pattern=f"call", outgoing=True))
+#@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
 
