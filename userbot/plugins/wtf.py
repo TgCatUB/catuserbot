602c043b14634e7fb386da1929ca7fb4ccc94be4
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-21 19:36:22 +0530

    Update wtf.py

--- userbot/plugins/wtf.py
+++ userbot/plugins/wtf.py
@@ -8,7 +8,10 @@ import asyncio
 
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd("wtf"))
+
+
+
+@borg.on(admin_cmd(pattern=f"wtf", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
