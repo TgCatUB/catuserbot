772faa0ab2a9ad3f9b348ffa49fd274bed2da8d4
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-21 19:41:21 +0530

    Update call.py

--- userbot/plugins/call.py
+++ userbot/plugins/call.py
@@ -13,7 +13,7 @@ from userbot.utils import admin_cmd
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
 @borg.on(admin_cmd(pattern=f"call", outgoing=True))
-#@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
 
