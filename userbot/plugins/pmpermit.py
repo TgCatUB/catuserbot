7cc8dcd8545e6cd5bc08d40c6e442de671764668
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-22 20:56:42 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -126,7 +126,7 @@ if Var.PRIVATE_GROUP_ID is not None:
             # userbot's should not reply to other userbot's
             # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
             return
-        sender = await bot.get_entity(chat_id)
+        sender = event.sender
 
         if chat_id == bot.uid:
 
