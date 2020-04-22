c0e20364b8b52f28338a9730dcd26ed18d3d1970
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-23 00:14:26 +0530

    Update auto_bio.py

--- userbot/plugins/auto_bio.py
+++ userbot/plugins/auto_bio.py
@@ -5,9 +5,41 @@ from telethon import events
 import random, re
 from telethon.tl import functions
 from telethon.errors import FloodWaitError
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
+
+from userbot import DEFAULT_BIO
 
 
+DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else "set DEFAULT_BIO in heroku config vars "
+
+DEL_TIME_OUT = 70
+
+
+@borg.on(admin_cmd("autobio"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    while True:
+        DMY = time.strftime("%d.%m.%Y")
+        HM = time.strftime("%H:%M:%S")
+        bio = f"📅 {DMY} | {DEFAULTUSERBIO} | ⌚️ {HM}"
+        logger.info(bio)
+        try:
+            await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                about=bio
+            ))
+        except FloodWaitError as ex:
+            logger.warning(str(e))
+            await asyncio.sleep(ex.seconds)
+        # else:
+            # logger.info(r.stringify())
+            # await borg.send_message(  # pylint:disable=E0602
+            #     Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+            #     "Changed Profile Picture"
+            # )
+        await asyncio.sleep(DEL_TIME_OUT)
+     
+     
 BIO_STRINGS = [
      "👉⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️🔲",
      "⬜️👉⬛️⬛️⬛️⬛️⬛️⬛️⬛️🔲",
