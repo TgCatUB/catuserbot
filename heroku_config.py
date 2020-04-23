de8403f111675f019e4d93b995796836a3152e43
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-23 19:42:25 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -16,8 +16,9 @@ class Var(object):
     LESS_SPAMMY = os.environ.get("LESS_SPAMMY", None)
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", -100))
     TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
+    # Send .get_id in any channel to fill this value.
+    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", -100))
     TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
     NO_SONGS = bool(os.environ.get("NO_SONGS", False))
     DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", None)
