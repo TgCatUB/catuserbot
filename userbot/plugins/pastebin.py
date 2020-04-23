b1a1c8d05a5bb1f9ef493668ec204178658ee0ee
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-24 02:04:31 +0530

    Update pastebin.py

--- userbot/plugins/pastebin.py
+++ userbot/plugins/pastebin.py
@@ -18,54 +18,126 @@ from userbot.uniborgConfig import Config
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 
-
-
 def progress(current, total):
     logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
+    
+from userbot import CMD_HELP, LOGS, TEMP_DOWNLOAD_DIRECTORY
+from userbot.events import register
 
+DOGBIN_URL = "https://del.dog/"
 
-@borg.on(admin_cmd("paste ?(.*)"))
-async def _(event):
-    if event.fwd_from:
+BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+BOTLOG = True
+
+@register(outgoing=True, pattern=r"^\.paste(?: |$)([\s\S]*)")
+async def paste(pstl):
+    """ For .paste command, pastes the text directly to dogbin. """
+    dogbin_final_url = ""
+    match = pstl.pattern_match.group(1).strip()
+    reply_id = pstl.reply_to_msg_id
+
+    if not match and not reply_id:
+        await pstl.edit("`Elon Musk said I cannot paste void.`")
         return
-    start = datetime.now()
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    input_str = event.pattern_match.group(1)
-    message = "SYNTAX: `.paste <long text to include>`"
-    if input_str:
-        message = input_str
-    elif event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.media:
-            downloaded_file_name = await borg.download_media(
-                previous_message,
-                Config.TMP_DOWNLOAD_DIRECTORY,
-                progress_callback=progress
+
+    if match:
+        message = match
+    elif reply_id:
+        message = (await pstl.get_reply_message())
+        if message.media:
+            downloaded_file_name = await pstl.client.download_media(
+                message,
+                TEMP_DOWNLOAD_DIRECTORY,
             )
             m_list = None
             with open(downloaded_file_name, "rb") as fd:
                 m_list = fd.readlines()
             message = ""
             for m in m_list:
-                message += m.decode("UTF-8") + "\r\n"
+                message += m.decode("UTF-8") + "\r"
             os.remove(downloaded_file_name)
         else:
-            message = previous_message.message
-    else:
-        message = "SYNTAX: `.paste <long text to include>`"
-    url = "https://del.dog/documents"
-    r = requests.post(url, data=message.encode("UTF-8")).json()
-    url = f"https://del.dog/{r['key']}"
-    end = datetime.now()
-    ms = (end - start).seconds
-    if r["isUrl"]:
-        nurl = f"https://del.dog/v/{r['key']}"
-        await event.edit("Dogged to {} in {} seconds. GoTo Original URL: {}".format(url, ms, nurl))
-    else:
-        await event.edit("Dogged to {} in {} seconds".format(url, ms))
+            message = message.message
 
-        
+    # Dogbin
+    await pstl.edit("`Pasting text . . .`")
+    resp = post(DOGBIN_URL + "documents", data=message.encode('utf-8'))
+
+    if resp.status_code == 200:
+        response = resp.json()
+        key = response['key']
+        dogbin_final_url = DOGBIN_URL + key
+
+        if response['isUrl']:
+            reply_text = ("`Pasted successfully!`\n\n"
+                          f"`Shortened URL:` {dogbin_final_url}\n\n"
+                          "`Original(non-shortened) URLs`\n"
+                          f"`Dogbin URL`: {DOGBIN_URL}v/{key}\n")
+        else:
+            reply_text = ("`Pasted successfully!`\n\n"
+                          f"`Dogbin URL`: {dogbin_final_url}")
+    else:
+        reply_text = ("`Failed to reach Dogbin`")
+
+    await pstl.edit(reply_text)
+    if BOTLOG:
+        await pstl.client.send_message(
+            BOTLOG_CHATID,
+            f"Paste query was executed successfully",
+        )
+
+
+@register(outgoing=True, pattern="^\.getpaste(?: |$)(.*)")
+async def get_dogbin_content(dog_url):
+    """ For .getpaste command, fetches the content of a dogbin URL. """
+    textx = await dog_url.get_reply_message()
+    message = dog_url.pattern_match.group(1)
+    await dog_url.edit("`Getting dogbin content...`")
+
+    if textx:
+        message = str(textx.message)
+
+    format_normal = f'{DOGBIN_URL}'
+    format_view = f'{DOGBIN_URL}v/'
+
+    if message.startswith(format_view):
+        message = message[len(format_view):]
+    elif message.startswith(format_normal):
+        message = message[len(format_normal):]
+    elif message.startswith("del.dog/"):
+        message = message[len("del.dog/"):]
+    else:
+        await dog_url.edit("`Is that even a dogbin url?`")
+        return
+
+    resp = get(f'{DOGBIN_URL}raw/{message}')
+
+    try:
+        resp.raise_for_status()
+    except exceptions.HTTPError as HTTPErr:
+        await dog_url.edit(
+            "Request returned an unsuccessful status code.\n\n" + str(HTTPErr))
+        return
+    except exceptions.Timeout as TimeoutErr:
+        await dog_url.edit("Request timed out." + str(TimeoutErr))
+        return
+    except exceptions.TooManyRedirects as RedirectsErr:
+        await dog_url.edit(
+            "Request exceeded the configured number of maximum redirections." +
+            str(RedirectsErr))
+        return
+
+    reply_text = "`Fetched dogbin URL content successfully!`\n\n`Content:` " + resp.text
+
+    await dog_url.edit(reply_text)
+    if BOTLOG:
+        await dog_url.client.send_message(
+            BOTLOG_CHATID,
+            "Get dogbin content query was executed successfully",
+        )
+
+
+      
         
 @borg.on(admin_cmd(pattern="npaste ?(.*)"))
 async def _(event):
@@ -75,7 +147,7 @@ async def _(event):
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     input_str = event.pattern_match.group(1)
-    message = "SYNTAX: `.paste <long text to include>`"
+    message = "SYNTAX: `.npaste <long text to include>`"
     if input_str:
         message = input_str
     elif event.reply_to_msg_id:
@@ -97,7 +169,7 @@ async def _(event):
         else:
             message = previous_message.message
     else:
-        message = "SYNTAX: `.paste <long text to include>`"
+        message = "SYNTAX: `.npaste <long text to include>`"
     py_file =  ""
     if downloaded_file_name.endswith(".py"):
         py_file += ".py"
@@ -113,13 +185,7 @@ async def _(event):
         reply_text = f'Nekofied to *Nekobin* : {url}'
         await event.edit(reply_text)
 
-# data = "tets sgdfgklj kdgjld"
 
-# key = requests.post('https://nekobin.com/api/documents', json={"content": data}).json().get('result').get('key')
-
-# url = f'https://nekobin.com/{key}'
-
-# reply_text = f'Nekofied to *Nekobin* : {url}'
  
     
     
@@ -219,3 +285,17 @@ async def _(event):
             await event.delete()
             await event.client.send_message(event.chat_id, response.message, reply_to=reply_message)
 
+CMD_HELP.update({
+    "pastebin":
+    ".paste <text/reply>\
+\nUsage: Create a paste or a shortened url using dogbin (https://del.dog/)\
+\n\n.getpaste\
+\nUsage: Gets the content of a paste or shortened url from dogbin (https://del.dog/)\
+\n\n.npaste <text/reply>\
+\nUsage: Create a paste or a shortened url using nekobin (https://nekobin.com)\
+\n\n.iffuci <text/reply>\
+\nUsage: Create a paste or a shortened url using iffuci (https://www.iffuci.tk)\
+\n\n.paster <text/reply>\
+\nUsage: Create a instant view or a paste it in telegraph file\
+  "
+})  
