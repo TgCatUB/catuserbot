100aea272ee94b648582661442e66a077392be94
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-24 03:11:44 +0530

    Update locks.py

--- userbot/plugins/locks.py
+++ userbot/plugins/locks.py
@@ -6,7 +6,7 @@ DB Options: bots, commands, email, forward, url"""
 from telethon import events, functions, types
 from userbot.plugins.sql_helper.locks_sql import update_lock, is_locked, get_locks
 from userbot.utils import admin_cmd
-
+from userbot import CMD_HELP
 
 @borg.on(admin_cmd("lock( (?P<target>\S+)|$)"))
 async def _(event):
