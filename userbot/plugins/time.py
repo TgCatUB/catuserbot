751449f038c85279120ab2a49856f5096fb42823
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-24 16:36:28 +0530

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -178,20 +178,7 @@ async def date_func(dat):
         return
 
 
-CMD_HELP.update({
-    "time":
-    ".time <country name/code> <timezone number>"
-    "\nUsage: Get the time of a country. If a country has "
-    "multiple timezones, it will list all of them "
-    "and let you select one."
-})
-CMD_HELP.update({
-    "date":
-    ".date <country name/code> <timezone number>"
-    "\nUsage: Get the date of a country. If a country has "
-    "multiple timezones, it will list all of them "
-    "and let you select one."
-})
+
 
 
 @borg.on(admin_cmd("time ?(.*)"))  # pylint:disable=E0602
