c9cd304d98021641f40135e28fd8898f2d5ef52f
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-04-27 12:53:21 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,13 +6,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [Check Guide.](https://how2techy.com/xtra-guide1/)"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Currently Alive!` **ψ(｀∇´)ψ**\n\n"
+    await alive.edit("`Currently Alive, my peru master!` **ψ(｀∇´)ψ**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
+                     # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
                      "`Bot created by:` [SnapDragon](tg://user?id=719877937), @anubisxx\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
                      "https://github.com/Dark-Princ3/X-tra-Telegram")
