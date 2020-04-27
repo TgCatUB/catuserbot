7a1bf05e1769c4afc2f707cd88133adef291456f
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-04-27 14:11:39 +0530

    Make sure SUDO_USERS doesn't exist.
    let's hope this doesn't crash things lmao.

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -10,6 +10,8 @@ from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
 from pathlib import Path
 import asyncio
 import telethon.utils
+import heroku3
+
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
@@ -33,6 +35,15 @@ else:
         print("Initialisation finished with no errors")
         print("Starting Userbot")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        if Var.HEROKU_APP_NAME and Var.HEROKU_API_KEY is not None:
+            Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+            app = Heroku.app(Var.HEROKU_APP_NAME)
+            heroku_var = app.config()
+            variable = "SUDO_USERS"
+            if variable in heroku_var:
+                del heroku_var[variable]
+            else:
+                print("All Good!")
         print("Startup Completed")
     else:
         bot.start()
@@ -55,5 +66,3 @@ if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
-
-
