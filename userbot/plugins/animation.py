82f27c5457ce8d140e4c419db889ca67508ffd0f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-04-29 18:11:15 +0530

    Update animation.py

--- userbot/plugins/animation.py
+++ userbot/plugins/animation.py
@@ -5,6 +5,7 @@ import asyncio
 from userbot.utils import admin_cmd
 from userbot import ALIVE_NAME
 import random, re
+from userbot import CMD_HELP
 from collections import deque
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
