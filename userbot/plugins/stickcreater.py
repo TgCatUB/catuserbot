44bfee54dde11fd15758d45a77fc1eb33deeb9b0
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-01 21:03:14 +0530

    .

--- userbot/plugins/stickcreater.py
+++ userbot/plugins/stickcreater.py
@@ -42,7 +42,7 @@ async def sticklet(event):
     image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
     draw = ImageDraw.Draw(image)
     fontsize = 230
-    FONT_FILE = await get_font_file(event.client, "@FontRes", font_file_name)
+    FONT_FILE = await get_font_file(event.client, "@catfonts", font_file_name)
     font = ImageFont.truetype(FONT_FILE, size=fontsize)
     while draw.multiline_textsize(sticktext, font=font) > (512, 512):
         fontsize -= 3
