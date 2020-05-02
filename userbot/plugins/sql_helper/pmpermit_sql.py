9d109c33695aae27c7ff39278ff5ddb0a8142a42
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-03 01:30:14 +0530

    edited

--- userbot/plugins/sql_helper/pmpermit_sql.py
+++ userbot/plugins/sql_helper/pmpermit_sql.py
@@ -41,3 +41,43 @@ def get_all_approved():
     rem = SESSION.query(PMPermit).all()
     SESSION.close()
     return rem
+
+try:
+    from userbot.modules.sql_helper import SESSION, BASE
+except ImportError:
+    raise AttributeError
+from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
+
+
+class PMPermit(BASE):
+    __tablename__ = "pmpermit"
+    chat_id = Column(String(14), primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = str(chat_id)  # ensure string
+
+
+PMPermit.__table__.create(checkfirst=True)
+
+
+def is_approved(chat_id):
+    try:
+        return SESSION.query(PMPermit).filter(
+            PMPermit.chat_id == str(chat_id)).one()
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+
+def approve(chat_id):
+    adder = PMPermit(str(chat_id))
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def dissprove(chat_id):
+    rem = SESSION.query(PMPermit).get(str(chat_id))
+    if rem:
+        SESSION.delete(rem)
+        SESSION.commit()
