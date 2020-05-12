1cd5e0974eb73457a61c8b2f206ecdc5692fd89a
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-12 19:17:32 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -783,66 +783,6 @@ async def insult(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit(random.choice(INSULT_STRINGS))
 
-@register(outgoing=True, pattern="^.cp(?: |$)(.*)")
-async def copypasta(cp_e):
-    """ Copypasta the famous meme """
-    if not cp_e.text[0].isalpha() and cp_e.text[0] not in ("/", "#", "@", "!"):
-        textx = await cp_e.get_reply_message()
-        message = cp_e.pattern_match.group(1)
-
-        if message:
-            pass
-        elif textx:
-            message = textx.text
-        else:
-            await cp_e.edit("`😂🅱️IvE👐sOME👅text👅for✌️Me👌tO👐MAkE👀iT💞funNy!💦`")
-            return
-
-        reply_text = random.choice(EMOJIS)
-        b_char = random.choice(
-            message
-        ).lower()  # choose a random character in the message to be substituted with 🅱️
-        for owo in message:
-            if owo == " ":
-                reply_text += random.choice(EMOJIS)
-            elif owo in EMOJIS:
-                reply_text += owo
-                reply_text += random.choice(EMOJIS)
-            elif owo.lower() == b_char:
-                reply_text += "🅱️"
-            else:
-                if bool(random.getrandbits(1)):
-                    reply_text += owo.upper()
-                else:
-                    reply_text += owo.lower()
-        reply_text += random.choice(EMOJIS)
-        await cp_e.edit(reply_text)
-
-
-@register(outgoing=True, pattern="^.vapor(?: |$)(.*)")
-async def vapor(vpr):
-    """ Vaporize everything! """
-    if not vpr.text[0].isalpha() and vpr.text[0] not in ("/", "#", "@", "!"):
-        reply_text = list()
-        textx = await vpr.get_reply_message()
-        message = vpr.pattern_match.group(1)
-        if message:
-            pass
-        elif textx:
-            message = textx.text
-        else:
-            await vpr.edit("`Ｇｉｖｅ ｓｏｍｅ ｔｅｘｔ ｆｏｒ ｖａｐｏｒ！`")
-            return
-
-        for charac in message:
-            if 0x21 <= ord(charac) <= 0x7F:
-                reply_text.append(chr(ord(charac) + 0xFEE0))
-            elif ord(charac) == 0x20:
-                reply_text.append(chr(0x3000))
-            else:
-                reply_text.append(charac)
-
-        await vpr.edit("".join(reply_text))
 
 			  
 @register(outgoing=True, pattern="^.repo$")
@@ -852,68 +792,6 @@ async def source(e):
 			  
 
 			  
-@register(outgoing=True, pattern="^.str(?: |$)(.*)")
-async def stretch(stret):
-    """ Stretch it."""
-    if not stret.text[0].isalpha() and stret.text[0] not in ("/", "#", "@", "!"):
-        textx = await stret.get_reply_message()
-        message = stret.text
-        message = stret.pattern_match.group(1)
-        if message:
-            pass
-        elif textx:
-            message = textx.text
-        else:
-            await stret.edit("`GiiiiiiiB sooooooomeeeeeee teeeeeeext!`")
-            return
-
-        count = random.randint(3, 10)
-        reply_text = re.sub(
-            r"([aeiouAEIOUａｅｉｏｕＡＥＩＯＵаеиоуюяыэё])",
-            (r"\1"*count),
-            message
-        )
-        await stret.edit(reply_text)
-
-
-@register(outgoing=True, pattern="^.zal(?: |$)(.*)")
-async def zal(zgfy):
-    """ Invoke the feeling of chaos. """
-    if not zgfy.text[0].isalpha() and zgfy.text[0] not in ("/", "#", "@", "!"):
-        reply_text = list()
-        textx = await zgfy.get_reply_message()
-        message = zgfy.pattern_match.group(1)
-        if message:
-            pass
-        elif textx:
-            message = textx.text
-        else:
-            await zgfy.edit(
-                "`gͫ ̆ i̛ ̺ v͇̆ ȅͅ   a̢ͦ   s̴̪ c̸̢ ä̸ rͩͣ y͖͞   t̨͚ é̠ x̢͖  t͔͛`"
-            )
-            return
-
-        for charac in message:
-            if not charac.isalpha():
-                reply_text.append(charac)
-                continue
-
-            for _ in range(0, 3):
-                randint = random.randint(0, 2)
-
-                if randint == 0:
-                    charac = charac.strip() + \
-                        random.choice(ZALG_LIST[0]).strip()
-                elif randint == 1:
-                    charac = charac.strip() + \
-                        random.choice(ZALG_LIST[1]).strip()
-                else:
-                    charac = charac.strip() + \
-                        random.choice(ZALG_LIST[2]).strip()
-
-            reply_text.append(charac)
-
-        await zgfy.edit("".join(reply_text))
 
 
 @register(outgoing=True, pattern="^.hey$")
@@ -1068,30 +946,6 @@ async def _(event):
 		    await event.edit("".join(deq))
 		    deq.rotate(1)
 
-@register(outgoing=True, pattern="^.mock(?: |$)(.*)")
-async def spongemocktext(mock):
-    """ Do it and find the real fun. """
-    if not mock.text[0].isalpha() and mock.text[0] not in ("/", "#", "@", "!"):
-        reply_text = list()
-        textx = await mock.get_reply_message()
-        message = mock.pattern_match.group(1)
-        if message:
-            pass
-        elif textx:
-            message = textx.text
-        else:
-            await mock.edit("`gIvE sOMEtHInG tO MoCk!`")
-            return
-
-        for charac in message:
-            if charac.isalpha() and random.randint(0, 1):
-                to_app = charac.upper() if charac.islower() else charac.lower()
-                reply_text.append(to_app)
-            else:
-                reply_text.append(charac)
-
-        await mock.edit("".join(reply_text))
-
 
 @register(outgoing=True, pattern="^.clap(?: |$)(.*)")
 async def claptext(memereview):
@@ -1112,15 +966,7 @@ async def claptext(memereview):
         await memereview.edit(reply_text)
 
 
-@register(outgoing=True, pattern="^.bt$")
-async def bluetext(bt_e):
-    """ Believe me, you will find this useful. """
-    if not bt_e.text[0].isalpha() and bt_e.text[0] not in ("/", "#", "@", "!"):
-        if await bt_e.get_reply_message():
-            await bt_e.edit(
-                "`BLUETEXT MUST CLICK.`\n"
-                "`Are you a stupid animal which is attracted to colours?`"
-            )
+
 
 
 @register(outgoing=True, pattern="^.smk (.*)")
@@ -1210,16 +1056,8 @@ CMD_HELP.update({
 \nUsage: Ok...\
 \n\n;_;\
 \nUsage: Like `-_-` but crying.\
-\n\n.cp\
-\nUsage: Copypasta the famous meme\
-\n\n.vapor\
-\nUsage: Vaporize everything!\
-\n\n.str\
-\nUsage: Stretch it.\
 \n\n.10iq\
 \nUsage: You retard !!\
-\n\n.zal\
-\nUsage: Invoke the feeling of chaos.\
 \n\n.oof\
 \nUsage: Ooooof\
 \n\n.moon\
@@ -1246,14 +1084,10 @@ CMD_HELP.update({
 \nUsage: Run, run, RUNNN! [`.disable runs`: disable | `.enable runs`: enable]\
 \n\n.metoo\
 \nUsage: Haha yes\
-\n\n.mock\
-\nUsage: Do it and find the real fun.\
 \n\n.clap\
 \nUsage: Praise people!\
 \n\n.f <emoji/character>\
 \nUsage: Pay Respects.\
-\n\n.bt\
-\nUsage: Believe me, you will find this useful.\
 \n\n.smk <text/reply>\
 \nUsage: A shit module for ツ , who cares.\
 \n\n.type\
