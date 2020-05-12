a9f964e33d95368fae9163baaa44b2ce4b060211
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-12 17:54:23 +0530

    Update wahack.py

--- userbot/plugins/wahack.py
+++ userbot/plugins/wahack.py
@@ -21,7 +21,7 @@ async def _(event):
     animation_ttl = range(0, 15)
     #input_str = event.pattern_match.group(1)
     #if input_str == "wahack":
-    await event.edit(input_str)
+    await event.edit("wahack..")
 
 
 
