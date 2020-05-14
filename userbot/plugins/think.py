98990ceccc1f5ce33cd626ae352327407db366ea
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-14 12:28:48 +0530

    Update think.py

--- userbot/plugins/think.py
+++ userbot/plugins/think.py
@@ -10,7 +10,7 @@ import asyncio
 
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd("think"))
+@borg.on(admin_cmd(pattern="think"))
 async def _(event):
     if event.fwd_from:
         return
