efebf8d0556a6ace8389bd5541033b435a84f165
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-15 00:30:18 +0530

    Update spamV2.py

--- userbot/plugins/spamV2.py
+++ userbot/plugins/spamV2.py
@@ -4,9 +4,8 @@ from asyncio import wait
 from userbot.utils import admin_cmd
 from telethon import events, utils
 
-from userbot.events import register
 
-@register(outgoing=True, pattern="^.sspam")
+@borg.on(admin_cmd(pattern="sspam"))
 async def spammer(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
