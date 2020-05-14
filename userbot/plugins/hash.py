ef3763506116578a3ab5c2d7c6f07a75448c2db9
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-14 23:49:42 +0530

    Update hash.py

--- userbot/plugins/hash.py
+++ userbot/plugins/hash.py
@@ -12,8 +12,7 @@ from userbot import CMD_HELP
 from userbot.events import register, errors_handler
 
 
-@register(outgoing=True, pattern="^.hash (.*)")
-@errors_handler
+@borg.on(admin_cmd(pattern="hash (.*)"))
 async def gethash(hash_q):
     """ For .hash command, find the md5, sha1, sha256, sha512 of the string. """
     hashtxt_ = hash_q.pattern_match.group(1)
@@ -45,8 +44,8 @@ async def gethash(hash_q):
         await hash_q.reply(ans)
 
 
-@register(outgoing=True, pattern="^.hbase (en|de) (.*)")
-@errors_handler
+
+@borg.on(admin_cmd(pattern="hbase (en|de) (.*)"))
 async def endecrypt(query):
     """ For .base64 command, find the base64 encoding of the given string. """
     if query.pattern_match.group(1) == "en":
