9aa60b76d9f8b962107a7c9ba8b5e538316caf57
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-14 10:49:17 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -151,17 +151,47 @@ def remove_plugin(shortname):
         raise ValueError
         
 
-def admin_cmd(pattern=None, allow_sudo=False, **args):
+def admin_cmd(**args):
     args["func"] = lambda e: e.via_bot_id is None
+    
+    pattern = args.get("pattern", None)
+    allow_sudo = args.get("allow_sudo", False)
 
+    # get the pattern from the decorator
     if pattern is not None:
-        args["pattern"] = re.compile(Config.COMMAND_HAND_LER + pattern)
+        if pattern.startswith("\#"):
+            # special fix for snip.py
+            args["pattern"] = re.compile(pattern)
+        else:
+            args["pattern"] = re.compile(Config.COMMAND_HAND_LER + pattern)
+
+    args["outgoing"] = True
+    # should this command be available for other users?
     if allow_sudo:
         args["from_users"] = list(Config.SUDO_USERS)
-    else:
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
         args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
     args["blacklist_chats"] = True
-    args["chats"] = list(Config.UB_BLACK_LIST_CHAT)
+    black_list_chats = list(Config.UB_BLACK_LIST_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
+
+    # check if the plugin should allow edited updates
+    allow_edited_updates = False
+    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+        allow_edited_updates = args["allow_edited_updates"]
+        del args["allow_edited_updates"]
+
+    # check if the plugin should listen for outgoing 'messages'
+    is_message_enabled = True
+
     return events.NewMessage(**args)
 
 
