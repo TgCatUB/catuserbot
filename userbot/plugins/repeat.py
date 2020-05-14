e2494170fa14482a7febd63018d2553c1475506d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-14 12:01:20 +0530

    Update repeat.py

--- userbot/plugins/repeat.py
+++ userbot/plugins/repeat.py
@@ -3,7 +3,7 @@ from asyncio import wait
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("repeat ?(.*)"))
+@borg.on(admin_cmd(pattern="repeat ?(.*)"))
 async def _(event):
     message = event.text[10:]
     count = int(event.text[8:10])
