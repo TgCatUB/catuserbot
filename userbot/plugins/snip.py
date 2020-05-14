675aae94b3c57fcebcc3ef2a3c94b92893efd021
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-14 11:37:57 +0530

    Update snip.py

--- userbot/plugins/snip.py
+++ userbot/plugins/snip.py
@@ -48,7 +48,7 @@ async def on_snip(event):
         await event.delete()
 
 
-@borg.on(admin_cmd("snips (.*)"))
+@borg.on(admin_cmd(pattern="snips (.*)"))
 async def on_snip_save(event):
     name = event.pattern_match.group(1)
     msg = await event.get_reply_message()
@@ -72,7 +72,7 @@ async def on_snip_save(event):
         await event.edit("Reply to a message with `snips keyword` to save the snip")
 
 
-@borg.on(admin_cmd("snipl"))
+@borg.on(admin_cmd(pattern="snipl"))
 async def on_snip_list(event):
     all_snips = get_all_snips()
     OUT_STR = "Available Snips:\n"
@@ -97,7 +97,7 @@ async def on_snip_list(event):
         await event.edit(OUT_STR)
 
 
-@borg.on(admin_cmd("snipd (\S+)"))
+@borg.on(admin_cmd(pattern="snipd (\S+)"))
 async def on_snip_delete(event):
     name = event.pattern_match.group(1)
     remove_snip(name)
