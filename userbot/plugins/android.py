f995b359a61744c35c5371b297e3f7619b3364a2
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-15 00:36:22 +0530

    Update android.py

--- userbot/plugins/android.py
+++ userbot/plugins/android.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 from requests import get
 
 from userbot import CMD_HELP
-from userbot.events import register
+from userbot.utils import admin_cmd
 
 GITHUB = 'https://github.com'
 DEVICES_DATA = 'https://raw.githubusercontent.com/androidtrackers/' \
