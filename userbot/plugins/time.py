4b73f6a1452c2c1029ca983e882bf07276c0447d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-15 13:06:50 +0530

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -5,7 +5,7 @@ import asyncio
 import os
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
-from userbot.utils import admin_cmd,register
+from userbot.utils import admin_cmd
 
 
 FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
