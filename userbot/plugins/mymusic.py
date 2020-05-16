35f0824b2d3b7676cabb72b859b0d34d04989249
arun-anjilik <52011992+arun-anjilik@users.noreply.github.com>
2020-05-17 00:33:33 +0530

    Create mymusic.py

---
+++ userbot/plugins/mymusic.py
@@ -0,0 +1,52 @@
+
+from telethon import events
+import subprocess
+from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
+import io
+import asyncio
+import time
+from userbot.utils import admin_cmd
+import glob
+import os
+try:
+ import instantmusic , subprocess
+except:
+ os.system("pip install instantmusic")
+ 
+
+
+os.system("rm -rf *.mp3")
+
+
+def bruh(name):
+    
+    os.system("instantmusic -q -s "+name)
+    
+
+@borg.on(admin_cmd(pattern="csong ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.8
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    await event.edit("`---- just a sec --- `"),
+    await event.edit("`wi8..! i am finding your song.. 😴`"),
+    bruh(str(cmd))
+    l = glob.glob("*.mp3")
+    loa = l[0]
+    await event.edit("`yeah..! i find something wi8..`")
+    await borg.send_file(
+                event.chat_id,
+                loa,
+                force_document=True,
+                allow_cache=False,
+                caption="`Uploaded by` : @Mr_Hops 🍃 \n`Channel` : [TOXIC BEATS](t.me/toxicbeats) ✅",
+                reply_to=reply_to_id
+            )
+    await event.delete()
+    os.system("rm -rf *.mp3")
+    subprocess.check_output("rm -rf *.mp3",shell=True)
