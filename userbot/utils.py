93515840a1d0877c477a69e4480edea6d19bfe41
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-20 13:55:25 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -156,57 +156,16 @@ def remove_plugin(shortname):
         raise ValueError
         
 
-def admin_cmd(**args):
-    args["func"] = lambda e: e.via_bot_id is None
-    stack = inspect.stack()
-    previous_stack_frame = stack[1]
-    file_test = Path(previous_stack_frame.filename)
-    file_test = file_test.stem.replace(".py", "")
-    pattern = args.get("pattern", None)
-    allow_sudo = args.get("allow_sudo", False)
-
-    # get the pattern from the decorator
+def admin_cmd(pattern=None, allow_sudo=False, **args):
     if pattern is not None:
-        if pattern.startswith("\#"):
-            # special fix for snip.py
-            args["pattern"] = re.compile(pattern)
-        else:
-            args["pattern"] = re.compile("\." + pattern)
-            cmd = "." + pattern
-            try:
-                CMD_LIST[file_test].append(cmd)
-            except:
-                CMD_LIST.update({file_test: [cmd]})
-                
-    args["outgoing"] = True
-    # should this command be available for other users?
+        args["pattern"] = re.compile(Config.COMMAND_HAND_LER + pattern)
     if allow_sudo:
         args["from_users"] = list(Config.SUDO_USERS)
-        # Mutually exclusive with outgoing (can only set one of either).
-        args["incoming"] = True
-        del args["allow_sudo"]
-
-    # error handling condition check
-    elif "incoming" in args and not args["incoming"]:
+    else:
         args["outgoing"] = True
-
-    # add blacklist chats, UB should not respond in these chats
     args["blacklist_chats"] = True
-    black_list_chats = list(Config.UB_BLACK_LIST_CHAT)
-    if len(black_list_chats) > 0:
-        args["chats"] = black_list_chats
-
-    # check if the plugin should allow edited updates
-    allow_edited_updates = False
-    if "allow_edited_updates" in args and args["allow_edited_updates"]:
-        allow_edited_updates = args["allow_edited_updates"]
-        del args["allow_edited_updates"]
-
-    # check if the plugin should listen for outgoing 'messages'
-    is_message_enabled = True
-
+    args["chats"] = list(Config.UB_BLACK_LIST_CHAT)
     return events.NewMessage(**args)
-
 
 async def is_read(borg, entity, message, is_out=None):
     """
