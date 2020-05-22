8b69b9ca2be513e6043fb1520b9656bf0fadeeb5
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-22 08:50:31 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -12,6 +12,14 @@ import math
 import os
 import time
 
+import asyncio
+from traceback import format_exc
+from time import gmtime, strftime
+import subprocess
+import sys
+import traceback
+import datetime
+
 from telethon.tl.functions.messages import GetPeerDialogsRequest
 
 from typing import List
@@ -23,6 +31,7 @@ def command(**args):
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
     file_test = file_test.stem.replace(".py", "")
+    allow_sudo = args.get("allow_sudo", False)
     if 1 == 0:
         return print("stupidity at its best")
     else:
@@ -31,43 +40,48 @@ def command(**args):
         allow_edited_updates = args.get('allow_edited_updates', False)
         args["incoming"] = args.get("incoming", False)
         args["outgoing"] = True
-        if bool(args["incoming"]):
-            args["outgoing"] = False
+        
 
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+
+    if "disable_edited" in args:
+        del args['disable_edited']
+    
+    reg = re.compile('(.*)')
+    if not pattern == None:
         try:
-            if pattern is not None and not pattern.startswith('(?i)'):
-                args['pattern'] = '(?i)' + pattern
-        except:
-            pass
-
-        reg = re.compile('(.*)')
-        if not pattern == None:
+            cmd = re.search(reg, pattern)
             try:
-                cmd = re.search(reg, pattern)
-                try:
-                    cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
-                except:
-                    pass
-
-                try:
-                    CMD_LIST[file_test].append(cmd)
-                except:
-                    CMD_LIST.update({file_test: [cmd]})
+                cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
             except:
                 pass
 
-        if allow_sudo:
-            args["from_users"] = list(Var.SUDO_USERS)
-            # Mutually exclusive with outgoing (can only set one of either).
-            args["incoming"] = True
-        del allow_sudo
-        try:
-            del args["allow_sudo"]
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except:
+                CMD_LIST.update({file_test: [cmd]})
         except:
             pass
+        
+    if allow_sudo:
+        args["from_users"] = list(Var.SUDO_USERS)
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
 
-        if "allow_edited_updates" in args:
-            del args['allow_edited_updates']
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
+    args["blacklist_chats"] = True
+    black_list_chats = list(Var.UB_BLACK_LIST_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
+        
+    if "allow_edited_updates" in args:
+        del args['allow_edited_updates']
 
         def decorator(func):
             if allow_edited_updates:
@@ -138,10 +152,11 @@ def remove_plugin(shortname):
     except:
         raise ValueError
 
-def admin_cmd(pattern=None, **args):
+def admin_cmd(**args):
     args["func"] = lambda e: e.via_bot_id is None
     stack = inspect.stack()
     previous_stack_frame = stack[1]
+    pattern = args.get("pattern", None)
     file_test = Path(previous_stack_frame.filename)
     file_test = file_test.stem.replace(".py", "")
     allow_sudo = args.get("allow_sudo", False)
@@ -162,7 +177,7 @@ def admin_cmd(pattern=None, **args):
     args["outgoing"] = True
     # should this command be available for other users?
     if allow_sudo:
-        args["from_users"] = list(Config.SUDO_USERS)
+        args["from_users"] = list(Var.SUDO_USERS)
         # Mutually exclusive with outgoing (can only set one of either).
         args["incoming"] = True
         del args["allow_sudo"]
@@ -170,6 +185,12 @@ def admin_cmd(pattern=None, **args):
     # error handling condition check
     elif "incoming" in args and not args["incoming"]:
         args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
+    args["blacklist_chats"] = True
+    black_list_chats = list(Var.UB_BLACK_LIST_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
 
     # add blacklist chats, UB should not respond in these chats
     allow_edited_updates = False
@@ -182,19 +203,7 @@ def admin_cmd(pattern=None, **args):
 
     return events.NewMessage(**args)
 
-""" Userbot module for managing events.
- One of the main components of the userbot. """
 
-from telethon import events
-import asyncio
-from userbot import bot
-from traceback import format_exc
-from time import gmtime, strftime
-import math
-import subprocess
-import sys
-import traceback
-import datetime
 
 
 def register(**args):
@@ -206,6 +215,7 @@ def register(**args):
     file_test = file_test.stem.replace(".py", "")
     pattern = args.get('pattern', None)
     disable_edited = args.get('disable_edited', False)
+    allow_sudo = args.get("allow_sudo", False)
 
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
@@ -228,6 +238,23 @@ def register(**args):
                 CMD_LIST.update({file_test: [cmd]})
         except:
             pass
+        
+    if allow_sudo:
+        args["from_users"] = list(Var.SUDO_USERS)
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
+    args["blacklist_chats"] = True
+    black_list_chats = list(Var.UB_BLACK_LIST_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats    
+        
 
     def decorator(func):
         if not disable_edited:
@@ -257,7 +284,7 @@ def errors_handler(func):
 
             text = "**USERBOT CRASH REPORT**\n\n"
 
-            link = "[here](https://t.me/PaperplaneExtendedSupport)"
+            link = "[here](https://t.me/sn12384)"
             text += "If you wanna you can report it"
             text += f"- just forward this message {link}.\n"
             text += "Nothing is logged except the fact of error and date\n"
@@ -360,176 +387,9 @@ class Loader():
 
         
 
-# https://github.com/andy-gh/prettyjson/blob/master/prettyjson.py
 
 
-def prettyjson(obj, indent=2, maxlinelength=80):
-    """Renders JSON content with indentation and line splits/concatenations to fit maxlinelength.
-    Only dicts, lists and basic types are supported"""
-
-    items, _ = getsubitems(obj, itemkey="", islast=True, maxlinelength=maxlinelength - indent, indent=indent)
-    return indentitems(items, indent, level=0)
 
 
-def getsubitems(obj, itemkey, islast, maxlinelength, indent):
-    items = []
-    is_inline = True      # at first, assume we can concatenate the inner tokens into one line
-
-    isdict = isinstance(obj, dict)
-    islist = isinstance(obj, list)
-    istuple = isinstance(obj, tuple)
-    isbasictype = not (isdict or islist or istuple)
-
-    maxlinelength = max(0, maxlinelength)
-
-    # build json content as a list of strings or child lists
-    if isbasictype:
-        # render basic type
-        keyseparator  = "" if itemkey == "" else ": "
-        itemseparator = "" if islast else ","
-        items.append(itemkey + keyseparator + basictype2str(obj) + itemseparator)
-
-    else:
-        # render lists/dicts/tuples
-        if isdict:    opening, closing, keys = ("{", "}", iter(obj.keys()))
-        elif islist:  opening, closing, keys = ("[", "]", range(0, len(obj)))
-        elif istuple: opening, closing, keys = ("[", "]", range(0, len(obj)))    # tuples are converted into json arrays
-
-        if itemkey != "": opening = itemkey + ": " + opening
-        if not islast: closing += ","
-
-        count = 0
-        itemkey = ""
-        subitems = []
-
-        # get the list of inner tokens
-        for (i, k) in enumerate(keys):
-            islast_ = i == len(obj)-1
-            itemkey_ = ""
-            if isdict: itemkey_ = basictype2str(k)
-            inner, is_inner_inline = getsubitems(obj[k], itemkey_, islast_, maxlinelength - indent, indent)
-            subitems.extend(inner)                        # inner can be a string or a list
-            is_inline = is_inline and is_inner_inline     # if a child couldn't be rendered inline, then we are not able either
-
-        # fit inner tokens into one or multiple lines, each no longer than maxlinelength
-        if is_inline:
-            multiline = True
-
-            # in Multi-line mode items of a list/dict/tuple can be rendered in multiple lines if they don't fit on one.
-            # suitable for large lists holding data that's not manually editable.
-
-            # in Single-line mode items are rendered inline if all fit in one line, otherwise each is rendered in a separate line.
-            # suitable for smaller lists or dicts where manual editing of individual items is preferred.
-
-            # this logic may need to be customized based on visualization requirements:
-            if (isdict): multiline = False
-            if (islist): multiline = True
-
-            if (multiline):
-                lines = []
-                current_line = ""
-                current_index = 0
-
-                for (i, item) in enumerate(subitems):
-                    item_text = item
-                    if i < len(inner)-1: item_text = item + ","
-
-                    if len (current_line) > 0:
-                        try_inline = current_line + " " + item_text
-                    else:
-                        try_inline = item_text
-
-                    if (len(try_inline) > maxlinelength):
-                        # push the current line to the list if maxlinelength is reached
-                        if len(current_line) > 0: lines.append(current_line)
-                        current_line = item_text
-                    else:
-                        # keep fitting all to one line if still below maxlinelength
-                        current_line = try_inline
-
-                    # Push the remainder of the content if end of list is reached
-                    if (i == len (subitems)-1): lines.append(current_line)
-
-                subitems = lines
-                if len(subitems) > 1: is_inline = False
-            else: # single-line mode
-                totallength = len(subitems)-1   # spaces between items
-                for item in subitems: totallength += len(item)
-                if (totallength <= maxlinelength): 
-                    str = ""
-                    for item in subitems: str += item + " "  # insert space between items, comma is already there
-                    subitems = [ str.strip() ]               # wrap concatenated content in a new list
-                else:
-                    is_inline = False
 
 
-        # attempt to render the outer brackets + inner tokens in one line 
-        if is_inline:
-            item_text = ""
-            if len(subitems) > 0: item_text = subitems[0]
-            if len(opening) + len(item_text) + len(closing) <= maxlinelength:
-                items.append(opening + item_text + closing)
-            else:
-                is_inline = False
-
-        # if inner tokens are rendered in multiple lines already, then the outer brackets remain in separate lines
-        if not is_inline:
-            items.append(opening)       # opening brackets
-            items.append(subitems)      # Append children to parent list as a nested list
-            items.append(closing)       # closing brackets
-
-    return items, is_inline
-
-
-def basictype2str(obj):
-    if isinstance (obj, str):
-        strobj = "\"" + str(obj) + "\""
-    elif isinstance(obj, bool): 
-        strobj = { True: "true", False: "false" }[obj]
-    else:
-        strobj = str(obj)
-    return strobj
-
-
-def indentitems(items, indent, level):
-    """Recursively traverses the list of json lines, adds indentation based on the current depth"""
-    res = ""
-    indentstr = " " * (indent * level)
-    for (i, item) in enumerate(items):
-        if isinstance(item, list): 
-            res += indentitems(item, indent, level+1)
-        else:
-            islast = (i==len(items)-1)
-            # no new line character after the last rendered line
-            if level==0 and islast:
-                res += indentstr + item
-            else:
-                res += indentstr + item + "\n"            
-    return res
-
-
-def parse_arguments(message: str, valid: List[str]) -> (dict, str):
-    options = {}
-
-    # Handle boolean values
-    for opt in findall(r'([.!]\S+)', message):
-        if opt[1:] in valid:
-            if opt[0] == '.':
-                options[opt[1:]] = True
-            elif opt[0] == '!':
-                options[opt[1:]] = False
-            message = message.replace(opt, '')
-
-    # Handle key/value pairs
-    for opt in findall(r'(\S+):(?:"([\S\s]+)"|(\S+))', message):
-        key, val1, val2 = opt
-        value = val2 or val1[1:-1]
-        if key in valid:
-            if value.isnumeric():
-                value = int(value)
-            elif match(r'[Tt]rue|[Ff]alse', value):
-                match(r'[Tt]rue', value)
-            options[key] = value
-            message = message.replace(f"{key}:{value}", '')
-
-    return options, message.strip()
