53bbfb57d9ceac6b47ababcb555d33d3a7060311
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-24 19:51:25 +0530

    Update mute_sql.py

--- userbot/plugins/sql_helper/mute_sql.py
+++ userbot/plugins/sql_helper/mute_sql.py
@@ -38,3 +38,8 @@ def unmute(sender, chat_id):
     if rem:
         SESSION.delete(rem)
         SESSION.commit()
+
+def get_all_muted():
+    rem = SESSION.query(Mute).all()
+    SESSION.close()
+    return rem
