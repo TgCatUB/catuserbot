ebacdd637c1f36600931883389a2848749f60192
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-28 03:05:24 +0530

    Changed to green colour

--- userbot/plugins/autoprofile.py
+++ userbot/plugins/autoprofile.py
@@ -47,7 +47,7 @@ async def autopic(event):
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-        drawn_text.text((350, 350), current_time, font=fnt, fill=(255, 255, 255))
+        drawn_text.text((350, 350), current_time, font=fnt, fill=(124, 252, 0))
         img.save(photo)
         file = await bot.upload_file(photo)  # pylint:disable=E0602
         try:
