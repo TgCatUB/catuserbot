e564226997ef9763f12fcbc1822496289487f33e
arun-anjilik <52011992+arun-anjilik@users.noreply.github.com>
2020-05-28 17:50:14 +0530

    Update autoprofile.py

--- userbot/plugins/autoprofile.py
+++ userbot/plugins/autoprofile.py
@@ -22,7 +22,7 @@ import shutil
 DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else " @hangout02 "
 
 DEL_TIME_OUT = 30
-DEFAULTUSER = str(AUTONAME) if AUTONAME else "𝙍𝘼𝘽𝘽𝙄𝙏" 
+DEFAULTUSER = str(AUTONAME) if AUTONAME else "『 𝙍𝘼𝘽⃤𝘽⸙𝙏 』" 
 
 
 
@@ -66,7 +66,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d-%m-%y")
         HM = time.strftime("%H:%M")
-        name = f"⏥⏥ {HM} ▰ ▱{DEFAULTUSER}▱ ▰ ⏥⏥"
+        name = f"▰ ▱{DEFAULTUSER}▰ ▱ {HM} ⏥⏥"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
