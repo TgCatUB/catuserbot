7bdaeecce6e54542a1902040e1a2aa0d50f34b07
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-31 18:16:42 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -6,6 +6,7 @@ import io
 import asyncio
 import time
 from userbot.utils import admin_cmd
+from userbot import CMD_HELP
 import glob
 import os
 try:
@@ -25,27 +26,37 @@ def bruh(name):
 
 @borg.on(admin_cmd(pattern="song ?(.*)"))
 async def _(event):
-    if event.fwd_from:
-        return
-    DELAY_BETWEEN_EDITS = 0.3
-    PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
+    await event.edit("wi8..! I am finding your song....`")
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("ok finding the song")    
-    bruh(str(cmd))
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+    elif reply.text:
+        query = reply.message
+    else:
+    	await event.edit("`What I am Supposed to find `")
+    	return
+    
+    bruh(str(query))
     l = glob.glob("*.mp3")
     loa = l[0]
-    await event.edit("sending song")
+    await event.edit("yeah..! i found something wi8..🥰")
     await borg.send_file(
                 event.chat_id,
                 loa,
                 force_document=True,
                 allow_cache=False,
-                caption=cmd,
+                caption=query,
                 reply_to=reply_to_id
             )
     await event.delete()
     os.system("rm -rf *.mp3")
     subprocess.check_output("rm -rf *.mp3",shell=True)
+    
+    
+    
+CMD_HELP.update({"getmusic": ['getmusic',
+    " - `.song` query : finds the song you entered in query and sends it "]
+})    
