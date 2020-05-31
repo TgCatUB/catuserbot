a792f380174e56888543c4e0fb8ed3a7b2022f15
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-31 20:47:31 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -23,6 +23,6 @@ async def amireallyalive(alive):
                     )
 
        
-CMD_HELP.update({"alive": ['alive',
-    " - `.alive` : Type .alive to see wether your bot is working or not. "]
+CMD_HELP.update({"alive": "'alive',\
+    \n`.alive` : Type .alive to see wether your bot is working or not. "
 }) 
