a1adfb7fd4c13b7c06dc8a21c463c8e9b20188f3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-05-31 20:52:02 +0530

    Update coronavirus.py

--- userbot/plugins/coronavirus.py
+++ userbot/plugins/coronavirus.py
@@ -2,12 +2,20 @@
 Syntax: .coronavirus <country>"""
 from covid import Covid
 from userbot.utils import admin_cmd
+from userbot import CMD_HELP
 
 @borg.on(admin_cmd(pattern="coronavirus (.*)"))
 async def _(event):
     covid = Covid()
     data = covid.get_data()
-    country = event.pattern_match.group(1)
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        country = event.pattern_match.group(1)
+    elif reply.text:
+        country = reply.message
+    else:
+    	await event.edit("`What data I am Supposed to Search `")
+    	return
     country_data = get_country_data(country, data)
     output_text = "" 
     for name, value in country_data.items():
@@ -19,3 +27,8 @@ def get_country_data(country, world):
         if country_data["country"] == country:
             return country_data
     return {"Status": "No information yet about this country!"}
+
+CMD_HELP.update({"coronavirus": "`.coronavirus` <country name> :\
+      \n USAGE:finds the covid data of the given country remember country name first letter must be capital. "
+}) 
+
