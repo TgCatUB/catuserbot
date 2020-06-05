7d18534e80c0a11eb7b3daed84bdb848c3cb7e13
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-05 18:28:25 +0530

    added sudo

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -4,7 +4,7 @@ from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 from platform import uname
 from userbot import CMD_HELP, ALIVE_NAME 
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd,sudo_cmd
 from telethon import version
 from platform import python_version, uname
 
@@ -23,7 +23,20 @@ async def amireallyalive(alive):
                      f"`☞My peru owner`: [{DEFAULTUSER}](https://github.com/sandy1709/catuserbot)\n"
                      #"[Deploy this userbot Now](https://github.com/sandy1709/catuserbot)"
                     )
+    
+    
 
+
+@borg.on(sudo_cmd(pattern="sudo", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.reply("YOU ARE SUDO FOR THIS BOT \n\n"
+                     f"☞Telethon version: {version.__version__}\n"
+                     f"☞Python: {python_version()}\n"
+                     f"☞My peru owner: {DEFAULTUSER}\n"
+                     #"Deploy this userbot Now"
+                    )
        
 CMD_HELP.update({"alive": "`.alive` :\
       \nUSAGE: Type .alive to see wether your bot is working or not. "
