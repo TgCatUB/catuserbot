105134f2608a40498e62e2ad2983eb57492fd35c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-07 21:11:05 +0530

    splitted

--- userbot/plugins/autopic.py
+++ userbot/plugins/autopic.py
@@ -4,21 +4,13 @@ import time
 from telethon.tl import functions
 from telethon.errors import FloodWaitError
 from userbot.utils import admin_cmd
-from userbot import AUTONAME, DEFAULT_BIO, CMD_HELP
+from userbot import CMD_HELP
 import random, re
 import os
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 from pySmartDL import SmartDL
 import shutil
-
-
-DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else " ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ  "
-
-DEL_TIME_OUT = 30
-DEFAULTUSER = str(AUTONAME) if AUTONAME else "cat" 
-
-
 
 
 FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
@@ -40,8 +32,8 @@ async def autopic(event):
         current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n  Time: %H:%M \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 50)
-        drawn_text.text((350, 350), current_time, font=fnt, fill=(124, 252, 0))
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+        drawn_text.text((95, 250), current_time, font=fnt, fill=(124, 252, 0))
         img.save(photo)
         file = await bot.upload_file(photo)  # pylint:disable=E0602
         try:
@@ -50,6 +42,15 @@ async def autopic(event):
             ))
             os.remove(photo)
             counter -= 30
-            await asyncio.sleep(30)
+            await asyncio.sleep(60)
         except:
             return
+        
+        
+CMD_HELP.update({
+    "autopic":
+    ".autopic\
+\n useage:tilted auto image along with time to work this you must set `DOWNLOAD_PFP_URL_CLOCK`\
+in the heroku vars first by telegraph link of that image \
+"
+})          
