e4340d70ccecf5dce3acdc43df3513d3bd64b834
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-08 02:07:07 +0530

    Update create_private_group.py

--- userbot/plugins/create_private_group.py
+++ userbot/plugins/create_private_group.py
@@ -15,7 +15,7 @@ async def _(event):
     if type_of_group == "b":
         try:
             result = await borg(functions.messages.CreateChatRequest(  # pylint:disable=E0602
-                users=["@BorgHelpBot"],
+                users=["@sarah_robot"],
                 # Not enough users (to create a chat, for example)
                 # Telegram, no longer allows creating a chat with ourselves
                 title=group_name
@@ -23,7 +23,7 @@ async def _(event):
             created_chat_id = result.chats[0].id
             await borg(functions.messages.DeleteChatUserRequest(
                 chat_id=created_chat_id,
-                user_id="@BorgHelpBot"
+                user_id="@sarah_robot"
             ))
             result = await borg(functions.messages.ExportChatInviteRequest(
                 peer=created_chat_id,
