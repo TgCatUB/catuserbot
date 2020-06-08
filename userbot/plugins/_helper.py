6e6b0525744ca2e2b9c07b0a3df3797eb861ac7d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-08 09:06:16 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -35,8 +35,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""Userbot Helper.. Provided by {DEFAULTUSER} \n\n
-`Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__\n__Do .info plugin_name for usage"""
+            help_string = f"""Userbot Helper.. Provided by {DEFAULTUSER} \n
+Userbot Helper to reveal all the commands\n__Do `.help` plugin_name for commands, in case popup doesn't appear.__\nDo `.info` plugin_name for usage"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
