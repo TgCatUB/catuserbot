5d744b8cf50ac0d0c5159dda795b064f524e330a
arun-anjilik <52011992+arun-anjilik@users.noreply.github.com>
2020-06-10 18:41:57 +0530

    Add files via upload

---
+++ userbot/plugins/tagallnew.py
@@ -0,0 +1,37 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("alltag"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "Hello"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, 100):
+        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
+    await event.reply(mentions)
+    await event.delete()
+
+
+@borg.on(admin_cmd("admin"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "Admins : "
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
