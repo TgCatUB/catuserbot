7078009ccc331a58a64267e87eb50c50eb165e63
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-10 19:49:02 +0530

    Update spotify_downloader.py

--- userbot/plugins/spotify_downloader.py
+++ userbot/plugins/spotify_downloader.py
@@ -31,7 +31,7 @@ async def _(event):
               await borg.send_message(event.chat_id, details)
               await conv.get_response()
               songh = await conv.get_response()
-              await borg.send_file(event.chat_id, songh, caption="🔆**Here's the requested song!**🔆\n`Check out` [X-tra userbot](https://github.com/Dark-Princ3/X-tra-Telegram)")
+              await borg.send_file(event.chat_id, songh, caption="🔆**Here's the requested song!**🔆")
               await event.delete()
           except YouBlockedUserError:
               await event.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
