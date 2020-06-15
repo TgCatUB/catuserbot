e013682f200df2802724d6f95bafa00a44cdfe3b
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-15 14:08:18 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -5,6 +5,9 @@ from platform import uname
 import sys
 from telethon import events, functions, __version__
 
+from userbot import SUDO_LIST
+from userbot.utils import sudo_cmd
+
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
 @command(pattern="^.help ?(.*)")
@@ -48,47 +51,6 @@ Userbot Helper to reveal all the commands\n__Do `.help` plugin_name for commands
             )
             await event.delete()
 
-@borg.on(sudo_cmd(pattern="dc", allow_sudo = True))            
-async def cmd_list(event):
-    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
-        input_str = event.pattern_match.group(1)
-        if tgbotusername is None or input_str == "text":
-            string = ""
-            for i in CMD_LIST:
-                string += "👉 " + i + "\n"
-                for iter_list in SUDO_LIST[i]:
-                    string += "    `" + str(iter_list) + "`"
-                    string += "\n"
-                string += "\n"
-            if len(string) > 4095:
-                await borg.send_message(event.chat_id, "Do .help cmd")
-                await asyncio.sleep(5)
-            else:
-                await event.edit(string)
-        elif input_str:
-            if input_str in SUDO_LIST:
-                string = "Commands found in {}:\n".format(input_str)
-                for i in SUDO_LIST[input_str]:
-                    string += "    " + i
-                    string += "\n"
-                await event.edit(string)
-            else:
-                await event.edit(input_str + " is not a valid plugin!")
-        else:
-            help_string = f"""Userbot Helper.. Provided by {DEFAULTUSER} \n
-Userbot Helper to reveal all the commands\n__Do `.help` plugin_name for commands, in case popup doesn't appear.__"""
-            results = await bot.inline_query(  # pylint:disable=E0602
-                tgbotusername,
-                help_string
-            )
-            await results[0].click(
-                event.chat_id,
-                reply_to=event.reply_to_msg_id,
-                hide_via=True
-            )
-            await event.delete()
-
 @borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
@@ -97,3 +59,29 @@ async def _(event):
     await event.edit(result.stringify())
 
 
+
+@borg.on(sudo_cmd(allow_sudo=True, pattern="help(?: |$)(.*)"))
+async def info(event):
+    if event.fwd_from:
+        return
+    """ For .info command,"""
+    args = event.pattern_match.group(1).lower()
+    input_str = event.pattern_match.group(1)
+    if args:
+        if args in SUDO_LIST:
+            if input_str in SUDO_LIST:
+                string = "Commands found in {}:\n".format(input_str)
+                for i in SUDO_LIST[input_str]:
+                    string += "    " + i
+                    string += "\n"
+                await event.reply(string)
+        else:
+                await event.reply(args + " is not a valid plugin!")
+    else:
+        await event.reply("Please specify which plugin do you want help for !!\
+            \nUsage: .info <plugin name>")
+        string = ""
+        for i in SUDO_LIST:
+            string += " " + str(i)
+            string += " \n"
+        await event.reply(string)
