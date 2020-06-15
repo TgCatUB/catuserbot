e98465a316c583f7b3221acaf1e0ad2984047c7d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-15 15:40:25 +0530

    Update zombies.py

--- userbot/plugins/zombies.py
+++ userbot/plugins/zombies.py
@@ -7,7 +7,7 @@ Usage: Searches for deleted accounts in a groups and channels.
 Use .zombies clean to remove deleted accounts from the groups and channels.
 \nPorted by ©[NIKITA](t.me/kirito6969) and ©[EYEPATCH](t.me/NeoMatrix90)"""
 
-from telethon import events
+from telethon import events, functions
 from userbot.utils import admin_cmd,sudo_cmd
 from asyncio import sleep
 from os import remove
