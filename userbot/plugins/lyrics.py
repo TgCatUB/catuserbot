49038b27e292ee053debc2cb0bbe122a3bcab7c6
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-18 01:25:22 +0530

    Update lyrics.py

--- userbot/plugins/lyrics.py
+++ userbot/plugins/lyrics.py
@@ -1,103 +1,35 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
+"@s_a_i_k_r_i_s_h_n_a idea"
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from uniborg.util import admin_cmd
 
-
-
-
-import os
-import lyricsgenius
-import random
-
-from userbot.utils import admin_cmd
-from userbot import CMD_HELP, LOGS
-
-GENIUS = os.environ.get("GENIUS_API_TOKEN", None)
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="lyrics(?: |$)(.*)"))
-async def lyrics(lyric):
-    if r"-" in lyric.text:
-        pass
-    else:
-        await lyric.edit("Error: please use '-' as divider for <artist> and <song>\n"
-                         "eg: `.lyrics Nicki Minaj - Super Bass`")
-        return
-
-    if GENIUS is None:
-        await lyric.edit(
-            "`Provide genius access token to config.py or Heroku Var first kthxbye!`")
-    else:
-        genius = lyricsgenius.Genius(GENIUS)
-        try:
-            args = lyric.text.split('.lyrics')[1].split('-')
-            artist = args[0].strip(' ')
-            song = args[1].strip(' ')
-        except Exception:
-            await lyric.edit("`LMAO please provide artist and song names`")
-            return
-
-    if len(args) < 1:
-        await lyric.edit("`Please provide artist and song names`")
-        return
-
-    await lyric.edit(f"`Searching lyrics for {artist} - {song}...`")
-
-    try:
-        songs = genius.search_song(song, artist)
-    except TypeError:
-        songs = None
-
-    if songs is None:
-        await lyric.edit(f"Song **{artist} - {song}** not found!")
-        return
-    if len(songs.lyrics) > 4096:
-        await lyric.edit("`Lyrics is too big, view the file to see it.`")
-        with open("lyrics.txt", "w+") as f:
-            f.write(f"Search query: \n{artist} - {song}\n\n{songs.lyrics}")
-        await lyric.client.send_file(
-            lyric.chat_id,
-            "lyrics.txt",
-            reply_to=lyric.id,
-            )
-        os.remove("lyrics.txt")
-    else:
-        await lyric.edit(f"**Search query**: \n`{artist} - {song}`\n\n```{songs.lyrics}```")
-    return
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="iff"))
-async def pressf(f):
-    """Pays respects"""
-    args = f.text.split()
-    arg = (f.text.split(' ', 1))[1] if len(args) > 1 else None
-    if len(args) == 1:
-        r = random.randint(0, 3)
-        LOGS.info(r)
-        if r == 0:
-            await f.edit("┏━━━┓\n┃┏━━┛\n┃┗━━┓\n┃┏━━┛\n┃┃\n┗┛")
-        elif r == 1:
-            await f.edit("╭━━━╮\n┃╭━━╯\n┃╰━━╮\n┃╭━━╯\n┃┃\n╰╯")
-        else:
-            arg = "F"
-    if arg is not None:
-        out = ""
-        F_LENGTHS = [5, 1, 1, 4, 1, 1, 1]
-        for line in F_LENGTHS:
-            c = max(round(line / len(arg)), 1)
-            out += (arg * c) + "\n"
-        await f.edit("`" + out + "`")
-
-
+@borg.on(admin_cmd(pattern="lyrics ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    input_str = event.pattern_match.group(1)
+    reply_message = await event.get_reply_message()
+    chat = "@sarah_robot"
+    await event.edit("Checking...")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1103306594))
+              await event.client.send_message(chat, "/lyrics {}".format(input_str))
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("unblock @sarah_robot and try")
+              return
+          if response.text.startswith("song not found"):
+             await event.edit("😐Song Not Found. Try other😐\n\n👇")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
+            
+            
 CMD_HELP.update({
     "lyrics":
-    "**Usage:** .`lyrics <artist name> - <song name>`\n"
-    "__note__: **-** is neccessary when searching the lyrics to divided artist and song \n"
-"Genius lyrics plugin \n"
- "get this value from https://genius.com/developers \n"
-
-"Add:-  GENIUS_API_TOKEN and token value in heroku app settings \n"
- 
-"Lyrics Plugin Syntax: .lyrics <aritst name - song nane>"
-})
+    ".lyrics songname\
+    \n USAGE: sends you song lyrics of required song "
+})            
