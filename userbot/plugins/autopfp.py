6b51dd512d3e9cd26fd998d12cafac92565913f6
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-18 22:45:28 +0530

    Update autopfp.py

--- userbot/plugins/autopfp.py
+++ userbot/plugins/autopfp.py
@@ -35,7 +35,8 @@ async def main(event):
     await event.edit("**Starting batman Profile Pic.**") #Owner @NihiNivi
     while True:
         await animepp()
-        file = await event.client.upload_file("donottouch.jpg")  
+        file = await event.client.upload_file("donottouch.jpg")
+        await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
         await event.client(functions.photos.UploadProfilePhotoRequest( file))
         os.system("rm -rf donottouch.jpg")
-        await asyncio.sleep(3600) #Edit this to your required needs
+        await asyncio.sleep(120) #Edit this to your required needs
