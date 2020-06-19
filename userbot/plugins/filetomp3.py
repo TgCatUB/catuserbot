ced4fa43b2010b87e5e6ac79d7b0365fe8f279aa
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-19 14:51:48 +0530

    Update filetomp3.py

--- userbot/plugins/filetomp3.py
+++ userbot/plugins/filetomp3.py
@@ -55,7 +55,7 @@ async def _(event):
         voice_note = False
         supports_streaming = False
         if input_str == "voice":
-            new_required_file_caption = "NLFC_" + str(round(time.time())) + ".opus"
+            new_required_file_caption = "AUDIO_" + str(round(time.time())) + ".opus"
             new_required_file_name = Config.TMP_DOWNLOAD_DIRECTORY + "/" + new_required_file_caption
             command_to_run = [
                 "ffmpeg",
@@ -74,7 +74,7 @@ async def _(event):
             voice_note = True
             supports_streaming = True
         elif input_str == "mp3":
-            new_required_file_caption = "mp3_" + str(round(time.time())) + ".mp3"
+            new_required_file_caption = "MP3_" + str(round(time.time())) + ".mp3"
             new_required_file_name = Config.TMP_DOWNLOAD_DIRECTORY + "/" + new_required_file_caption
             command_to_run = [
                 "ffmpeg",
