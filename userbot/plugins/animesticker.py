8bc1418e3ef9e55d605525e951dccfe409b2d5ee
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-20 10:57:36 +0530

    Add files via upload

---
+++ userbot/plugins/animesticker.py
@@ -0,0 +1,72 @@
+#imported from ppe-remix by @heyworld & @DeletedUser420
+#Translated & Updated by @Sur_vivor
+from asyncio import sleep
+from random import choice, getrandbits, randint
+import re
+from re import sub
+import random
+from os import execl
+import time
+from telethon import events
+from userbot import bot
+from collections import deque
+import requests
+import sys
+import os
+import io
+import html
+import json
+from PIL import ImageEnhance, ImageOps
+
+from userbot import CMD_HELP
+from userbot.events import register
+from userbot.plugins.admin import get_user_from_event
+
+
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats 
+    "]+")
+
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(EMOJI_PATTERN, '', inputString)
+
+
+@register(outgoing=True, pattern="^.waifu(?: |$)(.*)")
+
+async def waifu(animu):
+#"""Creates random anime sticker!"""
+
+    text = animu.pattern_match.group(1)
+    if not text:
+        if animu.is_reply:
+            text = (await animu.get_reply_message()).message
+        else:
+            await animu.answer("`You haven't written any article, Waifu is going away.`")
+            return
+    animus = [1, 3, 7, 9, 13, 22, 34, 35, 36, 37, 43, 44, 45, 52, 53, 55]
+    sticcers = await bot.inline_query(
+        "stickerizerbot", f"#{random.choice(animus)}{(deEmojify(text))}")
+    await sticcers[0].click(animu.chat_id,
+                            reply_to=animu.reply_to_msg_id,
+                            silent=True if animu.is_reply else False,
+                            hide_via=True)
+    await animu.delete()
+    
+    
+    CMD_HELP.update({
+    'waifu':
+    ".waifu : Anime that makes your writing fun."
+})
