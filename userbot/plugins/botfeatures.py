aeacb318cf03c47328c7d2ebb8ccfd6b5036d5d0
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-21 15:00:25 +0530

    Update botfeatures.py

--- userbot/plugins/botfeatures.py
+++ userbot/plugins/botfeatures.py
@@ -76,7 +76,34 @@ async def _(event):
               return
           await event.delete()
           await event.client.send_message(event.chat_id, response.message, reply_to=reply_message)
-
+        
+@borg.on(admin_cmd(pattern="ad ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit("```reply to media message```")
+       return
+    chat = "@audiotubebot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit("```Processing```")
+    async with borg.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=507379365))
+              await borg.send_message(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock @AudioTubeBot and try again```")
+              return
+          await event.delete()
+          await borg.send_file(event.chat_id, response.message.media)
 
 @borg.on(admin_cmd(pattern="connecter ?(.*)", allow_sudo=True))
 async def _(event):
