8ec722326fbd52079ed196ab9fee1dd30226181d
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-21 15:55:00 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -27,6 +27,9 @@ from userbot.utils import register, admin_cmd
 from userbot import ALIVE_NAME
 
 # ================= CONSTANT =================
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@Sur_vivor"
+
 RENDISTR = [
     "`I Know Uh ez Rendi Bhay Dont show Your Randi Pesa Here`",
     "`Jag Suna suna laage Sab #maderchod bhay`",
@@ -310,10 +313,10 @@ RAPE_STRINGS = [
      "`Lodu Andha hai kya Yaha tera rape ho raha hai aur tu abhi tak yahi gaand mara raha hai lulz`",
 ] 
 ABUSE_STRINGS = [
-	   "`Chutiya he rah jaye ga`",
-	   "`Ja be Gaandu`",
-	   "`Muh Me Lega Bhosdike ?`",
-	   "`Kro Gandu giri kam nhi toh Gand Maar lenge tumhari hum😂`",
+       "`Chutiya he rah jaye ga`",
+       "`Ja be Gaandu`",
+       "`Muh Me Lega Bhosdike ?`",
+       "`Kro Gandu giri kam nhi toh Gand Maar lenge tumhari hum😂`",
        "`Suno Lodu Jyda muh na chalo be muh me lawda pel Diyaa jayega`",
        "`Sharam aagyi toh aakhe juka lijia land me dam nhi hai apke toh Shilajit kha lijia`",
        "`Kahe Rahiman Kaviraaj C**t Ki Mahima Aisi,L**d Murjha Jaaye Par Ch**t Waisi Ki Waisi`",
@@ -484,95 +487,6 @@ CRI = [
     "༼ ༎ຶ ෴ ༎ຶ༽",
 ]
 
-SLAP_TEMPLATES = [
-    "{user1} {hits} {victim} with a {item}.",
-    "{user1} {hits} {victim} in the face with a {item}.",
-    "{user1} {hits} {victim} around a bit with a {item}.",
-    "{user1} {throws} a {item} at {victim}.",
-    "{user1} grabs a {item} and {throws} it at {victim}'s face.",
-    "{user1} {hits} a {item} at {victim}.", "{throws} a few {item} at {victim}.",
-    "{user1} grabs a {item} and {throws} it in {victim}'s face.",
-    "{user1} launches a {item} in {victim}'s general direction.",
-    "{user1} sits on {victim}'s face while slamming a {item} {where}.",
-    "{user1} starts slapping {victim} silly with a {item}.",
-    "{user1} pins {victim} down and repeatedly {hits} them with a {item}.",
-    "{user1} grabs up a {item} and {hits} {victim} with it.",
-    "{user1} starts slapping {victim} silly with a {item}.",
-    "{user1} holds {victim} down and repeatedly {hits} them with a {item}.",
-    "{user1} prods {victim} with a {item}.",
-    "{user1} picks up a {item} and {hits} {victim} with it.",
-    "{user1} ties {victim} to a chair and {throws} a {item} at them.",
-    "{user1} {hits} {victim} {where} with a {item}.",
-    "{user1} ties {victim} to a pole and whips them {where} with a {item}."
-    "{user1} gave a friendly push to help {victim} learn to swim in lava.",
-    "{user1} sent {victim} to /dev/null.", "sent {victim} down the memory hole.",
-    "{user1} beheaded {victim}.", "threw {victim} off a building.",
-    "{user1} replaced all of {victim}'s music with Nickelback.",
-    "{user1} spammed {victim}'s email.", "made {victim} a knuckle sandwich.",
-    "{user1} slapped {victim} with pure nothing.",
-    "{user1} hit {victim} with a small, interstellar spaceship.",
-    "{user1} quickscoped {victim}.", "put {victim} in check-mate.",
-    "{user1} RSA-encrypted {victim} and deleted the private key.",
-    "{user1} put {victim} in the friendzone.",
-    "{user1} slaps {victim} with a DMCA takedown request!"
-]
-
-ITEMS = [
-    "cast iron skillet",
-    "large trout",
-    "baseball bat",
-    "cricket bat",
-    "wooden cane",
-    "nail",
-    "printer",
-    "shovel",
-    "pair of trousers",
-    "CRT monitor",
-    "diamond sword",
-    "baguette",
-    "physics textbook",
-    "toaster",
-    "portrait of Richard Stallman",
-    "television",
-    "mau5head",
-    "five ton truck",
-    "roll of duct tape",
-    "book",
-    "laptop",
-    "old television",
-    "sack of rocks",
-    "rainbow trout",
-    "cobblestone block",
-    "lava bucket",
-    "rubber chicken",
-    "spiked bat",
-    "gold block",
-    "fire extinguisher",
-    "heavy rock",
-    "chunk of dirt",
-    "beehive",
-    "piece of rotten meat",
-    "bear",
-    "ton of bricks",
-]
-
-THROW = [
-    "throws",
-    "flings",
-    "chucks",
-    "hurls",
-]
-
-HIT = [
-    "hits",
-    "whacks",
-    "slaps",
-    "smacks",
-    "bashes",
-]
-
-WHERE = ["in the chest", "on the head", "on the butt", "on the crotch"]
-
 # ===========================================
 
 
@@ -602,73 +516,6 @@ async def kek(keks):
             time.sleep(0.3)
             await keks.edit(":" + uio[i % 2])
 			  
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@Sur_vivor"
-@borg.on(admin_cmd(pattern=r"slap(?: |$)(.*)", outgoing=True))
-async def who(event):
-    if event.fwd_from:
-        return
-    replied_user = await get_user(event)
-    caption = await slap(replied_user, event)
-    message_id_to_reply = event.message.reply_to_msg_id
-
-    if not message_id_to_reply:
-        message_id_to_reply = None
-
-    try:
-        await event.edit(caption)
-
-    except:
-        await event.edit("`Can't slap this nibba !!`")
-
-async def get_user(event):
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        replied_user = await event.client(GetFullUserRequest(previous_message.from_id))
-    else:
-        user = event.pattern_match.group(1)
-
-        if user.isnumeric():
-            user = int(user)
-
-        if not user:
-            self_user = await event.client.get_me()
-            user = self_user.id
-
-        if event.message.entities is not None:
-            probable_user_mention_entity = event.message.entities[0]
-
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user
-        try:
-            user_object = await event.client.get_entity(user)
-            replied_user = await event.client(GetFullUserRequest(user_object.id))
-
-        except (TypeError, ValueError):
-            await event.edit("`I don't slap strangers !!`")
-            return None
-
-    return replied_user
-			  
-async def slap(replied_user, event):
-    user_id = replied_user.user.id
-    first_name = replied_user.user.first_name
-    username = replied_user.user.username
-    if username:
-        slapped = "@{}".format(username)
-    else:
-        slapped = f"[{first_name}](tg://user?id={user_id})"
-
-    temp = random.choice(SLAP_TEMPLATES)
-    item = random.choice(ITEMS)
-    hit = random.choice(HIT)
-    throw = random.choice(THROW)
-    where = random.choice(WHERE)		  
-
-    caption = temp.format(user1=DEFAULTUSER, victim=slapped, item=item, hits=hit, throws=throw, where=where)
-
-    return caption
 
 @register(outgoing=True, pattern="^-_-$")
 async def lol(lel):
