ae088929d8041a0a1bf0deaffb791eac6492552c
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-21 14:09:32 +0530

    Update stickcreater.py

--- userbot/plugins/stickcreater.py
+++ userbot/plugins/stickcreater.py
@@ -60,56 +60,6 @@ async def sticklet(event):
         os.remove(FONT_FILE)
     except:
         pass
-
-    
-@borg.on(admin_cmd(pattern="stcr ?(?:(.*?) \| )?(.*)", allow_sudo=True))
-async def sticklet(event):
-    R = random.randint(0,256)
-    G = random.randint(0,256)
-    B = random.randint(0,256)
-    reply_message = event.message
-    # get the input text
-    # the text on which we would like to do the magic on
-    font_file_name = event.pattern_match.group(1)
-    if not font_file_name:
-        font_file_name = ""
-    sticktext = event.pattern_match.group(2)
-    if not sticktext and event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        sticktext = reply_message.message
-    elif not sticktext:
-        await event.edit("need something, hmm")
-        return
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-    # delete the userbot command,
-    # i don't know why this is required
-    await event.delete()
-    # https://docs.python.org/3/library/textwrap.html#textwrap.wrap
-    sticktext = textwrap.wrap(sticktext, width=10)
-    # converts back the list to a string
-    sticktext = '\n'.join(sticktext)
-    image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
-    draw = ImageDraw.Draw(image)
-    fontsize = 230
-    FONT_FILE = await get_font_file(event.client, "@catfonts", font_file_name)
-    font = ImageFont.truetype(FONT_FILE, size=fontsize)
-    while draw.multiline_textsize(sticktext, font=font) > (512, 512):
-        fontsize -= 3
-        font = ImageFont.truetype(FONT_FILE, size=fontsize)
-    width, height = draw.multiline_textsize(sticktext, font=font)
-    draw.multiline_text(((512-width)/2,(512-height)/2), sticktext, font=font, fill=(R, G, B))
-    image_stream = io.BytesIO()
-    image_stream.name = "@mrconfused.webp"
-    image.save(image_stream, "WebP")
-    image_stream.seek(0)
-    # finally, reply the sticker
-    await event.client.send_file(event.chat_id, image_stream, caption="Sur_vivor's Sticklet", reply_to=event.message.reply_to_msg_id)
-    # cleanup
-    try:
-        os.remove(FONT_FILE)
-    except:
-        pass
     
 async def get_font_file(client, channel_id, search_kw=""):
     # first get the font messages
