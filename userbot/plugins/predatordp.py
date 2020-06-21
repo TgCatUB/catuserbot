ebd3cbe7b355b38fb931435a7938d303e0ab1d6b
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-21 22:07:50 +0530

    Add files via upload

---
+++ userbot/plugins/predatordp.py
@@ -0,0 +1,40 @@
+#Made By @Nihinivi Keep Credits If You Are Goanna Kang This Lol
+#And Thanks To The Creator Of Autopic This Script Was Made from Snippets From That Script
+#Usage .gamerpfp  Im Not Responsible For Any Ban caused By This
+import requests , re , random 
+import urllib , os 
+from telethon.tl import functions
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from userbot.utils import admin_cmd
+import asyncio
+from time import sleep
+
+COLLECTION_STRING = [
+  "Predator-Wallpapers-Backgrounds",
+  "Alien-vs-Predator-Wallpaper"
+]
+
+async def animepp():
+    os.system("rm -rf donot.jpg")
+    rnd = random.randint(0, len(COLLECTION_STRING) - 1)
+    pack = COLLECTION_STRING[rnd]
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+    f = re.compile('/\w+/full.+.jpg')
+    f = f.findall(pc)
+    fy = "http://getwallpapers.com"+random.choice(f)
+    print(fy)
+    if not os.path.exists("f.ttf"):
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+    
+@borg.on(admin_cmd(pattern="predatordp ?(.*)"))
+async def main(event):
+    await event.edit("**Starting predator Profile Pic.**") #Owner @NihiNivi
+    while True:
+        await animepp()
+        file = await event.client.upload_file("donottouch.jpg")  
+        await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+        os.system("rm -rf donottouch.jpg")
+        await asyncio.sleep(120) #Edit this to your required needs
