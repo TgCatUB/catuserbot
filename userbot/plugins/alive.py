9738d76f530eb2c0e54710fe3bf1e3cad9c7e4f5
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-22 18:15:39 +0530

    Update alive.py
    SURVIVOR CAT-VERSION 3.0

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,26 +9,20 @@ from platform import python_version, uname
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@Sur_vivor"
-PM_IMG = "https://telegra.ph/file/be15be483400f07f7e442.jpg"
-pm_caption = "**MY BOT IS RUNNING SUCCESFULLY**\n\n"
-pm_caption += f"🛡`Telethon Version:` **{version.__version__}**\n\n"
-pm_caption += f"🛡`Python Version:` **{python_version()}**\n\n"
-pm_caption += "🛡**Bot Was Modified by**:[✰Şสͥℝสͣ✞ͫђ™✰](http://t.me/Sur_vivor)\n\n"
-pm_caption += "🛡**Created by**: Snapdragon,Anubis,Sandeep\n\n"
-pm_caption += "🛡**Database Status**: Databases Functioning Normally!\n\n"
-pm_caption += "🛡**Always With You, My Master!**\n\n"
-pm_caption += f"🛡**Owner Name**: {DEFAULTUSER}\n\n\n"
-pm_caption += "**[⚜️DEPLOY THIS USERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
+CAT_IMG = "https://telegra.ph/file/be15be483400f07f7e442.jpg"
+cat_caption = "**MY BOT IS RUNNING SUCCESFULLY**\n\n"
+cat_caption += f"🛡`Telethon Version:` **{version.__version__}**\n\n"
+cat_caption += f"🛡`Python Version:` **{python_version()}**\n\n"
+cat_caption += "🛡**Bot Was Modified by**:[✰Şสͥℝสͣ✞ͫђ™✰](http://t.me/Sur_vivor)\n\n"
+cat_caption += "🛡**Created by**: Snapdragon,Anubis,Sandeep\n\n"
+cat_caption += "🛡**Database Status**: Databases Functioning Normally!\n\n"
+cat_caption += "🛡**Always With You, My Master!**\n\n"
+cat_caption += f"🛡**Owner Name**: {DEFAULTUSER}\n\n\n"
+cat_caption += "[⚜️**DEPLOY THIS USERBOT**⚜️](https://github.com/Sur-vivor/CatUserbot)"
 
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.delete()
-    await borg.send_file(await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
-                         
-CMD_HELP.update({
-   "alive":
-   ".alive\
-  \nUsage: Type .alive to see wether your bot is working or not."
-})
+    await borg.send_file(await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption)
