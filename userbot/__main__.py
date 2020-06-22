8235b1e71fc378d0c402079d0d5eb2a43107771b
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-22 12:50:23 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Yay your userbot is officially working.")
+print("Yay your userbot is officially working.You should owe @Sur_vivor For This Cool Bot")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
