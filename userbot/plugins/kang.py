be3259bec1e6d7556213c84e98f87da434a0e68f
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-23 18:49:48 +0530

    Update kang.py

--- userbot/plugins/kang.py
+++ userbot/plugins/kang.py
@@ -117,7 +117,7 @@ async def _(event):
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/done")
 
-    await event.edit(f"Sticker added! Your pack can be found [here](t.me/addstickers/{packshortname})\nSticker Emoji {sticker_emoji}")
+    await event.edit(f"Sticker added! Your pack can be found [🔥here🔥](t.me/addstickers/{packshortname})\nSticker Emoji {sticker_emoji}")
 
 @borg.on(admin_cmd(outgoing=True, pattern="packinfo"))
 async def _(event):
