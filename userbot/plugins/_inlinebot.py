afc6378c3ae3296fb6b54e5284cb32ef347b7df3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-24 23:22:28 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -91,7 +91,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
-    number_of_cols = 3
+    number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
@@ -109,7 +109,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("<<previous<<", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline(">>next>>", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("Previous<<<<", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline(">>>>>next", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
