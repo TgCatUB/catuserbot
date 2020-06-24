117f4194fe2c36f64bd66b5a1ab1e46afd4fa871
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-24 22:44:53 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -18,7 +18,7 @@ async def cmd_list(event):
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "👉 " + i + "\n"
+                string += "⚚" + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
