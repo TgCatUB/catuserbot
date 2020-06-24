87834510eef0dddb59bbb47b4f61d273d4c29fca
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-24 14:17:18 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -101,7 +101,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("🔙", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("🔜", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("⏪Previous", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("Next⏩", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
