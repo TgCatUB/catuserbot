a4f43af0f2d6b92f9a376d439f943de2fa621f5c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-24 23:43:31 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -103,13 +103,13 @@ def paginate_help(page_number, loaded_plugins, prefix):
         for x in helpable_plugins]
     if number_of_cols == 1:
         pairs = list(zip(modules[::number_of_cols]))
-    elseif number_of_cols == 2:
+    elif number_of_cols == 2:
         pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
     else:
         pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols], modules[2::number_of_cols]))
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
-    elseif len(modules) % number_of_cols == 2:
+    elif len(modules) % number_of_cols == 2:
         pairs.append((modules[-1],modules[-1]))
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
