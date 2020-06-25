9a0cccd99fa40e7aa4720cf73886086bb8f211a8
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-25 13:17:21 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -93,7 +93,7 @@ class Config(object):
     # number of rows of buttons to be displayed in .helpme command
     NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD = int(os.environ.get("NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD", 3))
     #emoji to be displayed in .help
-    EMOJI_TO_DISPLAY_IN_HELP = os.environ.get("COMMAND_HAND_LER", "⚚")
+    EMOJI_TO_DISPLAY_IN_HELP = os.environ.get("EMOJI_TO_DISPLAY_IN_HELP", "⚚")
     # specify command handler that should be used for the plugins
     # this should be a valid "regex" pattern
     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", "\.")
