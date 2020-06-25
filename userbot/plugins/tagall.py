f441a5019d03bcea8a0659451a0b239729139bac
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-25 23:56:10 +0530

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -18,6 +18,26 @@ async def _(event):
     await event.reply(mentions)
     await event.delete()
     
+@borg.on(admin_cmd(pattern="all (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_to_id = event.message
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    input_str = event.pattern_match.group(1)
+
+    if not input_str:
+        return await event.edit("what should i do try `.all hello`.")
+
+    mentions = input_str 
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, 100):
+        mentions += f"[\u2063](tg://user?id={x.id})"
+    await reply_to_id.reply(mentions)
+    await event.delete()    
+    
+    
     
 @borg.on(admin_cmd(pattern=r"administrator", outgoing=True))
 async def _(event):
