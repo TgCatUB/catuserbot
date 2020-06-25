7bc88cbd7395b1101ec408189be37aecf1ed7a45
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-25 12:29:22 +0530

    Update logic.py

--- userbot/plugins/logic.py
+++ userbot/plugins/logic.py
@@ -5,17 +5,17 @@
 from telethon import events
 import asyncio
 import random
+import re
 
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern=f"logic", allow_sudo=True))
-@borg.on(events.NewMessage(pattern=r"\.logic", outgoing=True))
+
+@borg.on(admin_cmd(pattern=f"logic$", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("`processing man wait 2sec can't you wait......`")
-    
-    await asyncio.sleep(2)
+ 
+    await asyncio.sleep(0.5)
     
     x=(random.randrange(1,104))
     
