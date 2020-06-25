ee0ab7f404590d10d297d7efcef630ae9d549cd8
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-25 23:49:19 +0530

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -10,27 +10,32 @@ from userbot.utils import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
+    reply_to_id = event.message
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
     mentions = "@all"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
-    await event.reply(mentions)
+    await reply_to_id.reply(mentions)
     await event.delete()
 
 @borg.on(admin_cmd(pattern="all (.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    if event.pattern_match.group(1):
-        query = event.pattern_match.group(1)
-    else:
-    	await event.edit("`What I need to paste`")
-    	return
-    await event.delete()
-    mentions = query 
+    reply_to_id = event.message
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    input_str = event.pattern_match.group(1)
+
+    if not input_str:
+        return await event.edit("what should i do try `.all hello`.")
+
+    mentions = input_str 
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
-    await event.reply(mentions)
+    await reply_to_id.reply(mentions)
     await event.delete()
     
