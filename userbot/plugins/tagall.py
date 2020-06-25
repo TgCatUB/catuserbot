99881071436eda4a8714808748c1810c701b2204
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-25 22:41:45 +0530

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -6,7 +6,6 @@
 from telethon import events
 from userbot.utils import admin_cmd
 
-#@borg.on(admin_cmd(pattern="tagall"))
 @borg.on(admin_cmd(pattern=r"tagall", outgoing=True))
 async def _(event):
     if event.fwd_from:
@@ -18,3 +17,21 @@ async def _(event):
         mentions += f"[\u2063](tg://user?id={x.id})"
     await borg.send_message(
         chat, mentions, reply_to=event.message.reply_to_msg_id)
+
+@borg.on(admin_cmd(pattern=r"all", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+    else:
+    	await event.edit("`What I am Supposed to find `")
+    	return
+    await event.delete()
+    mentions = query 
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, 100):
+        mentions += f"[\u2063](tg://user?id={x.id})"
+    await borg.send_message(
+        chat, mentions, reply_to=event.message.reply_to_msg_id)
+    
