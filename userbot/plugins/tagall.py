244eb3991668bbf2dd54fa8b35a1cda516ae25bc
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-26 16:25:39 +0530

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -52,20 +52,3 @@ async def _(event):
     await reply_to_id.reply(mentions)
     await event.delete()    
     
-    
-    
-@borg.on(admin_cmd(pattern=r"administrator", outgoing=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "Administrators : "
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
