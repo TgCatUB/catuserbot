c9c555cddf21fe98ef9f22da81de267a4bcbe737
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-26 22:35:39 +0530

    Update link.py

--- userbot/plugins/link.py
+++ userbot/plugins/link.py
@@ -60,7 +60,7 @@ async def get_user_from_event(event):
             await event.edit("`Pass the user's username, id or reply!`")
             return
 
-        if event.message.entities is not None:
+        if not event.message.entities :
             probable_user_mention_entity = event.message.entities[0]
 
             if isinstance(probable_user_mention_entity,
