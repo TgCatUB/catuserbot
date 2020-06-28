dcf9ad1ce23b575022c66176996687e627f5831c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-06-28 21:17:24 +0530

    Update log_pms.py

--- userbot/plugins/log_pms.py
+++ userbot/plugins/log_pms.py
@@ -63,12 +63,14 @@ async def monito_p_m_s(event):
         chat = await event.get_chat()
         if chat.id not in NO_PM_LOG_USERS and chat.id != borg.uid:
             try:
-                e = await borg.get_entity(int(Config.PM_LOGGR_BOT_API_ID))
-                fwd_message = await borg.forward_messages(
-                    e,
-                    event.message,
+                if Config.PM_LOGGR_BOT_API_ID:
+                  fwd_message = await borg.forward_messages(
+                    Config.PM_LOGGR_BOT_API_ID,
+                    event.message.message,
                     silent=True
-                )
+                   )
+                else:
+                  return
             except Exception as e:
                 # logger.warn(str(e))
                 exc_type, exc_obj, exc_tb = sys.exc_info()
