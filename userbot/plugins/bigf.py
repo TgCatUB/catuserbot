fbb3372355d3aea0d556d25075c56991dcd2e9ac
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-29 16:00:37 +0530

    Update and rename kk.py to bigf.py

--- userbot/plugins/bigf.py
+++ userbot/plugins/bigf.py
@@ -9,7 +9,7 @@ from userbot.utils import admin_cmd
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
            input_str = event.pattern_match.group(1)
-           if input_str == "kk":                              
+           if input_str == "kf":                              
                  r = random.randint(0, 3)
                  logger.debug(r)
                  if r == 0:
