d72a0e3bb028f283fde8628580a109459dc61755
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-30 18:02:24 +0530

    Update animations4.py

--- userbot/plugins/animations4.py
+++ userbot/plugins/animations4.py
@@ -43,4 +43,31 @@ async def _(event):
 	for _ in range(999):
 		await asyncio.sleep(0.4)
 		await event.edit("".join(deq))
-		deq.rotate(1)		
+		deq.rotate(1)
+		
+@borg.on(admin_cmd(pattern=r"blk"))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🟥🟧🟨🟩🟦🟪🟫⬛⬜"))
+	for _ in range(999):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+		
+@borg.on(admin_cmd(pattern="ccry"))
+#@register(outgoing=True, pattern="^.cry$")
+async def cry(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("(;´༎ຶД༎ຶ)")
+
+@borg.on(admin_cmd(pattern="heart"))		
+#@register(outgoing=True, pattern="^.heart$")
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("❤️🧡💛💚💙💜🖤"))
+	for _ in range(32):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
