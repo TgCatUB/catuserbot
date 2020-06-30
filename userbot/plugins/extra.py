40ac661642f65a6433716b86744ef15959b8cb94
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-30 18:01:53 +0530

    Update extra.py

--- userbot/plugins/extra.py
+++ userbot/plugins/extra.py
@@ -21,27 +21,6 @@ async def leave(e):
         else:
             await e.edit('`Sar This is Not A Chat`')
 
-
-@borg.on(admin_cmd(pattern="ccry"))
-#@register(outgoing=True, pattern="^.cry$")
-async def cry(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("(;´༎ຶД༎ຶ)")
-
-
-
-@borg.on(admin_cmd(pattern="heart"))		
-#@register(outgoing=True, pattern="^.heart$")
-async def _(event):
-	if event.fwd_from:
-		return
-	deq = deque(list("❤️🧡💛💚💙💜🖤"))
-	for _ in range(32):
-		await asyncio.sleep(0.1)
-		await event.edit("".join(deq))
-		deq.rotate(1)
-		
-
 @borg.on(admin_cmd("myusernames"))
 #@register(outgoing=True, pattern="^.myusernames$")
 async def _(event):
