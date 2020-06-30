e2ab1a997e1efefe73c9c4a6e250ba18d88aa210
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-30 17:02:45 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -56,4 +56,4 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - StartTime))
-    await event.edit(f"🏓PONG!\nPing Speed: {ms}\nUptime: {uptime}")
+    await event.edit(f"🏓PONG!\nPing Speed: {ms}\n🤖Userbot Uptime: {uptime}")
