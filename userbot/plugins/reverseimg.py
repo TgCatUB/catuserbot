92b5f3c920f31f0320e1e36acf99ab1c5b1b8224
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-02 01:14:12 +0530

    Update reverseimg.py

--- userbot/plugins/reverseimg.py
+++ userbot/plugins/reverseimg.py
@@ -17,8 +17,8 @@ import re
 from telethon.tl.types import MessageMediaPhoto
 from PIL import Image
 
-from userbot import bot, CMD_HELP, admin_cmd
-from userbot.utils import register, errors_handler
+from userbot import bot, CMD_HELP
+from userbot.utils import admin_cmd, errors_handler
 
 opener = urllib.request.build_opener()
 useragent = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36'
