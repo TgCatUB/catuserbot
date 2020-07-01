e6ca3b1f6aa43d4bb5ff39721b3febf4001bb4ea
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-01 23:45:28 +0530

    Update lydia2.py

--- userbot/plugins/lydia2.py
+++ userbot/plugins/lydia2.py
@@ -84,7 +84,7 @@ async def lydia_disable_enable(event):
 async def on_new_message(event):
     if event.chat_id in Config.UB_BLACK_LIST_CHAT:
         return
-    if Config.LYDIA_API is None:
+    if Config.LYDIA_API_KEY is None:
         return
     reply = await event.get_reply_message()
     if reply is None:
