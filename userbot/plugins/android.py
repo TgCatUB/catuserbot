35e3addef5def5e814abb4c16b1a1b9bfaaa0a57
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-01 20:14:12 +0530

    Update android.py

--- userbot/plugins/android.py
+++ userbot/plugins/android.py
@@ -18,7 +18,7 @@ DEVICES_DATA = 'https://raw.githubusercontent.com/androidtrackers/' \
                'certified-android-devices/master/devices.json'
 
 
-@register(outgoing=True, pattern="^.magisk$")
+@borg.on(admin_cmd(outgoing=True, pattern="magisk$"))
 async def magisk(request):
     """ magisk latest releases """
     magisk_dict = {
@@ -39,7 +39,7 @@ async def magisk(request):
                     f'[Uninstaller]({data["uninstaller"]["link"]})\n'
     await request.edit(releases)
 
-@register(outgoing=True, pattern=r"^.device(?: |$)(\S*)")
+@borg.on(admin_cmd(outgoing=True, pattern=r"device(?: |$)(\S*)"))
 async def device_info(request):
     """ get android device basic info from its codename """
     textx = await request.get_reply_message()
@@ -66,7 +66,7 @@ async def device_info(request):
     await request.edit(reply)
 
 
-@register(outgoing=True, pattern=r"^.codename(?: |)([\S]*)(?: |)([\s\S]*)")
+@borg.on(admin_cmd(outgoing=True, pattern=r"codename(?: |)([\S]*)(?: |)([\s\S]*)"))
 async def codename_info(request):
     """ search for android codename """
     textx = await request.get_reply_message()
@@ -105,7 +105,7 @@ async def codename_info(request):
     await request.edit(reply)
 
 
-@register(outgoing=True, pattern=r"^.specs(?: |)([\S]*)(?: |)([\s\S]*)")
+@borg.on(admin_cmd(outgoing=True, pattern=r"specs(?: |)([\S]*)(?: |)([\s\S]*)"))
 async def devices_specifications(request):
     """ Mobile devices specifications """
     textx = await request.get_reply_message()
@@ -158,7 +158,7 @@ async def devices_specifications(request):
     await request.edit(reply)
 
 
-@register(outgoing=True, pattern=r"^.twrp(?: |$)(\S*)")
+@borg.on(admin_cmd(outgoing=True, pattern=r"twrp(?: |$)(\S*)"))
 async def twrp(request):
     """ get android device twrp """
     textx = await request.get_reply_message()
