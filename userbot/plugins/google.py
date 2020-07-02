819ffc017509a3c8db66dceb560c15fe0dd152ed
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-02 11:19:17 +0530

    Update google.py

--- userbot/plugins/google.py
+++ userbot/plugins/google.py
@@ -50,7 +50,7 @@ async def gsearch(q_event):
             title = gresults["titles"][i]
             link = gresults["links"][i]
             desc = gresults["descriptions"][i]
-            msg += f"[{title}]({link})\n`{desc}`\n\n"
+            msg += f"👉[{title}]({link})\n`{desc}`\n\n"
         except IndexError:
             break
     await q_event.edit("**Search Query:**\n`" + match + "`\n\n**Results:**\n" +
