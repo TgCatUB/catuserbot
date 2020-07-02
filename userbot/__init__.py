8110a448b6a5c06f022fca1ebeef274a4b5ed880
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-03 00:04:33 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -15,7 +15,6 @@ import time
 
 StartTime = time.time()
 
-os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
     bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
