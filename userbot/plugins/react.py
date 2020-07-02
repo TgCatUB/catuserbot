8897cda66c2211c492d5ff51ac14ca887f851c9f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-02 12:03:11 +0530

    Update react.py

--- userbot/plugins/react.py
+++ userbot/plugins/react.py
@@ -7,7 +7,7 @@ import random
 import asyncio
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern=f"react",outgoing=True))
+@borg.on(admin_cmd(pattern=f"react (.*)",outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
