91f88ecfca8db458448ab1fe86923838625edbaa
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-03 14:23:29 +0530

    Update clownbythekillerbob.py

--- userbot/plugins/clownbythekillerbob.py
+++ userbot/plugins/clownbythekillerbob.py
@@ -1,24 +1,14 @@
-"""Emoji
-
-Available Commands:
-
-.emoji shrug
-
-.emoji apple
-
-.emoji :/
-
-.emoji -_-"""
 
 from telethon import events
 
 import asyncio
+from userbot.utils import admin_cmd
 
 
 
 
 
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(admin_cmd(pattern=f"clown$", outgoing=True))
 
 async def _(event):
 
@@ -30,13 +20,7 @@ async def _(event):
 
     animation_ttl = range(0, 117)
 
-    input_str = event.pattern_match.group(1)
-
-    if input_str == "clown":
-
-        await event.edit(input_str)
-
-        animation_chars = [
+    animation_chars = [
         
 
             "COMMAND CREATE BY @Sur_vivor",
@@ -57,7 +41,7 @@ async def _(event):
             "You Are A Clown 🤡"
         ]
 
-        for i in animation_ttl:
+     for i in animation_ttl:
 
             await asyncio.sleep(animation_interval)
 
