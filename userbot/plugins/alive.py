95f5bfb7d2062b386ff5f12e608f5052e4fc5c5a
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-04 00:24:11 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -18,13 +18,13 @@ else:
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@Sur_vivor"
 
 cat_caption = "**MY BOT IS RUNNING SUCCESFULLY**\n\n"
-cat_caption += f"☞`Telethon Version:` **{version.__version__}**\n"
-cat_caption += f"☞`Python Version:` **{python_version()}**\n"
-cat_caption += "☞**Always With You, My Master!**\n"
-cat_caption += f"☞`Owner Name :` {DEFAULTUSER}\n"
-cat_caption += "☞`Database Status :` Databases Functioning Normally!\n"
-cat_caption += "☞`Modified by :` [✰Sᴀͥʀᴀͣᴛͫʜ™️✰](http://t.me/Sur_vivor)\n"
-cat_caption += "☞`Created by :` snapdragon, anubis, sandeep\n\n"
+cat_caption += f"💠`Telethon Version:` **{version.__version__}**\n"
+cat_caption += f"💠`Python Version:` **{python_version()}**\n"
+cat_caption += "💠**Always With You, My Master!**\n"
+cat_caption += f"💠`Owner Name :` {DEFAULTUSER}\n"
+cat_caption += "💠`Database Status :` Databases Functioning Normally!\n"
+cat_caption += "💠`Modified by :` [✰Sᴀͥʀᴀͣᴛͫʜ™️✰](http://t.me/Sur_vivor)\n"
+cat_caption += "💠`Created by :` snapdragon, anubis, sandeep\n\n"
 cat_caption += "**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
 
 
