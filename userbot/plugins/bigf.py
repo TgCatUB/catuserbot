17068dd1cfcea5d6b127e4a0c3323d11640a1127
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-03 13:38:57 +0530

    Update bigf.py

--- userbot/plugins/bigf.py
+++ userbot/plugins/bigf.py
@@ -6,10 +6,8 @@ import random
 import logging
 from userbot.utils import admin_cmd
 
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-async def _(event):
-           input_str = event.pattern_match.group(1)
-           if input_str == "kf":                              
+@borg.on(admin_cmd(outgoing=True, pattern="kf$(.*)"))
+async def _(event):                             
                  r = random.randint(0, 3)
                  logger.debug(r)
                  if r == 0:
