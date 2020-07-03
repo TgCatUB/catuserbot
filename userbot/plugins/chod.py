1fc1b066ffabc367fed723b1da6e6905bfdc4ea9
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-03 14:29:55 +0530

    Update chod.py

--- userbot/plugins/chod.py
+++ userbot/plugins/chod.py
@@ -1,24 +1,11 @@
-"""Emoji
 
-Available Commands:
-
-.emoji shrug
-
-.emoji apple
-
-.emoji :/
-
-.emoji -_-"""
 
 from telethon import events
 
 import asyncio
+from userbot.utils import admin_cmd
 
-
-
-
-
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(admin_cmd(pattern=f"chod$", outgoing=True))
 
 async def _(event):
 
@@ -30,13 +17,7 @@ async def _(event):
 
     animation_ttl = range(0, 11)
 
-    input_str = event.pattern_match.group(1)
-
-    if input_str == "chod":
-
-        await event.edit(input_str)
-
-        animation_chars = [
+    animation_chars = [
         
             "`Randi Founded`",
             "`Your Mom Is Going To Fuck`",
@@ -49,15 +30,15 @@ async def _(event):
             "`Fucking Your Mom\n\n\nYour Mom's Pussy Get Red\nCumming On Pussy\n\nAlmost Done...\n\nFucked Percentage... 84%\n█████████████████████▒▒▒▒ `",
             "`Fucking Your Mom\n\n\nYour Mom's Pussy Get Red\nCumming On Pussy\n\nAlmost Done...\n\nFucked Percentage... 100%\n█████████████████████████ `",
             "`Fucking Your Mom\n\n\nYour Mom's Pussy Get Red\nCumming On Pussy\n\nYour mom get Pregnant\n\nResult: Now You Have 1 More Younger Brother `"
-        ]
+            ]
 
-        for i in animation_ttl:
+    for i in animation_ttl:
 
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 11])
 
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(admin_cmd(pattern=f"sqh$", outgoing=True))
 
 async def _(event):
 
@@ -69,13 +50,8 @@ async def _(event):
 
     animation_ttl = range(0, 11)
 
-    input_str = event.pattern_match.group(1)
 
-    if input_str == "sqh":
-
-        await event.edit(input_str)
-
-        animation_chars = [
+    animation_chars = [
         
             "`Downloading File..`",
             "`File Downloaded....`",
@@ -88,16 +64,16 @@ async def _(event):
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pru User\nValid Until: 31/12/2099\n\nFile Scanned... 84%\n█████████████████████▒▒▒▒ `",
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pru User\nValid Until: 31/12/2099\n\nFile Scanned... 100%\n█████████████████████████ `",
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pru User\nValid Until: 31/12/2099\n\nTask: 01 of 01 Files Scanned...\n\nReault: No Virus Found...`"
-        ]
+            ]
 
-        for i in animation_ttl:
+     for i in animation_ttl:
 
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 11])
 
 
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(admin_cmd(pattern=f"vquickheal$", outgoing=True))
 
 async def _(event):
 
@@ -109,13 +85,8 @@ async def _(event):
 
     animation_ttl = range(0, 11)
 
-    input_str = event.pattern_match.group(1)
 
-    if input_str == "vquickheal":
-
-        await event.edit(input_str)
-
-        animation_chars = [
+    animation_chars = [
         
             "`Downloading File..`",
             "`File Downloaded....`",
@@ -128,9 +99,9 @@ async def _(event):
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pru User\nValid Until: 31/12/2099\n\nFile Scanned... 84%\n█████████████████████▒▒▒▒ `",
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pru User\nValid Until: 31/12/2099\n\nFile Scanned... 100%\n█████████████████████████ `",
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pru User\nValid Until: 31/12/2099\n\nTask: 01 of 01 Files Scanned...\n\nReault:⚠️Virus Found⚠️\nMore Info: Torzan, Spyware, Adware`"
-        ]
+            ]
 
-        for i in animation_ttl:
+    for i in animation_ttl:
 
             await asyncio.sleep(animation_interval)
 
