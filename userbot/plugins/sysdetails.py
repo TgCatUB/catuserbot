c0ffa76ed02840f5e1808b0cdfd4279d5edb4282
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-04 01:21:55 +0530

    Update sysdetails.py

--- userbot/plugins/sysdetails.py
+++ userbot/plugins/sysdetails.py
@@ -6,14 +6,16 @@ from shutil import which
 from telethon import events
 import asyncio
 from collections import deque
-from userbot.utils import admin_cmd, register
+from userbot.utils import admin_cmd
 from os import remove
 from telethon import version
 from userbot import CMD_HELP, ALIVE_NAME
-
+from datetime import datetime
+from userbot import StartTime
+import time
 
 # ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 # ============================================
 
 
@@ -38,7 +40,13 @@ async def sysdetails(sysd):
         except FileNotFoundError:
             await sysd.edit("`Hello, on catuserbot  install .neofetch first kthx`")
 
-@register(outgoing=True, pattern="^.botver$")
+@borg.on(admin_cmd(pattern="uptime$"))
+async def _(event):
+    uptime = get_readable_time((time.time() - StartTime))
+    OUTPUT = f"**[Cat's](tg://need_update_for_some_feature/) CPU UPTIME:**\n{uptime}"
+    await event.edit(OUTPUT)
+
+@borg.on(admin_cmd(outgoing=True, pattern="botver$"))
 async def bot_ver(event):
     """ For .botver command, get the bot version. """
     if which("git") is not None:
@@ -73,48 +81,6 @@ async def bot_ver(event):
             "Shame that you don't have git, You're running 9.0 - 'Extended' anyway"
         )
 
-
-@register(outgoing=True, pattern="^.pips(?: |$)(.*)")
-async def pipcheck(pip):
-    """ For .pip command, do a pip search. """
-    pipmodule = pip.pattern_match.group(1)
-    if pipmodule:
-        await pip.edit("`Searching . . .`")
-        invokepip = f"pip3 search {pipmodule}"
-        pipc = await asyncrunapp(
-            invokepip,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-
-        stdout, stderr = await pipc.communicate()
-        pipout = str(stdout.decode().strip()) \
-            + str(stderr.decode().strip())
-
-        if pipout:
-            if len(pipout) > 4096:
-                await pip.edit("`Output too large, sending as file`")
-                file = open("output.txt", "w+")
-                file.write(pipout)
-                file.close()
-                await pip.client.send_file(
-                    pip.chat_id,
-                    "output.txt",
-                    reply_to=pip.id,
-                )
-                remove("output.txt")
-                return
-            await pip.edit("**Query: **\n`"
-                           f"{invokepip}"
-                           "`\n**Result: **\n`"
-                           f"{pipout}"
-                           "`")
-        else:
-            await pip.edit("**Query: **\n`"
-                           f"{invokepip}"
-                           "`\n**Result: **\n`No Result Returned/False`")
-    else:
-        await pip.edit("`Use .info pip to see an example`")
 
 
 
