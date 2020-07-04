b703e3bf20915a7da0748292d0e2021e5238faed
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-04 18:26:19 +0530

    Update animation3.py

--- userbot/plugins/animation3.py
+++ userbot/plugins/animation3.py
@@ -25,7 +25,15 @@ async def _(event):
         await asyncio.sleep(0.3)
         await event.edit("EVERyBOdy iZ GangSTur UNtIL I ArRivE 🔥🔥🔥")     
         
-        
+@borg.on(admin_cmd(pattern="heart$"))		
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("❤️🧡💛💚💙💜🖤"))
+	for _ in range(32):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)        
         
 @borg.on(admin_cmd(pattern="gym$", outgoing=True))
 async def _(event):
