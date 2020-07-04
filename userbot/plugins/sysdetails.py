b3657061d2c21e9823caa0e71774305bf03f8463
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-04 12:44:31 +0530

    Update sysdetails.py

--- userbot/plugins/sysdetails.py
+++ userbot/plugins/sysdetails.py
@@ -11,15 +11,89 @@ from os import remove
 from telethon import version
 from userbot import CMD_HELP, ALIVE_NAME
 from datetime import datetime
-from userbot import StartTime
+from userbot import StartTime , def
 import time
-
+from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
+import os
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 # ============================================
 
 
-@borg.on(admin_cmd(pattern=f"sysd", outgoing=True))
+@borg.on(admin_cmd(pattern="cpu$"))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+#    dirname = event.pattern_match.group(1)
+#    tempdir = "localdir"
+    cmd = "cat /proc/cpuinfo | grep 'model name'"
+#    if dirname == tempdir:
+	
+    eply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    o = stdout.decode()
+    OUTPUT = f"**[Cat's](tg://need_update_for_some_feature/) CPU Model:**\n{o}"
+    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "env.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=eply_to_id
+            )
+            await event.delete()
+    else:
+        await event.edit(OUTPUT)
+
+
+@borg.on(admin_cmd(pattern="neofetch$"))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+#    dirname = event.pattern_match.group(1)
+#    tempdir = "localdir"
+    cmd = "git clone https://github.com/dylanaraps/neofetch.git"
+#    if dirname == tempdir:
+	
+    eply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+	
+    stdout, stderr = await process.communicate()
+    o = stdout.decode()
+    OUTPUT = f"Neofetch Installed, Use `.sysd`"
+    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "neofetch.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    else:
+        await event.edit(OUTPUT)         
+
+@borg.on(admin_cmd(pattern=f"sysd$", outgoing=True))
 async def sysdetails(sysd):
     """ a. """
     if not sysd.text[0].isalpha() and sysd.text[0] not in ("/", "#", "@", "!"):
@@ -43,7 +117,7 @@ async def sysdetails(sysd):
 #uptime idea and credits was from @Sur_vivor
 @borg.on(admin_cmd(pattern="uptime$"))
 async def _(event):
-    uptime = get_readable_time((time.time() - StartTime))
+    uptime = def.get_readable_time((time.time() - StartTime))
     OUTPUT = f"**[Cat's](tg://need_update_for_some_feature/) CPU UPTIME:**\n{uptime}"
     await event.edit(OUTPUT)
 
@@ -85,15 +159,15 @@ async def bot_ver(event):
 
 
 
-
 CMD_HELP.update(
     {"sysdetails": 
      ".sysd\
     \nUsage: Shows system information using neofetch.\
+    \n\n`.uptime`\
+    \nUsage:shows the uptime of your cpu\
     \n\n.botver\
     \nUsage: Shows the userbot version. \
-    \n\n.pip <module(s)>\
-    \nUsage: Does a search of pip modules(s).\
+    
     "
     })
 
