6159a6f76685cedce163771e319b4d1e1dad757f
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-04 17:42:51 +0530

    Update sysdetails.py

--- userbot/plugins/sysdetails.py
+++ userbot/plugins/sysdetails.py
@@ -10,7 +10,8 @@ from userbot.utils import admin_cmd, register
 from os import remove
 from telethon import version
 from userbot import CMD_HELP, ALIVE_NAME
-
+from userbot import StartTime
+import time
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
@@ -37,8 +38,15 @@ async def sysdetails(sysd):
             await sysd.edit("Neofetch Result: `" + result + "`")
         except FileNotFoundError:
             await sysd.edit("`Hello, on catuserbot  install .neofetch first kthx`")
+            
+#uptime idea and credits was from @Sur_vivor
+@borg.on(admin_cmd(pattern="uptime$"))
+async def _(event):
+    uptime = get_readable_time((time.time() - StartTime))
+    OUTPUT = f"**[Sᴜʀᴠɪᴠᴏʀ's Cat](tg://need_update_for_some_feature/) CPU UPTIME:**\n{uptime}"
+    await event.edit(OUTPUT)            
 
-@register(outgoing=True, pattern="^.botver$")
+@borg.on(admin_cmd(outgoing=True, pattern="botver$"))
 async def bot_ver(event):
     """ For .botver command, get the bot version. """
     if which("git") is not None:
@@ -73,8 +81,34 @@ async def bot_ver(event):
             "Shame that you don't have git, You're running 9.0 - 'Extended' anyway"
         )
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
 
-@register(outgoing=True, pattern="^.pips(?: |$)(.*)")
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+@borg.on(admin_cmd(outgoing=True, pattern="pip$"))
 async def pipcheck(pip):
     """ For .pip command, do a pip search. """
     pipmodule = pip.pattern_match.group(1)
@@ -114,7 +148,7 @@ async def pipcheck(pip):
                            f"{invokepip}"
                            "`\n**Result: **\n`No Result Returned/False`")
     else:
-        await pip.edit("`Use .info pip to see an example`")
+        await pip.edit("`Use .plinfo pip to see an example`")
 
 
 
