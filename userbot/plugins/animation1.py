debc7b97e8237329322b2ee76629fc989bf92ad4
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-06 14:49:00 +0530

    Update animation1.py

--- userbot/plugins/animation1.py
+++ userbot/plugins/animation1.py
@@ -201,8 +201,8 @@ async def _(event):
 	if event.fwd_from:
 		return
 	deq = deque(list("🍦🍧🍩🍪🎂🍰🧁🍫🍬🍭"))
-	for _ in range(999):
-		await asyncio.sleep(0.4)
+	for _ in range(48):
+		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
 		deq.rotate(1)
 		
