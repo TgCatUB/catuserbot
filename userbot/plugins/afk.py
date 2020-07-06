475839205bef37567fe540e0b699912eb0f571b8
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-06 11:03:03 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from telethon import events
 from telethon.tl import functions, types
 from userbot import CMD_HELP
-
+from userbot.utils import admin_cmd
 
 global USER_AFK  # pylint:disable=E0602
 global afk_time  # pylint:disable=E0602
