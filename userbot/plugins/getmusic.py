d2ce520faf64c476130bd7e43700ff4305fca39c
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-06 15:27:34 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -5,45 +5,49 @@ import io
 import asyncio
 import time
 from userbot.utils import admin_cmd
-from userbot import CMD_HELP
 import glob
 import os
-import instantmusic , subprocess    
+try:
+ import instantmusic , subprocess
+except:
+ os.system("pip install instantmusic")
+ 
 
-@borg.on(admin_cmd(pattern="song(?: |$)(.*)"))
-async def _(event):
+
+os.system("rm -rf *.mp3")
+
+
+def bruh(name):
     
+    os.system("instantmusic -q -s "+name)
+    
+
+@borg.on(admin_cmd(pattern="song ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    reply = await event.get_reply_message()
-    if event.pattern_match.group(1):
-        query = event.pattern_match.group(1)
-        await event.edit("wi8..! I am finding your song....`")
-    elif reply.message:
-        query = reply.message
-        await event.edit("wi8..! I am finding your song....`")
-    else:
-    	await event.edit("`What I am Supposed to find `")
-    	return
-    os.system("instantmusic -q -s "+str(query))
+    await event.edit("ok finding the song")    
+    bruh(str(cmd))
     l = glob.glob("*.mp3")
     loa = l[0]
-    await event.edit("yeah..! i found something wi8..🥰")
+    await event.edit("sending song")
     await borg.send_file(
                 event.chat_id,
                 loa,
                 force_document=True,
                 allow_cache=False,
-                caption=query,
+                caption=cmd,
                 reply_to=reply_to_id
             )
-    await event.delete()
     os.system("rm -rf *.mp3")
     subprocess.check_output("rm -rf *.mp3",shell=True)
-    
-    
-    
+            
 CMD_HELP.update({"getmusic":
     "`.song` query or `.song` reply to song name :\
     \nUSAGE:finds the song you entered in query and sends it"
