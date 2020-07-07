9e325cb6059f65ea2405a868746d847ad91bc5ce
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-07 16:00:28 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -1,49 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
 from telethon import events
 import subprocess
 from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
 import io
 import asyncio
-import time
 from userbot.utils import admin_cmd
 import glob
-import os
-try:
- import instantmusic , subprocess
-except:
- os.system("pip install instantmusic")
- 
-
-
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
-    
-    os.system("instantmusic -q -s "+name)
-    
-
-@borg.on(admin_cmd(pattern="song ?(.*)"))
+import os  
+from userbot import catdef, CMD_HELP
+@borg.on(admin_cmd(pattern="song(?: |$)(.*)"))
 async def _(event):
-    if event.fwd_from:
-        return
-    DELAY_BETWEEN_EDITS = 0.3
-    PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
+    
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("ok finding the song")    
-    bruh(str(cmd))
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        await event.edit("wi8..! I am finding your song....`")
+    elif reply.message:
+        query = reply.message
+        await event.edit("wi8..! I am finding your song....`")
+    else:
+    	await event.edit("`What I am Supposed to find `")
+    	return
+    
+    catdef.catmusic(str(query),"320k")
     l = glob.glob("*.mp3")
     loa = l[0]
-    await event.edit("sending song")
+    await event.edit("yeah..! i found something wi8..🥰")
     await borg.send_file(
                 event.chat_id,
                 loa,
                 force_document=True,
                 allow_cache=False,
-                caption=cmd,
+                caption=query,
                 reply_to=reply_to_id
             )
+    await event.delete()
     os.system("rm -rf *.mp3")
     subprocess.check_output("rm -rf *.mp3",shell=True)
