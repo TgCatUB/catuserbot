7b498e58c0ed1d70b303c18fa00615cc2782fd50
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-07 12:44:14 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -8,8 +8,7 @@ import asyncio
 from userbot.utils import admin_cmd , sudo_cmd
 import glob
 import os  
-from userbot import catdef
-
+from userbot import catdef, CMD_HELP
 @borg.on(admin_cmd(pattern="song(?: |$)(.*)"))
 async def _(event):
     
