29cb81df14a1d64853b9be47757aca2051949122
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-07 16:16:46 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,28 +8,40 @@ from userbot.utils import admin_cmd
 from telethon import version
 from platform import python_version, uname
 
-ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
-if ALIVE_PIC is None:
-  CAT_IMG = "https://telegra.ph/file/4117e39218991cdd7379f.jpg"
-else:
-  CAT_IMG = ALIVE_PIC
-
-
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@Sur_vivor"
 
-cat_caption = "🚴‍♂️**MY BOT IS RUNNING SUCCESFULLY**\n\n"
-cat_caption += f"⏳`Telethon Version:` **{version.__version__}**\n"
-cat_caption += f"⏳`Python Version:` **{python_version()}**\n"
-cat_caption += "⏳`Database Status :` Databases Functioning Normally!\n"
-cat_caption += "💠**Always With You, My Masters!**\n"
-cat_caption += "⏳`Modified by :` [✰Sᴀͥʀᴀͣᴛͫʜ™️✰](http://t.me/Sur_vivor)\n"
-cat_caption += "⏳`Created by :` Snapdragon, Anubis, Sandeep\n"
-cat_caption += f"⏳`Owner Name :` {DEFAULTUSER}\n\n"
-cat_caption += "**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
-
+ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
+CAT_IMG = ALIVE_PIC
 
 @borg.on(admin_cmd(outgoing=True, pattern="alive$"))
 async def amireallyalive(alive):
-    """ For .alive command, check if the bot is running."""    
-    await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption)
-    await alive.delete()
+    if alive.fwd_from:
+        return
+    reply_to_id = alive.message
+    if alive.reply_to_msg_id:
+        reply_to_id = await alive.get_reply_message()
+
+    if CAT_IMG:
+         cat_caption = "🚴‍♂️**MY BOT IS RUNNING SUCCESFULLY**\n\n"
+         cat_caption += f"⏳`Telethon Version:` **{version.__version__}**\n"
+         cat_caption += f"⏳`Python Version:` **{python_version()}**\n"
+         cat_caption += "⏳`Database Status :` Databases Functioning Normally!\n"
+         cat_caption += "💠**Always With You, My Masters!**\n"
+         cat_caption += "⏳`Modified by :` [✰Sᴀͥʀᴀͣᴛͫʜ™️✰](http://t.me/Sur_vivor)\n"
+         cat_caption += "⏳`Created by :` Snapdragon, Anubis, Sandeep\n"
+         cat_caption += f"⏳`Owner Name :` {DEFAULTUSER}\n\n"
+         cat_caption += "**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
+         await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption)
+         await alive.delete()
+    else:
+      """ For .alive command, check if the bot is running.  """
+        await alive.edit("🚴‍♂️**MY BOT IS RUNNING SUCCESFULLY**\n\n"
+                         f"⏳`Telethon Version:` **{version.__version__}**\n"
+                         f"⏳`Python Version:` **{python_version()}**\n"
+                         "⏳`Database Status :` Databases Functioning Normally!\n"
+                         "💠**Always With You, My Masters!**\n"
+                         "⏳`Modified by :` [✰Sᴀͥʀᴀͣᴛͫʜ™️✰](http://t.me/Sur_vivor)\n"
+                         "⏳`Created by :` Snapdragon, Anubis, Sandeep\n"
+                         f"⏳`Owner Name :` {DEFAULTUSER}\n\n"
+                         "**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
+                        )
