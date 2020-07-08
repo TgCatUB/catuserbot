f7bb5bddc80ef46a65570f6de43370065fe63f59
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-08 13:10:47 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -23,121 +23,24 @@ if Var.STRING_SESSION:
 else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
-"""
-# PaperPlaneExtended Support Vars
-ENV = os.environ.get("ENV", False)
-"""
-#PPE initialization.
-"""
-
-# Bot Logs setup:
-if bool(ENV):
-    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
-
-    if CONSOLE_LOGGER_VERBOSE:
-        basicConfig(
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-            level=DEBUG,
-        )
-    else:
-        basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                    level=INFO)
-    LOGS = getLogger(__name__)
-
-    # Check if the config was edited by using the already used variable.
-    # Basically, its the 'virginity check' for the config file ;)
-    CONFIG_CHECK = os.environ.get(
-        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
-
-    if CONFIG_CHECK:
-        LOGS.info(
-            "Please remove the line mentioned in the first hashtag from the config.env file"
-        )
-        quit(1)
-
-    # Logging channel/group configuration.
-    BOTLOG_CHATID = os.environ.get("BOTLOG_CHATID", None)
-    try:
-        BOTLOG_CHATID = int(BOTLOG_CHATID)
-    except:
-        pass
-
-    # Userbot logging feature switch.
-    BOTLOG = sb(os.environ.get("BOTLOG", "True"))
-
-    # Heroku Credentials for updater.
-    HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    # Console verbose logging
-    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
     
-    # SQL Database URI
-    DB_URI = os.environ.get("DATABASE_URL", None)
-
-    # OCR API key
-    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
-    LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", "False"))
-
-    # remove.bg API key
-    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-
-    # Chrome Driver and Headless Google Chrome Binaries
-    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
-    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
-
-    # OpenWeatherMap API Key
-    OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-
-    # Youtube API key
-    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
-    GENIUS_API_TOKEN = os.environ.get("GENIUS", None)
-    # Genius lyrics get this value from https://genius.com/developers both has same values
-    GENIUS = os.environ.get("GENIUS_API_TOKEN", None)
-    # Default .alive name
-    ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
-    AUTONAME = os.environ.get("AUTONAME", None)
-    LESS_SPAMMY = os.environ.get("LESS_SPAMMY", True)
-    MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 5))
-    UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL",
-    "https://github.com/sandy1709/catuserbot.git")
-    
-    # Time & Date - Country and Time Zone
-    COUNTRY = str(os.environ.get("COUNTRY", ""))
-
-    TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
-
-    # Clean Welcome
-    CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
-
-    # Last.fm Module
-    BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
-    DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
-
-    LASTFM_API = os.environ.get("LASTFM_API", None)
-    LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
-    LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
-    LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
-    LASTFM_PASS = md5(LASTFM_PASSWORD_PLAIN)
-    if LASTFM_API and LASTFM_SECRET and LASTFM_USERNAME and LASTFM_PASS:
+UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL","https://github.com/sandy1709/catuserbot.git")
+ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
+AUTONAME = os.environ.get("AUTONAME", None)
+DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
+LASTFM_API = os.environ.get("LASTFM_API", None)
+LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
+LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
+LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
+LASTFM_PASS = md5(LASTFM_PASSWORD_PLAIN)
+if LASTFM_API and LASTFM_SECRET and LASTFM_USERNAME and LASTFM_PASS:
         lastfm = LastFMNetwork(api_key=LASTFM_API,
                                api_secret=LASTFM_SECRET,
                                username=LASTFM_USERNAME,
                                password_hash=LASTFM_PASS)
-    else:
+else:
         lastfm = None
 
-    # Google Drive Module
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-    G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY",
-                                         "./DOWNLOADS/")
-else:
-    # Put your ppe vars here if you are using local hosting
-    PLACEHOLDER = None """
 
 # Setting Up CloudMail.ru and MEGA.nz extractor binaries,
 # and giving them correct perms to work properly.
