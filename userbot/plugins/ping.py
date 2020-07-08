2aa214c42cf9fe5eeb91673165d1f61ba88db807
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-08 19:47:48 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -5,7 +5,7 @@ from telethon import events
 from datetime import datetime
 from userbot.utils import admin_cmd
 from userbot import CMD_HELP
-from userbot import StartTime
+from userbot import StartTime, catdef
 import time
 import asyncio
 
@@ -62,37 +62,9 @@ async def _(event):
     await event.edit("🏓Ping!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    uptime = get_readable_time((time.time() - StartTime))
+    uptime = catdef.get_readable_time((time.time() - StartTime))
     await event.edit(f"🏓Pong!\nPing Speed: {ms}\nUserbot Uptime: {uptime}")
-    
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time    
-    
-    
+        
 CMD_HELP.update({
     "ping":
     "`.fping`\
