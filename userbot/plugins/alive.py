d1145fbd624d2e77f13d935be3c5e9742b8b1feb
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-08 10:14:48 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -4,7 +4,7 @@ import asyncio
 from telethon import events
 from userbot import StartTime , catdef
 from platform import uname
-from userbot import CMD_HELP, ALIVE_NAME 
+from userbot import CMD_HELP, ALIVE_NAME, catdef
 from userbot.utils import admin_cmd,sudo_cmd
 from telethon import version
 from platform import python_version, uname
@@ -18,14 +18,15 @@ async def amireallyalive(alive):
     if alive.fwd_from:
         return
     reply_to_id = alive.message
+    uptime = catdef.get_readable_time((time.time() - StartTime))
     if alive.reply_to_msg_id:
         reply_to_id = await alive.get_reply_message()
-        
     if CAT_IMG:
          cat_caption  = "**MY BOT IS RUNNING SUCCESFULLY**\n\n"
          cat_caption += "☞Database Status: Databases functioning normally!\n\n"   
          cat_caption += f"**☞Telethon version:** `{version.__version__}\n`"
          cat_caption += f"**☞Python Version:** `{python_version()}\n`"
+         cat_caption += f"**uptime :** `{uptime}\n`"
          cat_caption += "☞Always with you, my master!\n"
          cat_caption += f"**☞My peru owner:** {DEFAULTUSER}\n"
          cat_caption += "[Deploy Catuserbot Now](https://github.com/sandy1709/catuserbot)" 
@@ -37,6 +38,7 @@ async def amireallyalive(alive):
                          "☞Database Status: Databases functioning normally!\n\n" 
                          f"**☞Telethon version:** `{version.__version__}\n`"
                          f"**☞Python Version:** `{python_version()}\n`"
+                         f"**uptime :** `{uptime}\n`"
                          "☞Always with you, my master!\n"
                          f"**☞My peru owner:** {DEFAULTUSER}\n"
                          "[Deploy Catuserbot Now](https://github.com/sandy1709/catuserbot)"
@@ -47,10 +49,12 @@ async def amireallyalive(alive):
 async def _(event):
     if event.fwd_from:
         return
+    uptime = catdef.get_readable_time((time.time() - StartTime))
     await event.reply(" SUDO COMMANDS ARE WORKING PERFECTLY \n\n"
                      f"☞Telethon version: {version.__version__}\n"
                      f"☞Python: {python_version()}\n"
                      f"☞My peru owner: {DEFAULTUSER}\n"
+                     f"**uptime :** `{uptime}\n`"
                      #"Deploy this userbot Now"
                     )
        
