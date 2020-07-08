f06b62ad90c127b4182e82847c8b7a30aee5dcdd
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-08 11:56:01 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,10 +1,10 @@
 """Check if userbot alive or not . """
 
-import asyncio
+import asyncio , time
 from telethon import events
 from userbot import StartTime , catdef
 from platform import uname
-from userbot import CMD_HELP, ALIVE_NAME, catdef
+from userbot import CMD_HELP, ALIVE_NAME, catdef , catversion
 from userbot.utils import admin_cmd,sudo_cmd
 from telethon import version
 from platform import python_version, uname
@@ -22,28 +22,28 @@ async def amireallyalive(alive):
     if alive.reply_to_msg_id:
         reply_to_id = await alive.get_reply_message()
     if CAT_IMG:
-         cat_caption  = "**MY BOT IS RUNNING SUCCESFULLY**\n\n"
-         cat_caption += "☞Database Status: Databases functioning normally!\n"   
-         cat_caption += f"**☞Telethon version:** `{version.__version__}\n`"
-         cat_caption += f"**☞Python Version:** `{python_version()}\n`"
-         cat_caption += f"**uptime :** `{uptime}\n\n`"
-         cat_caption += "☞Always with you, my master!\n"
-         cat_caption += f"**☞My peru owner:** {DEFAULTUSER}\n"
-         cat_caption += "[Deploy Catuserbot Now](https://github.com/sandy1709/catuserbot)" 
+         cat_caption  = f"**MY BOT IS RUNNING SUCCESFULLY**\n\n"
+         cat_caption += "**☞Database Status: Databases functioning normally!\n**"   
+         cat_caption += f"☞Telethon version : `{version.__version__}\n`"
+         cat_caption += f"☞Catuserbot Version : `{catversion}`\n"
+         cat_caption += f"☞Python Version : `{python_version()}\n\n`"
+         cat_caption += f"**cat🐱 is always with you, my master!\n**"
+         cat_caption += f"☞My peru Master: {DEFAULTUSER}\n"
+         cat_caption += f"☞uptime : `{uptime}\n`"
+         cat_caption +=	f"**☞Click **[here](https://github.com/sandy1709/catuserbot) to deply catuserbot"
          await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id)
          await alive.delete()
     else:
-        """ For .alive command, check if the bot is running.  """
-        await alive.edit("**MY BOT IS RUNNING SUCCESFULLY**\n\n"
-                         "☞Database Status: Databases functioning normally!\n" 
-                         f"**☞Telethon version:** `{version.__version__}\n`"
-                         f"**☞Python Version:** `{python_version()}\n`"
-                         f"**uptime :** `{uptime}\n\n`"
-                         "☞Always with you, my master!\n"
-                         f"**☞My peru owner:** {DEFAULTUSER}\n"
-                         "[Deploy Catuserbot Now](https://github.com/sandy1709/catuserbot)"
-                        )
-         
+        await alive.edit(f"**MY BOT IS RUNNING SUCCESFULLY**\n\n"
+                         "**Database Status: Databases functioning normally!\n**" 
+                         f"☞Telethon version : `{version.__version__}\n`"
+						 f"☞Catuserbot Version : `{catversion}`\n"
+                         f"☞Python Version : `{python_version()}\n\n`"
+                         "**cat🐱 is always with you, my master!\n**"
+                         f"☞My peru Master: {DEFAULTUSER}\n"
+                         f"☞uptime : `{uptime}\n`"
+                         f"**☞Click **[here](https://github.com/sandy1709/catuserbot) to deply catuserbot"
+                        )         
 
 @borg.on(sudo_cmd(pattern="sudo", allow_sudo=True))
 async def _(event):
