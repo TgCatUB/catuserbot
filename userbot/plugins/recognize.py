36e8a2084f16f9d589484040f24f60fc8fcbc3e9
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-09 22:28:47 +0530

    Create recognize.py

---
+++ userbot/plugins/recognize.py
@@ -0,0 +1,86 @@
+#credits: @Mr_Hops
+
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot.utils import admin_cmd, sudo_cmd
+from userbot import CMD_HELP
+
+@borg.on(admin_cmd(pattern="recognize ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("Reply to any user's media message.")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit("reply to media file")
+       return
+    chat = "@Rekognition_Bot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("Reply to actual users message.")
+       return
+    cat = await event.edit("Processing this shit!")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461083923))
+              await event.client.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("unblock @Rekognition_Bot and try again")
+              await cat.delete()
+              return
+          if response.text.startswith("See next message."):
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461083923))
+              response = await response
+              await event.client.send_message(event.chat_id, response.message, reply = event.reply_to_msg_id)
+              await cat.delete()
+          else:
+              await event.client.send_message(event.chat_id, response.message, reply = event.reply_to_msg_id)
+              await cat.delete()
+              
+@borg.on(sudo_cmd(pattern="recognize ?(.*)" ,allow_sudo = True ))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.reply("Reply to any user's media message.")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.reply("reply to media file")
+       return
+    chat = "@Rekognition_Bot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.reply("Reply to actual users message.")
+       return
+    cat = await event.reply("recognizeing this media")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461083923))
+              await event.client.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("unblock @Rekognition_Bot and try again")
+              await cat.delete()
+              return
+          if response.text.startswith("See next message."):
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461083923))
+              response = await response
+              await event.client.send_message(event.chat_id, response.message , reply = event.reply_to_msg_id)
+              await cat.delete()
+          else:
+              await event.client.send_message(event.chat_id, response.message , reply = event.reply_to_msg_id) 
+              await cat.delete()
+
+CMD_HELP.update({
+    "recognize":
+    "`.recognize` reply this to any media file\
+    \nUSAGE : Get information about an image using AWS Rekognition.\ 
+     Find out information including detected labels, faces. text and \
+     moderation tags." 
+})
