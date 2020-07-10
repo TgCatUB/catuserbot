35e28dfa790e4296e0c75d72d4d40406296dc8d8
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-10 23:57:37 +0530

    Update functions.py

--- userbot/helpers/functions.py
+++ userbot/helpers/functions.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from asyncio import sleep
 from random import choice
 from telethon import events
-#from userbot import bot
+from emoji import get_emoji_regexp
 
 def get_readable_time(seconds: int) -> str:
     count = 0
@@ -66,20 +66,6 @@ def catmusicvideo(cat):
     os.system(command)
 
 # for stickertxt
-EMOJI_PATTERN = re.compile(
-    "["
-    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    "\U0001F300-\U0001F5FF"  # symbols & pictographs
-    "\U0001F600-\U0001F64F"  # emoticons
-    "\U0001F680-\U0001F6FF"  # transport & map symbols
-    "\U0001F700-\U0001F77F"  # alchemical symbols
-    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
-    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
-    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-    "\U0001FA00-\U0001FA6F"  # Chess Symbols
-    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-    "\U00002702-\U000027B0"  # Dingbats 
-    "]+")
 
 async def waifutxt(text, chat_id ,reply_to_id , bot, borg):
     animus = [0, 1, 2, 3, 4, 7, 9, 15, 20, 22, 27, 29, 32, 33, 34, 37, 38, 
@@ -92,7 +78,7 @@ async def waifutxt(text, chat_id ,reply_to_id , bot, borg):
         await borg.send_file(int(chat_id) , cat , reply_to = reply_to_id ) 
         await cat.delete()
 
-def deEmojify(inputString: str) -> str:
-    """Remove emojis and other non-safe characters from string"""
-    return re.sub(EMOJI_PATTERN, '', inputString)
+def deEmojify(inputString):
+    """ Remove emojis and other non-safe characters from string """
+    return get_emoji_regexp().sub(u'', inputString)
     
