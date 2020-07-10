590a2e0c947ee8ebfe5f7613b6c601b0a4389c50
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-10 23:06:58 +0530

    Update getsongs.py

--- userbot/plugins/getsongs.py
+++ userbot/plugins/getsongs.py
@@ -8,7 +8,7 @@ import asyncio
 from userbot.utils import admin_cmd , sudo_cmd
 import glob
 import os  
-from userbot import catdef, CMD_HELP
+from userbot import CMD_HELP
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeVideo
