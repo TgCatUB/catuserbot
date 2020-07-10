2fcfe7179bcc8c8f3083d63f224e3aa8d70a0693
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-10 22:40:26 +0530

    Update functions.py

--- userbot/helpers/functions.py
+++ userbot/helpers/functions.py
@@ -1,6 +1,9 @@
-import requests
+import requests , os, re
 from bs4 import BeautifulSoup
-import os
+from asyncio import sleep
+from random import choice
+from telethon import events
+from userbot import bot
 
 def get_readable_time(seconds: int) -> str:
     count = 0
@@ -61,3 +64,35 @@ def catmusicvideo(cat):
     video_link =  'http://www.youtube.com/'+video_link
     command = ('youtube-dl -f "[filesize<20M]" ' +video_link)  
     os.system(command)
+
+# for stickertxt
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats 
+    "]+")
+
+async def waifutxt(text, chat_id ,reply_to_id):
+    animus = [0, 1, 2, 3, 4, 7, 9, 15, 20, 22, 27, 29, 32, 33, 34, 37, 38, 
+              41, 42, 44, 45, 47, 48, 51, 52, 53, 55, 56, 57, 58, 61, 62, 63]
+    sticcers = await bot.inline_query(
+        "stickerizerbot", f"#{choice(animus)}{(deEmojify(text))}")
+    cat = await sticcers[0].click( "me" ,
+                            hide_via=True)
+    if cat:
+        await borg.send_file(int(chat_id) , cat , reply_to = reply_to_id ) 
+        await cat.delete()
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(EMOJI_PATTERN, '', inputString)
+    
