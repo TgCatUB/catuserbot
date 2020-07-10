33ea2be3f34f853c197b1262359d8e16d8e52627
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-11 01:03:12 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -5,36 +5,37 @@ import subprocess
 from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
 import io
 import asyncio
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd , sudo_cmd
 import glob
 import os  
-from userbot import catdef, CMD_HELP
+from userbot import CMD_HELP  , catdef
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeVideo
 
-
 @borg.on(admin_cmd(pattern="song(?: |$)(.*)"))
 async def _(event):
-    
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     reply = await event.get_reply_message()
     if event.pattern_match.group(1):
         query = event.pattern_match.group(1)
-        await event.edit("wi8..! I am finding your song....`")
+        await event.edit("wi8..! I am finding your song....")
     elif reply.message:
         query = reply.message
-        await event.edit("wi8..! I am finding your song....`")
+        await event.edit("wi8..! I am finding your song....")
     else:
     	await event.edit("`What I am Supposed to find `")
     	return
     
     catdef.catmusic(str(query),"320k")
     l = glob.glob("*.mp3")
-    loa = l[0]
-    await event.edit("yeah..! i found something wi8..🥰")
+    if l:
+        await event.edit("yeah..! i found something wi8..🥰")
+    else:
+        await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    loa = l[0]    
     await borg.send_file(
                 event.chat_id,
                 loa,
@@ -45,7 +46,7 @@ async def _(event):
             )
     await event.delete()
     os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3",shell=True)
+    subprocess.check_output("rm -rf *.mp3",shell=True)		      
     
 @borg.on(admin_cmd(pattern="videosong(?: |$)(.*)"))
 async def _(event):
