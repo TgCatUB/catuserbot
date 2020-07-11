c46c9da7da289cead01a75b7bf2ef95b99675c6b
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-11 23:32:13 +0530

    Update scrapers.py

--- userbot/plugins/scrapers.py
+++ userbot/plugins/scrapers.py
@@ -38,19 +38,16 @@ from telethon.tl.types import DocumentAttributeAudio
 from userbot.utils import progress, humanbytes, time_formatter
 from userbot.uniborgConfig import Config
 
+LANG = "en"
 CARBONLANG = "auto"
 TTS_LANG = "en"
 TRT_LANG = "en"
-
 BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
 BOTLOG = True
 
-LANG = "en"
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="krb(?: |$)(.*)"))
+@borg.on(admin_cmd(outgoing=True, pattern="krb"))
 async def carbon_api(e):
-   await e.edit("`Processing..`")
+   await e.edit("Processing..")
    CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
    global CARBONLANG
    textx = await e.get_reply_message()
@@ -65,9 +62,9 @@ async def carbon_api(e):
    elif textx:
          pcode = str(textx.message)
          skeme = None # Importing message to module
-   pcode = deEmojify(pcode)   
+   pcode = deEmojify(pcode)       
    code = quote_plus(pcode) # Converting to urlencoded
-   await e.edit("`Meking Carbon...\n25%`")
+   await e.edit("Meking Carbon...\n25%")
    url = CARBON.format(code=code, lang=CARBONLANG)
    chrome_options = Options()
    chrome_options.add_argument("--headless")
@@ -80,7 +77,7 @@ async def carbon_api(e):
    chrome_options.add_experimental_option('prefs', prefs)
    driver = webdriver.Chrome(executable_path=CHROME_DRIVER, options=chrome_options)
    driver.get(url)
-   await e.edit("`Be Patient...\n50%`")
+   await e.edit("Be Patient...\n50%")
    download_path = './'
    driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
    params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
@@ -97,13 +94,13 @@ async def carbon_api(e):
    driver.find_element_by_id("export-menu").click()
    driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
    driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-   await e.edit("`Processing..\n75%`")
+   await e.edit("Processing..\n75%")
    # Waiting for downloading
    sleep(2.5)
    color_name = driver.find_element_by_xpath('/html/body/div[1]/main/div[3]/div[2]/div[1]/div[1]/div/span[2]/input').get_attribute('value')
-   await e.edit("`Done Dana Done...\n100%`")
+   await e.edit("Done Dana Done...\n100%")
    file = './carbon.png'
-   await e.edit("`Uploading..`")
+   await e.edit("Uploading..")
    await e.client.send_file(
          e.chat_id,
          file,
@@ -115,7 +112,7 @@ async def carbon_api(e):
    driver.quit()
    # Removing carbon.png after uploading
    await e.delete() # Deleting msg
-    
+   
 @borg.on(admin_cmd(outgoing=True, pattern=r"wiki (.*)"))
 async def wiki(wiki_q):
     """ For .wiki command, fetch content from Wikipedia. """
