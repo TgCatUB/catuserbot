a63520b9efa25591c9ebb9b484dffba05a3c064c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-13 23:08:04 +0530

    Update functions.py

--- userbot/helpers/functions.py
+++ userbot/helpers/functions.py
@@ -80,9 +80,24 @@ async def waifutxt(text, chat_id ,reply_to_id , bot, borg):
         await borg.send_file(int(chat_id) , cat , reply_to = reply_to_id ) 
         await cat.delete()
 
-async def deEmojify(inputString):
-    """ Remove emojis and other non-safe characters from string """
-    return get_emoji_regexp().sub(u'', inputString)
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats 
+    "]+")
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(EMOJI_PATTERN, '', inputString)
     
 # for nekobot
 async def trumptweet(text):
