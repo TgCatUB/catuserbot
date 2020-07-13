0edb547f9866933a17e5d0fee154e009a4ecb64e
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-13 22:17:26 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -2,3 +2,7 @@ from userbot import catdef
 
 
 deEmojify = catdef.deEmojify
+trumptweet = catdef.trumptweet 
+changemymind = catdef.changemymind
+kannagen = catdef.kannagen
+moditweet = catdef.moditweet
