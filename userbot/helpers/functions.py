42a652a797156f248717e44a19473e38bfae6d2f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-15 01:19:18 +0530

    Update functions.py

--- userbot/helpers/functions.py
+++ userbot/helpers/functions.py
@@ -37,7 +37,7 @@ async def get_readable_time(seconds: int) -> str:
 
 #for getmusic
 
-async def catmusic(cat,DEFAULT_AUDIO_QUALITY):
+async def catmusic128(cat):
   search = cat
   headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
   html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
@@ -48,9 +48,22 @@ async def catmusic(cat,DEFAULT_AUDIO_QUALITY):
         video_link = link.get('href') 
         break
   video_link =  'http://www.youtube.com/'+video_link
-  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' +DEFAULT_AUDIO_QUALITY + ' ' + video_link)	
+  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' +"128" + ' ' + video_link)	
   os.system(command)
 
+async def catmusic320(cat):
+  search = cat
+  headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+  html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
+  soup = BeautifulSoup(html, 'html.parser')
+  for link in soup.find_all('a'):
+    if '/watch?v=' in link.get('href'):
+        # May change when Youtube Website may get updated in the future.
+        video_link = link.get('href') 
+        break
+  video_link =  'http://www.youtube.com/'+video_link
+  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' +"320" + ' ' + video_link)	
+  os.system(command)
 #for getmusicvideo
 
 async def catmusicvideo(cat):
