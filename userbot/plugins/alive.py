4b0cd970551ffaf86b9fdd5da584c1b3882821ff
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-14 16:03:32 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -32,7 +32,7 @@ async def amireallyalive(alive):
          cat_caption += f"⏳`Cat Uptime:` **{uptime}**\n\n"         
          cat_caption += f"**Cat is Always With You, My Masters!**\n"
          cat_caption += f"⏳`Owner Name:` {DEFAULTUSER}\n"   
-         cat_caption += f"⏳`Modified by:` **Sur_vivor**\n\n"         
+         cat_caption += f"⏳`Modified by:` [Sur_vivor](t.me/Sur_vivor)\n\n"         
          cat_caption += f"**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
          await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption)
          await alive.delete()
@@ -45,7 +45,7 @@ async def amireallyalive(alive):
                          f"⏳`Cat Uptime:` **{uptime}**\n\n"                        
                          "**Cat is Always With You, My Masters!**\n"                                                
                          f"⏳`Owner Name:` {DEFAULTUSER}\n"
-                         "⏳`Modified by:` **Sur_vivor**\n\n"
+                         "⏳`Modified by:` [Sur_vivor](t.me/Sur_vivor)\n\n"
                          f"**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
                         )
 
