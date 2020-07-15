63182b12f66515ab414c07f22c74e1172dac2ded
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-15 13:44:03 +0530

    Update animation5.py

--- userbot/plugins/animation5.py
+++ userbot/plugins/animation5.py
@@ -267,4 +267,4 @@ async def _(event):
       ]	
     for i in animation_ttl:	
             await asyncio.sleep(animation_interval)	
-            await event.edit(animation_chars[i % 1])            
+            await event.edit(animation_chars[i % 6])            
