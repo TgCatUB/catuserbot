df6f70dac8f0ef9e775b95fbf7b3158352682e66
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-16 15:06:06 +0530

    Update alanautodp.py

--- userbot/plugins/alanautodp.py
+++ userbot/plugins/alanautodp.py
@@ -35,10 +35,10 @@ async def autopic(event):
             place_holder = None   
         shutil.copy(downloaded_file_name, photo)
         im = Image.open(photo)
-        current_time = datetime.now().strftime(" @Sur_vivor \n\n %H:%M:%S \n %d/%m/%y")
+        current_time = datetime.now().strftime("@Sur_vivor \n\n  %H:%M:%S \n %d/%m/%y")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 40)
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 35)
         drawn_text.text((10,40), current_time, font=fnt, fill=(255,0,0))
         img.save(photo)
         file = await event.client.upload_file(photo)  # pylint:disable=E0602
