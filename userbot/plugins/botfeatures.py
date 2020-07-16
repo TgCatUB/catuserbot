3ab3b9a58f6b7e47afb808046336c0e3bd178259
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-16 13:52:38 +0530

    Update botfeatures.py

--- userbot/plugins/botfeatures.py
+++ userbot/plugins/botfeatures.py
@@ -5,7 +5,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from uniborg.util import admin_cmd
 
-@borg.on(admin_cmd(pattern="purl ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="purl ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return 
@@ -27,7 +27,7 @@ async def _(event):
           await event.delete()
           await event.client.send_message(event.chat_id, response.message, reply_to=reply_message)
 
-@borg.on(admin_cmd(pattern="sgm ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="sgm ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return 
@@ -52,7 +52,7 @@ async def _(event):
           await event.delete()
           await event.client.send_message(event.chat_id, response.message, reply_to=reply_message)
 
-@borg.on(admin_cmd(pattern="reader ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="reader ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return 
@@ -104,3 +104,25 @@ async def _(event):
               return
           await event.delete()
           await borg.send_file(event.chat_id, response.message.media)
+
+@borg.on(admin_cmd(pattern="insta ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    input_str = event.pattern_match.group(1)
+    reply_message = await event.get_reply_message()
+    chat = "@Regrambot"
+    await event.edit("```Checking...```")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=274562699))
+              await event.client.send_message(chat, "{}".format(input_str))
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Unblock @Regrambot```")
+              return
+          if response.text.startswith("I can't find that"):
+             await event.edit("😐")
+          else: 
+             await event.delete()
+             await event.client.send_file(event.chat_id, response.message)     
