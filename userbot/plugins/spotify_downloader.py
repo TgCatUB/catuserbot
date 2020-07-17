65fc4e697c1bc5c58bd7d4565d791a832098501f
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-17 23:20:00 +0530

    Update spotify_downloader.py

--- userbot/plugins/spotify_downloader.py
+++ userbot/plugins/spotify_downloader.py
@@ -18,10 +18,16 @@ async def _(event):
           await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
           try:
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              await bot.send_message(chat, link)
+              msg = await bot.send_message(chat, link)
               respond = await response
+              res = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              r = await res
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError:
               await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
               return
-          await event.delete()
           await bot.forward_messages(event.chat_id, respond.message)
+    await event.client.delete_messages(conv.chat_id,
+                                       [msg.id, r.id, respond.id])
+    await event.delete()
