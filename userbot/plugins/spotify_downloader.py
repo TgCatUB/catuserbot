5b934080c8011f35c56094fb2ad8eb67a0b3f3fc
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-17 22:58:56 +0530

    Update spotify_downloader.py

--- userbot/plugins/spotify_downloader.py
+++ userbot/plugins/spotify_downloader.py
@@ -1,5 +1,4 @@
-""" Spotify downloader plugin by @Sur_vivor | Syntax: .smd <name>"""
-import datetime
+""" Spotify downloader plugin by @Sur_vivor | Syntax: .spd <name>"""
 import asyncio
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError, UserAlreadyParticipantError
@@ -7,7 +6,7 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern="smd ?(.*)"))
+@borg.on(admin_cmd(outgoing=True, pattern="spd(?: |$)(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -19,16 +18,10 @@ async def _(event):
           await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
           try:
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              msg = await bot.send_message(chat, link)
+              await bot.send_message(chat, link)
               respond = await response
-              res = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              r = await res
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError:
               await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
               return
+          await event.delete()
           await bot.forward_messages(event.chat_id, respond.message)
-    await event.client.delete_messages(conv.chat_id,
-                                       [msg.id, r.id, respond.id])
-    await event.delete()
