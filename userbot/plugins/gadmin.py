4b0eb5728552dd9a0abe5084e64e9ba2e4042e60
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-18 12:43:12 +0530

    Update gadmin.py

--- userbot/plugins/gadmin.py
+++ userbot/plugins/gadmin.py
@@ -152,7 +152,7 @@ async def startgmute(event):
     private = False
     if event.fwd_from:
         return
-    elif event.is_private:
+    if event.is_private:
         await event.edit("Unexpected issues or ugly errors may occur!")
         await asyncio.sleep(3)
         private = True   
@@ -189,7 +189,7 @@ async def endgmute(event):
     private = False
     if event.fwd_from:
         return
-    elif event.is_private:
+    if event.is_private:
         await event.edit("Unexpected issues or ugly errors may occur!")
         await asyncio.sleep(3)
         private = True  
