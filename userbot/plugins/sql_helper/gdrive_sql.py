aae17fa19482189eedf5efc8266b3b3470b46fe3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-19 13:25:59 +0530

    Update gdrive_sql.py

--- userbot/plugins/sql_helper/gdrive_sql.py
+++ userbot/plugins/sql_helper/gdrive_sql.py
@@ -6,23 +6,26 @@ except ImportError:
 
 class Gdrive(BASE):
     __tablename__ = "gdrive"
-    folderid = Column(String , primary_key=True)
-
-    def __init__(self, folderid):
+    folderid = Column(String(50) , primary_key=True)
+    name = Column(String(127))
+    
+    def __init__(self, folderid, name = ""):
         self.folderid = str(folderid)
-
+        self.name = name
+        
+        
 Gdrive.__table__.create(checkfirst=True)
 
 def is_folder(folderid):
     try:
-        return SESSION.query(Gdrive).filter(Gdrive.folderid == str(folderid))
+        return SESSION.query(Gdrive).filter(Gdrive.folderid == str(folderid)).one()
     except:
         return None
     finally:
         SESSION.close()
 
-def gparent_id(folderid):
-    adder = Gdrive(str(folderid))
+def gparent_id(folderid , name):
+    adder = Gdrive(str(folderid), str(name))
     SESSION.add(adder)
     SESSION.commit()
 
