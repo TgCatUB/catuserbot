ed87de6e5e73cf7141ec27edce583c61176322a6
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-19 12:49:39 +0530

    Update gdrive_sql.py

--- userbot/plugins/sql_helper/gdrive_sql.py
+++ userbot/plugins/sql_helper/gdrive_sql.py
@@ -3,28 +3,28 @@ from userbot.plugins.sql_helper import SESSION, BASE
 
 class Gdrive(BASE):
     __tablename__ = "gdrive"
-    chatid = Column(String(50), primary_key=True)
+    chat_id = Column(String(50), primary_key=True)
 
-    def __init__(self, chatid):
-        self.chatid = str(chatid)
+    def __init__(self, chat_id):
+        self.chat_id = str(chat_id)
 
 Gdrive.__table__.create(checkfirst=True)
 
-def is_folder(chatid):
+def is_folder(chat_id):
     try:
-        return SESSION.query(Gdrive).filter(Gdrive.chatid == str(chatid)).one()
+        return SESSION.query(Gdrive).filter(Gdrive.chat_id == str(chat_id)).one()
     except:
         return None
     finally:
         SESSION.close()
 
-def gparent_id(chatid):
-    adder = Gdrive(str(chatid))
+def gparent_id(chat_id):
+    adder = Gdrive(str(chat_id))
     SESSION.add(adder)
     SESSION.commit()
 
-def rmparent_id(chatid):
-    rem = SESSION.query(Gdrive).get(str(chatid))
+def rmparent_id(chat_id):
+    rem = SESSION.query(Gdrive).get(str(chat_id))
     if rem:
         SESSION.delete(rem)
         SESSION.commit()
