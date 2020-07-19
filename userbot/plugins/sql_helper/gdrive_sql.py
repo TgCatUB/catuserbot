37ee7622fc8efad6c8f0aab91b4d9712616816a0
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-19 19:14:15 +0530

    Update gdrive_sql.py

--- userbot/plugins/sql_helper/gdrive_sql.py
+++ userbot/plugins/sql_helper/gdrive_sql.py
@@ -4,28 +4,30 @@ from userbot.plugins.sql_helper import SESSION, BASE
 
 class Gdrive(BASE):
     __tablename__ = "gdrive"
-    cat= Column(String(50), primary_key=True)
+    cat= Column(String(5), primary_key=True)
+    catid = Column(String(50))
 
-    def __init__(self,cat):
+    def __init__(self,cat,catid):
         self.cat = cat
+        self.catid = catid
 
 
 Gdrive.__table__.create(checkfirst=True)
 
 
-def is_folder(folder_id):
+def is_folder(hmm):
     try:
-        return SESSION.query(Gdrive).filter(Gdrive.cat == str(folder_id)).one()
+        return SESSION.query(Gdrive).filter(Gdrive.cat == str(hmm)).one()
     except:
         return None
     finally:
         SESSION.close()
 
 
-def gparent_id(folder_id):
-    adder = SESSION.query(Gdrive).get(folder_id)
+def gparent_id(hmm , folder_id):
+    adder = SESSION.query(Gdrive).get(hmm)
     if not adder:
-        adder = Gdrive(folder_id)
+        adder = Gdrive(hmm , folder_id)
     SESSION.add(adder)
     SESSION.commit()
     
@@ -37,8 +39,8 @@ def get_parent_id():
     finally:
         SESSION.close()
 
-def rmparent_id(folder_id):
-    note = SESSION.query(Gdrive).filter(Gdrive.cat == folder_id)
+def rmparent_id(hmm):
+    note = SESSION.query(Gdrive).filter(Gdrive.cat == hmm)
     if note:
         note.delete()
         SESSION.commit()
