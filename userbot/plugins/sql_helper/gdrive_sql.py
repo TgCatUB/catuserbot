7a9639907b4738c2883c9544d19977f41474a7ce
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-19 16:58:20 +0530

    Update gdrive_sql.py

--- userbot/plugins/sql_helper/gdrive_sql.py
+++ userbot/plugins/sql_helper/gdrive_sql.py
@@ -2,39 +2,42 @@ from sqlalchemy import Column, String
 from userbot.plugins.sql_helper import SESSION, BASE
 
 
-class GDrive(BASE):
+class Gdrive(BASE):
     __tablename__ = "gdrive"
-    folderid = Column(String(40), primary_key=True)
+    chat_id = Column(String(14), primary_key=True)
+    folder_id = Column(String(50), primary_key=True)
 
-    def __init__(self, folderid):
-        self.folderid = folderid
+    def __init__(self, chat_id, folder_id=""):
+        self.chat_id = chat_id
+        self.folder_id = folder_id
 
-GDrive.__table__.create(checkfirst=True)
+
+Gdrive.__table__.create(checkfirst=True)
 
 
-def is_folder(folderid):
+def is_folder(folder_id):
     try:
-        return SESSION.query(GDrive).filter(GDrive.folderid == str(folderid)).one()
+        return SESSION.query(Gdrive).filter(Gdrive.folder_id == str(folder_id)).one()
     except:
         return None
     finally:
         SESSION.close()
 
 
-def gparent_id(folderid):
-    adder = GDrive(str(folderid))
+def gparent_id (folder_id):
+    adder = Gdrive(str(folder_id))
     SESSION.add(adder)
     SESSION.commit()
 
 
-def rmparent_id(folderid):
-    rem = SESSION.query(GDrive).get(str(folderid))
+def rmparent_id(folder_id)):
+    rem = SESSION.query(Gdrive).get(str(folder_id))
     if rem:
         SESSION.delete(rem)
         SESSION.commit()
 
 
 def get_parent_id():
-    rem = SESSION.query(GDrive).all()
+    rem = SESSION.query(Gdrive).all()
     SESSION.close()
     return rem
