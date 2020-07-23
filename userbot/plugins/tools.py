dfaae04da020704a9db3bdecf1c8321193e02898
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-23 18:51:23 +0530

    Update and rename xkcd.py to tools.py

--- userbot/plugins/tools.py
+++ userbot/plugins/tools.py
@@ -12,7 +12,41 @@ import barcode
 from barcode.writer import ImageWriter
 from bs4 import BeautifulSoup
 from userbot import CMD_HELP
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
 
+@borg.on(admin_cmd(pattern="scan ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit("```reply to a media message```")
+       return
+    chat = "@DrWebBot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit(" `Sliding my tip, of fingers over it`")
+    async with borg.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=161163358))
+              await borg.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+              return
+          if response.text.startswith("Forward"):
+             await event.edit("```can you kindly disable your forward privacy settings for good?```")
+          else:
+          	if response.text.startswith("Select"):
+          		await event.edit("`Please go to` @DrWebBot `and select your language.`") 
+          	else: 
+          			await event.edit(f"**Antivirus scan was completed. I got dem final results.**\n {response.message.message}")
 
 @borg.on(admin_cmd(pattern=r"decode$", outgoing=True))
 async def parseqr(qr_e):
@@ -183,7 +217,9 @@ Year: {}""".format(img, input_str, xkcd_link, safe_title, alt, day, month, year)
 
 CMD_HELP.update({
     'tools':
-    ".makeqr <content>\
+    ".scan reply to media or file\
+\nUSEAGE: it scans the media or file and checks either any virus is in the file or media\
+\n\n.makeqr <content>\
 \nUSAGE: Make a QR Code from the given content.\
 \nExample: .makeqr www.google.com\
 \n\n.barcode <content>\
