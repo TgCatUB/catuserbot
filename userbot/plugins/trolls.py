2010ce7e2c883467941a68084397cffea46d9a86
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-24 22:01:11 +0530

    Create trolls.py

---
+++ userbot/plugins/trolls.py
@@ -0,0 +1,42 @@
+from telegraph import upload_file, exceptions
+from userbot.utils import admin_cmd
+from . import *
+
+@borg.on(admin_cmd(pattern = "threats(?: |$)(.*)"))
+async def catbot(catmemes):
+    replied = await catmemes.get_reply_message()
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if not replied:
+        await catmemes.edit("reply to a supported media file")
+        return
+    if replied.media:
+        await catmemes.edit("passing to telegraph...")
+    else:
+        await catmemes.edit("reply to a supported media file")
+        return
+    download_location = await borg.download_media(replied , Config.TMP_DOWNLOAD_DIRECTORY)
+    if download_location.endswith((".webp")):
+        download_location = convert_toimage(download_location)  
+    size = os.stat(download_location).st_size    
+    if download_location.endswith((".jpg", ".jpeg", ".png", ".bmp", ".ico")):
+        if size > 5242880:
+            await catmemes.edit("the replied file size is not supported it must me below 5 mb")
+            os.remove(download_location)
+            return 
+        await catmemes.edit("generating image..")
+    else:
+        await catmemes.edit("the replied file is not supported") 
+        os.remove(download_location)  
+        return    
+    try:
+        response = upload_file(download_location)
+        os.remove(download_location)
+    except exceptions.TelegraphException as exc:
+        await catmemes.edit("ERROR: " + str(exc))
+        os.remove(download_location)
+        return
+    cat = f"https://telegra.ph{response[0]}"
+    cat = await threats(cat)
+    await catmemes.delete()
+    await borg.send_file(catmemes.chat_id , cat,reply_to=replied)
