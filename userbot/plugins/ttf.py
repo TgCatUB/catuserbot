3e8cfc5ad565abaacf48a87481cc7f8b75214c47
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-27 01:58:27 +0530

    Update ttf.py

--- userbot/plugins/ttf.py
+++ userbot/plugins/ttf.py
@@ -6,9 +6,14 @@ import os
 @borg.on(admin_cmd(pattern="ttf ?(.*)"))
 async def get(event):
     name = event.text[5:]
+    if name is None:
+        await event.edit("reply to text message as `.ttf <file name>`")
     m = await event.get_reply_message()
-    with open(name, "w") as f:
-        f.write(m.message)
-    await event.delete()
-    await borg.send_file(event.chat_id,name,force_document=True)
-    os.remove(name)
+    if m.text:
+        with open(name, "w") as f:
+            f.write(m.message)
+        await event.delete()
+        await borg.send_file(event.chat_id,name,force_document=True)
+        os.remove(name)
+    else:
+        await event.edit("reply to text message as `.ttf <file name>`")
