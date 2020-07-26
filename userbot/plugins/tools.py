5812cc4f581da2029682ef1d9e384018612ff4ae
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-27 02:13:32 +0530

    Update tools.py

--- userbot/plugins/tools.py
+++ userbot/plugins/tools.py
@@ -20,33 +20,33 @@ async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
-       return
+        await event.edit("```Reply to any user message.```")
+        return
     reply_message = await event.get_reply_message() 
     if not reply_message.media:
-       await event.edit("```reply to a media message```")
-       return
+        await event.edit("```reply to a media message```")
+        return
     chat = "@DrWebBot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
-       return
+        await event.edit("```Reply to actual users message.```")
+        return
     await event.edit(" `Sliding my tip, of fingers over it`")
     async with borg.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=161163358))
-              await borg.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
-              return
-          if response.text.startswith("Forward"):
-             await event.edit("```can you kindly disable your forward privacy settings for good?```")
-          else:
-          	if response.text.startswith("Select"):
-          		await event.edit("`Please go to` @DrWebBot `and select your language.`") 
-          	else: 
-          			await event.edit(f"**Antivirus scan was completed. I got dem final results.**\n {response.message.message}")
+        try:
+            response = conv.wait_event(events.NewMessage(incoming=True,from_users=161163358))
+            await borg.forward_messages(chat, reply_message)
+            response = await response 
+        except YouBlockedUserError: 
+            await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+            return
+        if response.text.startswith("Forward"):
+            await event.edit("```can you kindly disable your forward privacy settings for good?```")
+        else:
+            if response.text.startswith("Select"):
+                await event.edit("`Please go to` @DrWebBot `and select your language.`") 
+            else: 
+                await event.edit(f"**Antivirus scan was completed. I got dem final results.**\n {response.message.message}")
 
 @borg.on(admin_cmd(pattern=r"decode$", outgoing=True))
 async def parseqr(qr_e):
@@ -228,4 +228,4 @@ CMD_HELP.update({
 \n\n**Note**: use .decode <reply to barcode/qrcode> to get decoded content.\
 \n\n`.xkcd` <query>\
 \nUSAGE:Searches for the query for the relevant XKCD comic "
-})        
+})
