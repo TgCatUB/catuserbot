4d89315cc3da0728b8d320f8b0d9e12a687120ea
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-28 02:10:52 +0530

    Update markdown.py

--- userbot/plugins/markdown.py
+++ userbot/plugins/markdown.py
@@ -141,7 +141,7 @@ async def reparse(event):
         peer=await event.get_input_chat(),
         id=event.message.id,
         message=message,
-        no_webpage=not bool(event.message.media),
+        no_webpage=False,
         entities=msg_entities
     ))
     raise events.StopPropagation
