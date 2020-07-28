61e7428bd64b1258079ad4a15cdb3167cc05e49d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-28 22:01:27 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,5 +1,11 @@
 from userbot import catdef
+from pySmartDL import SmartDL
+from PIL import Image, ImageDraw, ImageFont
+import shutil
 
+#thumb image
+downloaded_file_name = "./DOWNLOADS/thumb_image.jpg"
+downloader = SmartDL(Config.THUMB_IMAGE, downloaded_file_name, progress_bar=False)
 deEmojify = catdef.deEmojify
 trumptweet = catdef.trumptweet 
 changemymind = catdef.changemymind
