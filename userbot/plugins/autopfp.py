93c8e502926e3fd41faafeb7e5bf1f103786e198
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-07-28 18:14:19 +0530

    Update autopfp.py

--- userbot/plugins/autopfp.py
+++ userbot/plugins/autopfp.py
@@ -10,18 +10,24 @@ from userbot.utils import admin_cmd
 import asyncio
 from time import sleep
 
-COLLECTION_STRING = [
+COLLECTION_STRING1 = [
   "awesome-batman-wallpapers",
   "batman-arkham-knight-4k-wallpaper",
   "batman-hd-wallpapers-1080p",
   "the-joker-hd-wallpaper",
   "dark-knight-joker-wallpaper"
 ]
+COLLECTION_STRING2 = [
+  "thor-wallpapers",
+  "thor-wallpaper",
+  "thor-iphone-wallpaper",
+  "thor-wallpaper-hd"
+]
 
-async def animepp():
+async def animeppbat():
     os.system("rm -rf donot.jpg")
-    rnd = random.randint(0, len(COLLECTION_STRING) - 1)
-    pack = COLLECTION_STRING[rnd]
+    rnd = random.randint(0, len(COLLECTION_STRING1) - 1)
+    pack = COLLECTION_STRING1[rnd]
     pc = requests.get("http://getwallpapers.com/collection/" + pack).text
     f = re.compile('/\w+/full.+.jpg')
     f = f.findall(pc)
@@ -31,13 +37,37 @@ async def animepp():
         urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
     urllib.request.urlretrieve(fy,"donottouch.jpg")
     
+async def animeppthor():
+    os.system("rm -rf donot.jpg")
+    rnd = random.randint(0, len(COLLECTION_STRING2) - 1)
+    pack = COLLECTION_STRING2[rnd]
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+    f = re.compile('/\w+/full.+.jpg')
+    f = f.findall(pc)
+    fy = "http://getwallpapers.com"+random.choice(f)
+    print(fy)
+    if not os.path.exists("f.ttf"):
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+    urllib.request.urlretrieve(fy,"donottouch.jpg")    
+    
 @borg.on(admin_cmd(pattern="batmanpfp ?(.*)"))
 async def main(event):
-    await event.edit("**Starting batman Profile Pic.") #Owner @NihiNivi
+    await event.edit("Starting batman Profile Pic.") #Owner @NihiNivi
     while True:
-        await animepp()
+        await animeppbat()
         file = await event.client.upload_file("donottouch.jpg")  
         await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
         await event.client(functions.photos.UploadProfilePhotoRequest( file))
         os.system("rm -rf donottouch.jpg")
         await asyncio.sleep(120) #Edit this to your required needs
+
+@borg.on(admin_cmd(pattern="thorpfp ?(.*)"))
+async def main(event):
+    await event.edit("Starting thor Profile Pic.") #Owner @NihiNivi
+    while True:
+        await animeppthor()
+        file = await event.client.upload_file("donottouch.jpg")  
+        await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+        os.system("rm -rf donottouch.jpg")
+        await asyncio.sleep(120) #Edit this to your required needs        
