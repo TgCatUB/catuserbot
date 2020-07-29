c78ef563fb8506c773542bdb8ae5e048a7b88ea9
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-29 13:46:35 +0530

    Update echo.py

--- userbot/plugins/echo.py
+++ userbot/plugins/echo.py
@@ -4,6 +4,7 @@ import io
 from time import time
 from userbot.utils import admin_cmd
 from userbot  import CMD_HELP
+import requests
 from telethon import events, errors
 
 @borg.on(admin_cmd(pattern="addecho"))
@@ -30,8 +31,12 @@ async def echo(cat):
         reply_msg = await cat.get_reply_message()
         user_id = reply_msg.from_id
         chat_id = cat.chat_id
-        remove_echo(user_id , chat_id)
-        await cat.edit("No nearby Mountains to echo")
+        if is_echo(user_id, chat_id):
+            await cat.edit("The user is already enabled with echo ")
+            remove_echo(user_id , chat_id)
+            await cat.edit("No nearby Mountains to echo")
+        else:
+            await cat.edit("The user is not activated with echo")   
     else:
         await cat.edit("Reply To A User's Message to echo his messages")
         
@@ -44,24 +49,21 @@ async def echo(cat):
         output_str = "echo enabled users:\n\n"
         for echos in lsts:
             output_str += f"[User](tg://user?id={echos.user_id}) in chat `{echos.chat_id}`\n"
-        else:
-            output_str = "No echo enabled users "
-        if len(output_str) > Config.MAX_MESSAGE_SIZE_LIMIT:
-            with io.BytesIO(str.encode(output_str)) as out_file:
-                out_file.name = "echos.text"
-                await cat.client.send_file(
-                    cat.chat_id,
-                    out_file,
-                    force_document=True,
-                    allow_cache=False,
-                    caption="Echo enabled users",
-                    )
-        else:
-            await cat.edit(output_str)  
+    else:
+        output_str = "No echo enabled users "
+    if len(output_str) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        key = requests.post('https://nekobin.com/api/documents', json={"content": output_str}).json().get('result').get('key')
+        url = f'https://nekobin.com/{key}'
+        reply_text = f'echo enabled users: [here]({url})'
+        await cat.edit(reply_text)
+    else:
+        await cat.edit(output_str)
       
 @borg.on(events.NewMessage(incoming=True))
 async def samereply(cat):
     if cat.chat_id in Config.UB_BLACK_LIST_CHAT:
         return
     if is_echo(cat.sender_id, cat.chat_id):
-        await cat.reply(cat.message)
+        await asyncio.sleep(2)
+        if cat.message.text or cat.message.sticker:
+            await cat.reply(cat.message)
