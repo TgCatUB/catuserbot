dc389cfa6722c6e435f4f726f798b48b8c1d3c08
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-30 21:48:37 +0530

    Update getsongs.py

--- userbot/plugins/getsongs.py
+++ userbot/plugins/getsongs.py
@@ -56,7 +56,7 @@ async def _(event):
         await event.edit("yeah..! i found something wi8..🥰")
     else:
         await event.edit(f"Sorry..! i can't find anything with `{query}`")
-    thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
+    thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
     if os.path.exists(thumb_image_path):
         thumb = thumb_image_path
     loa = l[0]    
