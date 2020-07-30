d8a5d00e0f7d3dceb9970bc673aec1f79d2a8de6
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-30 13:43:24 +0530

    Update screenshot.py

--- userbot/plugins/screenshot.py
+++ userbot/plugins/screenshot.py
@@ -1,7 +1,7 @@
-"""Take screenshot of any website
-Syntax: .siteshot <Website URL with http://>
-\n `Credits` @amnd33p"""
-
+"""
+`Credits` @amnd33p
+Modified by @mrconfused
+"""
 import io
 import traceback
 from datetime import datetime
@@ -10,7 +10,8 @@ from telethon import events
 from userbot.utils import admin_cmd
 from userbot import CMD_HELP
 import requests
-       
+from validators.url import url
+
 @borg.on(admin_cmd(pattern="ss (.*)"))
 async def _(event):
     if event.fwd_from:
@@ -32,35 +33,39 @@ async def _(event):
         await event.edit("Starting Google Chrome BIN")
         driver = webdriver.Chrome(chrome_options=chrome_options)
         input_str = event.pattern_match.group(1)
+        caturl = url(input_str)
+        if not caturl:
+            await event.edit("the url must be in the format `https://www.google.com`")
+            return
         driver.get(input_str)
         await event.edit("Calculating Page Dimensions")
         height = driver.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
         width = driver.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
-        await event.edit("Painting web-page")
         driver.set_window_size(width + 100, height + 100)
         # Add some pixels on top of the calculated dimensions 
         # for good measure to make the scroll bars disappear
         im_png = driver.get_screenshot_as_png()
         # saves screenshot of entire page
+        await event.edit("Stoppping Chrome Bin")    
         driver.close()
-        await event.edit("Stopping Google Chrome BIN")
         message_id = event.message.id
         if event.reply_to_msg_id:
             message_id = event.reply_to_msg_id
+        end = datetime.now()
+        ms = (end - start).seconds
+        hmm = f"**url : **{input_str}/n**Time :** `{ms} seconds`"
+        await event.delete()
         with io.BytesIO(im_png) as out_file:
             out_file.name = input_str + ".PNG"
             await borg.send_file(
                 event.chat_id,
                 out_file,
-                caption=input_str,
+                caption=hmm,
                 force_document=True,
                 reply_to=message_id,
                 allow_cache=False,
                 silent=True
             )
-        end = datetime.now()
-        ms = (end - start).seconds
-        await event.edit(f"Completed screencapture Process in {ms} seconds")
     except Exception:
         await event.edit(traceback.format_exc())
         
@@ -68,12 +73,17 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
+    start = datetime.now()
     if Config.SCREEN_SHOT_LAYER_ACCESS_KEY is None:
         await event.edit("Need to get an API key from https://screenshotlayer.com/product \nModule stopping!")
         return
     await event.edit("Processing ...")
     sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}&fullpage={}&viewport={}&format={}&force={}"
     input_str = event.pattern_match.group(1)
+    caturl = url(input_str)
+    if not caturl:
+        await event.edit("the url must be in the format `https://www.google.com`")
+        return
     response_api = requests.get(sample_url.format(
         Config.SCREEN_SHOT_LAYER_ACCESS_KEY,
         input_str,
@@ -84,6 +94,9 @@ async def _(event):
     ))
     # https://stackoverflow.com/a/23718458/4723940
     contentType = response_api.headers['content-type']
+    end = datetime.now()
+    ms = (end - start).seconds
+    hmm = f"**url : **{input_str}/n**Time :** `{ms} seconds`"       
     if "image" in contentType:
         with io.BytesIO(response_api.content) as screenshot_image:
             screenshot_image.name = "screencapture.png"
@@ -91,7 +104,7 @@ async def _(event):
                 await borg.send_file(
                     event.chat_id,
                     screenshot_image,
-                    caption=input_str,
+                    caption=hmm,
                     force_document=True,
                     reply_to=event.message.reply_to_msg_id
                 )
@@ -109,4 +122,4 @@ CMD_HELP.update({
     \n\n.scapture <url>\
     \nUsage: Takes a screenshot of a website and sends the screenshot need to set config var for this.\
     \nExample of a valid URL : `https://www.google.com`"
-})        
+})
