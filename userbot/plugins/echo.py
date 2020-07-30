a90fddd46e9fd58ff30423d5230247273a3fa35d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-07-30 23:10:12 +0530

    Hmm 🌚🌚

--- userbot/plugins/echo.py
+++ userbot/plugins/echo.py
@@ -1,3 +1,25 @@
+
+"""
+created by @mrconfused and @sandy1709
+Idea by @BlazingRobonix
+
+"""
+#    Copyright (C) 2020  sandeep.n(π.$)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+ #   You should have received a copy of the GNU Affero General Public License
+ #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from .sql_helper.echo_sql import is_echo , get_all_echos , addecho , remove_echo
 import asyncio
 import io
