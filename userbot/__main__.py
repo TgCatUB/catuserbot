a3ed7bb136dd8ba9fa27a03a2568c0106c35bfed
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-31 10:57:43 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,15 +1,11 @@
 from userbot import bot
 from sys import argv
-import sys
-from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-import os
 from telethon import TelegramClient
 from var import Var
 from userbot.utils import load_module
 from userbot import LOAD_PLUG, LOGS
 from pathlib import Path
-import asyncio
 import telethon.utils
 import userbot._core
 import glob
@@ -19,22 +15,23 @@ async def add_bot(bot_token):
     bot.me = await bot.get_me() 
     bot.uid = telethon.utils.get_peer_id(bot.me)
 
+  
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
-    bot.tgbot = None
+    bot.tgbot = None 
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
-        print("Initiating Inline Bot")
+        LOGS.info("Initiating Inline Bot")
         # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
             "TG_BOT_TOKEN",
             api_id=Var.APP_ID,
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        print("Initialisation finished with no errors")
-        print("Starting Userbot")
+        LOGS.info("Initialisation finished with no errors")
+        LOGS.info("Starting Userbot")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        print("Startup Completed")
+        LOGS.info("Startup Completed")
     else:
         bot.start()
     
@@ -46,11 +43,12 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
         
-LOGS.info("Yay your userbot is officially working.You should owe @Sur_vivor For This Cool Bot.!!!")
+LOGS.info("Yay your userbot is officially working.!!!")
 LOGS.info("Congratulation, now type .alive to see message if bot is live\n"
           "If you need assistance, head to https://t.me/catuserbot_support")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
+    bot.tgbot = None
     bot.run_until_disconnected()
