aa3a5dd50d6dbe4501637af95a74e480dede627e
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-02 18:23:24 +0530

    Update ff_mpeg.py

--- userbot/plugins/ff_mpeg.py
+++ userbot/plugins/ff_mpeg.py
@@ -176,7 +176,6 @@ async def cult_small_video(video_file, output_directory, start_time, end_time):
     t_response = stdout.decode().strip()
     if os.path.lexists(out_put_file_name):
         return out_put_file_name
-    else:
-        logger.info(e_response)
-        logger.info(t_response)
-        return None
+    logger.info(e_response)
+    logger.info(t_response)
+    return None
