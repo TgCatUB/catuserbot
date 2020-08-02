03b3835765784523c00a4773212a451511ac51fd
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-02 19:39:18 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -1,8 +1,6 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
-#
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-#
 """ Userbot module for kanging stickers or making new ones. Thanks @rupansh"""
 
 import io
