f7071b68b748a3ede8c6386d4bc60e6589385a41
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-02 18:39:38 +0530

    Update deepfryer.py

--- userbot/plugins/deepfryer.py
+++ userbot/plugins/deepfryer.py
@@ -1,25 +1,18 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
-#
+
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-#
-
 # Original source for the deepfrying code (used under the following license): https://github.com/Ovyerus/deeppyer
-
 # MIT License
-#
 # Copyright (c) 2017 Ovyerus
-#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,13 +24,10 @@
 
 import io
 from random import randint, uniform
-
 from PIL import Image, ImageEnhance, ImageOps
 from telethon.tl.types import DocumentAttributeFilename
-
 from userbot.utils import admin_cmd
 from telethon import events
-
 
 @borg.on(admin_cmd(pattern="deepfry(?: |$)(.*)", outgoing=True)) 
 async def deepfryer(event):
@@ -47,45 +37,35 @@ async def deepfryer(event):
             raise ValueError
     except ValueError:
         frycount = 1
-
     if event.is_reply:
         reply_message = await event.get_reply_message()
         data = await check_media(reply_message)
-
         if isinstance(data, bool):
             await event.edit("`I can't deep fry that!`")
             return
-    else:
+    if not event.is_reply:
         await event.edit("`Reply to an image or sticker to deep fry it!`")
-        return
-
     # download last photo (highres) as byte array
     await event.edit("`Downloading media…`")
     image = io.BytesIO()
     await event.client.download_media(data, image)
     image = Image.open(image)
-
     # fry the image
     await event.edit("`Deep frying media…`")
     for _ in range(frycount):
         image = await deepfry(image)
-
     fried_io = io.BytesIO()
     fried_io.name = "image.jpeg"
     image.save(fried_io, "JPEG")
     fried_io.seek(0)
-
     await event.reply(file=fried_io)
-
 
 async def deepfry(img: Image) -> Image:
     colours = (
         (randint(50, 200), randint(40, 170), randint(40, 190)),
         (randint(190, 255), randint(170, 240), randint(180, 250))
     )
-
     img = img.copy().convert("RGB")
-
     # Crush image to hell and back
     img = img.convert("RGB")
     width, height = img.width, img.height
@@ -94,20 +74,15 @@ async def deepfry(img: Image) -> Image:
     img = img.resize((int(width ** uniform(0.89, 0.98)), int(height ** uniform(0.89, 0.98))), resample=Image.BICUBIC)
     img = img.resize((width, height), resample=Image.BICUBIC)
     img = ImageOps.posterize(img, randint(3, 7))
-
     # Generate colour overlay
     overlay = img.split()[0]
     overlay = ImageEnhance.Contrast(overlay).enhance(uniform(1.0, 2.0))
     overlay = ImageEnhance.Brightness(overlay).enhance(uniform(1.0, 2.0))
-
     overlay = ImageOps.colorize(overlay, colours[0], colours[1])
-
     # Overlay red and yellow onto main image and sharpen the hell out of it
     img = Image.blend(img, overlay, uniform(0.1, 0.4))
     img = ImageEnhance.Sharpness(img).enhance(randint(5, 300))
-
     return img
-
 
 async def check_media(reply_message):
     if reply_message and reply_message.media:
