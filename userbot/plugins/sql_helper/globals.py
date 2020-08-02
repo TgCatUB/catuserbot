cfa18bc9981d45d7575b29ba9f89f723955c1e27
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-02 18:44:29 +0530

    Update globals.py

--- userbot/plugins/sql_helper/globals.py
+++ userbot/plugins/sql_helper/globals.py
@@ -2,22 +2,17 @@ try:
     from userbot.plugins.sql_helper import SESSION, BASE
 except ImportError:
     raise AttributeError
-
 from sqlalchemy import Column, String, UnicodeText
-
 
 class Globals(BASE):
     __tablename__ = "globals"
     variable = Column(String, primary_key=True, nullable=False)
     value = Column(UnicodeText, primary_key=True, nullable=False)
-
     def __init__(self, variable, value):
         self.variable = str(variable)
         self.value = value
 
-
 Globals.__table__.create(checkfirst=True)
-
 
 def gvarstatus(variable):
     try:
@@ -28,7 +23,6 @@ def gvarstatus(variable):
     finally:
         SESSION.close()
 
-
 def addgvar(variable, value):
     if SESSION.query(Globals).filter(
             Globals.variable == str(variable)).one_or_none():
@@ -37,10 +31,8 @@ def addgvar(variable, value):
     SESSION.add(adder)
     SESSION.commit()
 
-
 def delgvar(variable):
     rem = SESSION.query(Globals).filter(Globals.variable == str(variable))\
         .delete(synchronize_session="fetch")
     if rem:
         SESSION.commit()
-globals.py
