ae29f68b12706bb5353ea7ed44445d3f2990e259
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-02 18:26:36 +0530

    Update shout.py

--- userbot/plugins/shout.py
+++ userbot/plugins/shout.py
@@ -15,20 +15,19 @@ import asyncio
 async def shout(args):
     if args.fwd_from:
         return
-    else:
-        msg = "```"
-        messagestr = args.text
-        messagestr = messagestr[7:]
-        text = " ".join(messagestr)
-        result = []
-        result.append(' '.join([s for s in text]))
-        for pos, symbol in enumerate(text[1:]):
-            result.append(symbol + ' ' + '  ' * pos + symbol)
-        result = list("\n".join(result))
-        result[0] = text[0]
-        result = "".join(result)
-        msg = "\n" + result
-        await args.edit("`"+msg+"`")
+    msg = "```"
+    messagestr = args.text
+    messagestr = messagestr[7:]
+    text = " ".join(messagestr)
+    result = []
+    result.append(' '.join([s for s in text]))
+    for pos, symbol in enumerate(text[1:]):
+        result.append(symbol + ' ' + '  ' * pos + symbol)
+    result = list("\n".join(result))
+    result[0] = text[0]
+    result = "".join(result)
+    msg = "\n" + result
+    await args.edit("`"+msg+"`")
         
 @borg.on(admin_cmd(pattern=f"sadmin", outgoing=True))
 async def _(event):
@@ -54,4 +53,4 @@ async def _(event):
         ]
     for i in animation_ttl:
             await asyncio.sleep(1)
-            await event.edit(animation_chars[i % 13])        
+            await event.edit(animation_chars[i % 13])
