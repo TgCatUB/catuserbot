685a339fef0d8c136186c82f9c1e82f22817938f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-04 00:34:09 +0530

    Update functions.py

--- userbot/helpers/functions.py
+++ userbot/helpers/functions.py
@@ -60,7 +60,7 @@ async def catmusic(cat , QUALITY):
   video_link =  'http://www.youtube.com/'+video_link
   if not os.path.isdir("./temp/"):
         os.makedirs("./temp/")
-  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' + QUALITY + ' ' + video_link)
+  command = ('youtube-dl -o "./temp/%(title)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality ' + QUALITY + ' ' + video_link)
   os.system(command)
   thumb = ("youtube-dl --write-thumbnail --skip-download " + video_link)
   os .system(thumb)
@@ -79,7 +79,7 @@ async def catmusicvideo(cat):
     video_link =  'http://www.youtube.com/'+video_link
     if not os.path.isdir("./temp/"):
         os.makedirs("./temp/")
-    command = ('youtube-dl -f "[filesize<20M]" ' +video_link)  
+    command = ('youtube-dl -o "./temp/%(title)s.%(ext)s" -f "[filesize<20M]" ' +video_link)  
     os.system(command)
     thumb = ("youtube-dl --write-thumbnail --skip-download " + video_link)
     os .system(thumb)
