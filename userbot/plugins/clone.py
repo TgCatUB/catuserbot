7ee297d835619bbb0a17b54804acc8d1ce315f1f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-04 00:24:11 +0530

    Update clone.py

--- userbot/plugins/clone.py
+++ userbot/plugins/clone.py
@@ -6,7 +6,6 @@ Syntax: .clone @username"""
 
 import html
 import os
-from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
@@ -113,37 +112,36 @@ async def get_full_user(event):
                 )
             )
         return replied_user, None
-    else:
-        input_str = None
+    input_str = None
+    try:
+        input_str = event.pattern_match.group(1)
+    except IndexError as e:
+        return None, e
+    if event.message.entities is not None:
+        mention_entity = event.message.entities
+        probable_user_mention_entity = mention_entity[0]
+        if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+            user_id = probable_user_mention_entity.user_id
+            replied_user = await event.client(GetFullUserRequest(user_id))
+            return replied_user, None
         try:
-            input_str = event.pattern_match.group(1)
-        except IndexError as e:
-            return None, e
-        if event.message.entities is not None:
-            mention_entity = event.message.entities
-            probable_user_mention_entity = mention_entity[0]
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            try:
-                user_object = await event.client.get_entity(input_str)
-                user_id = user_object.id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            except Exception as e:
-                return None, e
-        if event.is_private:
-            try:
-                user_id = event.chat_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            except Exception as e:
-                return None, e
-        try:
-            user_object = await event.client.get_entity(int(input_str))
+            user_object = await event.client.get_entity(input_str)
             user_id = user_object.id
             replied_user = await event.client(GetFullUserRequest(user_id))
             return replied_user, None
         except Exception as e:
             return None, e
+    if event.is_private:
+        try:
+            user_id = event.chat_id
+            replied_user = await event.client(GetFullUserRequest(user_id))
+            return replied_user, None
+        except Exception as e:
+            return None, e
+    try:
+        user_object = await event.client.get_entity(int(input_str))
+        user_id = user_object.id
+        replied_user = await event.client(GetFullUserRequest(user_id))
+        return replied_user, None
+    except Exception as e:
+        return None, e
