55b3a7843f25d6b7f0867aa453a9a1f339f1a308
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-05 19:39:44 +0530

    Create quoteshelper.py

---
+++ userbot/helpers/quoteshelper.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+from telethon.tl import types, functions
+from fontTools.ttLib import TTFont 
+from fontTools.unicode import Unicode 
+import emoji
+import textwrap
+import urllib
+import logging
+import random
+import json
+import os
+import re
+
