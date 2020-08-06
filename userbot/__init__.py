02d7337649c4f0dc8b21e0013bc099734fcbacd5
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-06 22:52:43 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -10,8 +10,9 @@ from dotenv import load_dotenv
 from requests import get
 import time
 from pySmartDL import SmartDL
-from userbot.helpers import fonts as fonts ,functions as catdef
-from userbot.helpers import memeshelper as memes
+from .helpers import fonts as fonts ,functions as catdef
+from .helpers import memeshelper as memes
+from .helpers import process as process
 
 StartTime = time.time()
 catversion = "2.6.2"
