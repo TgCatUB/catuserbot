343aa9f02b9f4c11d5d57e7773e5e3d12f20f847
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-08-06 10:06:47 +0530

    Update android.py

--- userbot/plugins/android.py
+++ userbot/plugins/android.py
@@ -105,59 +105,6 @@ async def codename_info(request):
     await request.edit(reply)
 
 
-@borg.on(admin_cmd(outgoing=True, pattern=r"specs(?: |)([\S]*)(?: |)([\s\S]*)"))
-async def devices_specifications(request):
-    """ Mobile devices specifications """
-    textx = await request.get_reply_message()
-    brand = request.pattern_match.group(1).lower()
-    device = request.pattern_match.group(2).lower()
-    if brand and device:
-        pass
-    elif textx:
-        brand = textx.text.split(' ')[0]
-        device = ' '.join(textx.text.split(' ')[1:])
-    else:
-        await request.edit("`Usage: .specs <brand> <device>`")
-        return
-    all_brands = BeautifulSoup(
-        get('https://www.devicespecifications.com/en/brand-more').content,
-        'lxml').find('div', {
-            'class': 'brand-listing-container-news'
-        }).findAll('a')
-    brand_page_url = None
-    try:
-        brand_page_url = [
-            i['href'] for i in all_brands if brand == i.text.strip().lower()
-        ][0]
-    except IndexError:
-        await request.edit(f'`{brand} is unknown brand!`')
-    devices = BeautifulSoup(get(brand_page_url).content, 'lxml') \
-        .findAll('div', {'class': 'model-listing-container-80'})
-    device_page_url = None
-    try:
-        device_page_url = [
-            i.a['href']
-            for i in BeautifulSoup(str(devices), 'lxml').findAll('h3')
-            if device in i.text.strip().lower()
-        ]
-    except IndexError:
-        await request.edit(f"`can't find {device}!`")
-    if len(device_page_url) > 2:
-        device_page_url = device_page_url[:2]
-    reply = ''
-    for url in device_page_url:
-        info = BeautifulSoup(get(url).content, 'lxml')
-        reply = '\n' + info.title.text.split('-')[0].strip() + '\n'
-        info = info.find('div', {'id': 'model-brief-specifications'})
-        specifications = re.findall(r'<b>.*?<br/>', str(info))
-        for item in specifications:
-            title = re.findall(r'<b>(.*?)</b>', item)[0].strip()
-            data = re.findall(r'</b>: (.*?)<br/>', item)[0] \
-                .replace('<b>', '').replace('</b>', '').strip()
-            reply += f'**{title}**: {data}\n'
-    await request.edit(reply)
-
-
 @borg.on(admin_cmd(outgoing=True, pattern=r"twrp(?: |$)(\S*)"))
 async def twrp(request):
     """ get android device twrp """
@@ -195,8 +142,6 @@ CMD_HELP.update({
 \nUsage: Get info about android device codename or model.\
 \n\n.codename <brand> <device>\
 \nUsage: Search for android device codename.\
-\n\n.specs <brand> <device>\
-\nUsage: Get device specifications info.\
 \n\n.twrp <codename>\
 \nUsage: Get latest twrp download for android device."
 })
