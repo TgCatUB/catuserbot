dced944030cca5580e640c4c6e963d77c7943807
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-07 22:45:38 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -216,8 +216,26 @@ async def fuckedd (abusehard):
         reply_text = memes.ABUSEHARD_STRING[index]
         await abusehard.edit(reply_text)
 			  
+RUNSREACTS = [
+    "`Congratulations and BRAVO!`",
+    "`You did it! So proud of you!`",
+    "`This calls for celebrating! Congratulations!`",
+    "`I knew it was only a matter of time. Well done!`",
+    "`Congratulations on your well-deserved success.`",
+    "`Heartfelt congratulations to you.`",
+    "`Warmest congratulations on your achievement.`",
+    "`Congratulations and best wishes for your next adventure!”`",
+    "`So pleased to see you accomplishing great things.`",
+    "`Feeling so much joy for you today. What an impressive achievement!`",
+]
 
-			  
+@borg.on(admin_cmd(pattern="congo"))
+async def _(event):
+    if event.fwd_from:
+         return
+    bro = random.randint(0, len(RUNSREACTS) - 1)    
+    reply_text = RUNSREACTS[bro]
+    await event.edit(reply_text)			  
 			  
 @borg.on(admin_cmd(outgoing=True, pattern="abusehim$"))
 async def abusing (abused):
