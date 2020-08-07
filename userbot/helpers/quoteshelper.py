78931d63a44ed6996403c050df59b04f3c1ce9dc
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-07 19:05:52 +0530

    Update quoteshelper.py

--- userbot/helpers/quoteshelper.py
+++ userbot/helpers/quoteshelper.py
@@ -177,7 +177,7 @@ async def process(msg, user, client, reply, replied=None):
             os.remove(sticker)
             return True, canvas
         elif reply.document and not reply.audio:
-            docname = ".".join(reply.document.attributes[-1].file_name.split(".")[:-1])
+            docname = ".".join(reply.document.DocumentAttributeFilename.file_name.split(".")[:-1])
             doctype = reply.document.DocumentAttributeFilename.file_name.split(".")[-1].upper()
             if reply.document.size < 1024:
                 docsize = str(reply.document.size) + " Bytes"
