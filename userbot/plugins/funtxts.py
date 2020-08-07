403e7c20b7597ba62733b347d8715a0756bbffcf
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-07 23:32:51 +0530

    Update funtxts.py

--- userbot/plugins/funtxts.py
+++ userbot/plugins/funtxts.py
@@ -3,3 +3,7 @@ from ..utils import admin_cmd
 
 @borg.on(admin_cmd(pattern = "reactcat$")
 async def hmm(cat):
+    if cat.fwd_from:
+        return
+    reactcat = nekos.textcat()
+    await cat.edit(reactcat)
