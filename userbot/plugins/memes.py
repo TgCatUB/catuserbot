56d5f5ee40b94c344e70da1b0b7f56abd5b812d7
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-07 21:47:57 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -43,6 +43,32 @@ async def kek(keks):
         for i in range(1, 15):
             time.sleep(0.3)
             await keks.edit(":" + uio[i % 2])
+		
+
+@borg.on(admin_cmd(pattern="coin ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    r = random.randint(1, 100)
+    input_str = event.pattern_match.group(1)
+    if input_str:
+        input_str = input_str.lower()
+    if r % 2 == 1:
+        if input_str == "heads":
+            await event.edit("The coin landed on: **Heads**. \n You were correct.")
+        elif input_str == "tails":
+            await event.edit("The coin landed on: **Heads**. \n You weren't correct, try again ...")
+        else:
+            await event.edit("The coin landed on: **Heads**.")
+    elif r % 2 == 0:
+        if input_str == "tails":
+            await event.edit("The coin landed on: **Tails**. \n You were correct.")
+        elif input_str == "heads":
+            await event.edit("The coin landed on: **Tails**. \n You weren't correct, try again ...")
+        else:
+            await event.edit("The coin landed on: **Tails**.")
+    else:
+        await event.edit("¯\_(ツ)_/¯")
 			  
 @borg.on(admin_cmd(pattern=r"slap(?: |$)(.*)", outgoing=True))
 async def who(event):
