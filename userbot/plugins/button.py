8ba4850010c84cb9b3a581108d2dcdd268465794
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-08-12 18:12:59 +0530

    Update button.py

--- userbot/plugins/button.py
+++ userbot/plugins/button.py
@@ -5,6 +5,7 @@ import re
 from telethon import custom
 from userbot.utils import admin_cmd
 from telethon import events
+from userbot.uniborgConfig import Config
 
 # regex obtained from: https://github.com/PaulSonOfLars/tgbot/blob/master/tg_bot/modules/helper_funcs/string_handling.py#L23
 BTN_URL_REGEX = re.compile(r"(\{([^\[]+?)\}\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
