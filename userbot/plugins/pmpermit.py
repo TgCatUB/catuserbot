5476556fdf3e466c45b2fc8be15f94233c337efd
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-08-12 13:42:50 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -1,6 +1,4 @@
 #Pmpermit_pic by @Sur_vivor
-import os
-import time
 import asyncio
 import io
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
@@ -8,12 +6,13 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon import events, errors, functions, types
 from userbot import ALIVE_NAME, CMD_HELP
 from userbot.utils import admin_cmd
+from . import check 
+
 
 PM_WARNS = {}
 PREV_REPLY_MESSAGE = {}
 CACHE = {}
-
-
+PMPERMIT_PIC = Config.PMPERMIT_PIC
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**Set ALIVE_NAME in config vars in Heroku**"
 USER_BOT_WARN_ZERO = "`You were spamming my sweet master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.`\n**Now GTFO, i'm playing minecraft**"
 
@@ -53,7 +52,7 @@ if Var.PRIVATE_GROUP_ID is not None:
                     rko = await borg.send_message(event.chat_id, bruh)
                     await asyncio.sleep(3)
                     await rko.delete()
-
+                    
     @borg.on(admin_cmd(pattern="disapprove ?(.*)"))
     async def approve_p_m(event):
         if event.fwd_from:
@@ -85,7 +84,7 @@ if Var.PRIVATE_GROUP_ID is not None:
           else:
             if pmpermit_sql.is_approved(chat.id):
                 pmpermit_sql.disapprove(chat.id)
-                await event.edit(" ███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ \n\n**Fuck Off Bitch, Now You Can't Message Me..**[{}](tg://user?id={})".format(firstname, chat.id))
+                await event.edit(" ███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ \n\n**You are blocked. Now You Can't Message Me..**[{}](tg://user?id={})".format(firstname, chat.id))
                 await asyncio.sleep(3)
                 await event.client(functions.contacts.BlockRequest(chat.id))
 
@@ -132,9 +131,8 @@ if Var.PRIVATE_GROUP_ID is not None:
         chat_id = event.from_id
         catid = chat_id
         current_message_text = message_text.lower()
-        WARN_PIC = Config.PMPERMIT_PIC
-        ASCII = (f"[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id={catid})\n\n")
-        USER_BOT_NO_WARN = ("`Hello, This is AntiSpam Security Service⚠️.You have found your way here to my master,`"
+        USER_BOT_NO_WARN = (f"[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id={catid})\n\n"
+                    "`Hello, This is AntiSpam Security Service⚠️.You have found your way here to my master,`"
                     f"{DEFAULTUSER}'s `inbox.\n\n"
                     "Leave your Name,Reason and 10k$ and hopefully you'll get a reply within 2 light years.`⭕️\n\n"
                     "❤️Register Your Request!❤️\nSend /start To Register Your Request!!🔥\n"
@@ -144,12 +142,10 @@ if Var.PRIVATE_GROUP_ID is not None:
             # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
             return
         if event.from_id in CACHE:
-
             sender = CACHE[event.from_id]
         else:
             sender = await bot.get_entity(event.from_id)
             CACHE[event.from_id] = sender
-
         if chat_id == bot.uid:
             # don't log Saved Messages
             return
@@ -158,8 +154,8 @@ if Var.PRIVATE_GROUP_ID is not None:
             return
         if sender.verified:
             # don't log verified accounts
-            return          
-        if any([x in event.raw_text for x in ("/start", "1", "2", "3", "4", "5")]):
+            return
+        if check(event.raw_text):
             return
         if not pmpermit_sql.is_approved(chat_id):
             # pm permit
@@ -168,7 +164,7 @@ if Var.PRIVATE_GROUP_ID is not None:
     async def do_pm_permit_action(chat_id, event):
         if chat_id not in PM_WARNS:
             PM_WARNS.update({chat_id: 0})
-        if PM_WARNS[chat_id] == 5:
+        if PM_WARNS[chat_id] == Config.MAX_FLOOD_IN_P_M_s:
             r = await event.reply(USER_BOT_WARN_ZERO)
             await asyncio.sleep(3)
             await event.client(functions.contacts.BlockRequest(chat_id))
@@ -194,26 +190,33 @@ if Var.PRIVATE_GROUP_ID is not None:
             except:
                 return
         catid = chat_id
-        WARN_PIC = Config.PMPERMIT_PIC
-        ASCII = (f"[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id={catid})\n\n")
-        USER_BOT_NO_WARN = ("`Hello, This is AntiSpam Security Service⚠️.You have found your way here to my master,`"
+        if PMPERMIT_PIC:
+            if Config.CUSTOM_PMPERMIT_TEXT:
+                USER_BOT_NO_WARN = (Config.CUSTOM_PMPERMIT_TEXT + '\n\n' + "**Send** `/start` ** so that my master can decide why you're here.**")
+            else:
+                USER_BOT_NO_WARN = ("`Hello, This is AntiSpam Security Service⚠️.You have found your way here to my master,`"
                     f"{DEFAULTUSER}'s `inbox.\n\n"
                     "Leave your Name,Reason and 10k$ and hopefully you'll get a reply within 2 light years.`⭕️\n\n"
                     "❤️Register Your Request!❤️\nSend /start To Register Your Request!!🔥\n"
                     "⭕️**Now You Are In Trouble So Send** 🔥 `/start` 🔥 **To Start A Valid Conversation!!**⭕️")
-        if WARN_PIC:      
-              r = await event.client.send_file(event.chat_id, WARN_PIC, caption=USER_BOT_NO_WARN)
-        else:    
-             r = await event.reply(ASCII + USER_BOT_NO_WARN)
+            r = await event.reply( USER_BOT_NO_WARN , file = PMPERMIT_PIC)
+        else:
+            if Config.CUSTOM_PMPERMIT_TEXT:
+                USER_BOT_NO_WARN = (Config.CUSTOM_PMPERMIT_TEXT + '\n\n' + "**Send** `/start` ** so that my master can decide why you're here.**")
+            else:    
+                USER_BOT_NO_WARN = (f"[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id={catid})\n\n"
+                      "`Hello, This is AntiSpam Security Service⚠️.You have found your way here to my master,`"
+                      f"{DEFAULTUSER}'s `inbox.\n\n"
+                      "Leave your Name,Reason and 10k$ and hopefully you'll get a reply within 2 light years.`⭕️\n\n"
+                      "❤️Register Your Request!❤️\nSend /start To Register Your Request!!🔥\n"
+                      "⭕️**Now You Are In Trouble So Send** 🔥 `/start` 🔥 **To Start A Valid Conversation!!**⭕️")
+            r = await event.reply(USER_BOT_NO_WARN)
         PM_WARNS[chat_id] += 1
         if chat_id in PREV_REPLY_MESSAGE:
             await PREV_REPLY_MESSAGE[chat_id].delete()
         PREV_REPLY_MESSAGE[chat_id] = r
-
-from userbot.utils import admin_cmd
-import io
-import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
-from telethon import events
+        
+        
 @bot.on(events.NewMessage(incoming=True, from_users=(1118936839)))
 async def hehehe(event):
     if event.fwd_from:
@@ -236,4 +239,4 @@ CMD_HELP.update({
 \n\n.listapproved\
 \nUsage: To list the all approved users.\
 "
-})
+})    
