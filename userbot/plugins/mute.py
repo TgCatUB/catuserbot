bbac4c52322f8dcf56641c89a59b87e48cc1c189
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-08-12 18:12:35 +0530

    Update mute.py

--- userbot/plugins/mute.py
+++ userbot/plugins/mute.py
@@ -2,13 +2,19 @@
 '''
 from userbot.plugins.sql_helper.mute_sql import is_muted, mute, unmute
 import asyncio
-from uniborg.util import admin_cmd
+from uniborg.util import admin_cmd, sudo_cmd
 from telethon.tl.functions.users import GetFullUserRequest
 from userbot import CMD_HELP, bot
 from userbot.uniborgConfig import Config
 
-BOTLOG = True
-BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+if Config.PRIVATE_GROUP_BOT_API_ID is None:
+    BOTLOG = False
+else:
+    BOTLOG = True
+    BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+        
+MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
+UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 
 @borg.on(admin_cmd(pattern="mute ?(\d+)?"))
 async def startmute(event):
@@ -96,3 +102,75 @@ async def endmute(event):
 async def watcher(event):
     if is_muted(event.sender_id, event.chat_id):
         await event.delete()
+        
+@borg.on(sudo_cmd(pattern=r"mute(?: |$)(.*)" ,allow_sudo = True))
+@errors_handler
+async def spider(spdr):
+    # Admin or creator check
+    chat = await spdr.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+    # If not admin and not creator, return
+    if not admin and not creator:
+        await spdr.reply(NO_ADMIN)
+        return
+    user, reason = await get_user_from_event(spdr)
+    if user:
+        pass
+    else:
+        return
+    self_user = await spdr.client.get_me()
+    if user.id == self_user.id:
+        await spdr.reply(f"Sorry, I can't mute my self")
+        return
+    if mute(spdr.chat_id, user.id) is False:
+        return await spdr.reply(f"Error! User probably already muted.")
+    try:
+        await spdr.client(EditBannedRequest(spdr.chat_id, user.id, MUTE_RIGHTS))
+        # Announce that the function is done
+        if reason:
+            await spdr.reply(f"{user.first_name} was muted in {spdr.chat.title}\n"f"`Reason:`{reason}")
+        else:
+            await spdr.reply(f"{user.first_name} was muted in {spdr.chat.title}")
+        # Announce to logging group
+        if BOTLOG:
+            await spdr.client.send_message(
+                    BOTLOG_CHATID, "#MUTE\n"
+                    f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                    f"CHAT: {spdr.chat.title}(`{spdr.chat_id}`)")
+    except UserIdInvalidError:
+            return await spdr.reply("`Uh oh my mute logic broke!`")
+
+
+@borg.on(sudo_cmd(pattern=r"unmute(?: |$)(.*)" ,allow_sudo = True))
+async def unmoot(unmot):
+    """ For .unmute command, unmute the replied/tagged person """
+    # Admin or creator check
+    chat = await unmot.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+    # If not admin and not creator, return
+    if not admin and not creator:
+        await unmot.reply(NO_ADMIN)
+        return
+    # If admin or creator, inform the user and start unmuting
+    await unmot.edit('```Unmuting...```')
+    user = await get_user_from_event(unmot)
+    user = user[0]
+    if user:
+        pass
+    else:
+        return
+    if unmute(unmot.chat_id, user.id) is False:
+        return await unmot.reply("`Error! User probably already unmuted.`")
+    try:
+       await unmot.client(EditBannedRequest(unmot.chat_id, user.id, UNBAN_RIGHTS))
+       await unmot.reply("Unmuted Successfully")
+    except UserIdInvalidError:
+       await unmot.reply("`Uh oh my unmute logic broke!`")
+       return
+    if BOTLOG:
+       await unmot.client.send_message(
+                BOTLOG_CHATID, "#UNMUTE\n"
+                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                f"CHAT: {unmot.chat.title}(`{unmot.chat_id}`)")        
