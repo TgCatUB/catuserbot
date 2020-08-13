7301ceef69ccd1a52d16f1862b89dc2578b65a40
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-08-13 23:19:19 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -31,10 +31,10 @@ async def amireallyalive(alive):
         cat_caption  = f"__**✮ MY BOT IS RUNNING SUCCESFULLY ✮**__\n\n"
         cat_caption += f"**✧ Database :** `{check_sgnirts}`\n"   
         cat_caption += f"**✧ Telethon version :** `{version.__version__}\n`"
-        cat_caption += f"**✧ Catuserbot Version :** `{catversion}`\n"
+        cat_caption += f"**✧ CatUserbot Version :** `{catversion}`\n"
         cat_caption += f"**✧ Python Version :** `{python_version()}\n`"
         cat_caption += f"**✧ Uptime :** `{uptime}\n`"  
-        cat_caption += f"**✧ My peru Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n\n"
+        cat_caption += f"**✧ My Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n\n"
         cat_caption += f"**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
         await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id)
         await alive.delete()
@@ -42,7 +42,7 @@ async def amireallyalive(alive):
         await alive.edit(f"__**✮ MY BOT IS RUNNING SUCCESFULLY ✮**__\n\n"
                          f"**✧ Database :** `{check_sgnirts}`\n"
                          f"**✧ Telethon Version :** `{version.__version__}\n`"
-                         f"**✧ Catuserbot Version :** `{catversion}`\n"
+                         f"**✧ CatUserbot Version :** `{catversion}`\n"
                          f"**✧ Python Version :** `{python_version()}\n`"
                          f"**✧ Uptime :** `{uptime}\n`"
                          f"**✧ My Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n\n"
