b8ad425c3c21ba5bddc271ff57e7fa94d19e5902
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-08-13 21:33:20 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,21 +1,20 @@
 """Check if userbot alive or not . """
 
-import time
-import asyncio
-from telethon import events
-from userbot import StartTime
-from userbot import ALIVE_NAME, CMD_HELP, catdef, catversion
-from userbot.utils import admin_cmd, sudo_cmd
-from telethon import version
-from platform import python_version, uname
-import requests
-import re
-from PIL import Image
 import os
+import re
 import nekos
+import requests
+import asyncio , time
+from PIL import Image
+from platform import uname
+from telethon import events
+from telethon import version
+from userbot import StartTime
+from platform import python_version, uname
+from userbot.utils import admin_cmd,sudo_cmd
+from userbot import CMD_HELP, ALIVE_NAME, catdef , catversion
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Sur_vivor"
-
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Cat"
 CAT_IMG = Config.ALIVE_PIC
 
 @borg.on(admin_cmd(outgoing=True, pattern="alive$"))
@@ -24,47 +23,31 @@ async def amireallyalive(alive):
         return
     reply_to_id = alive.message
     uptime = await catdef.get_readable_time((time.time() - StartTime))
+    _, check_sgnirts = check_data_base_heal_th()
+    hmm = bot.uid
     if alive.reply_to_msg_id:
         reply_to_id = await alive.get_reply_message()
-
     if CAT_IMG:
-         cat_caption  = f"🚴‍♂️**MY BOT IS RUNNING SUCCESFULLY**\n\n"
-         cat_caption += f"**Database Status: Databases Functioning Normally!**\n"   
-         cat_caption += f"⏳`Telethon Version:` **{version.__version__}**\n"
-         cat_caption += f"⏳`Python Version:` **{python_version()}**\n"
-         cat_caption += f"⏳`CatUserbot Version:` **{catversion}**\n"
-         cat_caption += f"⏳`Cat Uptime:` **{uptime}**\n\n"         
-         cat_caption += f"**Cat is Always With You, My Masters!**\n"
-         cat_caption += f"⏳`Owner Name:` {DEFAULTUSER}\n"   
-         cat_caption += f"⏳`Modified by:` [✰Sᴀͥʀᴀͣᴛͫʜ™️✰](t.me/Surv_ivor)\n\n"         
-         cat_caption += f"**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
-         await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption)
-         await alive.delete()
+        cat_caption  = f"__**✮ MY BOT IS RUNNING SUCCESFULLY ✮**__\n\n"
+        cat_caption += f"**✧ Database :** `{check_sgnirts}`\n"   
+        cat_caption += f"**✧ Telethon version :** `{version.__version__}\n`"
+        cat_caption += f"**✧ Catuserbot Version :** `{catversion}`\n"
+        cat_caption += f"**✧ Python Version :** `{python_version()}\n`"
+        cat_caption += f"**✧ Uptime :** `{uptime}\n`"  
+        cat_caption += f"**✧ My Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n\n"
+	cat_caption += f"**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
+        await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id)
+        await alive.delete()
     else:
-        await alive.edit("🚴‍♂️**MY BOT IS RUNNING SUCCESFULLY**\n\n"
-                         "**Database Status: Databases Functioning Normally!**\n"
-                         f"⏳`Telethon Version:` **{version.__version__}**\n"
-                         f"⏳`Python Version:` **{python_version()}**\n"
-                         f"⏳`Catuserbot Version:` **{catversion}**\n"
-                         f"⏳`Cat Uptime:` **{uptime}**\n\n"                        
-                         "**Cat is Always With You, My Masters!**\n"                                                
-                         f"⏳`Owner Name:` {DEFAULTUSER}\n"
-                         "⏳`Modified by:` [✰Sᴀͥʀᴀͣᴛͫʜ™️✰](t.me/Surv_ivor)\n\n"
-                         f"**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
+        await alive.edit(f"__**✮ MY BOT IS RUNNING SUCCESFULLY ✮**__\n\n"
+                         f"**✧ Database :** `{check_sgnirts}`\n"   
+                         f"**✧ Telethon Version :** `{version.__version__}\n`"
+                         f"**✧ CatUserbot Version :** `{catversion}`\n"
+                         f"**✧ Python Version :** `{python_version()}\n`"
+                         f"**✧ Uptime :** `{uptime}\n`"
+                         f"**✧ My Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n\n"
+			 f"**[⚜️DEPLOY CATUSERBOT⚜️](https://github.com/Sur-vivor/CatUserbot)**"
                         )
-
-@borg.on(sudo_cmd(pattern="sudo", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    uptime = await catdef.get_readable_time((time.time() - StartTime))
-    await event.reply(" SUDO COMMANDS ARE WORKING PERFECTLY \n\n"
-                     f"☞Telethon version: {version.__version__}\n"
-                     f"☞Python: {python_version()}\n"
-                     f"☞My Owner: {DEFAULTUSER}\n"
-                     f"**uptime :** `{uptime}\n`"
-                     #"Deploy this userbot Now"
-		     )
 
 @borg.on(admin_cmd(pattern="cat$"))
 async def _(event):
@@ -79,8 +62,36 @@ async def _(event):
     img.seek(0)
     await bot.send_file(event.chat_id , open("temp.webp", "rb"),reply_to=reply_to_id) 
 	
+#UniBorg Telegram UseRBot 
+#Copyright (C) 2020 @UniBorg
+#This code is licensed under
+#the "you can't use this for anything - public or private,
+#unless you know the two prime factors to the number below" license
+#543935563961418342898620676239017231876605452284544942043082635399903451854594062955
+#വിവരണം അടിച്ചുമാറ്റിക്കൊണ്ട് പോകുന്നവർ
+#ക്രെഡിറ്റ് വെച്ചാൽ സന്തോഷമേ ഉള്ളു..!
+#uniborg
+
+def check_data_base_heal_th():
+    # https://stackoverflow.com/a/41961968
+    is_database_working = False
+    output = "No Database is set"
+    if not Var.DB_URI:
+        return is_database_working, output
+    from userbot.plugins.sql_helper import SESSION
+    try:
+        # to check database we will execute raw query
+        SESSION.execute("SELECT 1")
+    except Exception as e:
+        output = f"❌ {str(e)}"
+        is_database_working = False
+    else:
+        output = "Functioning Normally"
+        is_database_working = True
+    return is_database_working, output
+
 CMD_HELP.update({"alive": "`.alive` :\
-      \n**USAGE:** Type .alive to see wether your bot is working or not.\
+      \n**USAGE:** status of bot.\
       \n\n`.cat`\
       \n**USAGE : **Random cat stickers"
 })
