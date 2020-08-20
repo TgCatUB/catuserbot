d7bd82b35f161b354c9e366d54ebd8300521d376
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-20 17:19:39 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -97,6 +97,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:
             await event.edit("menu closed")
+            await asyncio.sleep(2)
+            await event.delete()
         else:
             reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True) 
