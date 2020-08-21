1021a00f300d4c00e253f215b5c7cecb26336940
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-22 01:54:04 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -159,9 +159,8 @@ def admin_cmd(pattern=None, **args):
             # special fix for snip.py
             args["pattern"] = re.compile(pattern)
         else:
-            nls = re.compile(Config.COMMAND_HAND_LER + pattern)
+            args["pattern"] = re.compile(Config.COMMAND_HAND_LER + pattern)
             reg =Config.COMMAND_HAND_LER[1]
-            args["pattern"] = re.search(nls ,reg + pattern)
             cmd = (reg +pattern).replace("$", "").replace("\\", "").replace("^", "")
             try:
                 CMD_LIST[file_test].append(cmd)
