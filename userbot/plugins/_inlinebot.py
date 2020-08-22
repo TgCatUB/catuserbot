ecab872fdf3ceda2d590b9483db2f7d39d2f52d8
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-22 19:35:58 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -21,7 +21,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             result = builder.article(
                 "Alive cat",
                 text=query,
-                CAT_IMG,
+                thumb = CAT_IMG,
                 buttons=buttons
                 )
             await event.answer([result] if result else None)
