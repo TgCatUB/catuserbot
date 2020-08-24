cbd727a44978e127fbfb7cbad32d775c6a07edf3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-25 01:41:36 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,53 +1,53 @@
-"""Check if userbot alive or not . """
-
 import os
 import re
 import nekos
 import requests
 import asyncio , time
 from PIL import Image
+from platform import uname
+from telethon import events
+from telethon import version
 from userbot import StartTime
-from telethon import events,version
 from platform import python_version, uname
-from userbot.utils import admin_cmd,sudo_cmd
+from ..utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot import CMD_HELP, ALIVE_NAME, catdef , catversion
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 CAT_IMG = Config.ALIVE_PIC
 
 @borg.on(admin_cmd(outgoing=True, pattern="alive$"))
+@borg.on(sudo_cmd(pattern="alive$",allow_sudo = True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
+    tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
     reply_to_id = alive.message
-    uptime = await catdef.get_readable_time((time.time() - StartTime))
-    _, check_sgnirts = check_data_base_heal_th()
-    hmm = bot.uid
     if alive.reply_to_msg_id:
         reply_to_id = await alive.get_reply_message()
-    if CAT_IMG:
-        cat_caption  = f"__**✮ MY BOT IS RUNNING SUCCESFULLY ✮**__\n\n"
-        cat_caption += f"**✧ Database :** `{check_sgnirts}`\n"   
-        cat_caption += f"**✧ Telethon version :** `{version.__version__}\n`"
-        cat_caption += f"**✧ Catuserbot Version :** `{catversion}`\n"
-        cat_caption += f"**✧ Python Version :** `{python_version()}\n`"
-        cat_caption += f"**✧ Uptime :** `{uptime}\n`"  
-        cat_caption += f"**✧ My peru Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n"
-        await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id)
-        await alive.delete()
-    else:
-        await alive.edit(f"__**✮ MY BOT IS RUNNING SUCCESFULLY ✮**__\n\n"
-                         f"**✧ Database :** `{check_sgnirts}`\n"   
-                         f"**✧ Telethon Version :** `{version.__version__}\n`"
-                         f"**✧ Catuserbot Version :** `{catversion}`\n"
-                         f"**✧ Python Version :** `{python_version()}\n`"
-                         f"**✧ Uptime :** `{uptime}\n`"
-                         f"**✧ My Peru Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n"
-                        )         
+    hmm = bot.uid
+    cat_caption  = f"__**✮ CATUSERBOT IS ONLINE ✮**__\n\n"
+    cat_caption += f"**✧ Telethon version :** `{version.__version__}\n`"
+    cat_caption += f"**✧ Catuserbot Version :** `{catversion}`\n"
+    cat_caption += f"**✧ Python Version :** `{python_version()}\n`"
+    cat_caption += f"**✧ My peru Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n"
+    results = await bot.inline_query(  # pylint:disable=E0602
+                tgbotusername,
+                cat_caption
+            )
+    await results[0].click(
+                alive.chat_id,
+                reply_to=reply_to_id,
+                hide_via=True
+            )
+    await alive.delete()
 
 @borg.on(admin_cmd(pattern="cat$"))
+@borg.on(sudo_cmd(pattern="cat$",allow_sudo = True))
 async def _(event):
-    await event.delete() 
+    try:
+        await event.delete() 
+    except:
+        pass
     reply_to_id = event.message
     if event.reply_to_msg_id:
         reply_to_id = await event.get_reply_message()
@@ -57,7 +57,7 @@ async def _(event):
     img.save("temp.webp", "webp")
     img.seek(0)
     await bot.send_file(event.chat_id , open("temp.webp", "rb"),reply_to=reply_to_id) 
-	
+
 #UniBorg Telegram UseRBot 
 #Copyright (C) 2020 @UniBorg
 #This code is licensed under
