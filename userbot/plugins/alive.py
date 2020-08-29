78292849b6690b6e8da42377d1ac7de42b3a29ff
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-08-29 23:34:37 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -15,8 +15,41 @@ from userbot import CMD_HELP, ALIVE_NAME, catdef , catversion
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 CAT_IMG = Config.ALIVE_PIC
 
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
+CAT_IMG = Config.ALIVE_PIC
+
 @borg.on(admin_cmd(outgoing=True, pattern="alive$"))
-@borg.on(sudo_cmd(pattern="alive$",allow_sudo = True))
+async def amireallyalive(alive):
+    if alive.fwd_from:
+        return
+    reply_to_id = alive.message
+    uptime = await catdef.get_readable_time((time.time() - StartTime))
+    _, check_sgnirts = check_data_base_heal_th()
+    hmm = bot.uid
+    if alive.reply_to_msg_id:
+        reply_to_id = await alive.get_reply_message()
+    if CAT_IMG:
+        cat_caption  = f"__**✮ MY BOT IS RUNNING SUCCESFULLY ✮**__\n\n"
+        cat_caption += f"**✧ Database :** `{check_sgnirts}`\n"   
+        cat_caption += f"**✧ Telethon version :** `{version.__version__}\n`"
+        cat_caption += f"**✧ Catuserbot Version :** `{catversion}`\n"
+        cat_caption += f"**✧ Python Version :** `{python_version()}\n`"
+        cat_caption += f"**✧ Uptime :** `{uptime}\n`"  
+        cat_caption += f"**✧ My peru Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n"
+        await borg.send_file(alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id)
+        await alive.delete()
+    else:
+        await alive.edit(f"__**✮ MY BOT IS RUNNING SUCCESFULLY ✮**__\n\n"
+                         f"**✧ Database :** `{check_sgnirts}`\n"   
+                         f"**✧ Telethon Version :** `{version.__version__}\n`"
+                         f"**✧ Catuserbot Version :** `{catversion}`\n"
+                         f"**✧ Python Version :** `{python_version()}\n`"
+                         f"**✧ Uptime :** `{uptime}\n`"
+                         f"**✧ My Peru Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n"
+                        )
+        
+@borg.on(admin_cmd(outgoing=True, pattern="ialive$"))
+@borg.on(sudo_cmd(pattern="ialive$",allow_sudo = True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
@@ -25,11 +58,11 @@ async def amireallyalive(alive):
     if alive.reply_to_msg_id:
         reply_to_id = await alive.get_reply_message()
     hmm = bot.uid
-    cat_caption  = f"__**✮ CATUSERBOT IS ONLINE ✮**__\n\n"
-    cat_caption += f"**✧ Telethon version :** `{version.__version__}\n`"
-    cat_caption += f"**✧ Catuserbot Version :** `{catversion}`\n"
-    cat_caption += f"**✧ Python Version :** `{python_version()}\n`"
-    cat_caption += f"**✧ My peru Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n"
+    cat_caption  = f"__**Catuserbot is Up and Running**__\n"
+    cat_caption += f"**  -Telethon version :** `{version.__version__}\n`"
+    cat_caption += f"**  -Catuserbot Version :** `{catversion}`\n"
+    cat_caption += f"**  -Python Version :** `{python_version()}\n`"
+    cat_caption += f"**  -My peru Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n"
     results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 cat_caption
@@ -86,8 +119,11 @@ def check_data_base_heal_th():
         is_database_working = True
     return is_database_working, output
 
-CMD_HELP.update({"alive": "`.alive` :\
-      \n**USAGE:** status of bot.\
-      \n\n`.cat`\
-      \n**USAGE : **Random cat stickers"
+CMD_HELP.update({"alive": "**Plugin :** `alive`\
+      \n\n**Syntax : **`.alive` :\
+      \n**Usage : ** status of bot.\
+      \n\n**Syntax : **`.ialive` :\
+      \n**Usage : ** inline alive.\
+      \n\n**Synatx :** `.cat`\
+      \n**Usage : **Random cat stickers"
 })
