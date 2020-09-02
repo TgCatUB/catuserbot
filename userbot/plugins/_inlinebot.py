e71af6a7e8ee052b40da1f88bd8d05104ea7f16e
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-02 13:08:12 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -68,7 +68,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             result = builder.article(
                          title = "Inline creator",
                          text = message_text,
-                         buttons = tl_ib_buttons
+                         buttons = tl_ib_buttons,
+                         link_preview=False
                          )
             await event.answer([result] if result else None)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
