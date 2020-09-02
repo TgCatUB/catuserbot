4d5d18add3219ba33724ef9e9bddccf258fde221
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-03 00:09:52 +0530

    Create corecmds.py

---
+++ userbot/plugins/corecmds.py
@@ -0,0 +1,59 @@
+from .utils import admin_cmd, remove_plugin, load_module
+from pathlib import Path
+import asyncio
+import os
+
+DELETE_TIMEOUT = 5
+
+
+@borg.on(admin_cmd(pattern="install$"))
+async def install(event):
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        try:
+            downloaded_file_name = await event.client.download_media(  # pylint:disable=E0602
+                await event.get_reply_message(),
+                "userbot/plugins/"  # pylint:disable=E0602
+            )
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                await event.edit("Installed Plugin `{}`".format(os.path.basename(downloaded_file_name)))
+            else:
+                os.remove(downloaded_file_name)
+                await event.edit("Errors! This plugin is already installed/pre-installed.")
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await event.edit(str(e))
+            os.remove(downloaded_file_name)
+    await asyncio.sleep(DELETE_TIMEOUT)
+    await event.delete()
+
+
+@borg.on(admin_cmd(pattern=r"unload (?P<shortname>\w+)$", outgoing=True))
+async def unload(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    try:
+        remove_plugin(shortname)
+        await event.edit(f"Unloaded {shortname} successfully")
+    except Exception as e:
+        await event.edit("Successfully unload {shortname}\n{}".format(shortname, str(e)))
+
+
+@borg.on(admin_cmd(pattern=r"load (?P<shortname>\w+)$", outgoing=True))
+async def load(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    try:
+        try:
+            remove_plugin(shortname)
+        except BaseException:
+            pass
+        load_module(shortname)
+        await event.edit(f"Successfully loaded {shortname}")
+    except Exception as e:
+        await event.edit(f"Could not load {shortname} because of the following error.\n{str(e)}")
