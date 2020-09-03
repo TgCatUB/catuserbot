05803745dc6e9d484704e6d6315a953840b3b52d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-03 23:32:04 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,4 +1,4 @@
-from userbot import catdef
+from ..helpers import *
 import requests
 import os
 from userbot.uniborgConfig import Config
@@ -10,30 +10,6 @@ thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
 # thumb image
 with open(thumb_image_path, "wb") as f:
     f.write(requests.get(Config.THUMB_IMAGE).content)
-
-deEmojify = catdef.deEmojify
-trumptweet = catdef.trumptweet
-changemymind = catdef.changemymind
-kannagen = catdef.kannagen
-moditweet = catdef.moditweet
-tweets = catdef.tweets
-waifutxt = catdef.waifutxt
-catmusic = catdef.catmusic
-catmusicvideo = catdef.catmusicvideo
-admin_groups = catdef.admin_groups
-iphonex = catdef.iphonex
-baguette = catdef.baguette
-threats = catdef.threats
-lolice = catdef.lolice
-trash = catdef.trash
-awooify = catdef.awooify
-convert_toimage = catdef.convert_toimage
-trap = catdef.trap
-phcomment = catdef.phcomment
-extract_time = catdef.extract_time
-take_screen_shot = catdef.take_screen_shot
-runcmd = catdef.runcmd
-
 
 def check(cat):
     if "/start" in cat:
