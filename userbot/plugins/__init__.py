5562b3c0c98e7851fba3ca1bffd0acbbde255236
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-04 20:10:08 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -6,8 +6,6 @@ import requests
 from ..helpers import *
 from .. import StartTime
 from userbot.uniborgConfig import Config
-from .alive import check_data_base_heal_th
-
 
 Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
@@ -87,3 +85,33 @@ async def catalive():
                   \nDyno's : {dyno}\
                   "
     return conclusion
+
+
+# UniBorg Telegram UseRBot
+# Copyright (C) 2020 @UniBorg
+# This code is licensed under
+# the "you can't use this for anything - public or private,
+# unless you know the two prime factors to the number below" license
+# 543935563961418342898620676239017231876605452284544942043082635399903451854594062955
+# വിവരണം അടിച്ചുമാറ്റിക്കൊണ്ട് പോകുന്നവർ
+# ക്രെഡിറ്റ് വെച്ചാൽ സന്തോഷമേ ഉള്ളു..!
+# uniborg
+
+
+def check_data_base_heal_th():
+    # https://stackoverflow.com/a/41961968
+    is_database_working = False
+    output = "No Database is set"
+    if not Var.DB_URI:
+        return is_database_working, output
+    from userbot.plugins.sql_helper import SESSION
+    try:
+        # to check database we will execute raw query
+        SESSION.execute("SELECT 1")
+    except Exception as e:
+        output = f"❌ {str(e)}"
+        is_database_working = False
+    else:
+        output = "Functioning Normally"
+        is_database_working = True
+    return is_database_working, output
