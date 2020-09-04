aec99addb672748bd86a0f790d00582f8943b5eb
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-04 20:45:18 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -41,43 +41,43 @@ def catalive():
     else:
         sudo = "Disabled"
     uptime = get_readable_time((time.time() - StartTime))
-    useragent = ('Mozilla/5.0 (Linux; Android 10; SM-G975F) '
-                 'AppleWebKit/537.36 (KHTML, like Gecko) '
-                 'Chrome/80.0.3987.149 Mobile Safari/537.36'
-                 )
-    user_id = Heroku.account().id
-    headers = {
-        'User-Agent': useragent,
-        'Authorization': f'Bearer {Var.HEROKU_API_KEY}',
-        'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
-    }
-    path = "/accounts/" + user_id + "/actions/get-quota"
-    r = requests.get(heroku_api + path, headers=headers)
-    if r.status_code != 200:
-        return await dyno.edit("`Error: something bad happened`\n\n"
-                               f">.`{r.reason}`\n")
-    result = r.json()
-    quota = result['account_quota']
-    quota_used = result['quota_used']
-
-    """ - Used - """
-    remaining_quota = quota - quota_used
-    math.floor(remaining_quota / quota * 100)
-    minutes_remaining = remaining_quota / 60
-    hours = math.floor(minutes_remaining / 60)
-    minutes = math.floor(minutes_remaining % 60)
-    """ - Current - """
-    App = result['apps']
     try:
-        App[0]['quota_used']
-    except IndexError:
-        AppQuotaUsed = 0
-    else:
-        AppQuotaUsed = App[0]['quota_used'] / 60
-        math.floor(App[0]['quota_used'] * 100 / quota)
-    AppHours = math.floor(AppQuotaUsed / 60)
-    AppMinutes = math.floor(AppQuotaUsed % 60)
-    dyno = f"{AppHours}h {AppMinutes}m/{hours}h {minutes}"
+        useragent = ('Mozilla/5.0 (Linux; Android 10; SM-G975F) '
+                     'AppleWebKit/537.36 (KHTML, like Gecko) '
+                     'Chrome/80.0.3987.149 Mobile Safari/537.36'
+                     )
+        user_id = Heroku.account().id
+        headers = {
+            'User-Agent': useragent,
+            'Authorization': f'Bearer {Var.HEROKU_API_KEY}',
+            'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
+        }
+        path = "/accounts/" + user_id + "/actions/get-quota"
+        r = requests.get(heroku_api + path, headers=headers)
+        result = r.json()
+        quota = result['account_quota']
+        quota_used = result['quota_used']
+
+        """ - Used - """
+        remaining_quota = quota - quota_used
+        math.floor(remaining_quota / quota * 100)
+        minutes_remaining = remaining_quota / 60
+        hours = math.floor(minutes_remaining / 60)
+        minutes = math.floor(minutes_remaining % 60)
+        """ - Current - """
+        App = result['apps']
+        try:
+            App[0]['quota_used']
+        except IndexError:
+            AppQuotaUsed = 0
+        else:
+            AppQuotaUsed = App[0]['quota_used'] / 60
+            math.floor(App[0]['quota_used'] * 100 / quota)
+        AppHours = math.floor(AppQuotaUsed / 60)
+        AppMinutes = math.floor(AppQuotaUsed % 60)
+        dyno = f"{AppHours}h {AppMinutes}m/{hours}h {minutes}"
+    except Exception as e:
+        dyno = e
     conclusion = f"Database : {_}\
                   \nSudo function : {sudo}\
                   \nUptime : {uptime}\
