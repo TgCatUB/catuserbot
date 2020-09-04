22648ea5ced2ff39373832b381f86abaca4e19c7
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-04 21:42:13 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -155,14 +155,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
     async def on_plug_in_callback_query_handler(event):
-        if event.query.user_id == bot.uid:
-            statstext = await catalive()
-            reply_pop_up_alert = statstext
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-        else:
-            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
+        statstext = await catalive()
+        reply_pop_up_alert = statstext
+        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
