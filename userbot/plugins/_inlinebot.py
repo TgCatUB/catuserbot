f20f93a4670eb39710b1f7c0e821ac3a66da9bea
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-04 16:13:15 +0000

    pylint: auto fixes

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -159,6 +159,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         reply_pop_up_alert = statstext
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
+
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
     number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
