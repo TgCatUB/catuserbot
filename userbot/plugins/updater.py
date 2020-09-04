9a54b5a32fd1c037ee97a8bb38da254cafb076fa
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-04 17:07:26 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -245,6 +245,8 @@ async def upstream(event):
         repo.create_head("master", origin.refs.master)
         repo.heads.master.set_tracking_branch(origin.refs.master)
         repo.heads.master.checkout(True)
+    ups_rem = repo.remote("upstream")
+    ups_rem.fetch(ac_br)
     await event.edit("`Deploying userbot, please wait....`")
     await deploy(event, repo, ups_rem, ac_br, txt)
 
