fb848bc7a0ab6bdcee48a27121b7b1d4c1014c3e
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-05 22:38:08 +0530

    Update inline.py

--- userbot/plugins/inline.py
+++ userbot/plugins/inline.py
@@ -7,13 +7,13 @@ async def inline_handler(event):
     builder = event.builder
     result = None
     query = event.text
-    txt = re.findall(r'(\d+) ?(.*)', query)
-    text = txt[0][1]
-    id = []
-    id.append(txt[0][0])
-    id.append(bot.uid)
-    cat = (await bot.get_me()).id
     if event.query.user_id == bot.uid and query.startswith("Secret bot"):
+        txt = re.findall(r'(\d+) ?(.*)', query[10:])
+        text = txt[0][1]
+        id = []
+        id.append(txt[0][0])
+        id.append(bot.uid)
+        cat = (await bot.get_me()).id
         buttons = [custom.Button.inline("show message 🔐", data="secert")]
         result = builder.article(
             title="secret message",
@@ -21,13 +21,12 @@ async def inline_handler(event):
             buttons=buttons
         )
         await event.answer([result] if result else None)
-
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret")))
-    async def on_plug_in_callback_query_handler(event):
-        tcxt = text[10:]
-        if event.query.user_id in id:
-            reply_pop_up_alert = tcxt
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-        else:
-            reply_pop_up_alert = "why were you looking at this shit go away and do your own work,idiot"
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret")))
+        async def on_plug_in_callback_query_handler(event):
+            tcxt = text[10:]
+            if event.query.user_id in id:
+                reply_pop_up_alert = tcxt
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+            else:
+                reply_pop_up_alert = "why were you looking at this shit go away and do your own work,idiot"
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
