040a0d0740ede98f4b5d57aba30e66ff2c9a4ee4
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-05 23:30:10 +0530

    Update inline.py

--- userbot/plugins/inline.py
+++ userbot/plugins/inline.py
@@ -2,18 +2,12 @@ import re
 from telethon import custom, events
 
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
-    @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
+    @tgbot.on(events.InlineQuery)
     async def inline_handler(event):
         builder = event.builder
         result = None
         query = event.text
         if event.query.user_id == bot.uid and query.startswith("Secret bot"):
-            query = query[10:]
-            txt = re.findall(r'(\d+) ?(.*)', query)
-            txt[0][1]
-            id = []
-            id.append(txt[0][0])
-            id.append(bot.uid)
             buttons = [custom.Button.inline("show message 🔐", data="secert")]
             result = builder.article(
                 title="secret message",
