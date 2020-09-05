6adb93a0fd66228adb4e6f6314822f45d6870704
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-05 17:14:06 +0000

    pylint: auto fixes

--- userbot/plugins/inline.py
+++ userbot/plugins/inline.py
@@ -1,9 +1,5 @@
-import io
 import re
-import math
-from . import catalive
-from .. import CMD_LIST
-from telethon import Button, custom, events
+from telethon import custom, events
 
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
@@ -17,14 +13,14 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             id = []
             id.append(txt[0][0])
             id.append(bot.uid)
-            cat = (await bot.get_me()).id
+            (await bot.get_me()).id
             buttons = [custom.Button.inline("show message 🔐", data="secert")]
             result = builder.article(
                 title="secret message",
                 text=f"🔒 A whisper message to [user](tg://user?id={txt[0][0]}), Only he / she can open it.",
-                buttons=buttons
-            )
+                buttons=buttons)
             await event.answer([result] if result else None)
+
             @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret")))
             async def on_plug_in_callback_query_handler(event):
                 tcxt = text[10:]
@@ -33,4 +29,4 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
                 else:
                     reply_pop_up_alert = "why were you looking at this shit go away and do your own work,idiot"
-                    await event.answer(reply_pop_up_alert, cache_time=0, alert=True)    
+                    await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
