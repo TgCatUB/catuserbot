f931c318d6844155d13347fd48fabc81cba34566
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-06 01:29:51 +0530

    Update inline.py

--- userbot/plugins/inline.py
+++ userbot/plugins/inline.py
@@ -28,17 +28,3 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
                 buttons=buttons)
             await event.answer([result] if result else None)
-
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.+?)_(.+)")))
-    async def on_plug_in_callback_query_handler(event):
-        userid = event.pattern_match.group(1)
-        ids = []
-        ids.append(int(userid))
-        ids.append(bot.uid)
-        if event.query.user_id in ids:
-            encrypted_tcxt = event.pattern_match.group(2)
-            reply_pop_up_alert = encrypted_tcxt
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-        else:
-            reply_pop_up_alert = "why were you looking at this shit go away and do your own work,idiot"
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
