a09cbc8fc31c306dc256a23141aecbdb78720330
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-05 17:35:41 +0000

    pylint: auto fixes

--- userbot/plugins/inline.py
+++ userbot/plugins/inline.py
@@ -20,6 +20,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 text=f"🔒 A whisper message to [user](tg://user?id={txt[0][0]}), Only he / she can open it.",
                 buttons=buttons)
             await event.answer([result] if result else None)
+
             @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret")))
             async def on_plug_in_callback_query_handler(event):
                 tcxt = text
