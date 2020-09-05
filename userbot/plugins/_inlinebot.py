63a115acbc5a242c8713a0204a5f65bd5887a075
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-05 17:10:41 +0000

    pylint: auto fixes

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -90,6 +90,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 buttons=buttons
             )
             await event.answer([result] if result else None)
+
             @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret")))
             async def on_plug_in_callback_query_handler(event):
                 tcxt = text[10:]
@@ -98,9 +99,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
                 else:
                     reply_pop_up_alert = "why were you looking at this shit go away and do your own work,idiot"
-                    await event.answer(reply_pop_up_alert, cache_time=0, alert=True)            
-            
-            
+                    await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(rb"helpme_next\((.+?)\)")
     ))
