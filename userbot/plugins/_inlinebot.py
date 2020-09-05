8963425d2a56237d8c7d54a9d813e216d61db067
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-06 01:30:58 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -109,6 +109,20 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+    
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.+?)_(.+)")))
+    async def on_plug_in_callback_query_handler(event):
+        userid = event.pattern_match.group(1)
+        ids = Config.SUDO_USERS
+        ids.append(int(userid))
+        ids.append(bot.uid)
+        if event.query.user_id in ids:
+            encrypted_tcxt = event.pattern_match.group(2)
+            reply_pop_up_alert = encrypted_tcxt
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        else:
+            reply_pop_up_alert = "why were you looking at this shit go away and do your own work,idiot"
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
