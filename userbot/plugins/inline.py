5a549c7694282f9e0609e69f41ff83e4b3157d15
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-05 19:49:05 +0000

    pylint: auto fixes

--- userbot/plugins/inline.py
+++ userbot/plugins/inline.py
@@ -11,11 +11,11 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         query = event.text
         if event.query.user_id == bot.uid and query.startswith("secret"):
             query = query[10:]
-            user, txct = query.split(" " ,1)
+            user, txct = query.split(" ", 1)
             u = await event.client.get_entity(user)
-            buttons = [ custom.Button.inline(
-                    "show message 🔐",
-                    data=f"secret_{u.id}_ {txct}")]
+            buttons = [custom.Button.inline(
+                "show message 🔐",
+                data=f"secret_{u.id}_ {txct}")]
             if u.username:
                 sandy = f"@{u.username}"
             else:
