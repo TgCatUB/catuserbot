5ac0a04e78f3255ba7cfbff1ad886e532f8d6bce
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-06 00:46:54 +0530

    Update inline.py

--- userbot/plugins/inline.py
+++ userbot/plugins/inline.py
@@ -9,7 +9,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         builder = event.builder
         result = None
         query = event.text
-        if event.query.user_id == bot.uid and query.startswith("Secret bot"):
+        if event.query.user_id == bot.uid and query.startswith("Secret"):
             query = query[10:]
             txt = re.findall(r'(\d+) ?(.*)', query)
             buttons = [
