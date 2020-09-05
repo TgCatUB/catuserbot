261a3eff2dee3ba79f29fc7eb124a4e0042d28e1
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-05 20:02:00 +0000

    pylint: auto fixes

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -109,7 +109,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-    
+
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.+?)_(.+)")))
     async def on_plug_in_callback_query_handler(event):
         userid = event.pattern_match.group(1)
