d2d68109596bf03a733fa7311e2d0d871efe985a
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-05 23:04:32 +0530

    Update inline.py

--- userbot/plugins/inline.py
+++ userbot/plugins/inline.py
@@ -8,7 +8,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         result = None
         query = event.text
         if event.query.user_id == bot.uid and query.startswith("Secret bot"):
-            txt = re.findall(r'(\d+) ?(.*)', query[10:])
+            query = query[10:]
+            txt = re.findall(r'(\d+) ?(.*)', query)
             text = txt[0][1]
             id = []
             id.append(txt[0][0])
@@ -19,10 +20,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 text=f"🔒 A whisper message to [user](tg://user?id={txt[0][0]}), Only he / she can open it.",
                 buttons=buttons)
             await event.answer([result] if result else None)
-
             @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret")))
             async def on_plug_in_callback_query_handler(event):
-                tcxt = text[10:]
+                tcxt = text
                 if event.query.user_id in id:
                     reply_pop_up_alert = tcxt
                     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
