ba3239e6da1d46308c39d6c8d7e2a0c143d3e27b
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-06 12:42:36 +0530

    Update inline.py

--- userbot/plugins/inline.py
+++ userbot/plugins/inline.py
@@ -15,18 +15,35 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             query = query[7:]
             user, txct = query.split(" ", 1)
             try:
-                u = await event.client.get_entity(user)
+                #ValueError: Could not find the input entity for <telethon.tl.types.PeerUser object at 0x7f96e090dac0>. Please read https://docs.telethon.dev/en/latest/concepts/entities.html to find out more detail
+                u = int(user)
+                buttons = [custom.Button.inline(
+                    "show message 🔐",
+                    data=f"secret_{u}_ {txct}")]
+                try:
+                    u = await event.client.get_entity(user)
+                    if:
+                        sandy = f"@{u.username}"
+                    else:
+                        sandy = f"[{u.first_name}](tg://user?id={u.id})"
+                except ValueError:
+                    sandy = f"[{user}](tg://user?id={u})"
+                result = builder.article(
+                    title="secret message",
+                    text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
+                    buttons=buttons)
+                await event.answer([result] if result else None)
             except ValueError:
-                u = await event.client.get_entity(int(user))
-            buttons = [custom.Button.inline(
-                "show message 🔐",
-                data=f"secret_{u.id}_ {txct}")]
-            if u.username:
-                sandy = f"@{u.username}"
-            else:
-                sandy = f"[{u.first_name}](tg://user?id={u.id})"
-            result = builder.article(
-                title="secret message",
-                text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
-                buttons=buttons)
-            await event.answer([result] if result else None)
+                u = await event.client.get_entity(user)
+                buttons = [custom.Button.inline(
+                    "show message 🔐",
+                    data=f"secret_{u.id}_ {txct}")]
+                if u.username:
+                    sandy = f"@{u.username}"
+                else:
+                    sandy = f"[{u.first_name}](tg://user?id={u.id})"
+                result = builder.article(
+                    title="secret message",
+                    text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
+                    buttons=buttons)
+                await event.answer([result] if result else None)
