15c7758b9286a39e303a4209edaa0483e972aa2f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-10 01:30:41 +0530

    Update button.py

--- userbot/plugins/button.py
+++ userbot/plugins/button.py
@@ -69,8 +69,8 @@ async def _(event):
 # Helpers
 
 
-@borg.on(admin_cmd(pattern=r"ibutton( (.*)$|$)", outgoing=True))
-@borg.on(sudo_cmd(pattern=r"ibutton( (.*)$|$)", allow_sudo=True))
+@borg.on(admin_cmd(pattern=r"ibutton( (.*)|$)", outgoing=True))
+@borg.on(sudo_cmd(pattern=r"ibutton( (.*)|$)", allow_sudo=True))
 async def _(event):
     reply_to_id = None
     catinput = event.pattern_match.group(1)
