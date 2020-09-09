033557fa8c544e64e8178dd563b253c32acf03dc
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-09 07:57:16 +0000

    pylint: auto fixes

--- userbot/plugins/fun.py
+++ userbot/plugins/fun.py
@@ -41,7 +41,7 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "`😏/\n/▌ \n/ \\n████\n╬╬\n╬╬\n╬╬\n╬╬\n╬╬\n╬╬\n╬╬\😦\n╬╬/▌\n╬╬/\`"
+    mentions = r"`😏/\n/▌ \n/ \\n████\n╬╬\n╬╬\n╬╬\n╬╬\n╬╬\n╬╬\n╬╬\😦\n╬╬/▌\n╬╬/\`"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
