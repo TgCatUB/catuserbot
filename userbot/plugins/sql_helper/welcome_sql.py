9fb02f117feeef400d53b6b0caa57ab6b0f64aa6
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-10 12:21:34 +0530

    Update welcome_sql.py

--- userbot/plugins/sql_helper/welcome_sql.py
+++ userbot/plugins/sql_helper/welcome_sql.py
@@ -1,71 +1,73 @@
-from sqlalchemy import BigInteger, Boolean, Column, String, UnicodeText
+try:
+    from userbot.modules.sql_helper import SESSION, BASE
+except ImportError:
+    raise AttributeError
 
-from userbot.plugins.sql_helper import BASE, SESSION
+from sqlalchemy import BigInteger, Column, Numeric, String, UnicodeText
 
 
 class Welcome(BASE):
     __tablename__ = "welcome"
     chat_id = Column(String(14), primary_key=True)
-    custom_welcome_message = Column(UnicodeText)
-    media_file_id = Column(UnicodeText)
-    should_clean_welcome = Column(Boolean, default=False)
     previous_welcome = Column(BigInteger)
+    reply = Column(UnicodeText)
+    f_mesg_id = Column(Numeric)
 
-    def __init__(
-        self,
-        chat_id,
-        custom_welcome_message,
-        should_clean_welcome,
-        previous_welcome,
-        media_file_id=None,
-    ):
-        self.chat_id = chat_id
-        self.custom_welcome_message = custom_welcome_message
-        self.media_file_id = media_file_id
-        self.should_clean_welcome = should_clean_welcome
+    def __init__(self, chat_id, previous_welcome, reply, f_mesg_id):
+        self.chat_id = str(chat_id)
         self.previous_welcome = previous_welcome
+        self.reply = reply
+        self.f_mesg_id = f_mesg_id
 
 
 Welcome.__table__.create(checkfirst=True)
 
 
+def get_welcome(chat_id):
+    try:
+        return SESSION.query(Welcome).get(str(chat_id))
+    finally:
+        SESSION.close()
+
+
 def get_current_welcome_settings(chat_id):
     try:
-        return SESSION.query(Welcome).filter(Welcome.chat_id == str(chat_id)).one()
+        return SESSION.query(Welcome).filter(
+            Welcome.chat_id == str(chat_id)).one()
     except BaseException:
         return None
     finally:
         SESSION.close()
 
 
-def add_welcome_setting(
-    chat_id,
-    custom_welcome_message,
-    should_clean_welcome,
-    previous_welcome,
-    media_file_id,
-):
-    # adder = SESSION.query(Welcome).get(chat_id)
-    adder = Welcome(
-        chat_id,
-        custom_welcome_message,
-        should_clean_welcome,
-        previous_welcome,
-        media_file_id,
-    )
-    SESSION.add(adder)
-    SESSION.commit()
+def add_welcome_setting(chat_id, previous_welcome, reply, f_mesg_id):
+    to_check = get_welcome(chat_id)
+    if not to_check:
+        adder = Welcome(chat_id, previous_welcome, reply, f_mesg_id)
+        SESSION.add(adder)
+        SESSION.commit()
+        return True
+    else:
+        rem = SESSION.query(Welcome).get(str(chat_id))
+        SESSION.delete(rem)
+        SESSION.commit()
+        adder = Welcome(chat_id, previous_welcome, reply, f_mesg_id)
+        SESSION.commit()
+        return False
 
 
 def rm_welcome_setting(chat_id):
-    rem = SESSION.query(Welcome).get(str(chat_id))
-    if rem:
-        SESSION.delete(rem)
-        SESSION.commit()
+    try:
+        rem = SESSION.query(Welcome).get(str(chat_id))
+        if rem:
+            SESSION.delete(rem)
+            SESSION.commit()
+            return True
+    except BaseException:
+        return False
 
 
 def update_previous_welcome(chat_id, previous_welcome):
     row = SESSION.query(Welcome).get(str(chat_id))
     row.previous_welcome = previous_welcome
-    # commit the changes to the DB
     SESSION.commit()
