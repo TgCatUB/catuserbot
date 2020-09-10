cd5efcfa596971930500df9a2e6e2c4e2941923b
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-10 16:32:38 +0530

    Update snip_sql.py

--- userbot/plugins/sql_helper/snip_sql.py
+++ userbot/plugins/sql_helper/snip_sql.py
@@ -1,17 +1,13 @@
 from sqlalchemy import Column, Numeric, String, UnicodeText
-
 from . import BASE, SESSION
-
 
 class Snips(BASE):
     __tablename__ = "snip"
-    chat_id = Column(String(14), primary_key=True)
     keyword = Column(UnicodeText, primary_key=True, nullable=False)
     reply = Column(UnicodeText)
     f_mesg_id = Column(Numeric)
 
-    def __init__(self, chat_id, keyword, reply, f_mesg_id):
-        self.chat_id = str(chat_id)
+    def __init__(self, keyword, reply, f_mesg_id):
         self.keyword = keyword
         self.reply = reply
         self.f_mesg_id = f_mesg_id
@@ -20,43 +16,43 @@ class Snips(BASE):
 Snips.__table__.create(checkfirst=True)
 
 
-def get_note(chat_id, keyword):
+def get_note(keyword):
     try:
-        return SESSION.query(Snips).get((str(chat_id), keyword))
+        return SESSION.query(Snips).get(keyword)
     finally:
         SESSION.close()
 
 
-def get_notes(chat_id):
+def get_notes():
     try:
-        return SESSION.query(Snips).filter(Snips.chat_id == str(chat_id)).all()
+        return SESSION.query(Snips).all()
     finally:
         SESSION.close()
 
 
-def add_note(chat_id, keyword, reply, f_mesg_id):
-    to_check = get_note(chat_id, keyword)
+def add_note(keyword, reply, f_mesg_id):
+    to_check = get_note(keyword)
     if not to_check:
-        adder = Snips(str(chat_id), keyword, reply, f_mesg_id)
+        adder = Snips(keyword, reply, f_mesg_id)
         SESSION.add(adder)
         SESSION.commit()
         return True
     else:
-        rem = SESSION.query(Snips).get((str(chat_id), keyword))
+        rem = SESSION.query(Snips).get(keyword)
         SESSION.delete(rem)
         SESSION.commit()
-        adder = Snips(str(chat_id), keyword, reply, f_mesg_id)
+        adder = Snips(keyword, reply, f_mesg_id)
         SESSION.add(adder)
         SESSION.commit()
         return False
 
 
-def rm_note(chat_id, keyword):
-    to_check = get_note(chat_id, keyword)
+def rm_note(keyword):
+    to_check = get_note(keyword)
     if not to_check:
         return False
     else:
-        rem = SESSION.query(Snips).get((str(chat_id), keyword))
+        rem = SESSION.query(Snips).get(keyword))
         SESSION.delete(rem)
         SESSION.commit()
         return True
