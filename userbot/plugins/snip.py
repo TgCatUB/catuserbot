f0685f64283c201207068da8c50dc3e87c350f1a
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-10 19:10:50 +0530

    Update snip.py

--- userbot/plugins/snip.py
+++ userbot/plugins/snip.py
@@ -108,8 +108,6 @@ async def add_snip(fltr):
     elif fltr.reply_to_msg_id and not string:
         rep_msg = await fltr.get_reply_message()
         string = rep_msg.text
-    if not (string and msg_id):
-        return await edit_or_reply(fltr, "What should i set for the Given note")
     success = "Note {}  is successfully saved. Use` #{} `to get it"
     if add_note(keyword, string, msg_id) is False:
         rm_note(keyword)
