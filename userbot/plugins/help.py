6effb1a232f1229bb641bd40eae05f0043b319cb
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-11 13:27:57 +0000

    pylint: auto fixes

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -102,13 +102,16 @@ async def _(event):
     if event.fwd_from:
         return
     result = await borg(functions.help.GetNearestDcRequest())
-    result = result.stringify() + "\n\nList Of Telegram Data Centres:\
+    result = (
+        result.stringify()
+        + "\n\nList Of Telegram Data Centres:\
                                     \nDC1 : Miami FL, USA\
                                     \nDC2 : Amsterdam, NL\
                                     \nDC3 : Miami FL, USA\
                                     \nDC4 : Amsterdam, NL\
                                     \nDC5 : Singapore, SG\
                                     "
+    )
     await event.edit(result)
 
 
