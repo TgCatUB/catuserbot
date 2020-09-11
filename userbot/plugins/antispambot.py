ec6ec65cd240c55f6f781b20183169c0bd2cb07e
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-11 11:35:40 +0000

    pylint: auto fixes

--- userbot/plugins/antispambot.py
+++ userbot/plugins/antispambot.py
@@ -1,21 +1,25 @@
-from . import spamwatch
-from ..utils import is_admin
 from telethon import events
+
 from .. import LOGS
+from ..utils import is_admin
+from . import spamwatch
 from .gadmin import BANNED_RIGHTS
 
 if Config.SPAMWATCH_BAN:
+
     @bot.on(events.ChatAction)
     async def _(event):
         chat = event.chat_id
         if event.user_joined:
             user = await event.get_user()
-            if not is_admin(bot , chat, user.id):
+            if not is_admin(bot, chat, user.id):
                 return
             ban = spamwatch.get_ban(user.id)
             if ban:
-                await event.reply(f"This [{user.first_name}](tg://user?id={user.id}) was banned by spamwatch For the reason `{ban.reason}`")
+                await event.reply(
+                    f"This [{user.first_name}](tg://user?id={user.id}) was banned by spamwatch For the reason `{ban.reason}`"
+                )
                 try:
-                    await bot(EditBannedRequest(chat , user.id, BANNED_RIGHTS))
+                    await bot(EditBannedRequest(chat, user.id, BANNED_RIGHTS))
                 except Exception as e:
                     return LOGS.info(e)
