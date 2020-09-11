54b872e657f8d5008816dedd7a90045a5bad7eca
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-11 23:40:06 +0530

    Update antispambot.py

--- userbot/plugins/antispambot.py
+++ userbot/plugins/antispambot.py
@@ -57,6 +57,6 @@ if Config.ANTISPAMBOT_BAN:
                         BOTLOG_CHATID,
                         "#ANTISPAMBOT\n"
                         f"**User :** [{user.first_name}](tg://user?id={user.id})\n"
-                        f"**Chat :** {event.chat.title} (`{event.chat_id}`)\n",
-                        f"**Reason :** {hmm.text}",
+                        f"**Chat :** {event.chat.title} (`{event.chat_id}`)\n"
+                        f"**Reason :** {hmm.text}"
                     )
