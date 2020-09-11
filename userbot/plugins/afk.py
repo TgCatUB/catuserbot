958cc15235f3c99038cca1d69c52090930b0d1d5
Muskan Raj <62775010+turquoise-giggle@users.noreply.github.com>
2020-09-11 09:00:39 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -106,8 +106,8 @@ async def on_afk(event):
         #        else:
      #           afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"__My Master Has Been In afk For__ `{total_afk_time}`\nWhere He Is: ONLY GOD KNOWS " + \
-            f"\n\n__I promise He'll back in a few light years__\n**REASON**: {reason}" \
+        message_to_reply = f"__My Master Has Been In afk For__ `{total_afk_time}`\nWhere She Is: None of your business...!! " + \
+            f"\n\n__I promise She'll back in a few light years__\n**REASON**: {reason}" \
             if reason \
             else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? Soon __Whenever I feel like it__**( ಠ ʖ̯ ಠ)**  "
         msg = await event.reply(message_to_reply)
@@ -144,9 +144,9 @@ async def _(event):
             afk_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
-            await borg.send_message(event.chat_id, f"**I shall be Going afk!** __because ~ {reason}__")
+            await borg.send_message(event.chat_id, f"**I shall be AFK** __because ~ {reason}__")
         else:
-            await borg.send_message(event.chat_id, f"**I am Going afk!**")
+            await borg.send_message(event.chat_id, f"**ME NIKL....BYEE BYEE HIHIHIHIHI**")
         await asyncio.sleep(5)
         await event.delete()
         try:
