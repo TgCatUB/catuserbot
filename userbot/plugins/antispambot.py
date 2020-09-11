33bf397ef0c841fb99bcdd4ba4566e823e658b35
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-11 21:38:27 +0530

    Update antispambot.py

--- userbot/plugins/antispambot.py
+++ userbot/plugins/antispambot.py
@@ -4,7 +4,7 @@ from .. import LOGS
 from ..utils import is_admin
 from . import spamwatch
 
-if Config.SPAMWATCH_BAN and spamwatch:
+if Config.ANTISPAMBOT_BAN:
 
     @bot.on(events.ChatAction())
     async def _(event):
@@ -14,12 +14,13 @@ if Config.SPAMWATCH_BAN and spamwatch:
             catadmin = await is_admin(bot, chat, bot.uid)
             if not catadmin:
                 return
-            ban = spamwatch.get_ban(user.id)
-            if ban:
-                await event.reply(
-                    f"This [{user.first_name}](tg://user?id={user.id}) was banned by spamwatch For the reason `{ban.reason}`"
-                )
-                try:
-                    await bot.edit_permissions(chat, user.id, view_messages=False)
-                except Exception as e:
-                    return LOGS.info(e)
+            if spamwatch:
+                ban = spamwatch.get_ban(user.id)
+                if ban:
+                    await event.reply(
+                        f"This [{user.first_name}](tg://user?id={user.id}) was banned by spamwatch For the reason `{ban.reason}`"
+                    )
+                    try:
+                        await bot.edit_permissions(chat, user.id, view_messages=False)
+                    except Exception as e:
+                        return LOGS.info(e)
