15eb8c113aac71625fa03f44a9055b67a28545a6
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-11 19:49:43 +0530

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -47,7 +47,7 @@ async def _(event):
     if spamwatch:
         ban = spamwatch.get_ban(user_id)
         if ban:
-            sw = f"**Spamwatch Banned :**`True` \n     **-**🤷‍♂️**Reason : **{ban.reason}"
+            sw = f"**Spamwatch Banned :**`True` \n       **-**🤷‍♂️**Reason : **`{ban.reason}`"
         else:
             sw = f"**Spamwatch Banned :**`False`"
     else:
