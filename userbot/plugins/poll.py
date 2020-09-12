17bacb17031f44c7adc314c4177775d2bd7bd725
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-12 13:34:49 +0000

    pylint: auto fixes

--- userbot/plugins/poll.py
+++ userbot/plugins/poll.py
@@ -2,7 +2,7 @@ import random
 
 from telethon.tl.types import InputMediaPoll, Poll
 
-from ..utils import admin_cmd, sudo_cmd, edit_or_reply
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 from . import Build_Poll
 
 
