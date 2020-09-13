17c1498435f1416cdd306fbb40913332242f8545
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-13 16:11:31 +0530

    Update antispambot.py

--- userbot/plugins/antispambot.py
+++ userbot/plugins/antispambot.py
@@ -1,3 +1,8 @@
+#    Copyright (C) 2020  sandeep.n(π.$)
+#baning spmmers plugin for catuserbot by @sandy1709 and @mrconfused
+#included both cas(combot antispam service) and spamwatch (need to add more feaututres)
+
+
 from requests import get
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
