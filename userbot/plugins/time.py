d10ace429c36e7a91ac0d188280af925230cfe56
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-13 11:27:12 +0530

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -159,14 +159,13 @@ async def date_func(dat):
         return
 
 
-@borg.on(admin_cmd(pattern="time ?(.*)"))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="time ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     current_time = datetime.now().strftime(
         f"\n  USERBOT TIMEZONE  \nLOCATION:{LOCATION}\n  Time: %H:%M:%S \n  Date: %d.%m.%y \n"
     )
-    start = datetime.now()
     input_str = event.pattern_match.group(1)
     reply_msg_id = event.message.id
     if input_str:
@@ -174,29 +173,23 @@ async def _(event):
     elif event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         reply_msg_id = previous_message.id
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):  # pylint:disable=E0602
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
-    # pylint:disable=E0602
+    if not os.path.isdir("./temp/"): 
+        os.makedirs("./temp/")  
     required_file_name = (
         Config.TMP_DOWNLOAD_DIRECTORY + " " + str(datetime.now()) + ".webp"
     )
     img = Image.new("RGBA", (350, 220), color=(0, 0, 0, 115))
-    fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+    fnt = ImageFont.truetype(FONT_FILE_TO_USE, 50)
     drawn_text = ImageDraw.Draw(img)
     drawn_text.text((10, 10), current_time, font=fnt, fill=(255, 255, 255))
     img.save(required_file_name)
-    await borg.send_file(  # pylint:disable=E0602
+    await borg.send_file( 
         event.chat_id,
         required_file_name,
-        caption="Userbot: Powered by cat",
         # Courtesy: @ManueI15
         reply_to=reply_msg_id,
     )
     os.remove(required_file_name)
-    end = datetime.now()
-    time_taken_ms = (end - start).seconds
-    await event.edit("Created sticker in {} seconds".format(time_taken_ms))
-    await asyncio.sleep(5)
     await event.delete()
 
 
