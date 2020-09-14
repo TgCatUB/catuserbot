674f48d8106d6b7390c053c9cc858b91c05b6128
D'Rock <62856832+santo-surviver@users.noreply.github.com>
2020-09-14 09:11:09 +0530

    Progress bar change

--- userbot/utils.py
+++ userbot/utils.py
@@ -345,8 +345,8 @@ async def progress(current, total, event, start, type_of_ps, file_name=None):
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
         progress_str = "[{0}{1}] {2}%\n".format(
-            "".join(["▰" for i in range(math.floor(percentage / 10))]),
-            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            "".join(["🅢🅞🅝🅘🅒🅒🅐🅣" for i in range(math.floor(percentage / 8))]),
+            "".join(["ⓈⓄⓃⒾⒸⒸⒶⓉ" for i in range(1 - math.floor(percentage / 8))]),
             round(percentage, 2),
         )
         tmp = progress_str + "{0} of {1}\nETA: {2}".format(
