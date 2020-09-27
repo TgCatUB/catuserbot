1a20b864de03c57f18ee9150e7190657a408d73e
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-27 20:43:57 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -17,7 +17,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         builder = event.builder
         result = None
         query = event.text
-        if query.startswith("__**Catuserbot") and event.query.user_id == bot.uid:
+        if query.startswith("**Catuserbot") and event.query.user_id == bot.uid:
             buttons = [
                 (
                     custom.Button.inline("Stats", data="stats"),
