9372bf0bcb504e0ea787daf60877dcf18eb0ff72
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-30 18:05:59 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -94,7 +94,7 @@ async def on_afk(event):
             if not event.is_private:
                 await event.client.send_message(
                     Config.PM_LOGGR_BOT_API_ID,
-                    f"#AFK_TAGS \n<b>Group name : </b><code>{hmm.title}</code>\
+                    f"#AFK_TAGS \n<b>Group : </b><code>{hmm.title}</code>\
                             \n<b>Message : </b><a href = 'https://t.me/c/{hmm.id}/{event.message.id}'> link</a>",
                     parse_mode="html",
                     link_preview=False,
