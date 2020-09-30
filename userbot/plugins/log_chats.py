b9f833d4310dcc8eaa5699d778dbe786a32b3138
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-30 18:06:30 +0530

    Update log_chats.py

--- userbot/plugins/log_chats.py
+++ userbot/plugins/log_chats.py
@@ -44,7 +44,7 @@ async def log_tagged_messages(event):
         if not event.is_private:
             await event.client.send_message(
                 Config.PM_LOGGR_BOT_API_ID,
-                f"#TAGS \n<b>Group name : </b><code>{hmm.title}</code>\
+                f"#TAGS \n<b>Group : </b><code>{hmm.title}</code>\
                         \n<b>Message : </b><a href = 'https://t.me/c/{hmm.id}/{event.message.id}'> link</a>",
                 parse_mode="html",
                 link_preview=False,
