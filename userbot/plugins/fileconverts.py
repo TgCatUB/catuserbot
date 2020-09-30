611a11d616e7256516670f980f920d19a0a000e8
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-30 16:42:41 +0000

    pylint: auto fixes

--- userbot/plugins/fileconverts.py
+++ userbot/plugins/fileconverts.py
@@ -6,12 +6,11 @@ from io import BytesIO
 
 from telethon import types
 from telethon.errors import PhotoInvalidDimensionsError
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import SendMediaRequest
 
-from . import CMD_HELP, unzip
 from ..utils import admin_cmd, edit_or_reply, progress, sudo_cmd
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-
+from . import CMD_HELP, unzip
 
 if not os.path.isdir("./temp"):
     os.makedirs("./temp")
@@ -93,6 +92,7 @@ async def silently_send_message(conv, text):
     await conv.mark_read(message=response)
     return response
 
+
 @borg.on(admin_cmd(pattern="ttf ?(.*)"))
 @borg.on(sudo_cmd(pattern="ttf ?(.*)", allow_sudo=True))
 async def get(event):
@@ -144,6 +144,7 @@ async def on_file_to_photo(event):
         return
     await catt.delete()
 
+
 @borg.on(admin_cmd(pattern="gif$"))
 @borg.on(sudo_cmd(pattern="gif$", allow_sudo=True))
 async def _(event):
@@ -181,7 +182,7 @@ async def _(event):
             await catevent.edit("Unblock @tgstogifbot")
             return
 
-        
+
 @borg.on(admin_cmd(pattern="nfc ?(.*)"))
 @borg.on(sudo_cmd(pattern="nfc ?(.*)", allow_sudo=True))
 async def _(event):
