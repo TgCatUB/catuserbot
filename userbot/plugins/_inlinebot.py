a57db63bc85b59a96454db510745794046bdbeed
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-01 17:52:44 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -12,7 +12,7 @@ BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>
 
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
-    @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
+    @tgbot.on(events.InlineQuery)
     async def inline_handler(event):
         builder = event.builder
         result = None
@@ -114,18 +114,16 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.+?)_(.+)")))
     async def on_plug_in_callback_query_handler(event):
         userid = event.pattern_match.group(1)
-        ids = []
-        ids.append(int(userid))
-        ids.append(bot.uid)
+        ids = [int(userid), bot.uid]
         if event.query.user_id in ids:
             encrypted_tcxt = event.pattern_match.group(2)
             reply_pop_up_alert = encrypted_tcxt
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
         else:
             reply_pop_up_alert = (
                 "why were you looking at this shit go away and do your own work,idiot"
             )
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -184,10 +182,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
     number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
-    helpable_plugins = []
-    for p in loaded_plugins:
-        if not p.startswith("_"):
-            helpable_plugins.append(p)
+    helpable_plugins = [p for p in loaded_plugins if not p.startswith("_")]
     helpable_plugins = sorted(helpable_plugins)
     modules = [
         custom.Button.inline(
