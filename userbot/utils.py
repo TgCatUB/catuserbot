0a9186c290c9c1ce6a797cb5fff2169874a10668
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-22 01:08:23 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -216,7 +216,14 @@ def sudo_cmd(pattern=None, **args):
 
 # https://t.me/c/1220993104/623253
 # https://docs.telethon.dev/en/latest/misc/changelog.html#breaking-changes
-async def edit_or_reply(event, text):
+async def edit_or_reply(event, text , parsemode=None):
+    if parsemode:
+        if event.sender_id in Config.SUDO_USERS:
+            reply_to = await event.get_reply_message()
+            if reply_to:
+                return await reply_to.reply(text,parse_mode=parsemode)
+            return await event.reply(text,parse_mode=parsemode)
+        return await event.edit(text,parse_mode=parsemode)
     if event.sender_id in Config.SUDO_USERS:
         reply_to = await event.get_reply_message()
         if reply_to:
