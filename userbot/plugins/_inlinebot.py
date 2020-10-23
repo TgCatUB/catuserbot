a653c2edb2fee10b6ecabad87772adb904749a69
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-24 00:11:34 +0530

    small change

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -3,6 +3,7 @@ import json
 import math
 import os
 import re
+import time
 
 from telethon import Button, custom, events
 
@@ -115,7 +116,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 u = int(u.id)
             except:
                 return
-            timestamp = int(event.query.query_id)
+            timestamp = int(time.time() * 2)
             newsecret = {str(timestamp): {"userid": u, "text": txct}}
 
             buttons = [
