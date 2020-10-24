603937d8c6ba47495d753a2d3b7f2eaeb7fee91d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-24 22:40:54 +0530

    Update screenshot.py

--- userbot/plugins/screenshot.py
+++ userbot/plugins/screenshot.py
@@ -33,20 +33,21 @@ async def _(event):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.binary_location = Config.CHROME_BIN
-        await event.edit("Starting Google Chrome BIN")
+        await event.edit("`Starting Google Chrome BIN`")
         driver = webdriver.Chrome(chrome_options=chrome_options)
         input_str = event.pattern_match.group(1)
-        caturl = url(input_str)
+        inputstr = input_str
+        caturl = url(inputstr)
         if not caturl:
-            input_str = "https://" + input_str
-            caturl = url(input_str)
+            inputstr = "https://" + input_str
+            caturl = url(inputstr)
         if not caturl:
             await catevent.edit(
-                "the url must be in the format `https://www.google.com`"
+                "`The given input is not supported url`"
             )
             return
-        driver.get(input_str)
-        await catevent.edit("Calculating Page Dimensions")
+        driver.get(inputstr)
+        await catevent.edit("`Calculating Page Dimensions`")
         height = driver.execute_script(
             "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
         )
@@ -58,14 +59,14 @@ async def _(event):
         # for good measure to make the scroll bars disappear
         im_png = driver.get_screenshot_as_png()
         # saves screenshot of entire page
-        await catevent.edit("Stoppping Chrome Bin")
+        await catevent.edit("`Stoppping Chrome Bin`")
         driver.close()
         message_id = None
         if event.reply_to_msg_id:
             message_id = event.reply_to_msg_id
         end = datetime.now()
         ms = (end - start).seconds
-        hmm = f"**url : **{input_str} \n**Time :** `{ms} seconds`"
+        hmm = f"**url : **{inputstr} \n**Time :** `{ms} seconds`"
         await catevent.delete()
         with io.BytesIO(im_png) as out_file:
             out_file.name = input_str + ".PNG"
@@ -91,29 +92,30 @@ async def _(event):
     if Config.SCREEN_SHOT_LAYER_ACCESS_KEY is None:
         await edit_or_reply(
             event,
-            "Need to get an API key from https://screenshotlayer.com/product \nModule stopping!",
+            "`Need to get an API key from https://screenshotlayer.com/product and need to set it SCREEN_SHOT_LAYER_ACCESS_KEY !`",
         )
         return
-    catevent = await edit_or_reply(event, "Processing ...")
+    catevent = await edit_or_reply(event, "`Processing ...`")
     sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}&fullpage={}&viewport={}&format={}&force={}"
     input_str = event.pattern_match.group(1)
-    caturl = url(input_str)
+    inputstr = input_str
+    caturl = url(inputstr)
     if not caturl:
-        input_str = "https://" + input_str
-        caturl = url(input_str)
+        inputstr = "https://" + input_str
+        caturl = url(inputstr)
     if not caturl:
-        await catevent.edit("the url must be in the format `https://www.google.com`")
+        await catevent.edit("`The given input is not supported url`")
         return
     response_api = requests.get(
         sample_url.format(
-            Config.SCREEN_SHOT_LAYER_ACCESS_KEY, input_str, "1", "2560x1440", "PNG", "1"
+            Config.SCREEN_SHOT_LAYER_ACCESS_KEY, inputstr, "1", "2560x1440", "PNG", "1"
         )
     )
     # https://stackoverflow.com/a/23718458/4723940
     contentType = response_api.headers["content-type"]
     end = datetime.now()
     ms = (end - start).seconds
-    hmm = f"**url : **{input_str} \n**Time :** `{ms} seconds`"
+    hmm = f"**url : **{inputstr} \n**Time :** `{ms} seconds`"
     if "image" in contentType:
         with io.BytesIO(response_api.content) as screenshot_image:
             screenshot_image.name = "screencapture.png"
@@ -137,9 +139,7 @@ CMD_HELP.update(
         "screenshot": "**Plugin : **`screenshot`\
         \n\n**Syntax : **`.ss <url>`\
         \n**Function : **__Takes a screenshot of a website and sends the screenshot.__\
-        \n**Example of a valid URL :** `https://www.google.com`\
         \n\n**Syntax : **`.scapture <url>`\
-        \n**Usage : **__Takes a screenshot of a website and sends the screenshot need to set config var for this.__\
-        \n**Example of a valid URL : **`https://www.google.com`"
+        \n**Function : **__Takes a screenshot of a website and sends the screenshot need to set config var for this.__"
     }
 )
