7ab075a53d1255949da92bdbac46b8baa594e5a2
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-25 19:54:28 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -42,12 +42,12 @@ async def set_not_afk(event):
         s = time
         endtime = ""
         if d > 0:
-            endtime += d + "d " + h + "h " + m + "m " + s + "s "
+            endtime += f"{d}d {h}h {m}m {s}s"
         else:
             if h > 0:
-                endtime += h + "h " + m + "m " + s + "s "
+                endtime += f"{h}h {m}m {s}s"
             else:
-                endtime += m + "m " + s + "s " if m > 0 else s + "s "
+                endtime += f"{m}m {s}s" if m > 0 else f"{s}s"
     current_message = event.message.message
     if "afk" not in current_message and "on" in USERAFK_ON:
         shite = await event.client.send_message(
@@ -93,12 +93,12 @@ async def on_afk(event):
         s = time
         endtime = ""
         if d > 0:
-            endtime += d + "d " + h + "h " + m + "m " + s + "s "
+            endtime += f"{d}d {h}h {m}m {s}s"
         else:
             if h > 0:
-                endtime += h + "h " + m + "m " + s + "s "
+                endtime += f"{h}h {m}m {s}s"
             else:
-                endtime += m + "m " + s + "s " if m > 0 else s + "s "
+                endtime += f"{m}m {s}s" if m > 0 else f"{s}s"
     current_message_text = event.message.message.lower()
     if "afk" in current_message_text:
         # userbot's should not reply to other userbot's
