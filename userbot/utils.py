dae8d4e7b7373e3b35df2b3dfe315bdc4b4b1bad
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-26 19:13:22 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -214,20 +214,21 @@ def sudo_cmd(pattern=None, command=None, **args):
 
 # https://t.me/c/1220993104/623253
 # https://docs.telethon.dev/en/latest/misc/changelog.html#breaking-changes
-async def edit_or_reply(event, text, parsemode=None):
+async def edit_or_reply(event, text, parsemode=None,linkpreview=None):
+    linkpreview = linkpreview or False
     if parsemode:
         if event.sender_id in Config.SUDO_USERS:
             reply_to = await event.get_reply_message()
             if reply_to:
-                return await reply_to.reply(text, parse_mode=parsemode)
-            return await event.reply(text, parse_mode=parsemode)
-        return await event.edit(text, parse_mode=parsemode)
+                return await reply_to.reply(text, parse_mode=parsemode,link_preview=linkpreview)
+            return await event.reply(text, parse_mode=parsemode,link_preview=linkpreview)
+        return await event.edit(text, parse_mode=parsemode,link_preview=linkpreview)
     if event.sender_id in Config.SUDO_USERS:
         reply_to = await event.get_reply_message()
         if reply_to:
-            return await reply_to.reply(text)
-        return await event.reply(text)
-    return await event.edit(text)
+            return await reply_to.reply(text,link_preview=linkpreview)
+        return await event.reply(text,link_preview=linkpreview)
+    return await event.edit(text,link_preview=linkpreview)
 
 
 # from paperplaneextended
