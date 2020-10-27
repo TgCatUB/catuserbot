10e51619bb92ed3e165648f6c195dfc44beafa9f
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-27 20:26:52 +0000

    pylint: auto fixes

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -7,7 +7,6 @@ from html import unescape
 from pathlib import Path
 
 from googleapiclient.discovery import build
-from telethon.tl.types import DocumentAttributeAudio
 from youtube_dl import YoutubeDL
 from youtube_dl.utils import (
     ContentTooShortError,
