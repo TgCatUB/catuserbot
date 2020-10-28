e4dee256aea69bfc67a5f390e8d6faeffb838e80
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-28 21:03:49 +0000

    pylint: auto fixes

--- userbot/helpers/memeifyhelpers.py
+++ userbot/helpers/memeifyhelpers.py
@@ -16,39 +16,38 @@ from . import unzip
 
 MARGINS = [50, 150, 250, 350, 450]
 
-from colour import Color
 import numpy as np
+from colour import Color
 
 
-def asciiart(in_f, SC, GCF,  out_f, color1, color2, bgcolor='black'):
-    chars = np.asarray(list(' .,:irs?@9B&#'))
+def asciiart(in_f, SC, GCF, out_f, color1, color2, bgcolor="black"):
+    chars = np.asarray(list(" .,:irs?@9B&#"))
     font = ImageFont.load_default()
     letter_width = font.getsize("x")[0]
     letter_height = font.getsize("x")[1]
-    WCF = letter_height/letter_width
+    WCF = letter_height / letter_width
     img = Image.open(in_f)
-    widthByLetter=round(img.size[0]*SC*WCF)
-    heightByLetter = round(img.size[1]*SC)
+    widthByLetter = round(img.size[0] * SC * WCF)
+    heightByLetter = round(img.size[1] * SC)
     S = (widthByLetter, heightByLetter)
     img = img.resize(S)
     img = np.sum(np.asarray(img), axis=2)
     img -= img.min()
-    img = (1.0 - img/img.max())**GCF*(chars.size-1)
-    lines = ("\n".join( ("".join(r) for r in chars[img.astype(int)]) )).split("\n")
+    img = (1.0 - img / img.max()) ** GCF * (chars.size - 1)
+    lines = ("\n".join(("".join(r) for r in chars[img.astype(int)]))).split("\n")
     nbins = len(lines)
-    colorRange =list(Color(color1).range_to(Color(color2), nbins))
-    newImg_width= letter_width *widthByLetter
+    colorRange = list(Color(color1).range_to(Color(color2), nbins))
+    newImg_width = letter_width * widthByLetter
     newImg_height = letter_height * heightByLetter
     newImg = Image.new("RGBA", (newImg_width, newImg_height), bgcolor)
     draw = ImageDraw.Draw(newImg)
-    leftpadding=0
+    leftpadding = 0
     y = 0
     for lineIdx, line in enumerate(lines):
         color = colorRange[lineIdx]
         draw.text((leftpadding, y), line, color.hex, font=font)
         y += letter_height
     newImg.save(out_f)
-
 
 
 def get_warp_length(width):
