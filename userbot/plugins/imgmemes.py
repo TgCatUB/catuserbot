1ca06b27aa304363719cb7d5695c32b7a67806cd
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-29 02:18:44 +0530

    Update imgmemes.py

--- userbot/plugins/imgmemes.py
+++ userbot/plugins/imgmemes.py
@@ -34,6 +34,7 @@ async def nekobot(cat):
             await edit_delete(cat, "`What should i search in google.`", 5)
             return
     cate = await edit_or_reply(cat, "`Connecting to https://www.google.com/ ...`")
+    text = deEmojify(text)
     if ";" in text:
         search, result = text.split(";")
     else:
@@ -51,30 +52,27 @@ async def nekobot(cat):
         os.remove(catfile)
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="trump(?: |$)(.*)"))
-@bot.on(sudo_cmd(allow_sudo=True, pattern="trump(?: |$)(.*)"))
+@bot.on(admin_cmd(outgoing=True, pattern="trump(?: |$)(.*)",command="trump"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="trump(?: |$)(.*)",command="trump"))
 async def nekobot(cat):
     text = cat.pattern_match.group(1)
     text = re.sub("&", "", text)
     reply_to_id = await reply_id(cat)
+    hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
     if not text:
-        if cat.is_reply:
-            if not reply_to_id.media:
-                text = reply_to_id.message
-            else:
-                await edit_or_reply(cat, "Send your text to trump so he can tweet.")
-                return
+        if cat.is_reply and not reply_to_id.media:
+            text = reply_to_id.message
         else:
-            await edit_or_reply(cat, "send your text to trump so he can tweet.")
+            await edit_delete(cat, "**Trump : **`What should I tweet`", 5)
             return
-    cate = await edit_or_reply(cat, "Requesting trump to tweet...")
+    cate = await edit_or_reply(cat, "`Requesting trump to tweet...`")
     try:
-        hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
         hmm = Get(hmm)
         await cat.client(hmm)
     except BaseException:
         pass
     text = deEmojify(text)
+    await asyncio.sleep(2)
     catfile = await trumptweet(text)
     await cat.client.send_file(cat.chat_id, catfile, reply_to=reply_to_id)
     await cate.delete()
@@ -82,32 +80,27 @@ async def nekobot(cat):
         os.remove(catfile)
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="modi(?: |$)(.*)"))
-@bot.on(sudo_cmd(allow_sudo=True, pattern="modi(?: |$)(.*)"))
+@bot.on(admin_cmd(outgoing=True, pattern="modi(?: |$)(.*)",command="modi"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="modi(?: |$)(.*)",command="modi"))
 async def nekobot(cat):
     text = cat.pattern_match.group(1)
     text = re.sub("&", "", text)
-    reply_to_id = cat.message
-    if cat.reply_to_msg_id:
-        reply_to_id = await cat.get_reply_message()
+    reply_to_id = await reply_id(cat)
+    hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
     if not text:
-        if cat.is_reply:
-            if not reply_to_id.media:
-                text = reply_to_id.message
-            else:
-                await edit_or_reply(cat, "Send your text to modi so he can tweet.")
-                return
+        if cat.is_reply and not reply_to_id.media:
+            text = reply_to_id.message
         else:
-            await edit_or_reply(cat, "send your text to modi so he can tweet.")
+            await edit_delete(cat, "**Modi : **`What should I tweet`", 5)
             return
     cate = await edit_or_reply(cat, "Requesting modi to tweet...")
     try:
-        hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
         hmm = Get(hmm)
         await cat.client(hmm)
     except BaseException:
         pass
     text = deEmojify(text)
+    await asyncio.sleep(2)
     catfile = await moditweet(text)
     await cat.client.send_file(cat.chat_id, catfile, reply_to=reply_to_id)
     await cate.delete()
@@ -115,28 +108,27 @@ async def nekobot(cat):
         os.remove(catfile)
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="cmm(?: |$)(.*)"))
-@bot.on(sudo_cmd(allow_sudo=True, pattern="cmm(?: |$)(.*)"))
+@bot.on(admin_cmd(outgoing=True, pattern="cmm(?: |$)(.*)",command="cmm"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="cmm(?: |$)(.*)",command="cmm"))
 async def nekobot(cat):
     text = cat.pattern_match.group(1)
     text = re.sub("&", "", text)
-    reply_to_id = cat.message
-    if cat.reply_to_msg_id:
-        reply_to_id = await cat.get_reply_message()
+    reply_to_id = await reply_id(cat)
+    hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
     if not text:
         if cat.is_reply and not reply_to_id.media:
             text = reply_to_id.message
         else:
-            await edit_or_reply(cat, "Give text to write on banner, man")
+            await edit_delete(cat, "`Give text to write on banner, man`",5)
             return
-    cate = await edit_or_reply(cat, "Your banner is under creation wait a sec...")
+    cate = await edit_or_reply(cat, "`Your banner is under creation wait a sec...`")
     try:
-        hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
         hmm = Get(hmm)
         await cat.client(hmm)
     except BaseException:
         pass
     text = deEmojify(text)
+    await asyncio.sleep(2)
     catfile = await changemymind(text)
     await cat.client.send_file(cat.chat_id, catfile, reply_to=reply_to_id)
     await cate.delete()
@@ -144,32 +136,27 @@ async def nekobot(cat):
         os.remove(catfile)
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="kanna(?: |$)(.*)"))
-@bot.on(sudo_cmd(allow_sudo=True, pattern="kanna(?: |$)(.*)"))
+@bot.on(admin_cmd(outgoing=True, pattern="kanna(?: |$)(.*)",command="kanna"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="kanna(?: |$)(.*)",command="kanna"))
 async def nekobot(cat):
     text = cat.pattern_match.group(1)
     text = re.sub("&", "", text)
-    reply_to_id = cat.message
-    if cat.reply_to_msg_id:
-        reply_to_id = await cat.get_reply_message()
+    reply_to_id = await reply_id(cat)
+    hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
     if not text:
-        if cat.is_reply:
-            if not reply_to_id.media:
-                text = reply_to_id.message
-            else:
-                await edit_or_reply(cat, "what should kanna write give text ")
-                return
+        if cat.is_reply and not reply_to_id.media:
+            text = reply_to_id.message
         else:
-            await edit_or_reply(cat, "what should kanna write give text")
+            await edit_delete(cat, "**Kanna : **`What should i show you`", 5)
             return
-    cate = await edit_or_reply(cat, "Kanna is writing your text...")
+    cate = await edit_or_reply(cat, "`Kanna is writing your text...`")
     try:
-        hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
         hmm = Get(hmm)
         await e.client(hmm)
     except BaseException:
         pass
     text = deEmojify(text)
+    await asyncio.sleep(2)
     catfile = await kannagen(text)
     await cat.client.send_file(cat.chat_id, catfile, reply_to=reply_to_id)
     await cate.delete()
@@ -177,18 +164,12 @@ async def nekobot(cat):
         os.remove(catfile)
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="tweet(?: |$)(.*)"))
-@bot.on(sudo_cmd(allow_sudo=True, pattern="tweet(?: |$)(.*)"))
+@bot.on(admin_cmd(outgoing=True, pattern="tweet(?: |$)(.*)",command="tweet"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="tweet(?: |$)(.*)",command="tweet"))
 async def nekobot(cat):
-    if cat.pattern_match.group(1):
-        text = cat.pattern_match.group(1)
-    else:
-        reply_to_id = await cat.get_reply_message()
-        text = reply_to_id.text
+    text = cat.pattern_match.group(1)
     text = re.sub("&", "", text)
-    reply_to_id = cat.message
-    if cat.reply_to_msg_id:
-        reply_to_id = await cat.get_reply_message()
+    reply_to_id = await reply_id(cat)
     hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
     if not text:
         if cat.is_reply and not reply_to_id.media:
@@ -210,12 +191,13 @@ async def nekobot(cat):
     else:
         await edit_delete(
             cat,
-            "what should I tweet? Give some text and format must be like `.tweet username ; your text`",
+            "__what should I tweet? Give some text and format must be like__ `.tweet username ; your text`",
             5,
         )
         return
-    cate = await edit_or_reply(cat, f"Requesting {username} to tweet...")
+    cate = await edit_or_reply(cat, f"`Requesting {username} to tweet...`")
     text = deEmojify(text)
+    await asyncio.sleep(2)
     catfile = await tweets(text, username)
     await cat.client.send_file(cat.chat_id, catfile, reply_to=reply_to_id)
     await cate.delete()
