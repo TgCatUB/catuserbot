4a8d5db7aee78ed3e12ffeada5b5b4a3021c9f9b
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-31 22:23:05 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -191,6 +191,8 @@ CMD_HELP.update(
         \n**Function : **__Sets you as afk.\nReplies to anyone who tags/PM's \
         you telling them that you are AFK(reason).\n\nSwitches off AFK when you type back anything, anywhere.\
         \nafk means away from keyboard/keypad.__\
+        \n\n**Note :** If you want AFK with hyperlink use [ ; ] after reason, then paste the media link.\
+        \n**Example :** `.afk busy now ;<Media_link>`
 "
     }
 )
