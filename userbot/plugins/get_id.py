1369181f27fa7da0e296c86a98cec9391deeb454
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-01 00:40:57 +0530

    Update get_id.py

--- userbot/plugins/get_id.py
+++ userbot/plugins/get_id.py
@@ -1,14 +1,28 @@
 from telethon.utils import pack_bot_file_id
-
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
-@bot.on(admin_cmd(pattern="get_id"))
-@bot.on(sudo_cmd(pattern="get_id", allow_sudo=True))
+@bot.on(admin_cmd(pattern="(get_id|id)( (.*)|$)"))
+@bot.on(sudo_cmd(pattern="(get_id|id)( (.*)|$)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    if event.reply_to_msg_id:
+    input_str = event.pattern_match.group(1)
+    if input_str:
+        p = await event.client.get_entity(input_str)
+        try:
+            if p.first_name:
+                return await edit_or_reply( event , 
+                    f"The id of the user `{input_str}` is `{p.id}`")
+        except:
+            try:
+                if p.title:
+                    return await edit_or_reply( event , 
+                        f"The id of the chat/channel `{p.title}` is `{p.id}`")
+            except:
+                pass
+        await edit_or_reply( event , "`Either give input as username or reply to user`")
+    elif event.reply_to_msg_id:
         await event.get_input_chat()
         r_msg = await event.get_reply_message()
         if r_msg.media:
