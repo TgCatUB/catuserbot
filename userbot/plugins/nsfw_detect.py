bf4fc0906fbac51fb13fe79aae2317476e0be7a7
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-01 00:14:09 +0530

    Update and rename nsfw_detector.py to nsfw_detect.py

--- userbot/plugins/nsfw_detect.py
+++ userbot/plugins/nsfw_detect.py
@@ -6,7 +6,7 @@ import os
 import requests
 
 from ..utils import admin_cmd, sudo_cmd
-
+from . import CMD_HELP
 
 @bot.on(admin_cmd(pattern="detect$", outgoing=True))
 @bot.on(sudo_cmd(pattern="detect$", allow_sudo=True))
@@ -17,8 +17,10 @@ async def detect(event):
         )
     reply = await event.get_reply_message()
     if not reply:
-        return await edit_delete(event, "`Reply to media !`", 5)
+        return await edit_delete(event, "`Reply to any image or non animated sticker !`", 5)
     media = await event.client.download_media(reply)
+    if not media.endswith(("png" , "jpg" , "webp")):
+        return await edit_delete(event, "`Reply to any image or non animated sticker !`", 5)
     catevent = await edit_or_reply(event, "`Detecting NSFW limit...`")
     r = requests.post(
         "https://api.deepai.org/api/nsfw-detector",
@@ -40,10 +42,15 @@ async def detect(event):
         for parts in detections:
             name = parts["name"]
             confidence = int(float(parts["confidence"]) * 100)
-            result += f"• {name}:\n   <code>{confidence} %</code>\n"
+            result += f"<b>• {name}:</b>\n   <code>{confidence} %</code>\n"
     await edit_or_reply(
         catevent,
         result,
         link_preview=False,
         parse_mode="HTML",
     )
+
+CMD_HELP.update({
+    "nsfw_detect":"**Plugin : **`nsfw_detect`\
+    \n\n**Syntax : **`.detect`\
+    \n**Function : **__Reply .detect command to any image or non animated stickere to detect the nudity in that__"})
