0a1999a899364ba30324b14cb4742959d7b193d5
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-31 18:07:56 +0000

    pylint: auto fixes

--- userbot/plugins/gdrive_download.py
+++ userbot/plugins/gdrive_download.py
@@ -3,31 +3,18 @@ G-Drive File Downloader Plugin For Userbot.
 usage: .gdl File-Link
 By: @Zero_cool7870
 """
+import asyncio
 import os
 
 import requests
+from telethon.tl.types import DocumentAttributeVideo
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 from . import CMD_HELP, progress
 
-import asyncio
-import os
-import subprocess
-import time
-from datetime import datetime
-from pathlib import Path
-from shutil import copyfile
-
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-from pymediainfo import MediaInfo
-from telethon.tl.types import DocumentAttributeVideo
-
-from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import CMD_HELP, make_gif, progress, runcmd, thumb_from_audio
-
 PATH = os.path.join("./temp", "temp_vid.mp4")
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
+
 
 async def download_file_from_google_drive(gid):
     URL = "https://docs.google.com/uc?export=download"
@@ -116,25 +103,25 @@ async def g_download(event):
         await catevent.edit("**File Downloaded.\nName : **`" + str(file_name) + "`")
     else:
         await event.client.send_file(
-                event.chat_id,
-                file_name,
-                caption=f"**File Name : **`{file_name}`",
-                thumb=thumb,
-                force_document=False,
-                supports_streaming=True,
-                attributes=[
-                    DocumentAttributeVideo(
-                        duration=duration,
-                        w=width,
-                        h=height,
-                        round_message=False,
-                        supports_streaming=True,
-                    )
-                ],
-                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                    progress(d, t, udir_event, c_time, "Uploading...", caption_rts)
-                ),
-            )
+            event.chat_id,
+            file_name,
+            caption=f"**File Name : **`{file_name}`",
+            thumb=thumb,
+            force_document=False,
+            supports_streaming=True,
+            attributes=[
+                DocumentAttributeVideo(
+                    duration=duration,
+                    w=width,
+                    h=height,
+                    round_message=False,
+                    supports_streaming=True,
+                )
+            ],
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(d, t, udir_event, c_time, "Uploading...", caption_rts)
+            ),
+        )
         await edit_delete(
             catevent,
             "**File Downloaded and uploaded.\nName : **`" + str(file_name) + "`",
