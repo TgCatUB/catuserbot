c9fa46eac3bec0187ff3bc321be49803ff92e962
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-31 16:46:00 +0530

    Update tts.py

--- userbot/plugins/tts.py
+++ userbot/plugins/tts.py
@@ -65,7 +65,7 @@ async def _(event):
             required_file_name = required_file_name + ".opus"
         end = datetime.now()
         ms = (end - start).seconds
-        await catmemes.client.send_file(
+        await event.client.send_file(
             event.chat_id,
             required_file_name,
             # caption="Processed {} ({}) in {} seconds!".format(text[0:97], lan, ms),
