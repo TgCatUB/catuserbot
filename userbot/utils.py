0862245023baa51b59e20bd4eb80e30186ce0661
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-01 16:20:12 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -243,12 +243,15 @@ def on(**args):
     return decorater
 
 
-async def edit_delete(event, text, time):
+async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None):
+    parse_mode = parse_mode or "md"
+    link_preview = link_preview or False
+    time = time or 5
     if event.sender_id in Config.SUDO_USERS:
         reply_to = await event.get_reply_message()
-        catevent = await reply_to.reply(text) if reply_to else await event.reply(text)
+        catevent = await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode) if reply_to else await event.reply(text, link_preview=link_preview, parse_mode=parse_mode)
     else:
-        catevent = await event.edit(text)
+        catevent = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
     await asyncio.sleep(time)
     return await catevent.delete()
 
