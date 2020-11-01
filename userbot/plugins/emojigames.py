8c5b8771e042dfe7a0561e70a3650c6c23235cd1
Paरam <thakyo@gmail.com>
2020-11-02 00:19:34 +0530

    Renamed to emojigames and Added Jackpot

--- userbot/plugins/emojigames.py
+++ userbot/plugins/emojigames.py
@@ -9,6 +9,7 @@ DART_E_MOJI = "🎯"
 DICE_E_MOJI = "🎲"
 BALL_E_MOJI = "🏀"
 FOOT_E_MOJI = "⚽️"
+JPOT_E_MOJI = "🎰"
 # EMOJI CONSTANTS
 
 
@@ -127,6 +128,33 @@ async def _(event):
         except BaseException:
             pass
 
+@borg.on(admin_cmd(pattern=f"({JPOT_E_MOJI}|jp) ([1-5])"))
+@borg.on(
+    sudo_cmd(
+        pattern=f"({JPOT_E_MOJI}|jp) ([1-5])",
+        allow_sudo=True,
+    )
+)
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_message = event
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+    emoticon = event.pattern_match.group(1)
+    input_str = event.pattern_match.group(2)
+    await event.delete()
+    if emoticon == "jp":
+        emoticon = "🎰"
+    r = await reply_message.reply(file=InputMediaDice(emoticon=emoticon))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while r.media.value != required_number:
+                await r.delete()
+                r = await reply_message.reply(file=InputMediaDice(emoticon=emoticon))
+        except BaseException:
+            pass
 
 CMD_HELP.update(
     {
@@ -138,6 +166,8 @@ CMD_HELP.update(
     \n**Usage : **each number shows different animation for basket ball\
     \n\n**Syntax : **`.⚽️ [1-5] `or `.fb [1-5]`\
     \n**Usage : **each number shows different animation for football\
+    \n\n**Syntax : **`.🎰 [1-5] `or `.jp [1-5]`\
+    \n**Usage : **each number shows different animation for jackpot\
     "
     }
 )
