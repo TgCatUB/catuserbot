a26b8732a3b48e75f77fb9b30e6f2189763e33cd
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-02 10:01:12 +0530

    Update update.py

--- .github/update.py
+++ .github/update.py
@@ -1,6 +1,20 @@
 import asyncio
 
-from userbot import runcmd
+import os
+import shlex
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
 
 
 async def update_requirements():
