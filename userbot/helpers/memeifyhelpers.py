c310a6b2810e319e48ebc1e3003ab483704bda92
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-02 10:29:12 +0530

    Update memeifyhelpers.py

--- userbot/helpers/memeifyhelpers.py
+++ userbot/helpers/memeifyhelpers.py
@@ -7,7 +7,6 @@ from textwrap import wrap
 from typing import Optional, Tuple
 
 import numpy as np
-from colour import Color as asciiColor
 from PIL import Image, ImageDraw, ImageFont
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from wand.color import Color
@@ -17,38 +16,6 @@ from wand.image import Image as catimage
 from . import unzip
 
 MARGINS = [50, 150, 250, 350, 450]
-
-
-def asciiart(in_f, SC, GCF, out_f, color1, color2, bgcolor="black"):
-    chars = np.asarray(list(" .,:irs?@9B&#"))
-    font = ImageFont.load_default()
-    letter_width = font.getsize("x")[0]
-    letter_height = font.getsize("x")[1]
-    WCF = letter_height / letter_width
-    img = Image.open(in_f)
-    widthByLetter = round(img.size[0] * SC * WCF)
-    heightByLetter = round(img.size[1] * SC)
-    S = (widthByLetter, heightByLetter)
-    img = img.resize(S)
-    img = np.sum(np.asarray(img), axis=2)
-    img -= img.min()
-    img = (1.0 - img / img.max()) ** GCF * (chars.size - 1)
-    lines = ("\n".join(("".join(r) for r in chars[img.astype(int)]))).split("\n")
-    nbins = len(lines)
-    colorRange = list(asciiColor(color1).range_to(asciiColor(color2), nbins))
-    newImg_width = letter_width * widthByLetter
-    newImg_height = letter_height * heightByLetter
-    newImg = Image.new("RGBA", (newImg_width, newImg_height), bgcolor)
-    draw = ImageDraw.Draw(newImg)
-    leftpadding = 0
-    y = 0
-    for lineIdx, line in enumerate(lines):
-        color = colorRange[lineIdx]
-        draw.text((leftpadding, y), line, color.hex, font=font)
-        y += letter_height
-    if newImg.mode != "RGB":
-        newImg = newImg.convert("RGB")
-    newImg.save(out_f)
 
 
 def get_warp_length(width):
@@ -192,3 +159,36 @@ async def silently_send_message(conv, text):
 
 async def thumb_from_audio(audio_path, output):
     await runcmd(f"ffmpeg -i {audio_path} -filter:v scale=500:500 -an {output}")
+
+from colour import Color as asciiColor    
+def asciiart(in_f, SC, GCF, out_f, color1, color2, bgcolor="black"):
+    chars = np.asarray(list(" .,:irs?@9B&#"))
+    font = ImageFont.load_default()
+    letter_width = font.getsize("x")[0]
+    letter_height = font.getsize("x")[1]
+    WCF = letter_height / letter_width
+    img = Image.open(in_f)
+    widthByLetter = round(img.size[0] * SC * WCF)
+    heightByLetter = round(img.size[1] * SC)
+    S = (widthByLetter, heightByLetter)
+    img = img.resize(S)
+    img = np.sum(np.asarray(img), axis=2)
+    img -= img.min()
+    img = (1.0 - img / img.max()) ** GCF * (chars.size - 1)
+    lines = ("\n".join(("".join(r) for r in chars[img.astype(int)]))).split("\n")
+    nbins = len(lines)
+    colorRange = list(asciiColor(color1).range_to(asciiColor(color2), nbins))
+    newImg_width = letter_width * widthByLetter
+    newImg_height = letter_height * heightByLetter
+    newImg = Image.new("RGBA", (newImg_width, newImg_height), bgcolor)
+    draw = ImageDraw.Draw(newImg)
+    leftpadding = 0
+    y = 0
+    for lineIdx, line in enumerate(lines):
+        color = colorRange[lineIdx]
+        draw.text((leftpadding, y), line, color.hex, font=font)
+        y += letter_height
+    if newImg.mode != "RGB":
+        newImg = newImg.convert("RGB")
+    newImg.save(out_f)
+    
