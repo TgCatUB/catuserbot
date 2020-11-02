03c15a0dd5597b23862b63edf716a07aa42670cf
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-02 11:14:05 +0530

    fixing import error

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -13,9 +13,8 @@ from os import environ, execle, path, remove
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-from .. import CMD_HELP
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import runcmd
+from . import CMD_HELP, runcmd
 
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 HEROKU_API_KEY = Var.HEROKU_API_KEY
