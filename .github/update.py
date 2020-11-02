71f9967608194c983feab8622aedf16f808eb8c9
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-11-02 11:29:59 +0530

    Update update.py

--- .github/update.py
+++ .github/update.py
@@ -1,6 +1,21 @@
 import asyncio
+import os
+import shlex
+from typing import Optional, Tuple
 
-from userbot import runcmd
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
 
 
 async def update_requirements():
