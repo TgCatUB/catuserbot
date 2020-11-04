6da254648b5288314594d370db8f4b8d29c18d23
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-11-04 20:42:25 +0000

    pylint: auto fixes

--- userbot/plugins/fonts.py
+++ userbot/plugins/fonts.py
@@ -1,5 +1,5 @@
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import fonts, CMD_HELP
+from . import CMD_HELP, fonts
 
 
 @bot.on(admin_cmd(pattern="fmusical(?: |$)(.*)", command="fmusical"))
@@ -186,8 +186,10 @@ async def stylish_generator(event):
             string = string.replace(normaltextcharacter, smothtextcharacter)
     await edit_or_reply(event, string)
 
-CMD_HELP.update({
-    "fonts":"""**Plugin : **`fonts`
+
+CMD_HELP.update(
+    {
+        "fonts": """**Plugin : **`fonts`
 
 **Commands found in fonts are**
   •  `.fmusical`
@@ -200,4 +202,6 @@ CMD_HELP.update({
   •  `.boxf`
   •  `.smothtext`
 
-**Function : **__Reply the command to the text message or give input along with command to convert that text to given font style__"""})
+**Function : **__Reply the command to the text message or give input along with command to convert that text to given font style__"""
+    }
+)
