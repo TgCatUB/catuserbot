08f02090d8e62ef9891588c4778a01e19ef804df
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-05 00:07:45 +0530

    Update animation2.py

--- userbot/plugins/animation2.py
+++ userbot/plugins/animation2.py
@@ -189,6 +189,6 @@ CMD_HELP.update(
   •  `.smoon`
   •  `.tmoon`
   
-**Function : **__Different kinds of animation commands check yourself for there animation .__"""
+**Function : **__Different kinds of animation commands check yourself for their animation .__"""
     }
 )
