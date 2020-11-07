678d645ba5e9847bffb7cea19d22ed54ebca90ab
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-07 14:07:44 +0530

    Update utils.py

--- userbot/helpers/utils.py
+++ userbot/helpers/utils.py
@@ -28,7 +28,7 @@ def parse_pre(text):
     text = text.strip()
     return (
         text,
-        [MessageEntityPre(offset=0, length=len(add_surrogate(text)), language="")],
+        [MessageEntityPre(offset=0, length=len(add_surrogate(text)), language='')]
     )
 
 
@@ -43,26 +43,26 @@ def yaml_format(obj, indent=0, max_str_len=256, max_byte_len=64):
 
     if isinstance(obj, dict):
         if not obj:
-            return "dict:"
+            return 'dict:'
         items = obj.items()
         has_items = len(items) > 1
         has_multiple_items = len(items) > 2
-        result.append(obj.get("_", "dict") + (":" if has_items else ""))
+        result.append(obj.get('_', 'dict') + (':' if has_items else ''))
         if has_multiple_items:
-            result.append("\n")
+            result.append('\n')
             indent += 2
         for k, v in items:
-            if k == "_" or v is None:
+            if k == '_' or v is None:
                 continue
             formatted = yaml_format(v, indent)
             if not formatted.strip():
                 continue
-            result.append(" " * (indent if has_multiple_items else 1))
-            result.append(f"{k}:")
+            result.append(' ' * (indent if has_multiple_items else 1))
+            result.append(f'{k}:')
             if not formatted[0].isspace():
-                result.append(" ")
-            result.append(f"{formatted}")
-            result.append("\n")
+                result.append(' ')
+            result.append(f'{formatted}')
+            result.append('\n')
         if has_items:
             result.pop()
         if has_multiple_items:
@@ -71,26 +71,28 @@ def yaml_format(obj, indent=0, max_str_len=256, max_byte_len=64):
         # truncate long strings and display elipsis
         result = repr(obj[:max_str_len])
         if len(obj) > max_str_len:
-            result += "…"
+            result += '…'
         return result
-    if isinstance(obj, bytes):
+    elif isinstance(obj, bytes):
         # repr() bytes if it's printable, hex like "FF EE BB" otherwise
-        if all(0x20 <= c < 0x7F for c in obj):
+        if all(0x20 <= c < 0x7f for c in obj):
             return repr(obj)
-        return "<…>" if len(obj) > max_byte_len else " ".join(f"{b:02X}" for b in obj)
-    if isinstance(obj, datetime.datetime):
+        else:
+            return ('<…>' if len(obj) > max_byte_len else
+                    ' '.join(f'{b:02X}' for b in obj))
+    elif isinstance(obj, datetime.datetime):
         # ISO-8601 without timezone offset (telethon dates are always UTC)
-        return obj.strftime("%Y-%m-%d %H:%M:%S")
-    if hasattr(obj, "__iter__"):
+        return obj.strftime('%Y-%m-%d %H:%M:%S')
+    elif hasattr(obj, '__iter__'):
         # display iterables one after another at the base indentation level
-        result.append("\n")
+        result.append('\n')
         indent += 2
         for x in obj:
             result.append(f"{' ' * indent}- {yaml_format(x, indent + 2)}")
-            result.append("\n")
+            result.append('\n')
         result.pop()
         indent -= 2
     else:
         return repr(obj)
 
-    return "".join(result)
+    return ''.join(result)
