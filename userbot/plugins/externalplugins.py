f265a2dea2b5ce9d967475aee7ed78e556a3fcb7
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-11 22:09:01 +0530

    Update externalplugins.py

--- userbot/plugins/externalplugins.py
+++ userbot/plugins/externalplugins.py
@@ -1,38 +1,39 @@
-import asyncio
-import os
-from pathlib import Path
+if Config.PLUGIN_CHANNEL:    
+    import asyncio
+    import os
+    from pathlib import Path
 
-from telethon.tl.types import InputMessagesFilterDocument
+    from telethon.tl.types import InputMessagesFilterDocument
 
-from ..utils import load_module
-from . import BOTLOG_CHATID
+    from ..utils import load_module
+    from . import BOTLOG_CHATID
 
 
-async def install():
-    documentss = await bot.get_messages(
-        Config.PLUGIN_CHANNEL, None, filter=InputMessagesFilterDocument
-    )
-    total = int(documentss.total)
-    for module in range(total):
-        plugin_to_install = documentss[module].id
-        downloaded_file_name = await bot.download_media(
-            await bot.get_messages(chat, ids=plugin_to_install), "userbot/plugins/"
+    async def install():
+        documentss = await bot.get_messages(
+            Config.PLUGIN_CHANNEL, None, filter=InputMessagesFilterDocument
         )
-        if "(" not in downloaded_file_name:
-            path1 = Path(downloaded_file_name)
-            shortname = path1.stem
-            load_module(shortname.replace(".py", ""))
-            await bot.send_message(
-                BOTLOG_CHATID,
-                f"Installed Plugin `{os.path.basename(downloaded_file_name)}` successfully.",
-            ),
-        else:
-            await bot.send_message(
-                BOTLOG_CHATID,
-                f"Plugin `{os.path.basename(downloaded_file_name)}` has been pre-installed and cannot be installed.",
+        total = int(documentss.total)
+        for module in range(total):
+            plugin_to_install = documentss[module].id
+            downloaded_file_name = await bot.download_media(
+                await bot.get_messages(chat, ids=plugin_to_install), "userbot/plugins/"
             )
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                await bot.send_message(
+                    BOTLOG_CHATID,
+                    f"Installed Plugin `{os.path.basename(downloaded_file_name)}` successfully.",
+                ),
+            else:
+                await bot.send_message(
+                    BOTLOG_CHATID,
+                    f"Plugin `{os.path.basename(downloaded_file_name)}` has been pre-installed and cannot be installed.",
+                )
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(install())
-loop.close()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(install())
+    loop.close()
