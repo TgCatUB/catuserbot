1500c3989cf215e080a2e2543b93744c892536af
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-11 20:15:00 +0530

    Update externalplugins.py

--- userbot/plugins/externalplugins.py
+++ userbot/plugins/externalplugins.py
@@ -1,57 +1,33 @@
 import os
+import asyncio
 from pathlib import Path
-
 from telethon.tl.types import InputMessagesFilterDocument
+from ..utils import load_module
+from . import BOTLOG_CHATID
 
-from userbot.utils import load_module
-from var import Var
-
-from .. import CMD_HELP
-from ..utils import admin_cmd, sudo_cmd
-
-
-@bot.on(admin_cmd(pattern="extdl$", outgoing=True))
-@bot.on(sudo_cmd(pattern="extdl$", allow_sudo=True))
-async def install(event):
-    if event.fwd_from:
-        return
-    chat = Var.PLUGIN_CHANNEL
-    documentss = await event.client.get_messages(
-        chat, None, filter=InputMessagesFilterDocument
-    )
+async def install():
+    documentss = await bot.get_messages(Config.PLUGIN_CHANNEL, None, filter=InputMessagesFilterDocument)
     total = int(documentss.total)
-    total_doxx = range(total)
-    await event.delete()
-    for ixo in total_doxx:
-        mxo = documentss[ixo].id
-        downloaded_file_name = await event.client.download_media(
-            await event.client.get_messages(chat, ids=mxo), "userbot/plugins/"
+    for module in range(total):
+        plugin_to_install = documentss[module].id
+        downloaded_file_name = await bot.download_media(
+            await bot.get_messages(chat, ids=plugin_to_install), "userbot/plugins/"
         )
         if "(" not in downloaded_file_name:
             path1 = Path(downloaded_file_name)
             shortname = path1.stem
             load_module(shortname.replace(".py", ""))
-            await event.client.send_message(
-                event.chat_id,
-                "Installed Plugin `{}` successfully.".format(
-                    os.path.basename(downloaded_file_name)
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"Installed Plugin `{os.path.basename(downloaded_file_name)}` successfully."
                 ),
             )
         else:
-            await event.client.send_message(
-                event.chat_id,
-                "Plugin `{}` has been pre-installed and cannot be installed.".format(
-                    os.path.basename(downloaded_file_name)
-                ),
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"Plugin `{os.path.basename(downloaded_file_name)}` has been pre-installed and cannot be installed."
             )
 
-
-CMD_HELP.update(
-    {
-        "externalplugins": "**externalplugins**\
-    \n**Syntax :** `.extdl`\
-    \n**Usage : **Create a private channel and post all your external modules there and set a var in heroku as `PLUGIN_CHANNEL` and value with channel id \
-    type  `.extdl` to install all external modules after each restart or update. \
-    "
-    }
-)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(install())
+loop.close()
