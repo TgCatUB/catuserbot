d368275ea00833611cf543195f54ad288a250a2c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-17 17:15:45 +0530

    Update json.py

--- userbot/plugins/json.py
+++ userbot/plugins/json.py
@@ -2,7 +2,7 @@
 import io
 
 from ..utils import admin_cmd, sudo_cmd
-from . import parse_pre, reply_id, yaml_format
+from . import parse_pre, reply_id, yaml_format,CMD_HELP
 
 
 @bot.on(admin_cmd(pattern="json$"))
@@ -51,3 +51,13 @@ async def _(event):
             await event.delete()
     else:
         await edit_or_reply(event, the_real_message, parse_mode=parse_pre)
+
+CMD_HELP.update({
+    "json":"""**Plugin : **`json`
+
+  •  **Syntax : **`.json reply`
+  •  **Function : **__reply to a message to get details of that message in json format__  
+
+  •  **Syntax : **`.yaml reply`
+  •  **Function : **__reply to a message to get details of that message in yaml format__ """
+})        
