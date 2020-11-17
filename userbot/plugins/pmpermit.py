0e47c1ca4d2dbb32f9c2abce8fed098f5279d328
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-18 00:56:48 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -5,7 +5,7 @@ from telethon import events, functions
 from telethon.tl.functions.users import GetFullUserRequest
 
 from ..utils import admin_cmd
-from . import ALIVE_NAME, CMD_HELP, PM_START, PMMENU, check
+from . import ALIVE_NAME, CMD_HELP, PM_START, PMMENU, check, get_user_from_event
 from .sql_helper import pmpermit_sql as pmpermit_sql
 
 PM_WARNS = {}
@@ -16,65 +16,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 USER_BOT_WARN_ZERO = "You were spamming my peru master's inbox, henceforth you are blocked by my master's userbot. **Now GTFO, i'm playing minecraft** "
 
 if Config.PRIVATE_GROUP_ID is not None:
-
-    @bot.on(admin_cmd(pattern="approve ?(.*)"))
-    async def approve_p_m(event):
-        if event.fwd_from:
-            return
-        reason = event.pattern_match.group(1)
-        if event.is_private:
-            replied_user = await event.client(GetFullUserRequest(event.chat_id))
-            firstname = replied_user.user.first_name
-            chat = await event.get_chat()
-            if not pmpermit_sql.is_approved(chat.id):
-                if chat.id in PM_WARNS:
-                    del PM_WARNS[chat.id]
-                if chat.id in PREV_REPLY_MESSAGE:
-                    await PREV_REPLY_MESSAGE[chat.id].delete()
-                    del PREV_REPLY_MESSAGE[chat.id]
-                if chat.id in PM_START:
-                    PM_START.remove(chat.id)
-                pmpermit_sql.approve(chat.id, reason)
-                await event.edit(
-                    "Approved to pm [{}](tg://user?id={})".format(firstname, chat.id)
-                )
-            else:
-                await event.edit(
-                    "[{}](tg://user?id={}) is already in approved list".format(
-                        firstname, chat.id
-                    )
-                )
-            await asyncio.sleep(3)
-            await event.delete()
-            return
-        if event.reply_to_msg_id:
-            reply = await event.get_reply_message()
-            replied_user = await event.client.get_entity(reply.sender_id)
-            chat = replied_user.id
-            firstname = str(replied_user.first_name)
-            if not pmpermit_sql.is_approved(chat):
-                if chat in PM_WARNS:
-                    del PM_WARNS[chat]
-                if chat in PREV_REPLY_MESSAGE:
-                    await PREV_REPLY_MESSAGE[chat].delete()
-                    del PREV_REPLY_MESSAGE[chat]
-                if chat in PM_START:
-                    PM_START.remove(chat)
-                pmpermit_sql.approve(chat, reason)
-                await event.edit(
-                    "Approved to pm [{}](tg://user?id={})".format(firstname, chat)
-                )
-            else:
-                await event.edit(
-                    "[{}](tg://user?id={}) is already in approved list".format(
-                        firstname, chat
-                    )
-                )
-
-            await asyncio.sleep(3)
-            await event.delete()
-
-    @bot.on(events.NewMessage(outgoing=True))
+    @bot.on(admin_cmd(outgoing=True))
     async def you_dm_niqq(event):
         if event.fwd_from:
             return
@@ -88,93 +30,85 @@ if Config.PRIVATE_GROUP_ID is not None:
         ):
             pmpermit_sql.approve(chat.id, "outgoing")
 
-    @bot.on(admin_cmd(pattern="disapprove ?(.*)"))
-    async def disapprove_p_m(event):
-        if event.fwd_from:
-            return
+    @bot.on(admin_cmd(pattern="(a|approve) ?(.*)"))
+    async def approve_p_m(event):
         if event.is_private:
-            replied_user = await event.client(GetFullUserRequest(event.chat_id))
-            firstname = replied_user.user.first_name
-            chat = await event.get_chat()
-            if chat.id in PM_START:
-                PM_START.remove(chat.id)
-            if pmpermit_sql.is_approved(chat.id):
-                pmpermit_sql.disapprove(chat.id)
-                await event.edit(
-                    "disapproved to pm [{}](tg://user?id={})".format(firstname, chat.id)
-                )
-            else:
-                await event.edit(
-                    "[{}](tg://user?id={}) is not yet approved".format(
-                        firstname, chat.id
-                    )
-                )
-            return
-        if event.reply_to_msg_id:
-            reply = await event.get_reply_message()
-            chat = await event.client.get_entity(reply.sender_id)
-            firstname = str(chat.first_name)
-            if chat.id in PM_START:
-                PM_START.remove(chat.id)
-            if pmpermit_sql.is_approved(chat.id):
-                pmpermit_sql.disapprove(chat.id)
-                await event.edit(
-                    "disapproved to pm [{}](tg://user?id={})".format(firstname, chat.id)
-                )
-            else:
-                await event.edit(
-                    "[{}](tg://user?id={}) is not yet approved".format(
-                        firstname, chat.id
-                    )
-                )
+            user = await event.get_chat()
+            reason = event.pattern_match.group(1)
+        else:
+            user, reason = await get_user_from_event(event)
+            if not user:
+                return await edit_delete(event , "`Couldn't Fectch user`", 5)
+            if not reason:
+                reason = "Not mentioned"
+        if not pmpermit_sql.is_approved(user.id):
+            if user.id in PM_WARNS:
+                del PM_WARNS[user.id]
+            if user.id in PREV_REPLY_MESSAGE:
+                await PREV_REPLY_MESSAGE[user.id].delete()
+                del PREV_REPLY_MESSAGE[user.id]
+            if user.id in PM_START:
+                PM_START.remove(user.id)
+            pmpermit_sql.approve(user.id, reason)
+            await edit_delete(event , 
+                "Approved to pm [{user.first_name}](tg://user?id={user.id})", 5
+            )
+        else:
+            await edit_delete(event,
+                "[{user.first_name}](tg://user?id={user.id}) is already in approved list", 5
+            )
+
+    @bot.on(admin_cmd(pattern="(da|disapprove)$"))
+    async def disapprove_p_m(event):
+        if event.is_private:
+            user = await event.get_chat()
+        else:
+            user, reason = await get_user_from_event(event)
+            if not user:
+                return await edit_delete(event , "`Couldn't Fectch user`", 5)
+        if user.id in PM_START:
+            PM_START.remove(user.id)
+        if pmpermit_sql.is_approved(user.id):
+            pmpermit_sql.disapprove(user.id)
+            await edit_delete(event,
+                "disapproved to pm [{user.first_name}](tg://user?id={user.id})" , 5
+            )
+        else:
+            await edit_delete(event,
+                "[{user.first_name}](tg://user?id={user.id}) is not yet approved", 5
+            )
 
     @bot.on(admin_cmd(pattern="block$"))
     async def block_p_m(event):
-        if event.fwd_from:
-            return
         if event.is_private:
-            replied_user = await event.client(GetFullUserRequest(event.chat_id))
-            firstname = replied_user.user.first_name
-            chat = await event.get_chat()
-            if chat.id in PM_START:
-                PM_START.remove(chat.id)
-            await event.edit(
-                "`You have been blocked. Now You Can't Message Me..`[{}](tg://user?id={})".format(
-                    firstname, chat.id
-                )
-            )
-            await event.client(functions.contacts.BlockRequest(chat.id))
-            return
-        if event.reply_to_msg_id:
-            reply = await event.get_reply_message()
-            chat = await event.client.get_entity(reply.sender_id)
-            firstname = str(chat.first_name)
-            if chat.id in PM_START:
-                PM_START.remove(chat.id)
-            await event.edit(
-                "`You have been blocked. Now You Can't Message Me..`[{}](tg://user?id={})".format(
-                    firstname, chat.id
-                )
-            )
-            await event.client(functions.contacts.BlockRequest(chat.id))
+            user = await event.get_chat()
+        else:
+            user, reason = await get_user_from_event(event)
+            if not user:
+                return await edit_delete(event , "`Couldn't Fectch user`", 5)
+        if user.id in PM_START:
+            PM_START.remove(user.id)
+        await event.edit(
+            "`You are blocked Now .You Can't Message Me from now..`[{user.first_name}](tg://user?id={user.id})"
+        )
+        await event.client(functions.contacts.BlockRequest(user.id))
 
     @bot.on(admin_cmd(pattern="unblock$"))
     async def unblock_pm(event):
-        if event.reply_to_msg_id:
-            reply = await event.get_reply_message()
-            chat = await event.client.get_entity(reply.sender_id)
-            firstname = str(chat.first_name)
-            await event.client(functions.contacts.UnblockRequest(chat.id))
-            await event.edit(
-                "`You have been unblocked. Now You Can Message Me..`[{}](tg://user?id={})".format(
-                    firstname, chat.id
-                )
-            )
+        if event.is_private:
+            user = await event.get_chat()
+        else:
+            user, reason = await get_user_from_event(event)
+            if not user:
+                return await edit_delete(event , "`Couldn't Fectch user`", 5)
+        await event.client(functions.contacts.UnblockRequest(user.id))
+        await asyncio.sleep(2)
+        await event.edit(
+            "`You are Unblocked Now .You Can Message Me From now..`[{user.first_name}](tg://user?id={user.id})"
+        )
 
     @bot.on(admin_cmd(pattern="listapproved$"))
     async def approve_p_m(event):
-        if event.fwd_from:
-            return
         approved_users = pmpermit_sql.get_all_approved()
         APPROVED_PMs = "Current Approved PMs\n"
         if len(approved_users) > 0:
@@ -189,7 +123,7 @@ if Config.PRIVATE_GROUP_ID is not None:
             APPROVED_PMs = "no Approved PMs (yet)"
         if len(APPROVED_PMs) > 4095:
             with io.BytesIO(str.encode(APPROVED_PMs)) as out_file:
-                out_file.name = "approved.pms.text"
+                out_file.name = "approvedpms.text"
                 await event.client.send_file(
                     event.chat_id,
                     out_file,
@@ -215,7 +149,6 @@ if Config.PRIVATE_GROUP_ID is not None:
             message_text = event.message.message
             chat_id = event.sender_id
             USER_BOT_NO_WARN = (
-                f"[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id={chat_id})\n\n"
                 f"My master {DEFAULTUSER} haven't approved you yet. Don't spam his inbox "
                 "Leave your name,reason and 10k$ and hopefully you'll get a reply within 2 light years.\n\n"
                 "**Send** `/start` ** so that my master can decide why you're here.**"
@@ -271,34 +204,21 @@ if Config.PRIVATE_GROUP_ID is not None:
                 except BaseException:
                     return
             catid = chat_id
+            if Config.CUSTOM_PMPERMIT_TEXT:
+                USER_BOT_NO_WARN = (
+                    Config.CUSTOM_PMPERMIT_TEXT
+                    + "\n\n"
+                    + "**Send** `/start` ** so that my master can decide why you're here.**"
+                )
+            else:
+                USER_BOT_NO_WARN = (
+                    f"My master {DEFAULTUSER} haven't approved you yet. Don't spam his inbox "
+                    "Leave your name,reason and 10k$ and hopefully you'll get a reply within 2 light years.\n\n"
+                    "**Send** `/start` ** so that my master can decide why you're here.**"
+                )
             if PMPERMIT_PIC:
-                if Config.CUSTOM_PMPERMIT_TEXT:
-                    USER_BOT_NO_WARN = (
-                        Config.CUSTOM_PMPERMIT_TEXT
-                        + "\n\n"
-                        + "**Send** `/start` ** so that my master can decide why you're here.**"
-                    )
-                else:
-                    USER_BOT_NO_WARN = (
-                        f"My master {DEFAULTUSER} haven't approved you yet. Don't spam his inbox "
-                        "Leave your name,reason and 10k$ and hopefully you'll get a reply within 2 light years.\n\n"
-                        "**Send** `/start` ** so that my master can decide why you're here.**"
-                    )
                 r = await event.reply(USER_BOT_NO_WARN, file=PMPERMIT_PIC)
             else:
-                if Config.CUSTOM_PMPERMIT_TEXT:
-                    USER_BOT_NO_WARN = (
-                        Config.CUSTOM_PMPERMIT_TEXT
-                        + "\n\n"
-                        + "**Send** `/start` ** so that my master can decide why you're here.**"
-                    )
-                else:
-                    USER_BOT_NO_WARN = (
-                        f"[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id={catid})\n\n"
-                        f"My master {DEFAULTUSER} haven't approved you yet. Don't spam his inbox "
-                        "Leave your name,reason and 10k$ and hopefully you'll get a reply within 2 light years.\n\n"
-                        "**Send** `/start` ** so that my master can decide why you're here.**"
-                    )
                 r = await event.reply(USER_BOT_NO_WARN)
             PM_WARNS[chat_id] += 1
             if chat_id in PREV_REPLY_MESSAGE:
@@ -318,7 +238,6 @@ if Config.PRIVATE_GROUP_ID is not None:
             message_text = event.message.message
             chat_id = event.sender_id
             USER_BOT_NO_WARN = (
-                f"[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id={chat_id})\n\n"
                 f"My master {DEFAULTUSER} haven't approved you yet. Don't spam his inbox "
                 "Leave your name,reason and 10k$ and hopefully you'll get a reply within 2 light years.\n\n"
                 "**Send** `/start` ** so that my master can decide why you're here.**"
@@ -366,24 +285,16 @@ if Config.PRIVATE_GROUP_ID is not None:
                 except BaseException:
                     return
             catid = chat_id
+            if Config.CUSTOM_PMPERMIT_TEXT:
+                USER_BOT_NO_WARN = Config.CUSTOM_PMPERMIT_TEXT
+            else:
+                USER_BOT_NO_WARN = (
+                    f"My master {DEFAULTUSER} haven't approved you yet. Don't spam his inbox "
+                    "Leave your name,reason and 10k$ and hopefully you'll get a reply within 2 light years."
+                )
             if PMPERMIT_PIC:
-                if Config.CUSTOM_PMPERMIT_TEXT:
-                    USER_BOT_NO_WARN = Config.CUSTOM_PMPERMIT_TEXT
-                else:
-                    USER_BOT_NO_WARN = (
-                        f"My master {DEFAULTUSER} haven't approved you yet. Don't spam his inbox "
-                        "Leave your name,reason and 10k$ and hopefully you'll get a reply within 2 light years."
-                    )
                 r = await event.reply(USER_BOT_NO_WARN, file=PMPERMIT_PIC)
             else:
-                if Config.CUSTOM_PMPERMIT_TEXT:
-                    USER_BOT_NO_WARN = Config.CUSTOM_PMPERMIT_TEXT
-                else:
-                    USER_BOT_NO_WARN = (
-                        f"[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id={catid})\n\n"
-                        f"My master {DEFAULTUSER} haven't approved you yet. Don't spam his inbox "
-                        "Leave your name,reason and 10k$ and hopefully you'll get a reply within 2 light years."
-                    )
                 r = await event.reply(USER_BOT_NO_WARN)
             PM_WARNS[chat_id] += 1
             if chat_id in PREV_REPLY_MESSAGE:
@@ -394,16 +305,16 @@ if Config.PRIVATE_GROUP_ID is not None:
 CMD_HELP.update(
     {
         "pmpermit": "**Plugin : **`pmpermit`\
-        \n\n**Syntax : **`.approve`\
-        \n**Function : **__Approves the mentioned/replied person to PM.__\
-        \n\n**Syntax : **`.disapprove`\
-        \n**Function : **__dispproves the mentioned/replied person to PM.__\
-        \n\n**Syntax : **`.block`\
-        \n**Function : **__Blocks the person.__\
-        \n\n**Syntax : **`.unblock`\
-        \n**Function : **__Unblocks the person.__\
-        \n\n**Syntax : **`.listapproved`\
-        \n**Function : **__To list the all approved users.__\
+        \n\n  •  **Syntax : **`.approve or .a`\
+        \n  •  **Function : **__Approves the mentioned/replied person to PM.__\
+        \n\n  •  **Syntax : **`.disapprove or .da`\
+        \n  •  **Function : **__dispproves the mentioned/replied person to PM.__\
+        \n\n  •  **Syntax : **`.block`\
+        \n  •  **Function : **__Blocks the person.__\
+        \n\n  •  **Syntax : **`.unblock`\
+        \n  •  **Function : **__Unblocks the person.__\
+        \n\n  •  **Syntax : **`.listapproved`\
+        \n  •  **Function : **__To list the all approved users.__\
 "
     }
 )
