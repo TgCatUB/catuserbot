27a0707956712b6a63c825e3b7994038b482d3aa
baderA34 <47380139+baderA34@users.noreply.github.com>
2020-11-22 17:45:17 +0400

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -24,12 +24,12 @@ async def amireallyalive(alive):
         reply_to_id = await alive.get_reply_message()
     if CAT_IMG:
         cat_caption = f"**✮ MY BOT IS RUNNING SUCCESFULLY ✮**\n\n"
-        cat_caption += f"**✧ Database :** `{check_sgnirts}`\n"
-        cat_caption += f"**✧ Telethon version :** `{version.__version__}\n`"
-        cat_caption += f"**✧ Catuserbot Version :** `{catversion}`\n"
-        cat_caption += f"**✧ Python Version :** `{python_version()}\n`"
-        cat_caption += f"**✧ Uptime :** `{uptime}\n`"
-        cat_caption += f"**✧ Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n"
+        cat_caption += f"**☞ Database :** `{check_sgnirts}`\n"
+        cat_caption += f"**☞ Telethon version :** `{version.__version__}\n`"
+        cat_caption += f"**☞ Catuserbot Version :** `{catversion}`\n"
+        cat_caption += f"**☞ Python Version :** `{python_version()}\n`"
+        cat_caption += f"**☞ Uptime :** `{uptime}\n`"
+        cat_caption += f"**☞ Master:** [{DEFAULTUSER}](tg://user?id={hmm})\n"
         await alive.client.send_file(
             alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id
         )
