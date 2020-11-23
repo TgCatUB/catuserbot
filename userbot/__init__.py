143634387fbe406f4ea9834cf6205dcb5e7c39a3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-24 02:00:17 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -12,7 +12,7 @@ from telethon.sessions import StringSession
 from .Config import Config
 
 StartTime = time.time()
-catversion = "2.9.2"
+catversion = "2.9.3"
 
 if Config.STRING_SESSION:
     session_name = str(Config.STRING_SESSION)
