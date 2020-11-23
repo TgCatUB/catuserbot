6239acad0a372c54db56d8cfcfb37e5cea167fb0
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-11-23 19:17:48 +0000

    pylint: auto fixes

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -88,13 +88,15 @@ def check(cat):
         hi = False
     return bool(hi)
 
+
 def set_key(dictionary, key, value):
-     if key not in dictionary:
-         dictionary[key] = value
-     elif type(dictionary[key]) == list:
-         dictionary[key].append(value)
-     else:
-         dictionary[key] = [dictionary[key], value]
+    if key not in dictionary:
+        dictionary[key] = value
+    elif type(dictionary[key]) == list:
+        dictionary[key].append(value)
+    else:
+        dictionary[key] = [dictionary[key], value]
+
 
 # UniBorg Telegram UseRBot
 # Copyright (C) 2020 @UniBorg
