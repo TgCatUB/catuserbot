f98cde7e07db8f4f7782c8f7dd665a6fb9d68a3a
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-11-23 19:34:26 +0000

    pylint: auto fixes

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -10,7 +10,7 @@ from telethon import events, functions
 
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
 
-from . import ALIVE_NAME, PM_START, set_key, PMMESSAGE_CACHE
+from . import ALIVE_NAME, PM_START, PMMESSAGE_CACHE, set_key
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 PREV_REPLY_MESSAGE = {}
