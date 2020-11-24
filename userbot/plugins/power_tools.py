88c50436c27a1dd58fd88b85c48ed287990ca4b6
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-11-24 07:49:11 +0000

    pylint: auto fixes

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -3,7 +3,7 @@ from os import execl
 from time import sleep
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, HEROKU_APP
+from . import BOTLOG, BOTLOG_CHATID, CMD_HELP, HEROKU_APP, bot
 
 
 @bot.on(admin_cmd(pattern="restart$"))
@@ -28,11 +28,16 @@ async def _(event):
         return
     if HEROKU_APP is not None:
         if BOTLOG:
-            await event.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n" "Bot shut down")
+            await event.client.send_message(
+                BOTLOG_CHATID, "#SHUTDOWN \n" "Bot shut down"
+            )
         await edit_or_reply(event, "`Turning off bot now ...Manually turn me on later`")
-        HEROKU_APP.process_formation()['userbot'].scale(0)  
+        HEROKU_APP.process_formation()["userbot"].scale(0)
     else:
-        await edit_or_reply(event, "`Set HEROKU_APP_NAME and HEROKU_API_KEY to work this function properly`")
+        await edit_or_reply(
+            event,
+            "`Set HEROKU_APP_NAME and HEROKU_API_KEY to work this function properly`",
+        )
         await bot.disconnect()
 
 
