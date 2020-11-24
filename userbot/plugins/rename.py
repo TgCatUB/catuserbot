f0ea54bffa92651fe362001d33c18e11ce3373f3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-25 02:12:11 +0530

    Update rename.py

--- userbot/plugins/rename.py
+++ userbot/plugins/rename.py
@@ -61,6 +61,7 @@ async def _(event):
         event,
         "`Rename & Upload in process 🙄🙇‍♂️🙇‍♂️🙇‍♀️ It might take some time if file size is big`",
     )
+    reply_to_id = await reply_id(event)
     input_str = event.pattern_match.group(1)
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
@@ -92,7 +93,7 @@ async def _(event):
                 force_document=False,
                 supports_streaming=True,
                 allow_cache=False,
-                reply_to=event.message.id,
+                reply_to=reply_to_id,
                 thumb=thumb,
                 progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
                     progress(
@@ -115,14 +116,83 @@ async def _(event):
             "**Syntax : **`.rnupload file.name` as reply to a Telegram media"
         )
 
+@bot.on(admin_cmd(pattern="rnupf (.*)"))
+@bot.on(sudo_cmd(pattern="rnupf (.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    thumb = None
+    if os.path.exists(thumb_image_path):
+        thumb = thumb_image_path
+    catevent = await edit_or_reply(
+        event,
+        "`Rename & Upload in process 🙄🙇‍♂️🙇‍♂️🙇‍♀️ It might take some time if file size is big`",
+    )
+    reply_to_id = await reply_id(event)
+    input_str = event.pattern_match.group(1)
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if event.reply_to_msg_id:
+        start = datetime.now()
+        file_name = input_str
+        reply_message = await event.get_reply_message()
+        c_time = time.time()
+        to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
+        downloaded_file_name = os.path.join(to_download_directory, file_name)
+        downloaded_file_name = await event.client.download_media(
+            reply_message,
+            downloaded_file_name,
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(d, t, catevent, c_time, "trying to download", file_name)
+            ),
+        )
+        end = datetime.now()
+        ms_one = (end - start).seconds
+        try:
+            thumb = await reply_message.download_media(thumb=-1)
+        except Exception:
+            thumb = thumb
+        if os.path.exists(downloaded_file_name):
+            c_time = time.time()
+            caat = await event.client.send_file(
+                event.chat_id,
+                downloaded_file_name,
+                force_document=True,
+                supports_streaming=False,
+                allow_cache=False,
+                reply_to=reply_to_id,
+                thumb=thumb,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(
+                        d, t, event, c_time, "trying to upload", downloaded_file_name
+                    )
+                ),
+            )
+            end_two = datetime.now()
+            os.remove(downloaded_file_name)
+            ms_two = (end_two - end).seconds
+            await catevent.edit(
+                f"`Downloaded file in {ms_one} seconds.`\n`Uploaded in {ms_two} seconds.`"
+            )
+            await asyncio.sleep(3)
+            await catevent.delete()
+        else:
+            await catevent.edit("File Not Found {}".format(input_str))
+    else:
+        await catevent.edit(
+            "**Syntax : **`.rnupload file.name` as reply to a Telegram media"
+        )
+        
 
 CMD_HELP.update(
     {
         "rename": "**Plugin : **`rename`\
-    \n\n**Syntax : **`.rename filename`\
-    \n**Function : **__Reply to media with above command to save in your server with that given filename__\
-    \n\n**Syntax : **`.rnup filename`\
-    \n**Function : **__Reply to media with above command to rename and upload the file with given name__\
-    "
+        \n\n  •  **Syntax : **`.rename filename`\
+        \n  •  **Function : **__Reply to media with above command to save in your server with that given filename__\
+        \n\n  •  **Syntax : **`.rnup filename`\
+        \n  •  **Function : **__Reply to media with above command to rename and upload the file with given name as steam__\
+        \n\n  •  **Syntax : **`.rnupf filename`\
+        \n  •  **Function : **__Reply to media with above command to rename and upload the file with given name as file__\
+        "
     }
 )
