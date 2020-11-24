a7d88901083f88e50f5871e705eb25b3a93bb933
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-24 17:58:19 +0530

    Update torrentutils.py

--- userbot/plugins/torrentutils.py
+++ userbot/plugins/torrentutils.py
@@ -51,8 +51,10 @@ TEMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
 if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
     os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
 download_path = os.getcwd() + TEMP_DOWNLOAD_DIRECTORY.strip(".")
-
-aria2 = aria2p.API(aria2p.Client(host="http://localhost", port=6800, secret=""))
+try:
+    aria2 = aria2p.API(aria2p.Client(host="http://localhost", port=6800, secret=""))
+except:
+    aria2 = aria2p.API(aria2p.Client(host="http://localhost", port=8210, secret=""))
 
 aria2.set_global_options({"dir": download_path})
 
