4c94a6476746f468b2306db98610baf0a24da0ac
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-11-24 08:40:05 +0000

    pylint: auto fixes

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -252,11 +252,11 @@ if Config.PRIVATE_GROUP_ID is not None:
             else:
 
                 USER_BOT_NO_WARN = (
-                                    f"`Hi `{mention}`, I haven't approved you yet to personal message me, Don't spam my inbox."
-                                    f"Just say the reason and wait until you get approved.\
+                    f"`Hi `{mention}`, I haven't approved you yet to personal message me, Don't spam my inbox."
+                    f"Just say the reason and wait until you get approved.\
                                     \n\nyou have {warns}/{totalwarns} warns`\
                                     \n\n**Send** `/start` **so that my master can decide why you're here.**"
-                                )
+                )
         else:
             if Config.CUSTOM_PMPERMIT_TEXT:
                 USER_BOT_NO_WARN = Config.CUSTOM_PMPERMIT_TEXT.format(
@@ -276,8 +276,8 @@ if Config.PRIVATE_GROUP_ID is not None:
                 )
             else:
                 USER_BOT_NO_WARN = (
-                                    f"`Hi `{mention}`, I haven't approved you yet to personal message me, Don't spam my inbox."
-                                    f"Just say the reason and wait until you get approved.\
+                    f"`Hi `{mention}`, I haven't approved you yet to personal message me, Don't spam my inbox."
+                    f"Just say the reason and wait until you get approved.\
                                     \n\nyou have {warns}/{totalwarns} warns`"
                 )
         if PMPERMIT_PIC:
