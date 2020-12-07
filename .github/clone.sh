b6f68de898ff4c71fa3d74f9b96e219b5af1966a
Md Jisan <66010486+Jisan09@users.noreply.github.com>
2020-12-08 02:03:52 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -32,7 +32,7 @@ else
     rm -rf userbot
     rm -rf .github
     rm -rf requirements.txt
-    git clone -b upmaster https://github.com/Jisan09/catuserbot cat_ub
+    git clone https://github.com/Jisan09/catuserbot cat_ub
     mv cat_ub/userbot .
     mv cat_ub/.github . 
     mv cat_ub/.git .
