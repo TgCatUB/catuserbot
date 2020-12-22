c54633a03228fdef61cc4e125c561097a5749226
HiddenCrush <63924486+HiddenCrush@users.noreply.github.com>
2020-12-22 12:44:43 +0530

    Making it look nice. (#136)

--- userbot/plugins/covid.py
+++ userbot/plugins/covid.py
@@ -12,7 +12,7 @@ async def corona(event):
         country = (event.pattern_match.group(1)).title()
     else:
         country = "World"
-    catevent = await edit_or_reply(event, "`collecting data...........`")
+    catevent = await edit_or_reply(event, "`Collecting data...`")
     covid = Covid(source="worldometers")
     try:
         country_data = covid.get_status_by_country_name(country)
@@ -22,14 +22,14 @@ async def corona(event):
         hmm1 = country_data["confirmed"] + country_data["new_cases"]
         hmm2 = country_data["deaths"] + country_data["new_deaths"]
         data = ""
-        data += f"\n⚠️Confirmed   : <code>{hmm1}</code>"
-        data += f"\n😔Active           : <code>{country_data['active']}</code>"
-        data += f"\n⚰️Deaths         : <code>{hmm2}</code>"
-        data += f"\n🤕Critical          : <code>{country_data['critical']}</code>"
-        data += f"\n😊Recovered   : <code>{country_data['recovered']}</code>"
-        data += f"\n💉Total tests    : <code>{country_data['total_tests']}</code>"
-        data += f"\n🥺New Cases   : <code>{country_data['new_cases']}</code>"
-        data += f"\n😟New Deaths : <code>{country_data['new_deaths']}</code>"
+        data += f"\n⚠️ Confirmed   : <code>{hmm1}</code>"
+        data += f"\n😔 Active           : <code>{country_data['active']}</code>"
+        data += f"\n⚰️ Deaths         : <code>{hmm2}</code>"
+        data += f"\n🤕 Critical          : <code>{country_data['critical']}</code>"
+        data += f"\n😊 Recovered   : <code>{country_data['recovered']}</code>"
+        data += f"\n💉 Total tests    : <code>{country_data['total_tests']}</code>"
+        data += f"\n🥺 New Cases   : <code>{country_data['new_cases']}</code>"
+        data += f"\n😟 New Deaths : <code>{country_data['new_deaths']}</code>"
         await catevent.edit(
             "<b>Corona Virus Info of {}:\n{}</b>".format(country, data),
             parse_mode="html",
@@ -41,13 +41,13 @@ async def corona(event):
             cat2 = int(data["new_death"]) - int(data["death"])
             cat3 = int(data["new_cured"]) - int(data["cured"])
             result = f"<b>Corona virus info of {data['state_name']}\
-                \n\n⚠️Confirmed   : <code>{data['new_positive']}</code>\
-                \n😔Active           : <code>{data['new_active']}</code>\
-                \n⚰️Deaths         : <code>{data['new_death']}</code>\
-                \n😊Recovered   : <code>{data['new_cured']}</code>\
-                \n🥺New Cases   : <code>{cat1}</code>\
-                \n😟New Deaths : <code>{cat2}</code>\
-                \n😃New cured  : <code>{cat3}</code> </b>"
+                \n\n⚠️ Confirmed   : <code>{data['new_positive']}</code>\
+                \n😔 Active           : <code>{data['new_active']}</code>\
+                \n⚰️ Deaths         : <code>{data['new_death']}</code>\
+                \n😊 Recovered   : <code>{data['new_cured']}</code>\
+                \n🥺 New Cases   : <code>{cat1}</code>\
+                \n😟 New Deaths : <code>{cat2}</code>\
+                \n😃 New cured  : <code>{cat3}</code> </b>"
             await catevent.edit(result, parse_mode="html")
         else:
             await edit_delete(
@@ -62,10 +62,10 @@ async def corona(event):
 CMD_HELP.update(
     {
         "covid": "**Plugin : **`covid`\
-        \n\n**Syntax : **`.covid <country name>`\
-        \n**Function :** __Get an information about covid-19 data in the given country.__\
-        \n\n**Syntax : **`.covid <state name>`\
-        \n**Function :** __Get an information about covid-19 data in the given state of India only.__\
+        \n\n  •  **Syntax : **`.covid <country name>`\
+        \n  •  **Function :** __Get an information about covid-19 data in the given country.__\
+        \n\n  •  **Syntax : **`.covid <state name>`\
+        \n  •  **Function :** __Get an information about covid-19 data in the given state of India only.__\
         "
     }
 )
