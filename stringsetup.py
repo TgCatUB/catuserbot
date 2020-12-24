6e0f8cbbb34310889e7cec298b2af874e1c42c92
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-12-24 17:34:21 +0530

    Adjustment for selfhost (#141)

--- stringsetup.py
+++ stringsetup.py
@@ -18,3 +18,4 @@ API_HASH = input("Enter API HASH here: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
+    client.send_message("me", client.session.save())
