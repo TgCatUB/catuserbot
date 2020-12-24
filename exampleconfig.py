3a0108a1099aff24c5b472286565b5497791e310
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-12-24 16:25:25 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,3 +1,16 @@
 from sample_config import Config
 
 class Development(Config):
+  #get this values from the my.telegram.org
+  APP_ID = 
+  API_HASH = 
+  # the name to display in your alive message
+  ALIVE_NAME = 
+  #After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
+  STRING_SESSION = 
+  #create a new bot in @botfather and fill the following vales with bottoken and username respectively
+  TG_BOT_TOKEN_BF_HER =
+  TG_BOT_USER_NAME_BF_HER = 
+  #create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
+  PRIVATE_GROUP_BOT_API_ID = -100
+  
