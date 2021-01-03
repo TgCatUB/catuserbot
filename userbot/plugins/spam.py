5e2c7a12a2369b40f81d6cf99dd8930015a71cb9
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-03 18:07:03 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -1,7 +1,8 @@
 import asyncio
 import base64
-
-from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.tl.types import InputStickerSetID
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get, GetStickerSetRequest
 
 from . import BOTLOG, BOTLOG_CHATID
 
@@ -79,7 +80,58 @@ async def spammer(event):
                 + f"`{spam_message}`",
             )
 
-
+@bot.on(admin_cmd(pattern="spspam$"))
+@bot.on(sudo_cmd(pattern="spspam$",allow_sudo=True))
+async def stickerpack_spam(event):
+    if event.fwd_from:
+        return
+    reply = await event.get_reply_message()
+    if not reply or media_type(reply) is None or media_type(reply) != "Sticker":
+        return await edit_delete(event, "reply to any sticker to send all stickers in that pack")
+    hmm = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+    try:
+        stickerset_attr = reply.document.attributes[1]
+        catevent = await edit_or_reply(
+                event, "Fetching details of the sticker pack, please wait.."
+            )
+    except BaseException:
+        await edit_delete(event, "This is not a sticker. Reply to a sticker.", 5)
+        return
+    try:
+        get_stickerset = await event.client(
+                GetStickerSetRequest(
+                    InputStickerSetID(
+                        id=stickerset_attr.stickerset.id,
+                        access_hash=stickerset_attr.stickerset.access_hash,
+                    )
+                )
+            )
+    except:
+        return await edit_delete(catevent, "I guess this sticker is not part of any pack so i cant kang this sticker pack try kang for this sticker")
+    try:
+            hmm = Get(hmm)
+            await event.client(hmm)
+    except BaseException:
+            pass
+    reqd_sticker_set = await event.client(functions.messages.GetStickerSetRequest(stickerset=types.InputStickerSetShortName(short_name=f"{get_stickerset.set.short_name}")))
+    for m in reqd_sticker_set.documents:
+        await event.client.send_file(event.chat_id,m)
+        await asyncio.sleep(0.7)
+    if BOTLOG:
+            if event.is_private:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "#SPAM\n"
+                    + f"Sticker Pack Spam was executed successfully in [User](tg://user?id={event.chat_id}) chat with pack ",
+                )
+            else:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "#SPAM\n"
+                    + f"Sticker Pack Spam was executed successfully in {event.chat.title}(`{event.chat_id}`) chat with pack",
+                )
+            await event.client.send_file(BOTLOG_CHATID, reqd_sticker_set.documents[0])
+        
 @bot.on(admin_cmd("cspam (.*)"))
 @bot.on(sudo_cmd(pattern="cspam (.*)", allow_sudo=True))
 async def tmeme(event):
