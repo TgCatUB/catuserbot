788c762cfd13180130d4b409a424b92e2e066143
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-03 12:57:25 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -44,10 +44,17 @@ for name in files:
 
 LOGS.info("Yay your userbot is officially working.!!!")
 LOGS.info(
-    "Congratulation, now type .alive to see message if bot is live\n"
-    "If you need assistance, head to https://t.me/catuserbot_support"
+    "Congratulation, now type .alive to see message if bot is live\
+    \nIf you need assistance, head to https://t.me/catuserbot_support"
 )
-
+try:
+    if Config.PRIVATE_GROUP_BOT_API_ID:
+        bot.send_message(Config.PRIVATE_GROUP_BOT_API_ID,"Congratulation, now type .alive to see message if bot is live\
+    \nIf you need assistance, head to https://t.me/catuserbot_support"
+    )
+except Exception as e:
+    LOGS.info(str(e))
+    
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
