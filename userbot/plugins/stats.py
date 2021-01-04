28b40e6f07579542e7b537e78df409c16af9f44a
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-04 12:29:36 +0530

    Update stats.py

--- userbot/plugins/stats.py
+++ userbot/plugins/stats.py
@@ -4,8 +4,8 @@ from telethon.tl.custom import Dialog
 from telethon.tl.types import Channel, Chat, User
 
 
-@bot.on(admin_cmd(pattern="stats$"))
-@bot.on(sudo_cmd(pattern="stats$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="stat$"))
+@bot.on(sudo_cmd(pattern="stat$", allow_sudo=True))
 async def stats(event):
     cat = await edit_or_reply(event, "`Collecting stats, Wait man`")
     start_time = time.time()
@@ -68,8 +68,8 @@ async def stats(event):
     await cat.edit(response)
 
 
-@bot.on(admin_cmd(pattern="mychannels$"))
-@bot.on(sudo_cmd(pattern="mychannels$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="stat c$"))
+@bot.on(sudo_cmd(pattern="stat c$", allow_sudo=True))
 async def stats(event):
     catevent = await edit_or_reply(event, "`Collecting stats, Wait man`")
     start_time = time.time()
@@ -93,8 +93,58 @@ async def stats(event):
         )
 
 
-@bot.on(admin_cmd(pattern="mygroups$"))
-@bot.on(sudo_cmd(pattern="mygroups$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="stat ca$"))
+@bot.on(sudo_cmd(pattern="stat ca$", allow_sudo=True))
+async def stats(event):
+    catevent = await edit_or_reply(event, "`Collecting stats, Wait man`")
+    start_time = time.time()
+    hi = []
+    async for dialog in event.client.iter_dialogs():
+        entity = dialog.entity
+        if isinstance(entity, Channel) and entity.broadcast:
+            if entity.creator or entity.admin_rights:
+                hi.append([entity.title, entity.id])
+    output = "**The channels in which you are admin are : **\n\n"
+    for k, i in enumerate(hi, start=1):
+        output += f"{k} .) [{i[0]}](https://t.me/c/{i[1]}/1)\n"
+    stop_time = time.time() - start_time
+    output += f"\n**Time Taken : ** {stop_time:.02f}s"
+    try:
+        await catevent.edit(output)
+    except:
+        await edit_or_reply(
+            catevent,
+            output,
+            caption="The list of channels in which you are admin ",
+        )
+
+@bot.on(admin_cmd(pattern="stat co$"))
+@bot.on(sudo_cmd(pattern="stat co$", allow_sudo=True))
+async def stats(event):
+    catevent = await edit_or_reply(event, "`Collecting stats, Wait man`")
+    start_time = time.time()
+    hi = []
+    async for dialog in event.client.iter_dialogs():
+        entity = dialog.entity
+        if isinstance(entity, Channel) and entity.broadcast:
+            if entity.creator:
+                hi.append([entity.title, entity.id])
+    output = "**The channels in which you are owner are : **\n\n"
+    for k, i in enumerate(hi, start=1):
+        output += f"{k} .) [{i[0]}](https://t.me/c/{i[1]}/1)\n"
+    stop_time = time.time() - start_time
+    output += f"\n**Time Taken : ** {stop_time:.02f}s"
+    try:
+        await catevent.edit(output)
+    except:
+        await edit_or_reply(
+            catevent,
+            output,
+            caption="The list of channels in which you are owner ",
+        )
+        
+@bot.on(admin_cmd(pattern="stat g$"))
+@bot.on(sudo_cmd(pattern="stat g$", allow_sudo=True))
 async def stats(event):
     catevent = await edit_or_reply(event, "`Collecting stats, Wait man`")
     start_time = time.time()
@@ -125,9 +175,8 @@ async def stats(event):
             caption="The list of groups in which you are",
         )
 
-
-@bot.on(admin_cmd(pattern="mygroups$"))
-@bot.on(sudo_cmd(pattern="mygroups$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="stat ga$"))
+@bot.on(sudo_cmd(pattern="stat ga$", allow_sudo=True))
 async def stats(event):
     catevent = await edit_or_reply(event, "`Collecting stats, Wait man`")
     start_time = time.time()
@@ -143,8 +192,9 @@ async def stats(event):
             and not isinstance(entity, User)
             and isinstance(entity, Chat)
         ):
-            hi.append([entity.title, entity.id])
-    output = "**The groups you are in are: **\n\n"
+            if entity.creator or entity.admin_rights:
+                hi.append([entity.title, entity.id])
+    output = "**The groups in which you are admin are: **\n\n"
     for k, i in enumerate(hi, start=1):
         output += f"{k} .) [{i[0]}](https://t.me/c/{i[1]}/1)\n"
     stop_time = time.time() - start_time
@@ -155,9 +205,42 @@ async def stats(event):
         await edit_or_reply(
             catevent,
             output,
-            caption="The list of groups in which you are",
+            caption="The list of groups in which you admin ",
         )
 
+@bot.on(admin_cmd(pattern="stat go$"))
+@bot.on(sudo_cmd(pattern="stat go$", allow_sudo=True))
+async def stats(event):
+    catevent = await edit_or_reply(event, "`Collecting stats, Wait man`")
+    start_time = time.time()
+    hi = []
+    async for dialog in event.client.iter_dialogs():
+        entity = dialog.entity
+        if isinstance(entity, Channel) and entity.broadcast:
+            continue
+        elif (
+            isinstance(entity, Channel)
+            and entity.megagroup
+            or not isinstance(entity, Channel)
+            and not isinstance(entity, User)
+            and isinstance(entity, Chat)
+        ):
+            if entity.creator or entity.admin_rights:
+                hi.append([entity.title, entity.id])
+    output = "**The groups in which you are owmer are: **\n\n"
+    for k, i in enumerate(hi, start=1):
+        output += f"{k} .) [{i[0]}](https://t.me/c/{i[1]}/1)\n"
+    stop_time = time.time() - start_time
+    output += f"\n**Time Taken : ** {stop_time:.02f}s"
+    try:
+        await catevent.edit(output)
+    except:
+        await edit_or_reply(
+            catevent,
+            output,
+            caption="The list of groups in which you owner ",
+        )
+        
 
 def inline_mention(user):
     full_name = user_full_name(user) or "No Name"
