8b3dbd69eaa5347e9a6d91f8fb3df05c821f0a13
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-01-11 13:04:34 +0000

    pylint: auto fixes

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -2,7 +2,9 @@ import asyncio
 import base64
 
 from telethon.tl import functions, types
-from telethon.tl.functions.messages import GetStickerSetRequest, ImportChatInviteRequest as Get
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+
 from . import BOTLOG, BOTLOG_CHATID
 
 
