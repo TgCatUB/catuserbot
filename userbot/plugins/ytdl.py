b7cbed33db4d1698592c346905c5dc7ddadff01f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-11 18:10:28 +0530

    Update ytdl.py

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -197,7 +197,7 @@ async def yt_search(event):
     except Exception as e:
         return await edit_delete(video_q, str(e), time=10, parse_mode=parse_pre)
     reply_text = (
-        f"**•  Search Query:**\n`{query}`\n\n**•  Results:**\n\n{full_response}"
+        f"**•  Search Query:**\n`{query}`\n\n**•  Results:**\n{full_response}"
     )
     await video_q.edit(reply_text)
 
