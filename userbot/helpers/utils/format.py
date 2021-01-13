de16eddbef1bcf61a63149447d89e76e6ba86810
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-13 15:39:17 +0530

    Update format.py

--- userbot/helpers/utils/format.py
+++ userbot/helpers/utils/format.py
@@ -4,6 +4,35 @@ from telethon.tl.tlobject import TLObject
 from telethon.tl.types import MessageEntityPre
 from telethon.utils import add_surrogate
 
+import asyncio
+import functools
+import re
+
+import requests
+
+def paste_text(text):
+    asciich = ["*", "`", "_"]
+    for i in asciich:
+        text = re.sub(rf"\{i}", "", text)
+    try:
+        key = (
+            requests.post("https://nekobin.com/api/documents", json={"content": text})
+            .json()
+            .get("result")
+            .get("key")
+        )
+        link = f"https://nekobin.com/{key}"
+    except:
+        text = re.sub(r"•", ">>", text)
+        kresult = requests.post(
+            "https://del.dog/documents", data=text.encode("UTF-8")
+        ).json()
+        link = f"https://del.dog/{kresult['key']}"
+    return link
+
+
+
+
 
 def mentionuser(name, userid):
     return f"[{name}](tg://user?id={userid})"
