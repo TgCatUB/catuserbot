d6188af7f406b2f49bb6e70231b774926201e865
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-13 19:24:05 +0530

    Update json.py

--- userbot/plugins/json.py
+++ userbot/plugins/json.py
@@ -1,17 +1,10 @@
 # yaml_format is ported from uniborg
-
-
 @bot.on(admin_cmd(pattern="json$"))
 @bot.on(sudo_cmd(pattern="json$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    the_real_message = None
-    await reply_id(event)
-    if event.reply_to_msg_id:
-        catevent = await event.get_reply_message()
-    else:
-        catevent = event
+    catevent = await event.get_reply_message() if event.reply_to_msg_id else event
     the_real_message = catevent.stringify()
     await edit_or_reply(event, the_real_message, parse_mode=parse_pre)
 
@@ -21,10 +14,7 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    the_real_message = None
-    await reply_id(event)
-    if event.reply_to_msg_id:
-        catevent = await event.get_reply_message()
+    catevent = await event.get_reply_message() if event.reply_to_msg_id else event
     the_real_message = _format.yaml_format(catevent)
     await edit_or_reply(event, the_real_message, parse_mode=parse_pre)
 
