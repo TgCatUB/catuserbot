7743fab9e6f3b109f7e0c3028b2184a780cc90b0
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-13 19:21:22 +0530

    Update json.py

--- userbot/plugins/json.py
+++ userbot/plugins/json.py
@@ -1,9 +1,6 @@
 # yaml_format is ported from uniborg
 import io
 
-from . import parse_pre
-
-
 @bot.on(admin_cmd(pattern="json$"))
 @bot.on(sudo_cmd(pattern="json$", allow_sudo=True))
 async def _(event):
@@ -13,19 +10,10 @@ async def _(event):
     reply_to_id = await reply_id(event)
     if event.reply_to_msg_id:
         catevent = await event.get_reply_message()
-    the_real_message = catevent.stringify()
-    if len(the_real_message) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(the_real_message)) as out_file:
-            out_file.name = "json.text"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                reply_to=reply_to_id,
-            )
-            await event.delete()
     else:
-        await edit_or_reply(event, the_real_message, parse_mode=parse_pre)
+        catevent = event
+    the_real_message = catevent.stringify()
+    await edit_or_reply(event, the_real_message, parse_mode=parse_pre)
 
 
 @bot.on(admin_cmd(pattern="yaml$"))
@@ -38,18 +26,7 @@ async def _(event):
     if event.reply_to_msg_id:
         catevent = await event.get_reply_message()
     the_real_message = _format.yaml_format(catevent)
-    if len(the_real_message) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(the_real_message)) as out_file:
-            out_file.name = "yaml.text"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                reply_to=reply_to_id,
-            )
-            await event.delete()
-    else:
-        await edit_or_reply(event, the_real_message, parse_mode=parse_pre)
+    await edit_or_reply(event, the_real_message, parse_mode=parse_pre)
 
 
 CMD_HELP.update(
