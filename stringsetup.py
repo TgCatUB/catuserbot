5b2a818496d961d71b1b4ec5133f1f4280b99629
AkashPattanaik <akashjio66666@gmail.com>
2021-01-13 14:06:07 +0530

    better visualisation

--- stringsetup.py
+++ stringsetup.py
@@ -7,15 +7,16 @@
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
-print(
-    """Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details"""
-)
+print("""
+Goto - my.telegram.org
+Login Using Your Telegram Account
+Get The AppId And AppHash !""")
+
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    print('The Following Is The Session String\nKeep It Safe !\n\n')
     print(client.session.save())
+    print('\n\n')
     client.send_message("me", client.session.save())
