26d7e8431c22382e3ace4ea0d6dac17a5e92af5d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-13 12:47:14 +0530

    Update __init__.py

--- userbot/helpers/__init__.py
+++ userbot/helpers/__init__.py
@@ -1,7 +1,6 @@
 from . import fonts
 from . import memeshelper as catmemes
 from .aiohttp_helper import AioHttp
-from .exceptions import CancelProcess
 from .functions import *
 from .memeifyhelpers import *
 from .progress import *
