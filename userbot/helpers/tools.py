d88371b1ca194bb1001bc3b38a0303b6033d4186
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-13 12:37:34 +0530

    Update tools.py

--- userbot/helpers/tools.py
+++ userbot/helpers/tools.py
@@ -1,27 +1,5 @@
 from telethon import functions, types
 
-
-def media_type(message):
-    if message and message.photo:
-        return "Photo"
-    elif message and message.audio:
-        return "Audio"
-    elif message and message.voice:
-        return "Voice"
-    elif message and message.video_note:
-        return "Round Video"
-    elif message and message.gif:
-        return "Gif"
-    elif message and message.sticker:
-        return "Sticker"
-    elif message and message.video:
-        return "Video"
-    elif message and message.document:
-        return "Document"
-    else:
-        return None
-
-
 async def unsavegif(event, sandy):
     try:
         await event.client(
