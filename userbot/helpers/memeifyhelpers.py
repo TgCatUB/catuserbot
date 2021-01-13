f675025e0e725517da32a60b96d1acd8c89cb68c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-13 12:41:41 +0530

    Update memeifyhelpers.py

--- userbot/helpers/memeifyhelpers.py
+++ userbot/helpers/memeifyhelpers.py
@@ -127,43 +127,6 @@ async def cat_meeme(upper_text, lower_text, CNG_FONTS, picture_name, endname):
     main_image.save(filename=endname)
 
 
-# executing of terminal commands
-
-
-async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
-    args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(
-        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    return (
-        stdout.decode("utf-8", "replace").strip(),
-        stderr.decode("utf-8", "replace").strip(),
-        process.returncode,
-        process.pid,
-    )
-
-
-# For using gif , animated stickers and videos in some parts , this
-# function takes  take a screenshot and stores ported from userge
-
-
-async def take_screen_shot(
-    video_file: str, duration: int, path: str = ""
-) -> Optional[str]:
-    print(
-        "[[[Extracting a frame from %s ||| Video duration => %s]]]",
-        video_file,
-        duration,
-    )
-    ttl = duration // 2
-    thumb_image_path = path or os.path.join("./temp/", f"{basename(video_file)}.jpg")
-    command = f"ffmpeg -ss {ttl} -i '{video_file}' -vframes 1 '{thumb_image_path}'"
-    err = (await runcmd(command))[1]
-    if err:
-        print(err)
-    return thumb_image_path if os.path.exists(thumb_image_path) else None
-
 
 async def silently_send_message(conv, text):
     await conv.send_message(text)
