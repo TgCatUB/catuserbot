9293ff1638005ab3cd55f46e2dfda86c4436f225
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-16 11:44:27 +0530

    Update locks.py

--- userbot/plugins/locks.py
+++ userbot/plugins/locks.py
@@ -390,7 +390,7 @@ async def _(event):
 @bot.on(events.MessageEdited())
 @bot.on(events.NewMessage())
 async def check_incoming_messages(event):
-    if is_admin(event.client, event.chat_id, event.sender_id):
+    if is_admin(event.client, event.chat_id, event.from_id):
         return
     peer_id = event.chat_id
     if is_locked(peer_id, "commands"):
@@ -452,7 +452,7 @@ async def check_incoming_messages(event):
 
 @bot.on(events.ChatAction())
 async def _(event):
-    if is_admin(event.client, event.chat_id, event.sender_id):
+    if is_admin(event.client, event.chat_id, event.from_id):
         return
     # check for "lock" "bots"
     if not is_locked(event.chat_id, "bots"):
