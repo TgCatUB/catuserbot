73ef121d59f4e3e4dddbe45a3624ec82f56a42cf
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-01-30 21:01:11 +0530

    Update google_image_download.py

--- userbot/helpers/google_image_download.py
+++ userbot/helpers/google_image_download.py
@@ -15,7 +15,7 @@ import re
 import ssl
 import sys
 import time  # Importing the time library to check the time of code execution
-
+from .. import LOGS
 from . import _format
 
 version = (3, 0)
@@ -1403,7 +1403,7 @@ class googleimagesdownload:
         end_object = s.find("</script>", start_object + 1) - 4
         object_raw = str(s[start_object:end_object])
         object_decode = bytes(object_raw[:-1], "utf-8").decode("unicode_escape")
-        paste(_format.paste_text(object_decode))
+        LOGS.info(_format.paste_text(object_decode))
         image_objects = json.loads(object_decode[:-15])[31][0][12][2]
         return image_objects
 
