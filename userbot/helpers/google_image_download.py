75cf7d5dd8fd89c4a5cfff71d4a29acd1a6b6073
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-01-30 17:14:44 +0000

    pylint: auto fixes

--- userbot/helpers/google_image_download.py
+++ userbot/helpers/google_image_download.py
@@ -1,139 +1,147 @@
 import os
 import time
 import urllib
-import requests
+from urllib.parse import quote
+
 import magic
 import progressbar
-from urllib.parse import quote
+import requests
+
 
 class googleimagesdownload:
     def __init__(self):
         pass
 
-    def urls(self, keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', '.jpeg'}):
-        keyword_to_search = [str(item).strip() for item in keywords.split(',')]
+    def urls(
+        self, keywords, limit, extensions={".jpg", ".png", ".ico", ".gif", ".jpeg"}
+    ):
+        keyword_to_search = [str(item).strip() for item in keywords.split(",")]
         links = []
 
         things = len(keyword_to_search) * limit
 
-        bar = progressbar.ProgressBar(maxval=things, \
-                                      widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
+        bar = progressbar.ProgressBar(
+            maxval=things,
+            widgets=[progressbar.Bar("=", "[", "]"), " ", progressbar.Percentage()],
+        ).start()
 
         for item_ in keyword_to_search:
             url = (
-                ('https://www.google.com/search?q=' + quote(item_.encode('utf-8')))
-                + '&biw=1536&bih=674&tbm=isch&sxsrf=ACYBGNSXXpS6YmAKUiLKKBs6xWb4uUY5gA:1581168823770&source=lnms&sa=X&ved=0ahUKEwioj8jwiMLnAhW9AhAIHbXTBMMQ_AUI3QUoAQ'
+                ("https://www.google.com/search?q=" + quote(item_.encode("utf-8")))
+                + "&biw=1536&bih=674&tbm=isch&sxsrf=ACYBGNSXXpS6YmAKUiLKKBs6xWb4uUY5gA:1581168823770&source=lnms&sa=X&ved=0ahUKEwioj8jwiMLnAhW9AhAIHbXTBMMQ_AUI3QUoAQ"
             )
 
             raw_html = self._download_page(url)
 
-            end_object = -1;
-            google_image_seen = False;
+            end_object = -1
+            google_image_seen = False
             j = 0
 
             while j < limit:
-                while (True):
+                while True:
                     try:
                         new_line = raw_html.find('"https://', end_object + 1)
                         end_object = raw_html.find('"', new_line + 1)
 
-                        buffor = raw_html.find('\\', new_line + 1, end_object)
+                        buffor = raw_html.find("\\", new_line + 1, end_object)
                         if buffor != -1:
-                            object_raw = (raw_html[new_line + 1:buffor])
+                            object_raw = raw_html[new_line + 1 : buffor]
                         else:
-                            object_raw = (raw_html[new_line + 1:end_object])
+                            object_raw = raw_html[new_line + 1 : end_object]
 
                         if any(extension in object_raw for extension in extensions):
                             break
 
-                    except Exception as e:
+                    except Exception:
                         break
-
 
                 try:
                     r = requests.get(object_raw, allow_redirects=True, timeout=1)
-                    if('html' not in str(r.content)):
+                    if "html" not in str(r.content):
                         mime = magic.Magic(mime=True)
                         file_type = mime.from_buffer(r.content)
                         file_extension = f'.{file_type.split("/")[1]}'
-                        if file_extension == '.png' and not google_image_seen:
+                        if file_extension == ".png" and not google_image_seen:
                             google_image_seen = True
-                            raise ValueError();
+                            raise ValueError()
                         links.append(object_raw)
                         bar.update(bar.currval + 1)
                     else:
                         j -= 1
-                except Exception as e:
+                except Exception:
                     j -= 1
                 j += 1
 
         bar.finish()
-        return(links)
+        return links
 
-    def download(self, keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', '.jpeg'}):
-        keyword_to_search = [str(item).strip() for item in keywords.split(',')]
+    def download(
+        self, keywords, limit, extensions={".jpg", ".png", ".ico", ".gif", ".jpeg"}
+    ):
+        keyword_to_search = [str(item).strip() for item in keywords.split(",")]
         main_directory = "simple_images/"
         things = len(keyword_to_search) * limit
 
-        bar = progressbar.ProgressBar(maxval=things, \
-                                      widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+        bar = progressbar.ProgressBar(
+            maxval=things,
+            widgets=[progressbar.Bar("=", "[", "]"), " ", progressbar.Percentage()],
+        )
 
         bar.start()
 
         for item_ in keyword_to_search:
             self._create_directories(main_directory, item_)
             url = (
-                ('https://www.google.com/search?q=' + quote(item_.encode('utf-8')))
-                + '&biw=1536&bih=674&tbm=isch&sxsrf=ACYBGNSXXpS6YmAKUiLKKBs6xWb4uUY5gA:1581168823770&source=lnms&sa=X&ved=0ahUKEwioj8jwiMLnAhW9AhAIHbXTBMMQ_AUI3QUoAQ'
+                ("https://www.google.com/search?q=" + quote(item_.encode("utf-8")))
+                + "&biw=1536&bih=674&tbm=isch&sxsrf=ACYBGNSXXpS6YmAKUiLKKBs6xWb4uUY5gA:1581168823770&source=lnms&sa=X&ved=0ahUKEwioj8jwiMLnAhW9AhAIHbXTBMMQ_AUI3QUoAQ"
             )
 
             raw_html = self._download_page(url)
 
-            end_object = -1;
-            google_image_seen = False;
+            end_object = -1
+            google_image_seen = False
             j = 0
             while j < limit:
-                while (True):
+                while True:
                     try:
                         new_line = raw_html.find('"https://', end_object + 1)
                         end_object = raw_html.find('"', new_line + 1)
 
-                        buffor = raw_html.find('\\', new_line + 1, end_object)
+                        buffor = raw_html.find("\\", new_line + 1, end_object)
                         if buffor != -1:
-                            object_raw = (raw_html[new_line+1:buffor])
+                            object_raw = raw_html[new_line + 1 : buffor]
                         else:
-                            object_raw = (raw_html[new_line+1:end_object])
+                            object_raw = raw_html[new_line + 1 : end_object]
 
                         if any(extension in object_raw for extension in extensions):
                             break
 
-                    except Exception as e:
+                    except Exception:
                         break
                 path = main_directory + item_.replace(" ", "_")
 
                 try:
                     r = requests.get(object_raw, allow_redirects=True, timeout=1)
-                    if ('html' not in str(r.content)):
+                    if "html" not in str(r.content):
                         mime = magic.Magic(mime=True)
                         file_type = mime.from_buffer(r.content)
                         file_extension = f'.{file_type.split("/")[1]}'
                         if file_extension not in extensions:
                             raise ValueError()
-                        if file_extension == '.png' and not google_image_seen:
+                        if file_extension == ".png" and not google_image_seen:
                             google_image_seen = True
                             raise ValueError()
                         file_name = str(item_) + "_" + str(j + 1) + file_extension
-                        with open(os.path.join(path, file_name), 'wb') as file:
+                        with open(os.path.join(path, file_name), "wb") as file:
                             file.write(r.content)
                         bar.update(bar.currval + 1)
                     else:
                         j -= 1
-                except Exception as e:
+                except Exception:
                     j -= 1
                 j += 1
 
         bar.finish()
-
 
     def _create_directories(self, main_directory, name):
         name = name.replace(" ", "_")
@@ -141,7 +149,7 @@ class googleimagesdownload:
             if not os.path.exists(main_directory):
                 os.makedirs(main_directory)
                 time.sleep(0.2)
-            path = (name)
+            path = name
             sub_directory = os.path.join(main_directory, path)
             if not os.path.exists(sub_directory):
                 os.makedirs(sub_directory)
@@ -150,11 +158,11 @@ class googleimagesdownload:
                 raise
         return
 
-    def _download_page(self,url):
+    def _download_page(self, url):
 
         try:
             headers = {
-                'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
             }
 
             req = urllib.request.Request(url, headers=headers)
