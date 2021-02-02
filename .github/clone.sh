6e646a116ada0ff22d458c313c3ac7d0112cd1ff
ashdroid4 <77984053+ashdroid4@users.noreply.github.com>
2021-02-03 00:12:10 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -32,7 +32,7 @@ else
     rm -rf userbot
     rm -rf .github
     rm -rf sample_config.py
-    git clone https://github.com/ashdroid4/catuserbot
+    git clone https://github.com/ashdroid4/catuserbot cat_ub
     mv cat_ub/userbot .
     mv cat_ub/.github . 
     mv cat_ub/.git .
