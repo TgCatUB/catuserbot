6806afd59e09807083f506ff3101d40be4ed85f3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-05 02:04:27 +0530

    Update stats.py

--- userbot/plugins/stats.py
+++ userbot/plugins/stats.py
@@ -4,6 +4,8 @@ import time
 from telethon.tl.custom import Dialog
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from telethon.tl.types import Channel, Chat, User
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
 
 # =========================================================== #
 #                           STRINGS                           #
@@ -195,6 +197,43 @@ async def stats(event):
             caption=caption,
         )
 
+        
+@bot.on(admin_cmd(pattern="ustat ?(.*)"))
+@bot.on(sudo_cmd(pattern="ustat ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = "".join(event.text.split(maxsplit=1)[1:])
+    reply_message = await event.get_reply_message()
+    if not input_str and not reply_message:
+        await edit_delete(
+            event,
+            "`reply to  user's text message to get name/username history or give userid/username`",
+        )
+    if input_str:
+        try:
+            uid = int(input_str)
+        except ValueError:
+            try:
+                u = await event.client.get_entity(input_str)
+            except ValueError:
+                await edit_delete(
+                    event, "`Give userid or username to find name history`"
+                )
+            uid = u.id
+    else:
+        uid = reply_message.sender_id
+    chat = "@tgscanrobot"
+    catevent = await edit_or_reply(event, "`Processing...`")
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message(f"{uid}")
+        except:
+            await edit_delete(catevent , "`unblock `@tgscanrobot` and then try`")
+        response = await conv.get_response()
+        await event.client.send_read_acknowledge(conv.chat_id)
+        await catevent.edit(response.text)
+
 
 def inline_mention(user):
     full_name = user_full_name(user) or "No Name"
@@ -216,6 +255,8 @@ CMD_HELP.update(
     \n  •  **Function : **__Shows you the list of all groups  in which you are if you use g , all groups in which you are admin if you use ga and all groups created by you if you use go__\
     \n\n  •  **Syntax : **`.stat (c|ca|co)`\
     \n  •  **Function : **__Shows you the list of all channels in which you are if you use c , all channels in which you are admin if you use ca and all channels created by you if you use co__\
+    \n\n  •  **Syntax : **`.ustat (reply/userid/username)`\
+    \n  •  **Function : **__Shows the list of public groups of that paticular user__\
     "
     }
 )
