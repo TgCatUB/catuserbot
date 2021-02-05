8203490c638677272eebee771733d86b1deb3c00
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-05 23:53:06 +0530

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -14,7 +14,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 
-from . import LOGS, spamwatch
+from . import LOGS, spamwatch, get_user_from_event
 
 TMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
 
@@ -99,6 +99,8 @@ async def get_full_user(event):
             return None, e
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
+        if previous_message.from_id is None and not event.is_private:
+            return None , "Well that's an anonymous admin. I can't fetch deatils!"
         if previous_message.forward:
             replied_user = await event.client(
                 GetFullUserRequest(
@@ -128,6 +130,10 @@ async def who(event):
     if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TMP_DOWNLOAD_DIRECTORY)
     replied_user = await get_user(event)
+    if replied_user is None:
+        return await edit_or_reply(cat,
+            "`Well that's an anonymous admin. I can't fetch deatils!`"
+        )
     try:
         photo, caption = await fetch_info(replied_user, event)
     except AttributeError:
@@ -157,6 +163,8 @@ async def get_user(event):
     """ Get the user from argument or replied message. """
     if event.reply_to_msg_id and not event.pattern_match.group(1):
         previous_message = await event.get_reply_message()
+        if previous_message.from_id is None and not event.is_private:
+            return None 
         replied_user = await event.client(
             GetFullUserRequest(previous_message.sender_id)
         )
@@ -249,36 +257,6 @@ async def permalink(mention):
         )
         await edit_or_reply(mention, f"[{tag}](tg://user?id={user.id})")
 
-
-async def get_user_from_event(event):
-    """ Get the user from argument or replied message. """
-    args = event.pattern_match.group(1).split(":", 1)
-    extra = None
-    if event.reply_to_msg_id and len(args) != 2:
-        previous_message = await event.get_reply_message()
-        user_obj = await event.client.get_entity(previous_message.sender_id)
-        extra = event.pattern_match.group(1)
-    elif len(args[0]) > 0:
-        user = args[0]
-        if len(args) == 2:
-            extra = args[1]
-        if user.isnumeric():
-            user = int(user)
-        if not user:
-            await event.edit("`Pass the user's username, id or reply!`")
-            return
-        if event.message.entities:
-            probable_user_mention_entity = event.message.entities[0]
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                user_obj = await event.client.get_entity(user_id)
-                return user_obj
-        try:
-            user_obj = await event.client.get_entity(user)
-        except (TypeError, ValueError) as err:
-            await event.edit(str(err))
-            return None
-    return user_obj, extra
 
 
 async def ge(user, event):
