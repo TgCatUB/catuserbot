b2fe60529f1f68d0f12d8afeda747a893b4d8e63
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-05 17:23:04 +0530

    Update tools.py

--- userbot/helpers/tools.py
+++ userbot/helpers/tools.py
@@ -1,19 +1,18 @@
 def media_type(message):
     if message and message.photo:
         return "Photo"
-    elif message and message.audio:
+    if message and message.audio:
         return "Audio"
-    elif message and message.voice:
+    if message and message.voice:
         return "Voice"
-    elif message and message.video_note:
+    if message and message.video_note:
         return "Round Video"
-    elif message and message.gif:
+    if message and message.gif:
         return "Gif"
-    elif message and message.sticker:
+    if message and message.sticker:
         return "Sticker"
-    elif message and message.video:
+    if message and message.video:
         return "Video"
-    elif message and message.document:
+    if message and message.document:
         return "Document"
-    else:
-        return None
+    return None
