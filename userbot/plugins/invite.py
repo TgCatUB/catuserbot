fdd875e8a67f126e20298b5fab0ac224dd2c26b7
ashdroid4 <77984053+ashdroid4@users.noreply.github.com>
2021-02-05 19:21:04 +0530

    Update invite.py

--- userbot/plugins/invite.py
+++ userbot/plugins/invite.py
@@ -36,7 +36,44 @@ async def _(event):
                     await edit_delete(event, f"`{str(e)}`", 5)
 
         await edit_or_reply(event, f"`{to_add_users} is/are Invited Successfully`")
-    
+     
+async def get_users(event):
+    sender = await event.get_sender()
+        me = await event.client.get_me()
+            if not sender.id == me.id:
+                    
+await edit_delete(event, "`processing...`")
+                        else:
+                                await edit_or_delete(event, "`processing...`")
+                                    await get_chatinfo(event)
+                                        chat = await event.get_chat()
+                                            if event.is_private:
+
+event, "`.invite` users to a chat, not to a Private Message", 5
+        )
+                                                        s = 0
+                                                            f = 0
+                                                                error = "None"
+    await edit_delete("**TerminalStatus**\n\n`Collecting Users.......`")
+        async for user in event.client.iter_participants(kraken.full_chat.id):
+                try:
+                            if error.startswith("Too"):
+                                            return await edit_delete(
+                                                                f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people"
+                                                                                )
+                                                                                            await event.client(
+                                                                                                            functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
+                                                                                                                        )
+                                                                                                                                    s = s + 1
+                                                                                                                                                await edit_delete(
+                                                                                                                                                                f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`"
+                                                                                                                                                                            )
+                                                                                                                                                                                    except Exception as e:
+                                                                                                                                                                                                error = str(e)
+                                                                                                                                                                                                            f = f + 1
+                                                                                                                                                                                                                return await edit_delete(
+                                                                                                                                                                                                                        f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people"
+     )
 
 CMD_HELP.update(
     {
