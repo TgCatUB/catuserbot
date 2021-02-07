27ce5a35e67f88b28b6ee34d3b444714109bce85
ashdroid4 <77984053+ashdroid4@users.noreply.github.com>
2021-02-07 09:16:21 +0530

    Update undlt.py

--- userbot/plugins/undlt.py
+++ userbot/plugins/undlt.py
@@ -1,8 +1,5 @@
 import asyncio
 
-from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot.cmdhelp import CmdHelp
-
 
 @bot.on(admin_cmd(pattern="undlt"))
 @bot.on(sudo_cmd(pattern="undlt", allow_sudo=True))
@@ -12,7 +9,7 @@ async def _(event):
     c = await event.get_chat()
     if c.admin_rights or c.creator:
         a = await borg.get_admin_log(
-            event.chat_id, limit=5, search="", edit=False, delete=True
+            event.chat_id, limit=7, search="", edit=False, delete=True
         )
         for i in a:
             await event.reply(i.original.action.message)
