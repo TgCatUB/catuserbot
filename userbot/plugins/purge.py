a07f0c2ee63841e967595231b82c0a43ff9be945
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-07 13:15:04 +0530

    Update purge.py

--- userbot/plugins/purge.py
+++ userbot/plugins/purge.py
@@ -7,8 +7,63 @@ from telethon.errors import rpcbaseerrors
 from ..utils import errors_handler
 from . import BOTLOG, BOTLOG_CHATID
 
+purgelist = {}
 
-@bot.on(admin_cmd(outgoing=True, pattern="purge(?: |$)(.*)"))
+@bot.on(admin_cmd(pattern="purgefrom$"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="purgefrom$"))
+@errors_handler
+async def purge_from(event):
+    if event.fwd_from:
+        return
+    reply = await event.get_reply_message()
+    if reply:
+        reply_message = await reply_id(event)
+        purgelist[event.chat_id] = reply_message
+        await edit_delete(event , "`This Message marked for deletion. Reply to another message with purgeto to delete all messages in between.`")
+    else:
+        await edit_delete(event , "`Reply to a message to let me know what to delete.`")
+    
+@bot.on(admin_cmd(pattern="purgeto$"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="purgeto$"))
+@errors_handler
+async def purge_to(event):
+    chat = await event.get_input_chat()
+    if event.fwd_from:
+        return
+    reply = await event.get_reply_message()
+    try: 
+        from_message = purgelist[event.chat_id]
+    except KeyError:
+        return await edit_delete(event , "`First mark the messsage with purgefrom and then mark purgeto .So, I can delete in between Messages`")
+    if not reply or not from_message.isnumeric():
+        return await edit_delete(event , "`First mark the messsage with purgefrom and then mark purgeto .So, I can delete in between Messages`")
+    to_message = await reply_id(event)
+    msgs = []
+    count = 0
+    async for msg in event.client.iter_messages(event.chat_id, min_id = (from_message-1),max_id = (to_message+1)):
+        msgs.append(msg)
+        count += 1
+        msgs.append(event.reply_to_msg_id)
+        if len(msgs) == 100:
+            await event.client.delete_messages(chat, msgs)
+            msgs = []
+    if msgs:
+        await event.client.delete_messages(chat, msgs)
+    await edit_delete(
+        event,
+        "`Fast purge complete!\nPurged " + str(count) + " messages.`",
+    )
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "#PURGE \n`Purge of " + str(count) + " messages done successfully.`",
+        )
+
+
+
+
+
+@bot.on(admin_cmd(pattern="purge(?: |$)(.*)"))
 @bot.on(sudo_cmd(allow_sudo=True, pattern="purge(?: |$)(.*)"))
 @errors_handler
 async def fastpurger(event):
@@ -65,7 +120,7 @@ async def fastpurger(event):
         )
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="purgeme"))
+@bot.on(admin_cmd(pattern="purgeme"))
 @bot.on(sudo_cmd(allow_sudo=True, pattern="purgeme"))
 @errors_handler
 async def purgeme(event):
@@ -92,7 +147,7 @@ async def purgeme(event):
     await smsg.delete()
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="del$"))
+@bot.on(admin_cmd(pattern="del$"))
 @bot.on(sudo_cmd(allow_sudo=True, pattern="del$"))
 @errors_handler
 async def delete_it(event):
