a5a355f87a862f75695bda9a46d3e48145529050
ashdroid4 <77984053+ashdroid4@users.noreply.github.com>
2021-02-07 09:13:55 +0530

    Add files via upload

--- userbot/plugins/undlt.py
+++ userbot/plugins/undlt.py
@@ -1,39 +1,27 @@
 import asyncio
 
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
-@bot.on(admin_cmd(pattern="undlt ?(.*)"))
-@bot.on(sudo_cmd(pattern="undlt ?(.*)", allow_sudo=True))
+
+@bot.on(admin_cmd(pattern="undlt"))
+@bot.on(sudo_cmd(pattern="undlt", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    if event.pattern_match.group(1):
-        try:
-            cat = int(event.pattern_match.group(1))
-            input_str = cat
-        except:
-            input_str = 5
-    else:
-        input_str = 5
     c = await event.get_chat()
     if c.admin_rights or c.creator:
-        a = await event.client.get_admin_log(
-            event.chat_id, limit=input_str, search="", edit=False, delete=True
+        a = await borg.get_admin_log(
+            event.chat_id, limit=5, search="", edit=False, delete=True
         )
         for i in a:
             await event.reply(i.original.action.message)
     else:
-        event = await edit_or_reply(
-            event, "`You need administrative permissions in order to do this command`"
+        await edit_or_reply(event, "You need administrative permissions in order to do this command"
         )
         await asyncio.sleep(3)
         await event.delete()
 
-
-CMD_HELP.update(
-    {
-        "undlt": "**Plugin :** `undlt`\
-        \n**Syntax : **`.undlt <count>`\
-        \n**Usage: **Fetches last <count> number of deleted messages and sends you(you must be admin in that group)  \
-"
-    }
-)
+CmdHelp("undlt").add_command(
+  "undlt", None, "Sends 5 recently deleted message from that group. Requires admin position"
+).add()
