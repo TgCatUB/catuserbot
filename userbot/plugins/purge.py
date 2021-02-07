9a884180e60dbeb0110928b4fe7d59e64e6f8dc9
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-07 13:37:37 +0530

    Update purge.py

--- userbot/plugins/purge.py
+++ userbot/plugins/purge.py
@@ -9,6 +9,61 @@ from . import BOTLOG, BOTLOG_CHATID
 
 purgelist = {}
 
+@bot.on(admin_cmd(pattern="purge(?: |$)(.*)"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="purge(?: |$)(.*)"))
+@errors_handler
+async def fastpurger(event):
+    chat = await event.get_input_chat()
+    msgs = []
+    count = 0
+    input_str = event.pattern_match.group(1)
+    reply = await event.get_reply_message()
+    if reply:
+        if input_str and input_str.isnumeric():
+            count += 1
+            async for msg in event.client.iter_messages(
+                event.chat_id,
+                limit=(int(input_str) - 1),
+                offset_id=reply.id,
+                reverse=True,
+            ):
+                msgs.append(msg)
+                count += 1
+                msgs.append(event.reply_to_msg_id)
+                if len(msgs) == 100:
+                    await event.client.delete_messages(chat, msgs)
+                    msgs = []
+        elif input_str:
+            return await edit_or_reply(
+                event, f"**Error**\n`{input_str} is not an integer. Use proper syntax.`"
+            )
+        else:
+            async for msg in event.client.iter_messages(
+                chat, min_id=event.reply_to_msg_id
+            ):
+                msgs.append(msg)
+                count += 1
+                msgs.append(event.reply_to_msg_id)
+                if len(msgs) == 100:
+                    await event.client.delete_messages(chat, msgs)
+                    msgs = []
+    else:
+        await edit_or_reply(
+            event,
+            "`No message specified.`",
+        )
+        return
+    if msgs:
+        await event.client.delete_messages(chat, msgs)
+    await edit_delete(
+        event,
+        "`Fast purge complete!\nPurged " + str(count) + " messages.`",
+    )
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "#PURGE \n`Purge of " + str(count) + " messages done successfully.`",
+        )
 
 @bot.on(admin_cmd(pattern="purgefrom$"))
 @bot.on(sudo_cmd(allow_sudo=True, pattern="purgefrom$"))
@@ -75,64 +130,6 @@ async def purge_to(event):
     except Exception as e:
         await edit_delete(event, f"**Error**\n`{str(e)}`")
 
-
-@bot.on(admin_cmd(pattern="purge(?: |$)(.*)"))
-@bot.on(sudo_cmd(allow_sudo=True, pattern="purge(?: |$)(.*)"))
-@errors_handler
-async def fastpurger(event):
-    chat = await event.get_input_chat()
-    msgs = []
-    count = 0
-    input_str = event.pattern_match.group(1)
-    reply = await event.get_reply_message()
-    if reply:
-        if input_str and input_str.isnumeric():
-            count += 1
-            async for msg in event.client.iter_messages(
-                event.chat_id,
-                limit=(int(input_str) - 1),
-                offset_id=reply.id,
-                reverse=True,
-            ):
-                msgs.append(msg)
-                count += 1
-                msgs.append(event.reply_to_msg_id)
-                if len(msgs) == 100:
-                    await event.client.delete_messages(chat, msgs)
-                    msgs = []
-        elif input_str:
-            return await edit_or_reply(
-                event, f"**Error**\n`{input_str} is not an integer. Use proper syntax.`"
-            )
-        else:
-            async for msg in event.client.iter_messages(
-                chat, min_id=event.reply_to_msg_id
-            ):
-                msgs.append(msg)
-                count += 1
-                msgs.append(event.reply_to_msg_id)
-                if len(msgs) == 100:
-                    await event.client.delete_messages(chat, msgs)
-                    msgs = []
-    else:
-        await edit_or_reply(
-            event,
-            "`No message specified.`",
-        )
-        return
-    if msgs:
-        await event.client.delete_messages(chat, msgs)
-    await edit_delete(
-        event,
-        "`Fast purge complete!\nPurged " + str(count) + " messages.`",
-    )
-    if BOTLOG:
-        await event.client.send_message(
-            BOTLOG_CHATID,
-            "#PURGE \n`Purge of " + str(count) + " messages done successfully.`",
-        )
-
-
 @bot.on(admin_cmd(pattern="purgeme"))
 @bot.on(sudo_cmd(allow_sudo=True, pattern="purgeme"))
 @errors_handler
@@ -181,8 +178,12 @@ async def delete_it(event):
 CMD_HELP.update(
     {
         "purge": "**Plugin : **`purge`\
-        \n\n•  **Syntax : **`.purge reply to message to start purge from there`\
-        \n•  **Function : **__Purges all messages starting from the reply.__\
+        \n\n•  **Syntax : **`.purge <count>`\
+        \n•  **Function : **__Deletes the x(count) amount messages from the replied message if you don'y use count then deletes all messages from there.__\
+        \n\n•  **Syntax : **`.purgefrom reply`\
+        \n•  **Function : **__Will Mark that message as oldest message of interval to delete messages.__\
+        \n\n•  **Syntax : **`.purgeto reply`\
+        \n•  **Function : **__Will Mark that message as newest message of interval to delete messages and will delete all messages in that interval.__\
         \n\n•  **Syntax : **`.purgeme <x>`\
         \n•  **Function : **__Deletes x amount of your latest messages.__\
         \n\n•  **Syntax : **`.del reply to message to delete`\
