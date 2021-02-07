fd7e994367e05fa68ecd3e8ff359c6e2dc7cf138
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-07 22:58:23 +0530

    Update figlet.py

--- userbot/plugins/figlet.py
+++ userbot/plugins/figlet.py
@@ -1,14 +1,7 @@
 import pyfiglet
-
 from . import deEmojify
 
-
-@bot.on(admin_cmd(pattern="figlet (\w+) (.+)", outgoing=True))
-@bot.on(sudo_cmd(pattern="figlet (\w+) (.+)", allow_sudo=True))
-async def figlet(event):
-    if event.fwd_from:
-        return
-    CMD_FIG = {
+CMD_FIG = {
         "slant": "slant",
         "3D": "3-d",
         "5line": "5lineoblique",
@@ -25,24 +18,41 @@ async def figlet(event):
         "bulb": "bulbhead",
         "digi": "digital",
     }
-    style = event.pattern_match.group(1)
-    text = event.pattern_match.group(2)
-    try:
-        font = CMD_FIG[style]
-    except KeyError:
-        return await edit_delete(
-            event, "**Invalid styleselected**, __Check__ `.info figlet`."
-        )
-    result = pyfiglet.figlet_format(deEmojify(text), font=font)
-    await event.respond(f"‌‌‎`{result}`")
-    await event.delete()
+
+@bot.on(admin_cmd(pattern="figlet(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="figlet(?: |$)(.*)", allow_sudo=True))
+async def figlet(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    if ";" in input_str:
+        cmd, text = input_str.split(";", maxsplit=1)
+    elif input_str is not None:
+        cmd = None
+        text = input_str
+    else:
+        await edit_or_reply(event, "`Give some text to change it`")
+        return
+    style = cmd.strip()
+    text = text.strip()
+    if style is not None:
+        try:
+            font = CMD_FIG[style]
+        except KeyError:
+            return await edit_delete(
+                event, "**Invalid style selected**, __Check__ `.info figlet`."
+            )
+        result = pyfiglet.figlet_format(deEmojify(text), font=font)
+    else:
+        result = pyfiglet.figlet_format(deEmojify(text))
+    await edit_or_reply(event, result , parse_mode=parse_pre)
 
 
 CMD_HELP.update(
     {
         "figlet": "**Plugin :**`figlet`\
-        \n\n• **Syntax : **`.figlet type text`\
+        \n\n• **Syntax : **`.figlet type ; text`\
     \n• **Usage : **the types are `slant`, `3D`, `5line`, `alpha`, `banner`, `doh`, `iso`, `letter`, `allig`, `dotm`, `bubble`, `bulb`, `digi`, `binary`, `basic`\
-    \n• **Example : **`.figlet digi hello`"
+    \n• **Example : **`.figlet digi ; hello`"
     }
 )
