c24bf7b2448a88e0bdc15cbcdcbeaef33a0a1b5c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-08 00:24:34 +0530

    Update tools.py

--- userbot/plugins/tools.py
+++ userbot/plugins/tools.py
@@ -197,8 +197,8 @@ async def _(event):
         yyyy = input_sgra[0]
         mm = input_sgra[1]
         try:
-            input = calendar.month(int(yyyy.strip()), int(mm.strip()))
-            await edit_or_reply(event, f"```{input}```")
+            output_result = calendar.month(int(yyyy.strip()), int(mm.strip()))
+            await edit_or_reply(event, f"```{output_result}```")
         except Exception as e:
             await edit_delete(event, f"`{e}`", 5)
     else:
