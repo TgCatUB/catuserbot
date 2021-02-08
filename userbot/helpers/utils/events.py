166808aa1311cefa57fb50f4f37a2c4248cd17e2
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-02-08 15:33:11 +0000

    pylint: auto fixes

--- userbot/helpers/utils/events.py
+++ userbot/helpers/utils/events.py
@@ -13,7 +13,7 @@ async def reply_id(event):
     return reply_to_id
 
 
-async def get_user_from_event(event, catevent , secondgroup=None):
+async def get_user_from_event(event, catevent, secondgroup=None):
     if secondgroup:
         args = event.pattern_match.group(2).split(" ", 1)
     else:
