28b45080651e867282052af31291e60275263213
ashdroid4 <77984053+ashdroid4@users.noreply.github.com>
2021-02-08 09:36:51 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -91,6 +91,7 @@ class Config(object):
     NO_LOAD = [x for x in os.environ.get("NO_LOAD", "").split()]
     # in alive message pic
     ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
+    ALIVE_PIC_2 = os.environ.get("ALIVE_PIC_2", None)
     # in pm permit pic
     PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
     CUSTOM_PMPERMIT_TEXT = os.environ.get("CUSTOM_PMPERMIT_TEXT", None)
