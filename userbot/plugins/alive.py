4c99d80980476ee6c6e7d13daceb5c44f2596fa9
ashdroid4 <77984053+ashdroid4@users.noreply.github.com>
2021-02-10 23:39:11 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,7 +7,10 @@ from telethon import version
 from . import ALIVE_NAME, StartTime, catversion, get_readable_time, mention, reply_id
 
 DEFAULTUSER = ALIVE_NAME or "cat"
-CAT_IMG= Config.ALIVE_PIC
+CAT_IMG= [ 
+         "Config.ALIVE_PIC",
+         "Config.Alive_PIC_2",
+     ]
 CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✮ MY BOT IS RUNNING SUCCESSFULLY ✮"
 EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  ✥ "
 
@@ -29,7 +32,7 @@ async def amireallyalive(alive):
         cat_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
         cat_caption += f"**{EMOJI} Master:** {mention}\n"
         await alive.client.send_file(
-            alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id
+            alive.chat_id, random.choose(CAT_IMG), caption=cat_caption, reply_to=reply_to_id
         )
         await alive.delete()
     else:
