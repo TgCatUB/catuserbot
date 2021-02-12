885a5508eab9297799a88ed0557599c61dcd3484
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-13 00:26:59 +0530

    Update google.py

--- userbot/plugins/google.py
+++ userbot/plugins/google.py
@@ -14,7 +14,7 @@ from ..utils import errors_handler
 from . import BOTLOG, BOTLOG_CHATID
 
 opener = urllib.request.build_opener()
-useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
 opener.addheaders = [("User-agent", useragent)]
 
 
