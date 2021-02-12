361903eeb75d25a99d176eea5d6b09ff107aec38
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-13 02:10:35 +0530

    Update logchats.py

--- userbot/plugins/logchats.py
+++ userbot/plugins/logchats.py
@@ -63,7 +63,7 @@ async def log_tagged_messages(event):
         (no_log_pms_sql.is_approved(hmm.id))
         or (not Config.PM_LOGGR_BOT_API_ID)
         or ("on" in AFK_.USERAFK_ON)
-        or (await event.get_sender() and (await event.get_sender()).bot):
+        or (await event.get_sender() and (await event.get_sender()).bot)):
         return
     full = None
     try:
