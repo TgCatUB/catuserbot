1ca8c360e1613ffd18be0a442c9f9f634224e779
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-13 18:39:19 +0530

    Update logchats.py

--- userbot/plugins/logchats.py
+++ userbot/plugins/logchats.py
@@ -16,80 +16,80 @@ class LOG_CHATS:
 
 LOG_CHATS_ = LOG_CHATS()
 
-
-@bot.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
-async def monito_p_m_s(event):
-    if not Config.PM_LOGGR_BOT_API_ID:
-        return
-    sender = await event.get_sender()
-    if Config.NO_LOG_PMS and not sender.bot:
-        chat = await event.get_chat()
-        if not no_log_pms_sql.is_approved(chat.id) and chat.id != 777000:
-            if LOG_CHATS_.RECENT_USER != chat.id:
-                LOG_CHATS_.RECENT_USER = chat.id
-                if LOG_CHATS_.NEWPM:
-                    if LOG_CHATS_.COUNT > 1:
-                        await LOG_CHATS_.NEWPM.edit(
-                            LOG_CHATS_.NEWPM.text.replace(
-                                "new message", f"{LOG_CHATS_.COUNT} messages"
+if Config.NO_LOG_PMS:
+    @bot.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+    async def monito_p_m_s(event):
+        if not Config.PM_LOGGR_BOT_API_ID:
+            return
+        sender = await event.get_sender()
+        if not sender.bot:
+            chat = await event.get_chat()
+            if not no_log_pms_sql.is_approved(chat.id) and chat.id != 777000:
+                if LOG_CHATS_.RECENT_USER != chat.id:
+                    LOG_CHATS_.RECENT_USER = chat.id
+                    if LOG_CHATS_.NEWPM:
+                        if LOG_CHATS_.COUNT > 1:
+                            await LOG_CHATS_.NEWPM.edit(
+                                LOG_CHATS_.NEWPM.text.replace(
+                                    "new message", f"{LOG_CHATS_.COUNT} messages"
+                                )
                             )
-                        )
-                    else:
-                        await LOG_CHATS_.NEWPM.edit(
-                            LOG_CHATS_.NEWPM.text.replace(
-                                "new message", f"{LOG_CHATS_.COUNT} message"
+                        else:
+                            await LOG_CHATS_.NEWPM.edit(
+                                LOG_CHATS_.NEWPM.text.replace(
+                                    "new message", f"{LOG_CHATS_.COUNT} message"
+                                )
                             )
-                        )
-                    LOG_CHATS_.COUNT = 0
-                LOG_CHATS_.NEWPM = await event.client.send_message(
-                    Config.PM_LOGGR_BOT_API_ID,
-                    f"👤{_format.mentionuser(sender.first_name , sender.id)} has sent a new message \nId : `{chat.id}`",
-                )
-            try:
-                if event.message:
-                    await event.client.forward_messages(
-                        Config.PM_LOGGR_BOT_API_ID, event.message, silent=True
+                        LOG_CHATS_.COUNT = 0
+                    LOG_CHATS_.NEWPM = await event.client.send_message(
+                        Config.PM_LOGGR_BOT_API_ID,
+                        f"👤{_format.mentionuser(sender.first_name , sender.id)} has sent a new message \nId : `{chat.id}`",
                     )
-                LOG_CHATS_.COUNT += 1
-            except Exception as e:
-                LOGS.warn(str(e))
+                try:
+                    if event.message:
+                        await event.client.forward_messages(
+                            Config.PM_LOGGR_BOT_API_ID, event.message, silent=True
+                        )
+                    LOG_CHATS_.COUNT += 1
+                except Exception as e:
+                    LOGS.warn(str(e))
 
+if Config.NO_LOG_GROUPS:
+    @bot.on(events.NewMessage(incoming=True, func=lambda e: e.mentioned))
+    async def log_tagged_messages(event):
+        hmm = await event.get_chat()
+        from .afk import AFK_
 
-@bot.on(events.NewMessage(incoming=True, func=lambda e: e.mentioned))
-async def log_tagged_messages(event):
-    hmm = await event.get_chat()
-    from .afk import AFK_
-
-    if (
-        (no_log_pms_sql.is_approved(hmm.id))
-        or (not Config.PM_LOGGR_BOT_API_ID)
-        or ("on" in AFK_.USERAFK_ON)
-        or (await event.get_sender() and (await event.get_sender()).bot)
-    ):
-        return
-    full = None
-    try:
-        full = await event.client.get_entity(event.message.from_id)
-    except Exception as e:
-        LOGS.info(str(e))
-    messaget = media_type(event)
-    resalt = f"#TAGS \n<b>Group : </b><code>{hmm.title}</code>"
-    if full is not None:
-        resalt += (
-            f"\n<b>From : </b> 👤{_format.htmlmentionuser(full.first_name , full.id)}"
-        )
-    if messaget is not None:
-        resalt += f"\n<b>Message type : </b><code>{messaget}</code>"
-    else:
-        resalt += f"\n<b>Message : </b>{event.message.message}"
-    resalt += f"\n<b>Message link: </b><a href = 'https://t.me/c/{hmm.id}/{event.message.id}'> link</a>"
-    if not event.is_private:
-        await event.client.send_message(
-            Config.PM_LOGGR_BOT_API_ID,
-            resalt,
-            parse_mode="html",
-            link_preview=False,
-        )
+        if (
+            (no_log_pms_sql.is_approved(hmm.id))
+            or (not Config.PM_LOGGR_BOT_API_ID)
+            or ("on" in AFK_.USERAFK_ON)
+            or (await event.get_sender() and (await event.get_sender()).bot)
+        ):
+            return
+        full = None
+        try:
+            full = await event.client.get_entity(event.message.from_id)
+        except Exception as e:
+            LOGS.info(str(e))
+        messaget = media_type(event)
+        resalt = f"#TAGS \n<b>Group : </b><code>{hmm.title}</code>"
+        if full is not None:
+            resalt += (
+                f"\n<b>From : </b> 👤{_format.htmlmentionuser(full.first_name , full.id)}"
+            )
+        if messaget is not None:
+            resalt += f"\n<b>Message type : </b><code>{messaget}</code>"
+        else:
+            resalt += f"\n<b>Message : </b>{event.message.message}"
+        resalt += f"\n<b>Message link: </b><a href = 'https://t.me/c/{hmm.id}/{event.message.id}'> link</a>"
+        if not event.is_private:
+            await event.client.send_message(
+                Config.PM_LOGGR_BOT_API_ID,
+                resalt,
+                parse_mode="html",
+                link_preview=False,
+            )
 
 
 @bot.on(admin_cmd(outgoing=True, pattern=r"save(?: |$)(.*)"))
