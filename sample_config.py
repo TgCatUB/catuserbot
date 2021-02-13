c58304d1b7db5a1512a0422c56627b31b5f9c14d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-13 18:47:14 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -62,8 +62,6 @@ class Config(object):
     # Telegram BOT Token from @BotFather
     TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
     TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
-    NO_LOG_PMS = bool(os.environ.get("NO_LOG_PMS", True))
-    NO_LOG_GROUPS = bool(os.environ.get("NO_LOG_GROUPS", True))
     THUMB_IMAGE = os.environ.get(
         "THUMB_IMAGE", "https://telegra.ph/file/ca95524e4734b0d5461b5.jpg"
     )
