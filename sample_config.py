ebe0da9fa1928b5ea6e8d46e5127b014fde24336
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-02-13 18:40:00 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -63,6 +63,7 @@ class Config(object):
     TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
     TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
     NO_LOG_PMS = bool(os.environ.get("NO_LOG_PMS", True))
+    NO_LOG_GROUPS = bool(os.environ.get("NO_LOG_GROUPS", True))
     THUMB_IMAGE = os.environ.get(
         "THUMB_IMAGE", "https://telegra.ph/file/ca95524e4734b0d5461b5.jpg"
     )
