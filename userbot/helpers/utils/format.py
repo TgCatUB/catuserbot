4ec941c85e9fba3687c57f0539f101b532b37510
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-02-14 14:18:01 +0000

    pylint: auto fixes

--- userbot/helpers/utils/format.py
+++ userbot/helpers/utils/format.py
@@ -5,7 +5,9 @@ import requests
 from telethon.tl.tlobject import TLObject
 from telethon.tl.types import MessageEntityPre
 from telethon.utils import add_surrogate
+
 from ..functions import utc_to_local
+
 
 def paste_text(text):
     asciich = ["**", "`", "__"]
