acbb2557d810bf467e1dc487686d41f10e8bab61
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-03-11 22:16:07 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -16,7 +16,7 @@ async def add_bot(bot_token):
         bot.me = await bot.get_me()
         bot.uid = telethon.utils.get_peer_id(bot.me)
     except Exception as e:
-        LOGS.error(str(e))
+        LOGS.error(f"STRING_SESSION - {str(e)}")
         exit()
 
 
