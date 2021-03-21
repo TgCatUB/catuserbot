9b9ddca5ad7e8244f3e2677679e143f9352efdf8
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-03-21 16:25:41 +0000

    pylint: auto fixes

--- userbot/plugins/gdrive.py
+++ userbot/plugins/gdrive.py
@@ -523,8 +523,12 @@ async def gdrive_download(event, gdrive, service, uri):
                     speed = round(downloaded / diff, 2)
                     eta = round((file_size - downloaded) / speed)
                     prog_str = "`[{0}{1}] {2}%`".format(
-                        "".os.path.join("▰" for i in range(math.floor(percentage / 10))),
-                        "".os.path.join("▱" for i in range(10 - math.floor(percentage / 10))),
+                        "".os.path.join(
+                            "▰" for i in range(math.floor(percentage / 10))
+                        ),
+                        "".os.path.join(
+                            "▱" for i in range(10 - math.floor(percentage / 10))
+                        ),
                         round(percentage, 2),
                     )
                     current_message = (
@@ -564,8 +568,12 @@ async def gdrive_download(event, gdrive, service, uri):
                     speed = round(downloaded / diff, 2)
                     eta = round((file_size - downloaded) / speed)
                     prog_str = "`[{0}{1}] {2}%`".format(
-                        "".os.path.join("▰" for i in range(math.floor(percentage / 10))),
-                        "".os.path.join("▱" for i in range(10 - math.floor(percentage / 10))),
+                        "".os.path.join(
+                            "▰" for i in range(math.floor(percentage / 10))
+                        ),
+                        "".os.path.join(
+                            "▱" for i in range(10 - math.floor(percentage / 10))
+                        ),
                         round(percentage, 2),
                     )
                     current_message = (
@@ -1476,8 +1484,12 @@ async def check_progress_for_dl(event, gid, previous):
                     percentage = int(file.progress)
                     downloaded = percentage * int(file.total_length) / 100
                     prog_str = "**Downloading : **`[{0}{1}] {2}`".format(
-                        "".os.path.join("▰" for i in range(math.floor(percentage / 10))),
-                        "".os.path.join("▱" for i in range(10 - math.floor(percentage / 10))),
+                        "".os.path.join(
+                            "▰" for i in range(math.floor(percentage / 10))
+                        ),
+                        "".os.path.join(
+                            "▱" for i in range(10 - math.floor(percentage / 10))
+                        ),
                         file.progress_string(),
                     )
 
