e204db9c6d0c124bf9794d6f719d77b7b52304c6
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-13 23:12:43 +0530

    Update __init__.py

--- userbot/sql_helper/__init__.py
+++ userbot/sql_helper/__init__.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-
+from .. import catub
 # the secret configuration specific things
 from ..Config import Config
 from ..core.logger import logging
