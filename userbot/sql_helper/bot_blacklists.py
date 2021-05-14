484ee07abfd7961d870ec402bb29b8bcbcf29da6
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-14 21:51:46 +0530

    Create bot_blacklists.py

---
+++ userbot/sql_helper/bot_blacklists.py
@@ -0,0 +1,60 @@
+from sqlalchemy import (
+    Column,
+    String,
+    UnicodeText
+)
+from . import (
+    SESSION,
+    BASE
+)
+
+
+class Bot_BlackList(BASE):
+    __tablename__ = "bot_blacklist"
+    chat_id = Column(String(14), primary_key=True)
+    reason = Column(UnicodeText)
+
+    def __init__(self, chat_id, reason):
+        self.chat_id = str(chat_id)
+        self.reason = reason
+
+    def __repr__(self):
+        return "<BL %s>" % self.chat_id
+
+
+Bot_BlackList.__table__.create(checkfirst=True)
+
+
+def add_user_to_bl(chat_id: int, reason: str):
+    """ add the user to the blacklist """
+    __user = Bot_BlackList(str(chat_id), reason)
+    SESSION.add(__user)
+    SESSION.commit()
+
+
+def check_is_black_list(chat_id: int):
+    """ check if user_id is blacklisted """
+    try:
+        return SESSION.query(Bot_BlackList).get(str(chat_id))
+    finally:
+        SESSION.close()
+
+
+def rem_user_from_bl(chat_id: int):
+    """ remove the user from the blacklist """
+    s__ = SESSION.query(Bot_BlackList).get(str(chat_id))
+    if s__:
+        SESSION.delete(s__)
+        SESSION.commit()
+        return True
+    SESSION.close()
+    return False
+
+
+def get_all_bl_users():
+    try:
+        return SESSION.query(Bot_BlackList).all()
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()    
