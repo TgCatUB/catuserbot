9d773f021575fc36831056dae0e4eadb2eee7724
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-14 11:51:34 +0530

    Update __init__.py

--- userbot/assistant/__init__.py
+++ userbot/assistant/__init__.py
@@ -1,1 +1,9 @@
 from userbot import catub
+from ..Config import Config
+
+if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+    BOTLOG = False
+    BOTLOG_CHATID = "me"
+else:
+    BOTLOG = True
+    BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
