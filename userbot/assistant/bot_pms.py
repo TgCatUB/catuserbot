ea4a8e7f2c81d88d2e41ad6ea35da2328f6ffe00
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-15 02:11:29 +0530

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -107,6 +107,8 @@ async def bot_pms(event):  # sourcery no-metrics
                     f"**Error**\nWhile storing messages details in database\n`{str(e)}`",
                 )
     else:
+        if event.text in ["/info"]:
+            return
         reply_to = await reply_id(event)
         if reply_to is None:
             return
