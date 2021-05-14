b484c1dfef489f00e524d681820333140c55a279
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-14 13:47:09 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -11,8 +11,9 @@ from userbot.Config import Config
 from userbot.core.logger import logging
 from userbot.core.session import catub
 from userbot.utils import load_module
+from userbot import BOTLOG , BOTLOG_CHATID
 
-LOGS = logging.getLogger(__name__)
+LOGS = logging.getLogger("CatUserbot")
 
 print(userbot.__copyright__)
 print("Licensed under the terms of the " + userbot.__license__)
@@ -39,6 +40,49 @@ async def testing_bot():
         LOGS.error(f"STRING_SESSION - {str(e)}")
         sys.exit()
 
+def verifyLoggerGroup():
+    if BOTLOG:
+        try:
+            entity = catub.loop.run_until_complete(catub.get_entity(BOTLOG_CHATID))
+            if not isinstance(entity, types.User) and not entity.creator:
+                if entity.default_banned_rights.send_messages:
+                    LOGS.info(
+                        "Permissions missing to send messages for the specified Logger group."
+                    )
+                if entity.default_banned_rights.invite_users:
+                    LOGS.info("Permissions missing to addusers for the specified Logger group.")
+        except ValueError:
+            LOGS.error("Logger group ID cannot be found. " "Make sure it's correct.")
+        except TypeError:
+            LOGS.error("Logger group ID is unsupported. " "Make sure it's correct.")
+        except Exception as e:
+            LOGS.error(
+                'An Exception occured upon trying to verify the logger group.\n'
+                + str(e)
+            )
+    else:
+        LOGS.info("You haven't set the PRIVATE_GROUP_BOT_API_ID in vars please set it for proper functioning of userbot.")
+
+async def add_bot_to_logger_group():
+    try:
+        bot_details = await catub.tgbot.get_me()
+        Config.TG_BOT_USERNAME = f"@{bot_details.username}"
+        await catub(
+                    functions.messages.AddChatUserRequest(
+                        channel=BOTLOG_CHATID, users=bot_details.id, fwd_limit=1000000
+                    )
+                )
+    except Exception as e:
+        LOGS.error(str(e))
+    
+async def startupmessage():
+    if BOTLOG:
+            await catub.tgbot.send_message(
+                BOTLOG_CHATID,
+                f"**Congratulation, now type {Config.COMMAND_HAND_LER}alive to see message if catub is working or not\
+                \nIf you need assistance, **head to https://t.me/catuserbot_support",
+                link_preview=False,
+            )
 
 if len(sys.argv) not in (1, 3, 4):
     catub.disconnect()
@@ -68,27 +112,32 @@ for name in files:
             os.remove(Path(f"userbot/plugins/{shortname}.py"))
             LOGS.info(f"unable to load {shortname} because of error {e}")
 
+path = "userbot/assistant/*.py"
+files = glob.glob(path)
+files.sort()
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        try:
+            if shortname.replace(".py", "") not in Config.NO_LOAD:
+                load_module(shortname.replace(".py", ""))
+            else:
+                os.remove(Path(f"userbot/plugins/{shortname}.py"))
+        except Exception as e:
+            os.remove(Path(f"userbot/plugins/{shortname}.py"))
+            LOGS.info(f"unable to load {shortname} because of error {e}")            
 
+print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
 print("Yay your userbot is officially working.!!!")
 print(
-    "Congratulation, now type .alive to see message if catub is live\
+    f"Congratulation, now type {Config.COMMAND_HAND_LER}alive to see message if catub is live\
       \nIf you need assistance, head to https://t.me/catuserbot_support"
 )
+print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
 
-
-async def startupmessage():
-    try:
-        if Config.PRIVATE_GROUP_BOT_API_ID != 0:
-            await catub.send_message(
-                Config.PRIVATE_GROUP_BOT_API_ID,
-                "**Congratulation, now type .alive to see message if catub is live\
-        \nIf you need assistance, **head to https://t.me/catuserbot_support",
-                link_preview=False,
-            )
-    except Exception as e:
-        LOGS.info(str(e))
-
-
+verifyLoggerGroup()
+catub.loop.create_task(add_bot_to_logger_group())
 catub.loop.create_task(startupmessage())
 
 if len(sys.argv) not in (1, 3, 4):
