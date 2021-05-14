96ffba37344db5fb9b97af4d379ec7d849789830
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-05-14 13:12:55 +0000

    pylint: auto fixes

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -149,7 +149,7 @@ async def bot_pms_edit(event):
             await event.client.send_message(
                 Config.OWNER_ID,
                 f"⬆️ This message was edited by the user {get_display_name(chat)} as :",
-                reply_to = reply_msg
+                reply_to=reply_msg,
             )
             msg = await event.forward_to(Config.OWNER_ID)
             try:
