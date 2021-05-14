389eba4e59d280f1c104de9e06d1b9c925a2821e
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-14 16:25:09 +0530

    Update session.py

--- userbot/core/session.py
+++ userbot/core/session.py
@@ -31,7 +31,7 @@ except Exception as e:
     sys.exit()
 
 
-catub.tgbot = CatUserBotClient(
+catub.tgbot = tgbot = CatUserBotClient(
     session="CatTgbot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
