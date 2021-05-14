3388a54f310387d87f513fcac82761d288c88cf9
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-15 02:59:54 +0530

    Create bot_users.py

---
+++ userbot/assistant/bot_users.py
@@ -0,0 +1,102 @@
+
+from datetime import datetime
+
+from telethon import Button, events
+from telethon.utils import get_display_name
+
+from userbot import UPSTREAM_REPO_URL, catub
+
+from ..Config import Config
+from ..core.logger import logging
+from ..core.session import tgbot
+from ..helpers import reply_id
+from ..helpers.utils import _format
+from ..sql_helper.bot_pms_sql import (
+    add_user_to_db,
+    get_user_id,
+    get_user_logging,
+    get_user_reply,
+)
+from ..sql_helper.bot_starters import add_starter_to_db, get_starter_details
+from ..sql_helper.bot_blacklists import add_user_to_bl, check_is_black_list, rem_user_from_bl, get_all_bl_users
+from . import BOTLOG, BOTLOG_CHATID
+
+LOGS = logging.getLogger(__name__)
+
+plugin_category = "bot"
+botusername = Config.TG_BOT_USERNAME
+
+
+
+async def get_user_and_reason(event):  # Modified a bound method
+    id_reason = event.pattern_match.group(1)
+    replied = await reply_id(event)
+    user_id, reason = None, None
+    if replied:
+        users = get_user_id(replied)
+        if users is not None:
+            for usr in users:
+                user_id = int(usr.chat_id)
+                break
+            reason = id_reason
+    else:
+        if id_reason:
+            data = id_reason.split(maxsplit=1)
+            if len(data) == 2:
+                user, reason = data
+            elif len(data) == 1:
+                user = data[0]
+            if user.isdigit():
+                user_id = int(user)
+            if user.startswith("@"):
+                user_id = user
+    return user_id, reason
+  
+async def ban_user_from_bot(user, reason, event, reply_to):
+    try:
+        add_user_to_bl(user.id, get_display_name(user),reason)
+    except Exception as e:
+        LOGS.error(str(e))
+    banned_msg = (
+            f"**You Have been Banned Forever\nReason** : {reason}"
+        )
+    await event.client.send_message(event.chat_id, banned_msg)
+    info = (
+            f"**#Banned_Bot_PM_User**\
+            \n\n👤 {_format.mentionuser(get_display_name(user) , user.id)}\
+            \n**First Name:** {user.first_name}\
+            \n**User ID:** `{user.id}`\
+            \n**Reason:** `{reason}`"
+        )
+    if BOTLOG:
+        await event.client.send_message(BOTLOG_CHATID,info)
+    return info
+@catub.bot_cmd(
+    pattern=f"^/ban({botusername})?([\s]+)?$",
+    from_users=Config.OWNER_ID,
+)
+async def ban_botpms(event):
+    user_id, reason = await get_user_and_reason(event)
+    reply_to = await reply_id(event)
+    if not user_id:
+        return await event.client.send_message(event.chat_id,"`I can't find user to ban`",reply_to=reply_to)
+    if not reason:
+        return await event.client.send_message(event.chat_id,"`To ban the user provide reason first`",reply_to=reply_to)
+    try:
+        user = await event.client.get_entity(user_id)
+    except Exception as e:
+        return await event.reply(f"**Error:**\n`{str(e)}`")
+    if user_id == Config.OWNER_ID:
+        return await event.reply("I can't ban you master")
+    check = check_is_black_list(user.id)
+    if check:
+        return await event.client.send_message(event.chat_id, 
+                                f"#Already_in_ban\
+                                \nUser Already Exists in My Bot BAN List.\
+                                \n**Reason For Bot BAN:** `{check.reason}`",
+        )
+    else:
+        msg = await ban_user_from_bot(user, reason, event, reply_to)
+        await event.reply(msg)
+
+    
