3bfe6d9e5c0dad91cdbd1d296cce8be40cb6a2c4
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-05-14 17:15:14 +0000

    pylint: auto fixes

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -195,11 +195,7 @@ async def handler(event):
             except Exception as e:
                 LOGS.error(str(e))
         user_id, reply_msg, result_id = get_user_id(msg_id)
-        if (
-            user_id is not None
-            and user_id == Config.OWNER_ID
-            and result_id != 0
-        ):
+        if user_id is not None and user_id == Config.OWNER_ID and result_id != 0:
             try:
                 await event.client.delete_messages(user_id, result_id)
             except Exception as e:
