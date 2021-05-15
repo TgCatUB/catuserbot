6bdaffb84b5a3d1899d9f1a08f7354cea624bfbe
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-15 16:19:59 +0530

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -66,8 +66,8 @@ async def bot_start(event):
                     \n\nPowered by [Catuserbot](https://t.me/catuserbot17)"
         buttons = [
             (
-                Button.url("Repo", UPSTREAM_REPO_URL),
-                Button.url("Deploy", "https://github.com/Mr-confused/catpack"),
+                Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
+                Button.url("Deploy", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack"),
             )
         ]
     else:
