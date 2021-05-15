c116bc4b2c3065e3600604477368319863ef9d11
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-05-15 11:38:27 +0000

    pylint: auto fixes

--- userbot/assistant/botcontrols.py
+++ userbot/assistant/botcontrols.py
@@ -1,6 +1,7 @@
 import asyncio
 from datetime import datetime
 from math import floor
+
 from telethon.errors import BadRequestError, FloodWaitError, ForbiddenError
 from telethon.utils import get_display_name
 
