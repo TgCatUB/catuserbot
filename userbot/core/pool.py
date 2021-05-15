58a7031cea1f6c41ca889498012f18c114da4c9d
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-05-15 13:58:20 +0000

    pylint: auto fixes

--- userbot/core/pool.py
+++ userbot/core/pool.py
@@ -1,9 +1,10 @@
 import asyncio
+from concurrent.futures import Future, ThreadPoolExecutor
+from functools import partial, wraps
 from typing import Any, Callable
-from concurrent.futures import ThreadPoolExecutor, Future
-from functools import wraps, partial
 
 from motor.frameworks.asyncio import _EXECUTOR
+
 from .logger import logging
 
 _LOG = logging.getLogger(__name__)
@@ -11,16 +12,18 @@ _LOG_STR = "<<<!  ||||  %s  ||||  !>>>"
 
 
 def submit_thread(func: Callable[[Any], Any], *args: Any, **kwargs: Any) -> Future:
-    """ submit thread to thread pool """
+    """submit thread to thread pool"""
     return _EXECUTOR.submit(func, *args, **kwargs)
 
 
 def run_in_thread(func: Callable[[Any], Any]) -> Callable[[Any], Any]:
-    """ run in a thread """
+    """run in a thread"""
+
     @wraps(func)
     async def wrapper(*args: Any, **kwargs: Any) -> Any:
         loop = asyncio.get_running_loop()
         return await loop.run_in_executor(_EXECUTOR, partial(func, *args, **kwargs))
+
     return wrapper
 
 
