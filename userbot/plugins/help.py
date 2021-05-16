7ab62a82d430eb5409c7fc69eac16a32ec603e8d
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-17 00:15:51 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -53,7 +53,7 @@ async def cmdinfo(input_str, event, plugin=False):
         category = getkey(plugin)
         if category is not None:
             outstr += f"**Category :** `{category}`\n\n"
-    outstr += f"**Intro**\n{about[0]}"
+    outstr += f"**•  Intro :**\n{about[0]}"
     return outstr
 
 
