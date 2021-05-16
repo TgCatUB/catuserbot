3509d77bc3cf219270ebecd8965fc8738a3c3300
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-17 00:30:58 +0530

    Update cmdinfo.py

--- userbot/core/cmdinfo.py
+++ userbot/core/cmdinfo.py
@@ -16,31 +16,31 @@ def _format_about(
         tmp_chelp += "\n\n•  **Description** :\n" f"{about['description'].capitalize()}"
         del about["description"]
     if "flags" in about:
-        tmp_chelp += "\n\n•  **Available Flags** :\n"
+        tmp_chelp += "\n\n•  **Available Flags** :"
         if isinstance(about["flags"], dict):
             for f_n, f_d in about["flags"].items():
-                tmp_chelp += f"    ▫ `{f_n}` : {f_d.lower()}\n"
+                tmp_chelp += f"\n    ▫ `{f_n}` : {f_d.lower()}"
         else:
             tmp_chelp += f"\n    {about['flags']}"
         del about["flags"]
     if "options" in about:
-        tmp_chelp += "\n\n•  **Available Options** :\n"
+        tmp_chelp += "\n\n•  **Available Options** :"
         if isinstance(about["options"], dict):
             for o_n, o_d in about["options"].items():
-                tmp_chelp += f"    ▫ `{o_n}` : {o_d.lower()}\n"
+                tmp_chelp += f"\n    ▫ `{o_n}` : {o_d.lower()}"
         else:
-            tmp_chelp += f"    {about['options']}\n"
+            tmp_chelp += f"\n    {about['options']}"
         del about["options"]
     if "types" in about:
-        tmp_chelp += "\n\n•  **Supported Types** :\n"
+        tmp_chelp += "\n\n•  **Supported Types** :"
         if isinstance(about["types"], list):
             for _opt in about["types"]:
-                tmp_chelp += f"    `{_opt}` ,"
+                tmp_chelp += f"\n    `{_opt}` ,"
         else:
-            tmp_chelp += f"    {about['types']}"
+            tmp_chelp += f"\n    {about['types']}"
         del about["types"]
     if "usage" in about:
-        tmp_chelp += "\n•  **Usage** :"
+        tmp_chelp += "\n\n•  **Usage** :"
         if isinstance(about["usage"], list):
             for ex_ in about["usage"]:
                 tmp_chelp += f"\n    `{ex_}`"
