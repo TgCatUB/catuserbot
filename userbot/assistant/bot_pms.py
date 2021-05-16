1317d0d13cf38d1453b74fc7cd6e9e9c142d1dcf
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-16 12:46:58 +0530

    Create bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -7,7 +7,7 @@ from typing import Optional, Union
 from telethon import Button, events
 from telethon.events import CallbackQuery, StopPropagation
 from telethon.utils import get_display_name
-
+from telethon.errors import UserIsBlockedError
 from userbot import Config, catub
 
 from ..core import check_owner, pool
@@ -24,13 +24,13 @@ from ..sql_helper.bot_pms_sql import (
 )
 from ..sql_helper.bot_starters import add_starter_to_db, get_starter_details
 from . import BOTLOG, BOTLOG_CHATID
-from .botmanagers import ban_user_from_bot
+
+from .botmanagers import get_user_and_reason, progress_str, ban_user_from_bot, unban_user_from_bot
 
 LOGS = logging.getLogger(__name__)
 
 plugin_category = "bot"
 botusername = Config.TG_BOT_USERNAME
-
 
 class FloodConfig:
     BANNED_USERS = set()
@@ -40,7 +40,6 @@ class FloodConfig:
     OWNER = [Config.OWNER_ID]
     ALERT = defaultdict(dict)
     AUTOBAN = 10
-
 
 async def check_bot_started_users(user, event):
     if user.id == Config.OWNER_ID:
@@ -288,6 +287,7 @@ async def bot_start(event):
     await info_msg.edit(uinfo)
 
 
+
 async def send_flood_alert(user_) -> None:
     # sourcery no-metrics
     buttons = [
@@ -355,9 +355,9 @@ async def send_flood_alert(user_) -> None:
         try:
             await catub.tgbot.send_message(
                 Config.OWNER_ID,
-                f"⚠️  **[Bot Flood Warning !]({fa_msg.link})**",
+                f"⚠️  **[Bot Flood Warning !](https://t.me/c/{BOTLOG_CHATID}/{fa_msg.id})**",
             )
-        except UserIsBlocked:
+        except UserIsBlockedError:
             if BOTLOG:
                 await catub.tgbot.send_message(BOTLOG_CHATID, "**Unblock your bot !**")
     if FloodConfig.ALERT[user_.id].get("fa_id") is None and fa_msg:
