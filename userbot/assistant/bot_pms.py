8b7e6823ec7928b0e7d587f73f3c228b1c7c4fb7
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-16 11:59:26 +0530

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -2,7 +2,7 @@ import asyncio
 from collections import defaultdict
 from datetime import datetime
 from typing import Optional, Union
-
+import re
 from telethon import Button, events
 from telethon.events import CallbackQuery
 from telethon.utils import get_display_name
