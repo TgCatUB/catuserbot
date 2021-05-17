e5565621569c372c67352eb7c4609f862e075eec
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-17 17:48:13 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -28,8 +28,8 @@ ALIVE_NAME = Config.ALIVE_NAME
 AUTONAME = Config.AUTONAME
 DEFAULT_BIO = Config.DEFAULT_BIO
 # mention user
-mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
-hmention = f"<a href = tg://user?id={USERID}>{DEFAULTUSER}</a>"
+mention = f"[{ALIVE_NAME}](tg://user?id={USERID})"
+hmention = f"<a href = tg://user?id={USERID}>{ALIVE_NAME}</a>"
 
 
 Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
