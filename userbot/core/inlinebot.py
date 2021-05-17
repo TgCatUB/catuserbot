8446bbf43d610fc8e249eff0a4c231e16da87944
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-17 16:52:21 +0530

    Update inlinebot.py

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -131,7 +131,7 @@ async def inline_handler(event):  # sourcery no-metrics
         timestamp = int(time.time() * 2)
         newsecret = {str(timestamp): {"userid": u, "text": txct}}
 
-        buttons = [custom.Button.inline("show message 🔐", data=f"secret_{timestamp}")]
+        buttons = [Button.inline("show message 🔐", data=f"secret_{timestamp}")]
         result = builder.article(
             title="secret message",
             text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
