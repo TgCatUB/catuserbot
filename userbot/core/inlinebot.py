95e308a0fbbbf581914fea0a903840140cb9b73f
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-17 21:20:36 +0530

    Update inlinebot.py

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -12,7 +12,7 @@ from ..Config import Config
 
 CAT_IMG = Config.ALIVE_PIC or None
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
-
+CAT_LOGO = "https://telegra.ph/file/3f91309862d81438e6710.jpg"
 
 def ibuild_keyboard(buttons):
     keyb = []
@@ -29,121 +29,147 @@ async def inline_handler(event):  # sourcery no-metrics
     builder = event.builder
     result = None
     query = event.text
-    hmm = re.compile("secret (.*) (.*)")
-    match = re.findall(hmm, query)
-    if query.startswith("**Catuserbot") and event.query.user_id == catub.uid:
-        buttons = [
-            (
-                Button.inline("Stats", data="stats"),
-                Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
-            )
-        ]
-        if CAT_IMG and CAT_IMG.endswith((".jpg", ".png")):
-            result = builder.photo(
-                CAT_IMG,
-                # title="Alive cat",
-                text=query,
-                buttons=buttons,
-            )
-        elif CAT_IMG:
-            result = builder.document(
-                CAT_IMG,
-                title="Alive cat",
-                text=query,
-                buttons=buttons,
-            )
-        else:
-            result = builder.article(
-                title="Alive cat",
-                text=query,
-                buttons=buttons,
-            )
-        await event.answer([result] if result else None)
-    elif event.query.user_id == catub.uid and query.startswith("Inline buttons"):
-        markdown_note = query[14:]
-        prev = 0
-        note_data = ""
-        buttons = []
-        for match in BTN_URL_REGEX.finditer(markdown_note):
-            # Check if btnurl is escaped
-            n_escapes = 0
-            to_check = match.start(1) - 1
-            while to_check > 0 and markdown_note[to_check] == "\\":
-                n_escapes += 1
-                to_check -= 1
-            # if even, not escaped -> create button
-            if n_escapes % 2 == 0:
-                # create a thruple with button label, url, and newline
-                # status
-                buttons.append((match.group(2), match.group(3), bool(match.group(4))))
-                note_data += markdown_note[prev : match.start(1)]
-                prev = match.end(1)
-            # if odd, escaped -> move along
-            elif n_escapes % 2 == 1:
-                note_data += markdown_note[prev:to_check]
-                prev = match.start(1) - 1
+    string = query.lower()
+    str_x = query.split(" ", 2)
+    str_y = query.split(" ", 1)
+    string_split = string.split()
+    query_user_id = event.query.user_id
+    if query_user_id == Config.OWNER_ID or query_user_id in Config.SUDO_USERS:
+        hmm = re.compile("secret (.*) (.*)")
+        match = re.findall(hmm, query)
+        if query.startswith("**Catuserbot"):
+            buttons = [
+                (
+                    Button.inline("Stats", data="stats"),
+                    Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
+                )
+            ]
+            if CAT_IMG and CAT_IMG.endswith((".jpg", ".png")):
+                result = builder.photo(
+                    CAT_IMG,
+                    # title="Alive cat",
+                    text=query,
+                    buttons=buttons,
+                )
+            elif CAT_IMG:
+                result = builder.document(
+                    CAT_IMG,
+                    title="Alive cat",
+                    text=query,
+                    buttons=buttons,
+                )
             else:
-                break
-        else:
-            note_data += markdown_note[prev:]
-        message_text = note_data.strip()
-        tl_ib_buttons = ibuild_keyboard(buttons)
-        result = builder.article(
-            title="Inline creator",
-            text=message_text,
-            buttons=tl_ib_buttons,
-            link_preview=False,
-        )
-        await event.answer([result] if result else None)
-    elif event.query.user_id == catub.uid and match:
-        query = query[7:]
-        user, txct = query.split(" ", 1)
-        builder = event.builder
-        secret = os.path.join("./userbot", "secrets.txt")
-        try:
-            jsondata = json.load(open(secret))
-        except Exception:
-            jsondata = False
-        try:
-            # if u is user id
-            u = int(user)
+                result = builder.article(
+                    title="Alive cat",
+                    text=query,
+                    buttons=buttons,
+                )
+            await event.answer([result] if result else None)
+        elif query.startswith("Inline buttons"):
+            markdown_note = query[14:]
+            prev = 0
+            note_data = ""
+            buttons = []
+            for match in BTN_URL_REGEX.finditer(markdown_note):
+                # Check if btnurl is escaped
+                n_escapes = 0
+                to_check = match.start(1) - 1
+                while to_check > 0 and markdown_note[to_check] == "\\":
+                    n_escapes += 1
+                    to_check -= 1
+                # if even, not escaped -> create button
+                if n_escapes % 2 == 0:
+                    # create a thruple with button label, url, and newline
+                    # status
+                    buttons.append((match.group(2), match.group(3), bool(match.group(4))))
+                    note_data += markdown_note[prev : match.start(1)]
+                    prev = match.end(1)
+                # if odd, escaped -> move along
+                elif n_escapes % 2 == 1:
+                    note_data += markdown_note[prev:to_check]
+                    prev = match.start(1) - 1
+                else:
+                    break
+            else:
+                note_data += markdown_note[prev:]
+            message_text = note_data.strip()
+            tl_ib_buttons = ibuild_keyboard(buttons)
+            result = builder.article(
+                title="Inline creator",
+                text=message_text,
+                buttons=tl_ib_buttons,
+                link_preview=False,
+            )
+            await event.answer([result] if result else None)
+        elif match:
+            query = query[7:]
+            user, txct = query.split(" ", 1)
+            builder = event.builder
+            secret = os.path.join("./userbot", "secrets.txt")
             try:
-                u = await event.client.get_entity(u)
+                jsondata = json.load(open(secret))
+            except Exception:
+                jsondata = False
+            try:
+                # if u is user id
+                u = int(user)
+                try:
+                    u = await event.client.get_entity(u)
+                    if u.username:
+                        sandy = f"@{u.username}"
+                    else:
+                        sandy = f"[{u.first_name}](tg://user?id={u.id})"
+                except ValueError:
+                    # ValueError: Could not find the input entity
+                    sandy = f"[user](tg://user?id={u})"
+            except ValueError:
+                # if u is username
+                try:
+                    u = await event.client.get_entity(user)
+                except ValueError:
+                    return
                 if u.username:
                     sandy = f"@{u.username}"
                 else:
                     sandy = f"[{u.first_name}](tg://user?id={u.id})"
-            except ValueError:
-                # ValueError: Could not find the input entity
-                sandy = f"[user](tg://user?id={u})"
-        except ValueError:
-            # if u is username
-            try:
-                u = await event.client.get_entity(user)
-            except ValueError:
+                u = int(u.id)
+            except Exception:
                 return
-            if u.username:
-                sandy = f"@{u.username}"
-            else:
-                sandy = f"[{u.first_name}](tg://user?id={u.id})"
-            u = int(u.id)
-        except Exception:
-            return
-        timestamp = int(time.time() * 2)
-        newsecret = {str(timestamp): {"userid": u, "text": txct}}
+            timestamp = int(time.time() * 2)
+            newsecret = {str(timestamp): {"userid": u, "text": txct}}
 
-        buttons = [Button.inline("show message 🔐", data=f"secret_{timestamp}")]
-        result = builder.article(
-            title="secret message",
-            text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
-            buttons=buttons,
-        )
+            buttons = [Button.inline("show message 🔐", data=f"secret_{timestamp}")]
+            result = builder.article(
+                title="secret message",
+                text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
+                buttons=buttons,
+            )
+            await event.answer([result] if result else None)
+            if jsondata:
+                jsondata.update(newsecret)
+                json.dump(jsondata, open(secret, "w"))
+            else:
+                json.dump(newsecret, open(secret, "w"))
+    else:
+        buttons = [
+            (
+                Button.url("Source code", "https://github.com/sandy1709/catuserbot"),
+                Button.url(
+                    "Deploy",
+                    "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack",
+                ),
+            )
+        ]
+        result = builder.photo(
+                    CAT_LOGO,
+                    title="Alive cat",
+                    url="https://github.com/sandy1709/catuserbot",
+                    text="Deploy your own catuserbot",
+                    link_preview=False,
+                    buttons=buttons,
+                )
         await event.answer([result] if result else None)
-        if jsondata:
-            jsondata.update(newsecret)
-            json.dump(jsondata, open(secret, "w"))
-        else:
-            json.dump(newsecret, open(secret, "w"))
+
 
 
 @catub.tgbot.on(CallbackQuery(data=re.compile(b"close")))
