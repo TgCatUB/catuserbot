0bd9720ac0f7f3822631f643cc74fd0f14dd19ac
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-17 17:09:41 +0530

    Update inlinebot.py

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -145,7 +145,6 @@ async def inline_handler(event):  # sourcery no-metrics
         else:
             json.dump(newsecret, open(secret, "w"))
 
-
 @catub.tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
@@ -166,7 +165,6 @@ async def on_plug_in_callback_query_handler(event):
         reply_pop_up_alert = "This message no longer exists "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-
 @catub.tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
 async def on_plug_in_callback_query_handler(event):
     if event.query.user_id == catub.uid:
@@ -175,9 +173,7 @@ async def on_plug_in_callback_query_handler(event):
         reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-
 @catub.tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
 async def on_plug_in_callback_query_handler(event):
     statstext = await catalive()
-    print(statstext)
     await event.answer(statstext, cache_time=0, alert=True)
