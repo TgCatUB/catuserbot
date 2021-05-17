fe721ab88a4ec809987b4396e63a69eafb12649a
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-17 17:19:18 +0530

    Update ialivetext.py

--- userbot/helpers/functions/ialivetext.py
+++ userbot/helpers/functions/ialivetext.py
@@ -41,9 +41,7 @@ def check_data_base_heal_th():
     return is_database_working, output
 
 
-async def catalive():
-    from userbot import StartTime
-
+async def catalive(StartTime):
     _, check_sgnirts = check_data_base_heal_th()
     sudo = "Enabled" if Config.SUDO_USERS else "Disabled"
     uptime = await get_readable_time((time.time() - StartTime))
