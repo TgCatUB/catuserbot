4c63dba8a215fa6901769e4001bc7df5b48e52f3
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-17 16:35:49 +0530

    Update sangmata.py

--- userbot/plugins/sangmata.py
+++ userbot/plugins/sangmata.py
@@ -26,7 +26,7 @@ plugin_category = "utils"
         "examples": "{tr}sg @missrose_bot",
     },
 )
-async def _(event):  # sourcery no-metrics
+async def _(event):    # sourcery no-metrics
     "To get name/username history."
     input_str = "".join(event.text.split(maxsplit=1)[1:])
     reply_message = await event.get_reply_message()
@@ -60,19 +60,11 @@ async def _(event):  # sourcery no-metrics
         await edit_delete(catevent, "`The user doesn't have any record`")
     names, usernames = await sanga_seperator(responses)
     cmd = event.pattern_match.group(1)
-    if cmd == "sg":
-        sandy = None
-        for i in names:
-            if sandy:
+    sandy = None
+    check = usernames if cmd == "u" else names
+    for i in check:
+        if sandy:
                 await event.reply(i, parse_mode=_format.parse_pre)
-            else:
-                sandy = True
-                await catevent.edit(i, parse_mode=_format.parse_pre)
-    elif cmd == "sgu":
-        sandy = None
-        for i in usernames:
-            if sandy:
-                await event.reply(i, parse_mode=_format.parse_pre)
-            else:
+        else:
                 sandy = True
                 await catevent.edit(i, parse_mode=_format.parse_pre)
