5339aa822c1492b79804304c33e7c86a720630e4
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-05-17 18:33:48 +0000

    pylint: auto fixes

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -165,16 +165,16 @@ files = glob.glob(path)
 files.sort()
 for name in files:
     with open(name) as f:
-            path1 = Path(f.name)
-            shortname = path1.stem
-        #try:
-            if shortname.replace(".py", "") not in Config.NO_LOAD:
-                load_module(shortname.replace(".py", ""))
-            else:
-                os.remove(Path(f"userbot/plugins/{shortname}.py"))
-        #except Exception as e:
-         #   os.remove(Path(f"userbot/plugins/{shortname}.py"))
-          #  LOGS.info(f"unable to load {shortname} because of error {e}")
+        path1 = Path(f.name)
+        shortname = path1.stem
+        # try:
+        if shortname.replace(".py", "") not in Config.NO_LOAD:
+            load_module(shortname.replace(".py", ""))
+        else:
+            os.remove(Path(f"userbot/plugins/{shortname}.py"))
+    # except Exception as e:
+    #   os.remove(Path(f"userbot/plugins/{shortname}.py"))
+    #  LOGS.info(f"unable to load {shortname} because of error {e}")
 
 path = "userbot/assistant/*.py"
 files = glob.glob(path)
