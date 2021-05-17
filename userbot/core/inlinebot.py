c74e380226d3972b6b51c68828fe33737bcd7e8b
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-05-17 11:40:25 +0000

    pylint: auto fixes

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -145,6 +145,7 @@ async def inline_handler(event):  # sourcery no-metrics
         else:
             json.dump(newsecret, open(secret, "w"))
 
+
 @catub.tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
@@ -165,6 +166,7 @@ async def on_plug_in_callback_query_handler(event):
         reply_pop_up_alert = "This message no longer exists "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
+
 @catub.tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
 async def on_plug_in_callback_query_handler(event):
     if event.query.user_id == catub.uid:
@@ -172,6 +174,7 @@ async def on_plug_in_callback_query_handler(event):
     else:
         reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
 
 @catub.tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
 async def on_plug_in_callback_query_handler(event):
