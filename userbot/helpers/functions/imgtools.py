c9e4aec2964ab3a3a583f3e8aea2180a5de0a4ab
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-17 16:14:02 +0530

    Create imgtools.py

---
+++ userbot/helpers/functions/imgtools.py
@@ -0,0 +1,83 @@
+import os
+import re
+import time
+import urllib.request
+import zipfile
+from datetime import datetime
+from random import choice
+
+import PIL.ImageOps
+import requests
+from emoji import get_emoji_regexp
+from PIL import Image, ImageDraw, ImageFont
+from telethon.tl.types import Channel, PollAnswer
+from validators.url import url
+from youtubesearchpython import VideosSearch
+
+from ..resources.states import states
+
+
+def convert_toimage(image, filename=None):
+    filename = filename or os.path.join("./temp/", "temp.jpg")
+    img = Image.open(image)
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+    img.save(filename, "jpeg")
+    os.remove(image)
+    return filename
+
+
+def convert_tosticker(response, filename=None):
+    filename = filename or os.path.join("./temp/", "temp.webp")
+    image = Image.open(response)
+    if image.mode != "RGB":
+        image.convert("RGB")
+    image.save(filename, "webp")
+    os.remove(response)
+    return filename
+
+
+# http://effbot.org/imagingbook/imageops.html
+# https://stackoverflow.com/questions/2498875/how-to-invert-colors-of-image-with-pil-python-imaging/38378828
+
+
+async def invert_colors(imagefile, endname):
+    image = Image.open(imagefile)
+    inverted_image = PIL.ImageOps.invert(image)
+    inverted_image.save(endname)
+
+
+async def flip_image(imagefile, endname):
+    image = Image.open(imagefile)
+    inverted_image = PIL.ImageOps.flip(image)
+    inverted_image.save(endname)
+
+
+async def grayscale(imagefile, endname):
+    image = Image.open(imagefile)
+    inverted_image = PIL.ImageOps.grayscale(image)
+    inverted_image.save(endname)
+
+
+async def mirror_file(imagefile, endname):
+    image = Image.open(imagefile)
+    inverted_image = PIL.ImageOps.mirror(image)
+    inverted_image.save(endname)
+
+
+async def solarize(imagefile, endname):
+    image = Image.open(imagefile)
+    inverted_image = PIL.ImageOps.solarize(image, threshold=128)
+    inverted_image.save(endname)
+
+
+async def add_frame(imagefile, endname, x, color):
+    image = Image.open(imagefile)
+    inverted_image = PIL.ImageOps.expand(image, border=x, fill=color)
+    inverted_image.save(endname)
+
+
+async def crop(imagefile, endname, x):
+    image = Image.open(imagefile)
+    inverted_image = PIL.ImageOps.crop(image, border=x)
+    inverted_image.save(endname)
