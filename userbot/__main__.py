2924e2f78615576e4b824375fa7b687613e82363
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-18 21:50:00 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -210,7 +210,7 @@ print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
 
 verifyLoggerGroup()
 add_bot_to_logger_group()
-userbot.catublogo = catub.loop.create_task(startupmessage())
+Config.CATUBLOGO = catub.loop.create_task(startupmessage())
 
 if len(sys.argv) not in (1, 3, 4):
     catub.disconnect()
