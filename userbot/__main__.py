4b03eaba15aef7ede0be99545c8b2fbc3a58a6c2
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-18 22:05:36 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -141,7 +141,7 @@ async def startupmessage():
         if BOTLOG:
             Config.CATUBLOGO = await catub.tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/2d36d118f140ff059065b.jpg",
+                "https://telegra.ph/file/4e3ba8e8f7e535d5a2abe.jpg",
                 caption="**Your CatUserbot has been started successfully.**",
                 buttons=[(Button.url("Support", "https://t.me/catuserbot"),)],
             )
