150a2877427761edf245a69b93a45cea0c020894
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-05-20 11:27:32 +0000

    pylint: auto fixes

--- userbot/helpers/aiohttp_helper.py
+++ userbot/helpers/aiohttp_helper.py
@@ -22,7 +22,7 @@ class AioHttp:
         async with aiohttp.ClientSession() as session:
             async with session.get(link) as resp:
                 return await resp.read()
-    
+
     @staticmethod
     async def get_status(link):
         async with aiohttp.ClientSession() as session:
