a0d8792d670d8c3d903e07bb9de239463d89a5fb
sandy-1709 <75612908+sandy-1709@users.noreply.github.com>
2021-05-22 19:39:26 +0530

    Update reddit.py

--- userbot/plugins/reddit.py
+++ userbot/plugins/reddit.py
@@ -49,7 +49,7 @@ async def reddit_fetch(event):
                 BOTLOG_CHATID, f"**Error Code: {code}**\n`{code_message}`"
             )
             await edit_delete(
-                event, BOTLOG_CHATID, f"**Error Code: {code}**\n`{code_message}`"
+                event, f"**Error Code: {code}**\n`{code_message}`"
             )
     else:
         if "url" not in r:
