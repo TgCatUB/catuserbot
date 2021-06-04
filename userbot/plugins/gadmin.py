9b3fdba279ecdbac1e608895cff21d54e3fffd9a
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-06-04 19:09:29 +0000

    pylint: auto fixes

--- userbot/plugins/gadmin.py
+++ userbot/plugins/gadmin.py
@@ -6,6 +6,7 @@ from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChatBannedRights
 from telethon.utils import get_display_name
+
 from userbot import catub
 
 from ..core.managers import edit_delete, edit_or_reply
