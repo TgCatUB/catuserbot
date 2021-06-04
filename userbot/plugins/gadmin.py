82eec2e51cff8892e1e798df1e3f325c2fe93a37
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-05 00:38:53 +0530

    Update gadmin.py

--- userbot/plugins/gadmin.py
+++ userbot/plugins/gadmin.py
@@ -5,7 +5,7 @@ from telethon.errors import BadRequestError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChatBannedRights
-
+from telethon.utils import get_display_name
 from userbot import catub
 
 from ..core.managers import edit_delete, edit_or_reply
