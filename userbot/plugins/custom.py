95e0b2701ad690481ce6f6bbf0eb4f1f4534585a
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-05 19:21:57 +0530

    Update custom.py

--- userbot/plugins/custom.py
+++ userbot/plugins/custom.py
@@ -1,27 +1,110 @@
-from urlextract import URLExtract
-
 from userbot import catub
 from userbot.core.logger import logging
 
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from validators.url import url
 
 plugin_category = "utils"
 LOGS = logging.getLogger(__name__)
 cmdhd = Config.COMMAND_HAND_LER
-extractor = URLExtract()
+vlist = [
+    "ALIVE_PIC",
+    "ALIVE_EMOJI",
+    "ALIVE_TEXT",
+    "ALLOW_NSFW",
+    "HELP_EMOJI",
+    "HELP_TEXT",
+    "IALIVE_PIC",
+    "PM_PIC",
+]
 
 
 @catub.cat_cmd(
-    pattern="custom (pmpermit|pmblock|pmpic)$",
+    pattern="(set|get|del)dv(?: |$)(.*)",
+    command=("dv", plugin_category),
+    info={
+        "header": "Set vars in database or Check or Delete",
+        "description": "Set , Fetch or Delete values or vars directly in database without restart or heroku vars.\n\nYou can set multiple pics by giving space after links in alive, ialive, pm permit.",
+        "flags": {
+            "set": "To set new var in database or modify the old var",
+            "get": "To show the already existing var value.",
+            "del": "To delete the existing value",
+        },
+        "var name": {
+            "ALIVE_PIC": "To set picture in alive",
+            "ALIVE_EMOJI": "To set custom emoji in alive",
+            "ALIVE_TEXT": "To set custom text in alive",
+            "ALLOW_NSFW": "To acess NSFW stuff by bot set is value 'True'",
+            "IALIVE_PIC": "To set picture in alive",
+            "HELP_EMOJI": "To set custom emoji in help",
+            "HELP_TEXT": "To set custom text in help",
+            "PM_PIC": "To customize pmpermit pic",
+        },
+        "usage": [
+            "{tr}setdv <var name> <var value>",
+            "{tr}getdv <var name>",
+            "{tr}deldv <var name>",
+        ],
+        "examples": [
+            "{tr}setdv ALIVE_PIC <pic link>",
+            "{tr}setdv ALIVE_PIC <pic link 1> <pic link 2>",
+            "{tr}getdv ALIVE_PIC",
+            "{tr}deldv ALIVE_PIC",
+        ],
+    },
+)
+async def bad(event):  # sourcery no-metrics
+    "To manage vars in database"
+    cmd = event.pattern_match.group(1).lower()
+    vname = event.pattern_match.group(2)
+    vnlist = "".join(f"{i}. `{each}`\n" for i, each in enumerate(vlist, start=1))
+    if not vname:
+        return await edit_delete(
+            event, f"**📑 Give correct var name from the list :\n\n**{vnlist}", time=60
+        )
+    if " " in vname:
+        vname, vinfo = vname.split(" ", 1)
+    if vname in vlist:
+        if cmd == "set":
+            if not vinfo:
+                return await edit_delete(
+                    event, f"Give some values which you want to save for **{vname}**"
+                )
+            if "PIC" in vname and not url(vinfo):
+                await edit_delete(event, "**Give me a correct link...**")
+            else:
+                addgvar(vname, vinfo)
+                await edit_delete(
+                    event, f"📑 Value of **{vname}** is changed to :- `{vinfo}`", time=20
+                )
+        if cmd == "get":
+            var_data = gvarstatus(vname)
+            await edit_delete(
+                event, f"📑 Value of **{vname}** is  `{var_data}`", time=20
+            )
+        elif cmd == "del":
+            delgvar(vname)
+            await edit_delete(
+                event,
+                f"📑 Value of **{vname}** is now deleted & set to default.",
+                time=20,
+            )
+    else:
+        await edit_delete(
+            event, f"**📑 Give correct var name from the list :\n\n**{vnlist}", time=60
+        )
+
+
+@catub.cat_cmd(
+    pattern="custom (pmpermit|pmblock)$",
     command=("custom", plugin_category),
     info={
         "header": "To customize your CatUserbot.",
         "options": {
             "pmpermit": "To customize pmpermit text. ",
             "pmblock": "To customize pmpermit block message.",
-            "pmpic": "To customize pmpermit pic. Reply to media url or text containing media.",
         },
         "custom": {
             "{mention}": "mention user",
@@ -48,30 +131,24 @@ async def custom_catuserbot(event):
     "To customize your CatUserbot."
     reply = await event.get_reply_message()
     text = reply.text
-    if not reply:
+    if not reply and text:
         return await edit_delete(event, "__Reply to custom text or url__")
     input_str = event.pattern_match.group(1)
     if input_str == "pmpermit":
         addgvar("pmpermit_txt", text)
     if input_str == "pmblock":
         addgvar("pmblock", text)
-    if input_str == "pmpic":
-        urls = extractor.find_urls(reply.text)
-        if not urls:
-            return await edit_delete(event, "`the given link is not supported`", 5)
-        addgvar("pmpermit_pic", urls[0])
     await edit_or_reply(event, f"__Your custom {input_str} has been updated__")
 
 
 @catub.cat_cmd(
-    pattern="delcustom (pmpermit|pmblock|pmpic)$",
+    pattern="delcustom (pmpermit|pmblock)$",
     command=("delcustom", plugin_category),
     info={
         "header": "To delete costomization of your CatUserbot.",
         "options": {
             "pmpermit": "To delete custom pmpermit text",
             "pmblock": "To delete custom pmpermit block message",
-            "pmpic": "To delete custom pmpermit pic.",
         },
         "usage": [
             "{tr}delcustom <option>",
@@ -89,10 +166,6 @@ async def custom_catuserbot(event):
         if gvarstatus("pmblock") is None:
             return await edit_delete(event, "__You haven't customzied your pmblock.__")
         delgvar("pmblock")
-    if input_str == "pmpic":
-        if gvarstatus("pmpermit_pic") is None:
-            return await edit_delete(event, "__You haven't customzied your pmpic.__")
-        delgvar("pmpermit_pic")
     await edit_or_reply(
         event, f"__Succesfully deleted your customization of {input_str}.__"
     )
