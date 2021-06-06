9563bb5482be5824a825c02825c144077e3ee2d0
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-06-06 11:08:35 +0000

    pylint: auto fixes

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import sys
-from ..helpers.utils import _catutils
+
 import heroku3
 import urllib3
 from git import Repo
@@ -12,6 +12,7 @@ from userbot import UPSTREAM_REPO_URL, catub
 from ..Config import Config
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.utils import _catutils
 from ..sql_helper.global_collection import (
     add_to_collectionlist,
     del_keyword_collectionlist,
@@ -111,64 +112,63 @@ async def update(event, repo, ups_rem, ac_br):
 
 
 async def deploy(event, repo, ups_rem, ac_br, txt):
-        if HEROKU_API_KEY is None:
-            return await event.edit("`Please set up`  **HEROKU_API_KEY**  ` Var...`")
-        heroku = heroku3.from_key(HEROKU_API_KEY)
-        heroku_app = None
-        heroku_applications = heroku.apps()
-        if HEROKU_APP_NAME is None:
-            await event.edit(
-                "`Please set up the` **HEROKU_APP_NAME** `Var`"
-                " to be able to deploy your userbot...`"
-            )
-            repo.__del__()
-            return
-        for app in heroku_applications:
-            if app.name == HEROKU_APP_NAME:
-                heroku_app = app
-                break
-        if heroku_app is None:
-            await event.edit(
-                f"{txt}\n" "`Invalid Heroku credentials for deploying userbot dyno.`"
-            )
-            return repo.__del__()
-        sandy = await event.edit(
-            "`Userbot dyno build in progress, please wait until the process finishes it usually takes 4 to 5 minutes .`"
+    if HEROKU_API_KEY is None:
+        return await event.edit("`Please set up`  **HEROKU_API_KEY**  ` Var...`")
+    heroku = heroku3.from_key(HEROKU_API_KEY)
+    heroku_app = None
+    heroku_applications = heroku.apps()
+    if HEROKU_APP_NAME is None:
+        await event.edit(
+            "`Please set up the` **HEROKU_APP_NAME** `Var`"
+            " to be able to deploy your userbot...`"
         )
-        try:
-            ulist = get_collectionlist_items()
-            for i in ulist:
-                if i == "restart_update":
-                    del_keyword_collectionlist("restart_update")
-        except Exception as e:
-            LOGS.error(e)
-        try:
-            add_to_collectionlist("restart_update", [sandy.chat_id, sandy.id])
-        except Exception as e:
-            LOGS.error(e)
-        ups_rem.fetch(ac_br)
-        repo.git.reset("--hard", "FETCH_HEAD")
-        heroku_git_url = heroku_app.git_url.replace(
-            "https://", "https://api:" + HEROKU_API_KEY + "@"
+        repo.__del__()
+        return
+    for app in heroku_applications:
+        if app.name == HEROKU_APP_NAME:
+            heroku_app = app
+            break
+    if heroku_app is None:
+        await event.edit(
+            f"{txt}\n" "`Invalid Heroku credentials for deploying userbot dyno.`"
         )
-        if "heroku" in repo.remotes:
-            remote = repo.remote("heroku")
-            remote.set_url(heroku_git_url)
-        else:
-            remote = repo.create_remote("heroku", heroku_git_url)
-        try:
-            remote.push(refspec="HEAD:refs/heads/master", force=True)
-        except Exception as error:
-            await event.edit(f"{txt}\n`Here is the error log:\n{error}`")
-            return repo.__del__()
-        build = heroku_app.builds(order_by="created_at", sort="desc")[0]
-        if build.status == "failed":
-            await edit_delete(event,
-                "`Build failed!\n" "Cancelled or there were some errors...`"
-            )
-            return
-        await event.edit("`Deploy was failed better to do manual deploy.`")
-
+        return repo.__del__()
+    sandy = await event.edit(
+        "`Userbot dyno build in progress, please wait until the process finishes it usually takes 4 to 5 minutes .`"
+    )
+    try:
+        ulist = get_collectionlist_items()
+        for i in ulist:
+            if i == "restart_update":
+                del_keyword_collectionlist("restart_update")
+    except Exception as e:
+        LOGS.error(e)
+    try:
+        add_to_collectionlist("restart_update", [sandy.chat_id, sandy.id])
+    except Exception as e:
+        LOGS.error(e)
+    ups_rem.fetch(ac_br)
+    repo.git.reset("--hard", "FETCH_HEAD")
+    heroku_git_url = heroku_app.git_url.replace(
+        "https://", "https://api:" + HEROKU_API_KEY + "@"
+    )
+    if "heroku" in repo.remotes:
+        remote = repo.remote("heroku")
+        remote.set_url(heroku_git_url)
+    else:
+        remote = repo.create_remote("heroku", heroku_git_url)
+    try:
+        remote.push(refspec="HEAD:refs/heads/master", force=True)
+    except Exception as error:
+        await event.edit(f"{txt}\n`Here is the error log:\n{error}`")
+        return repo.__del__()
+    build = heroku_app.builds(order_by="created_at", sort="desc")[0]
+    if build.status == "failed":
+        await edit_delete(
+            event, "`Build failed!\n" "Cancelled or there were some errors...`"
+        )
+        return
+    await event.edit("`Deploy was failed better to do manual deploy.`")
 
 
 @catub.cat_cmd(
