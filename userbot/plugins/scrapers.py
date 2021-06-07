6baeea020d20b0a4222a5e2adc22de78d0e1d4be
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-08 02:29:20 +0530

    Update scrapers.py

--- userbot/plugins/scrapers.py
+++ userbot/plugins/scrapers.py
@@ -1,11 +1,11 @@
 import bs4
+import os
 import requests
 from pySmartDL import SmartDL
 from wikipedia import summary
 from wikipedia.exceptions import DisambiguationError, PageError
 
 from userbot import catub
-
 from ..core.managers import edit_or_reply
 from ..helpers.utils import reply_id
 from . import BOTLOG, BOTLOG_CHATID
