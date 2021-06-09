6ff1a5f3e01a48a39e36c9f4281ec2515093d8c3
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-06-09 19:57:21 +0000

    pylint: auto fixes

--- userbot/plugins/create.py
+++ userbot/plugins/create.py
@@ -3,9 +3,9 @@ from telethon.tl import functions
 from telethon.tl.functions.channels import EditAdminRequest
 from telethon.tl.types import ChatAdminRights
 
-from ..Config import Config
-from ..core.managers import edit_or_reply,edit_delete
 from .. import catub
+from ..Config import Config
+from ..core.managers import edit_delete, edit_or_reply
 
 plugin_category = "tools"
 
@@ -33,7 +33,6 @@ async def _(event):
         descript = "This is a Test Channel created using catuserbot"
     else:
         descript = "This is a Test Group created using catuserbot"
-    flag = False
     if type_of_group == "g":
         try:
             result = await event.client(
@@ -50,12 +49,12 @@ async def _(event):
                     peer=created_chat_id,
                 )
             )
-            await edit_or_reply(event,
-                f"Group `{group_name}` created successfully. Join {result.link}"
+            await edit_or_reply(
+                event, f"Group `{group_name}` created successfully. Join {result.link}"
             )
         except Exception as e:
-            await edit_delete(event,f"**Error:**\n{str(e)}")
-    elif type_of_group =="c":
+            await edit_delete(event, f"**Error:**\n{str(e)}")
+    elif type_of_group == "c":
         try:
             r = await event.client(
                 functions.channels.CreateChannelRequest(
@@ -71,62 +70,67 @@ async def _(event):
                     peer=created_chat_id,
                 )
             )
-            await edit_or_reply(event,
-                f"Channel `{group_name}` created successfully. Join {result.link}"
+            await edit_or_reply(
+                event,
+                f"Channel `{group_name}` created successfully. Join {result.link}",
             )
         except Exception as e:
-            await edit_delete(event,f"**Error:**\n{str(e)}")
-    elif type_of_group =="b":
-        answer = await create_supergroup(group_name,event.client,Config.TG_BOT_USERNAME)
+            await edit_delete(event, f"**Error:**\n{str(e)}")
+    elif type_of_group == "b":
+        answer = await create_supergroup(
+            group_name, event.client, Config.TG_BOT_USERNAME
+        )
         if answer[0] != "error":
-            await edit_or_reply(event,
-                f"Mega group `{group_name}` created successfully. Join {answer[0].link}"
+            await edit_or_reply(
+                event,
+                f"Mega group `{group_name}` created successfully. Join {answer[0].link}",
             )
             print(answer[1])
         else:
-            await edit_delete(event,f"**Error:**\n{str(answer[1])}")
+            await edit_delete(event, f"**Error:**\n{str(answer[1])}")
     else:
-        await edit_delete(event,"Read `.help create` to know how to use me")
+        await edit_delete(event, "Read `.help create` to know how to use me")
 
-async def create_supergroup(groupname,client,botusername):
+
+async def create_supergroup(groupname, client, botusername):
     try:
-            r = await client(
-                functions.channels.CreateChannelRequest(
-                    title=group_name,
-                    about=descript,
-                    megagroup=False,
-                )
+        r = await client(
+            functions.channels.CreateChannelRequest(
+                title=group_name,
+                about=descript,
+                megagroup=False,
             )
-            created_chat_id = r.chats[0].id
-            result = await client(
-                functions.messages.ExportChatInviteRequest(
-                    peer=created_chat_id,
-                )
+        )
+        created_chat_id = r.chats[0].id
+        result = await client(
+            functions.messages.ExportChatInviteRequest(
+                peer=created_chat_id,
             )
-            new_rights = ChatAdminRights(
-                add_admins=False,
-                invite_users=True,
-                change_info=True,
-                ban_users=True,
-                delete_messages=True,
-                pin_messages=True,
-                manage_call=True,
+        )
+        new_rights = ChatAdminRights(
+            add_admins=False,
+            invite_users=True,
+            change_info=True,
+            ban_users=True,
+            delete_messages=True,
+            pin_messages=True,
+            manage_call=True,
+        )
+        await client(
+            functions.channels.InviteToChannelRequest(
+                channel=created_chat_id,
+                users=[botusername],
             )
-            await client(
-                    functions.channels.InviteToChannelRequest(
-                        channel=created_chat_id,
-                        users=[botusername],
-                    )
-                )
-            print(created_chat_id)
-            try:
-                rank = "admin"
-                p = await event.client.get_entity(botusername)
-                result = await event.client(
-                    EditAdminRequest(created_chat_id, p.id, new_rights, rank)
-                )
-            except BadRequestError:
-                pass
-            return result, created_chat_id
+        )
+        print(created_chat_id)
+        try:
+            rank = "admin"
+            p = await event.client.get_entity(botusername)
+            result = await event.client(
+                EditAdminRequest(created_chat_id, p.id, new_rights, rank)
+            )
+        except BadRequestError:
+            pass
+        return result, created_chat_id
     except Exception as e:
-            return "error",str(e)
+        return "error", str(e)
