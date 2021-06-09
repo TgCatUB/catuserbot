595db29287c3f4a847d07065bdf0021f80ec14bb
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-09 16:34:06 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -122,12 +122,11 @@ You have {warns}/{totalwarns} warns until you get blocked by the CatUserbot.
 
 Choose an option from below to specify the reason of your message and wait for me to check it. __⬇️"""
         else:
-            f"""__Hi__ {mention}__, I haven't approved you yet to personal message me.
+            USER_BOT_NO_WARN = f"""__Hi__ {mention}__, I haven't approved you yet to personal message me.
 
-            You have {warns}/{totalwarns} warns until you get blocked by the CatUserbot.
+You have {warns}/{totalwarns} warns until you get blocked by the CatUserbot.
 
-            Don't spam my inbox. say reason and wait until my response.__"""
-
+Don't spam my inbox. say reason and wait until my response.__"""
     addgvar("pmpermit_text", USER_BOT_NO_WARN)
     PM_WARNS[str(chat.id)] += 1
     try:
