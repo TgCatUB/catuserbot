d01d678f067cd1a34bdfdb5b2f0d08f33f24a6d2
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-06-11 09:58:22 +0000

    pylint: auto fixes

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -1,5 +1,6 @@
 from telethon.tl import functions
 
+
 async def create_supergroup(group_name, client, botusername, descript):
     try:
         result = await client(
@@ -24,5 +25,5 @@ async def create_supergroup(group_name, client, botusername, descript):
     except Exception as e:
         return "error", str(e)
     if not str(created_chat_id).startswith("-100"):
-        created_chat_id = int("-100"+ str(created_chat_id))
+        created_chat_id = int("-100" + str(created_chat_id))
     return result, created_chat_id
