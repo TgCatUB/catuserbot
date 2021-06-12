6f321cb143936202267ab9acb6f50e7a94858b5d
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-12 14:22:15 +0530

    Update tools.py

--- userbot/plugins/tools.py
+++ userbot/plugins/tools.py
@@ -147,7 +147,7 @@ async def parseqr(qr_e):
     )
     # parse the Official ZXing webpage to decode the QRCode
     command_to_exec = (
-        f"curl -X POST -F f=@{downloaded_file_name} https://zxing.org/w/decode"
+        f"curl -s -F f=@{downloaded_file_name} https://zxing.org/w/decode"
     )
     t_response, e_response = (await _catutils.runcmd(command_to_exec))[:2]
     if not t_response:
