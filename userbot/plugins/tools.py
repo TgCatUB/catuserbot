2d7b7736256b2bce72d126dedd5c345b925d85df
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-12 14:36:25 +0530

    Update tools.py

--- userbot/plugins/tools.py
+++ userbot/plugins/tools.py
@@ -147,7 +147,7 @@ async def parseqr(event):
     )
     # parse the Official ZXing webpage to decode the QRCode
     command_to_exec = f"curl -s -F f=@{downloaded_file_name} https://zxing.org/w/decode"
-    t_response, e_response = (await _catutils.runcmd(command_to_exec))[:2]e
+    t_response, e_response = (await _catutils.runcmd(command_to_exec))[:2]
     soup = BeautifulSoup(t_response, "html.parser")
     try:
         qr_contents = soup.find_all("pre")[0].text
