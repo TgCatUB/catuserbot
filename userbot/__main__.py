24be4133b5732f2725b4522319b10b06f49f23d7
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-13 13:25:23 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -53,53 +53,59 @@ async def testing_bot():
 
 
 def verifyLoggerGroup():
-    if BOTLOG:
-        try:
-            entity = catub.loop.run_until_complete(catub.get_entity(BOTLOG_CHATID))
-            if not isinstance(entity, types.User) and not entity.creator:
-                if entity.default_banned_rights.send_messages:
-                    LOGS.info(
-                        "Permissions missing to send messages for the specified Logger group."
-                    )
-                if entity.default_banned_rights.invite_users:
-                    LOGS.info(
-                        "Permissions missing to addusers for the specified Logger group."
-                    )
-        except ValueError:
-            LOGS.error("Logger group ID cannot be found. " "Make sure it's correct.")
-        except TypeError:
-            LOGS.error("Logger group ID is unsupported. " "Make sure it's correct.")
-        except Exception as e:
-            LOGS.error(
-                "An Exception occured upon trying to verify the logger group.\n"
-                + str(e)
+        if BOTLOG:
+            try:
+                entity = catub.loop.run_until_complete(catub.get_entity(BOTLOG_CHATID))
+                if not isinstance(entity, types.User) and not entity.creator:
+                    if entity.default_banned_rights.send_messages:
+                        LOGS.info(
+                            "Permissions missing to send messages for the specified PRIVATE_GROUP_BOT_API_ID."
+                        )
+                    if entity.default_banned_rights.invite_users:
+                        LOGS.info(
+                            "Permissions missing to addusers for the specified PRIVATE_GROUP_BOT_API_ID."
+                        )
+            except ValueError:
+                LOGS.error("PRIVATE_GROUP_BOT_API_ID cannot be found. Make sure it's correct.")
+            except TypeError:
+                LOGS.error("PRIVATE_GROUP_BOT_API_ID is unsupported. Make sure it's correct.")
+            except Exception as e:
+                LOGS.error(
+                    "An Exception occured upon trying to verify the PRIVATE_GROUP_BOT_API_ID.\n"
+                    + str(e)
+                )
+        else:
+            LOGS.info(
+                "You haven't set the PRIVATE_GROUP_BOT_API_ID in vars please set it for proper functioning of userbot."
             )
-        try:
-            entity = catub.loop.run_until_complete(
-                catub.get_entity(Config.PM_LOGGER_GROUP_ID)
+        if PM_LOGGER_GROUP_ID:
+            try:
+                entity = catub.loop.run_until_complete(
+                    catub.get_entity(PM_LOGGER_GROUP_ID)
+                )
+                if not isinstance(entity, types.User) and not entity.creator:
+                    if entity.default_banned_rights.send_messages:
+                        LOGS.info(
+                            "Permissions missing to send messages for the specified PM_LOGGER_GROUP_ID."
+                        )
+                    if entity.default_banned_rights.invite_users:
+                        LOGS.info(
+                            "Permissions missing to addusers for the specified PM_LOGGER_GROUP_ID."
+                        )
+            except ValueError:
+                LOGS.error("PM_LOGGER_GROUP_ID cannot be found. Make sure it's correct.")
+            except TypeError:
+                LOGS.error("PM_LOGGER_GROUP_ID is unsupported. Make sure it's correct.")
+            except Exception as e:
+                LOGS.error(
+                    "An Exception occured upon trying to verify the PM_LOGGER_GROUP_ID.\n"
+                    + str(e)
+                )
+        else:
+            LOGS.info(
+                "You haven't set the PM_LOGGER_GROUP_ID in vars please set it for proper functioning of userbot."
             )
-            if not isinstance(entity, types.User) and not entity.creator:
-                if entity.default_banned_rights.send_messages:
-                    LOGS.info(
-                        "Permissions missing to send messages for the specified Pm logger group."
-                    )
-                if entity.default_banned_rights.invite_users:
-                    LOGS.info(
-                        "Permissions missing to addusers for the specified Pm Logger group."
-                    )
-        except ValueError:
-            LOGS.error("Pm Logger group ID cannot be found. " "Make sure it's correct.")
-        except TypeError:
-            LOGS.error("Pm Logger group ID is unsupported. " "Make sure it's correct.")
-        except Exception as e:
-            LOGS.error(
-                "An Exception occured upon trying to verify the Pm logger group.\n"
-                + str(e)
-            )
-    else:
-        LOGS.info(
-            "You haven't set the PRIVATE_GROUP_BOT_API_ID in vars please set it for proper functioning of userbot."
-        )
+
 
 
 def add_bot_to_logger_group():
