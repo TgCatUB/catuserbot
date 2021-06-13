acc48e580b156dc637aa48bce015ee3723f5981e
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-14 03:14:39 +0530

    Update chatbot.py

--- userbot/helpers/chatbot.py
+++ userbot/helpers/chatbot.py
@@ -1,5 +1,9 @@
-import randomstuff
-
+try:
+    import randomstuff
+except ModuleNotFoundError:
+    from .utils.extdl import install_pip
+    install_pip(randomstuff.py)
+    
 from ..Config import Config
 
 rs_client = randomstuff.AsyncClient(api_key=Config.RANDOM_STUFF_API_KEY, version="4")
