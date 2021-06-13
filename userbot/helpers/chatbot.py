803c7e431e52be8fc03da543bdb7801b76201a35
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-06-13 21:45:18 +0000

    pylint: auto fixes

--- userbot/helpers/chatbot.py
+++ userbot/helpers/chatbot.py
@@ -2,8 +2,9 @@ try:
     import randomstuff
 except ModuleNotFoundError:
     from .utils.extdl import install_pip
+
     install_pip(randomstuff.py)
-    
+
 from ..Config import Config
 
 rs_client = randomstuff.AsyncClient(api_key=Config.RANDOM_STUFF_API_KEY, version="4")
