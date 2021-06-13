e8b4c4c56da9423c4dff680eeca40c0ce623266e
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-13 13:10:43 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -18,10 +18,10 @@ bot = catub
 StartTime = time.time()
 catversion = "3.0.0"
 
-if Config.UPSTREAM_REPO == "goodcat":
-    UPSTREAM_REPO_URL = "https://github.com/sandy1709/catuserbot"
-elif Config.UPSTREAM_REPO == "badcat":
+if Config.UPSTREAM_REPO == "badcat":
     UPSTREAM_REPO_URL = "https://github.com/Jisan09/catuserbot"
+elif Config.UPSTREAM_REPO == "goodcat":
+    UPSTREAM_REPO_URL = "https://github.com/sandy1709/catuserbot"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
@@ -30,10 +30,17 @@ if Config.PRIVATE_GROUP_BOT_API_ID == 0:
     BOTLOG_CHATID = "me"
 else:
     if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
-        BOTLOG_CHATID = int("-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
+        BOTLOG_CHATID = int("-"+str(Config.PRIVATE_GROUP_BOT_API_ID))
     else:
         BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
     BOTLOG = True
+
+if Config.PM_LOGGER_GROUP_ID == 0:
+    PM_LOGGER_GROUP_ID = None
+elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
+    PM_LOGGER_GROUP_ID = int("-"+str(Config.PM_LOGGER_GROUP_ID))
+else:
+    PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
 
 try:
     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
