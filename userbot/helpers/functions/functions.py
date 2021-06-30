e2e26c0e4877344824bbda974d7a97f2e1d2b1d0
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-30 17:04:44 +0530

    Update functions.py

--- userbot/helpers/functions/functions.py
+++ userbot/helpers/functions/functions.py
@@ -3,9 +3,14 @@ import zipfile
 from random import choice
 from textwrap import wrap
 from uuid import uuid4
+from ..utils.extdl import install_pip
 
 import requests
-from imdb import IMDb
+try:
+    from imdb import IMDb
+except ModuleNotFoundError:
+    install_pip("IMDbPY")
+    from imdb import IMDb
 from PIL import Image, ImageColor, ImageDraw, ImageFont
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
