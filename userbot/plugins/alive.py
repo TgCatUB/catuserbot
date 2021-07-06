d656fd9b8dea4a759cac136234f12d2029842aef
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-07-06 11:13:03 +0000

    pylint: auto fixes

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -40,7 +40,9 @@ async def amireallyalive(event):
     uptime = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
-    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**✮ MY BOT IS RUNNING SUCCESSFULLY ✮**"
+    CUSTOM_ALIVE_TEXT = (
+        gvarstatus("ALIVE_TEXT") or "**✮ MY BOT IS RUNNING SUCCESSFULLY ✮**"
+    )
     CAT_IMG = gvarstatus("ALIVE_PIC")
     if CAT_IMG:
         CAT = [x for x in CAT_IMG.split()]
