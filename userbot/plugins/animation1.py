b483bc80c78a608ad255c7c3c8483344f19e91ad
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-07-09 19:01:07 +0530

    Update animation1.py

--- userbot/plugins/animation1.py
+++ userbot/plugins/animation1.py
@@ -161,7 +161,7 @@ async def _(event):
         "What The",
         "What The F",
         "What The F Brah",
-        "What The F Brah\nhttps://telegra.ph/file/f3b760e4a99340d331f9b.jpg",
+        "[What The F Brah](https://telegra.ph/file/f3b760e4a99340d331f9b.jpg)",
     ]
     for i in animation_ttl:
         await asyncio.sleep(animation_interval)
