2e936c1ebc8721072611a099ab5c7ccd8d4d8371
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-07-09 17:45:01 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -676,10 +676,11 @@ async def pic2packcmd(event):
             )
             await event.client.send_read_acknowledge(conv.chat_id)
             for packname in ending.raw_text.split():
+                stick_pack_name = packname 
                 if packname.startswith("https://t.me/"):
                     break
             await catevent.edit(
-                f"__successfully created the pack for the replied media : __[{args}]({packname})"
+                f"__successfully created the pack for the replied media : __[{args}]({stick_pack_name})"
             )
 
         except YouBlockedUserError:
