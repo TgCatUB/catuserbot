03ac4465a5709ef28fad1ac8a75053bd12894b87
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-07-09 14:28:51 +0530

    Update tools.py

--- userbot/helpers/utils/tools.py
+++ userbot/helpers/utils/tools.py
@@ -58,7 +58,7 @@ async def media_to_pic(event, reply, noedits=False):  # sourcery no-metrics
             clip = VideoFileClip(media)
             try:
                 clip = clip.save_frame(catfile, 0.1)
-            except:
+            except Exception:
                 clip = clip.save_frame(catfile, 0)
     elif mediatype == "Document":
         mimetype = reply.document.mime_type
