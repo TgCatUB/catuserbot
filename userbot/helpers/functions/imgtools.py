16c75ac03d1c2daf428eee557ade23f443e3f4c8
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-07-09 14:17:18 +0530

    Update imgtools.py

--- userbot/helpers/functions/imgtools.py
+++ userbot/helpers/functions/imgtools.py
@@ -118,7 +118,6 @@ async def dotify(image, pix, mode):
         im_ = temp
     im = im_.convert("L")
     im_ = im if mode else im_
-    [_.thumbnail((pix, pix)) for _ in [im, im_]]
     w, h = im.size
     img = Image.new(im_.mode, (w * count + (count // 2), h * count + (count // 2)), 0)
     ImageDraw.Draw(img)
