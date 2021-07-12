b7abe31bdaf360ec74c93b71aa2dbe730d27b0a2
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-07-12 16:56:48 +0530

    Update iytdl.py

--- userbot/assistant/iytdl.py
+++ userbot/assistant/iytdl.py
@@ -210,7 +210,7 @@ async def ytdl_callback(c_q: CallbackQuery):
     with open(PATH) as f:
         view_data = ujson.load(f)
     search_data = view_data.get(data_key)
-    total = len(search_data)
+    total = len(search_data) if search_data is not None else 0
     if choosen_btn == "back":
         index = int(page) - 1
         del_back = index == 1
