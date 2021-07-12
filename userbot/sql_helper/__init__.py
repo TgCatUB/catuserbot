5cbd244151cfca8059cb37914e47c0d4548eedce
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-07-12 11:22:30 +0000

    pylint: auto fixes

--- userbot/sql_helper/__init__.py
+++ userbot/sql_helper/__init__.py
@@ -13,7 +13,7 @@ LOGS = logging.getLogger(__name__)
 
 def start() -> scoped_session:
     if "postgres://" in Config.DB_URI:
-        database_url = Config.DB_URI.replace("postgres:","postgresql:")
+        database_url = Config.DB_URI.replace("postgres:", "postgresql:")
     engine = create_engine(database_url)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
