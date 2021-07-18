f90344f42d33be9db112ff5d1813c1510941b8d1
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-07-18 16:07:29 +0530

    Update figlet.py

--- userbot/plugins/figlet.py
+++ userbot/plugins/figlet.py
@@ -55,7 +55,7 @@ async def figlet(event):
     input_str = event.pattern_match.group(1)
     if ";" in input_str:
         cmd, text = input_str.split(";", maxsplit=1)
-    elif input_str is not None:
+    elif input_str:
         cmd = None
         text = input_str
     else:
