5b5b11708f28bae0ad1132bfb0c01da73158a6f2
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-07-21 17:39:43 +0530

    Update events.py

--- userbot/core/events.py
+++ userbot/core/events.py
@@ -136,7 +136,7 @@ async def send_message(
     reply_to: "typing.Union[int, types.Message]" = None,
     parse_mode: typing.Optional[str] = (),
     formatting_entities: typing.Optional[typing.List[types.TypeMessageEntity]] = None,
-    link_preview: bool = False,
+    link_preview: bool = True,
     file: "typing.Union[hints.FileLike, typing.Sequence[hints.FileLike]]" = None,
     force_document: bool = False,
     clear_draft: bool = False,
