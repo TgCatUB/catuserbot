f582e1c478177f3500e70a9c6e72d452be205472
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-07-22 15:22:29 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -11,14 +11,11 @@ class Development(Config):
     DB_URI = "Your value"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
     STRING_SESSION = "Your value"
-    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
+    # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "Your value"
-    TG_BOT_USERNAME = "Your value"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID = -100
     # command handler
     COMMAND_HAND_LER = "."
-    # sudo enter the id of sudo users userid's in that array
-    SUDO_USERS = []
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
