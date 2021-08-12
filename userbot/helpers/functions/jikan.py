dd41e1bb4fe6b4c3186ff3a0e90e060985736927
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-08-13 00:07:19 +0530

    Update jikan.py

--- userbot/helpers/functions/jikan.py
+++ userbot/helpers/functions/jikan.py
@@ -16,7 +16,9 @@ from ..progress import readable_time
 from ..tools import post_to_telegraph
 
 jikan = Jikan()
+
 anilisturl = "https://graphql.anilist.co"
+animnefillerurl =  "https://www.animefillerlist.com/shows/"
 # Anime Helper
 
 weekdays = {
@@ -530,3 +532,69 @@ def is_gif(file):
     if not is_video(file):
         return False
     return DocumentAttributeAnimated() in getattr(file, "document", file).attributes
+
+async def search_in_animefiller(query):
+    "To search anime name and get its id"
+    html = requests.get(animnefillerurl).text
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    div = soup.findAll("div", attrs={"class": "Group"})
+    index = {}
+    for i in div:
+        li = i.findAll("li")
+        for jk in li:
+            yum = jk.a["href"].split("/")[-1]
+            cum = jk.text
+            index[cum] = yum
+    keys = list(index.keys())
+    return {
+        keys[i]: index[keys[i]]
+        for i in range(len(keys))
+        if query.lower() in keys[i].lower()
+    }
+
+
+def get_filler_episodes(filler_id):  # sourcery no-metrics
+    "to get eppisode numbers"
+    html = requests.get(animnefillerurl+ filler_id).text
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    div = soup.find("div", attrs={"id": "Condensed"})
+    complete_anime = div.find_all("span", attrs={"class": "Episodes"})
+    if len(complete_anime) == 1:
+        total_episodes = complete_anime[0].findAll("a")
+        mixed_episodes = None
+        filler_episodes = None
+        anime_canon_episodes = None
+        total_ep = "".join(f"{total_no.text}, " for total_no in total_episodes)
+    elif len(complete_anime) == 2:
+        total_episodes = complete_anime[0].findAll("a")
+        filler_ep = complete_anime[1].findAll("a")
+        mixed_episodes = None
+        anime_canon_episodes = None
+        total_ep = "".join(f"{total_no.text}, " for total_no in total_episodes)
+        filler_episodes = "".join(f"{filler_no.text}, " for filler_no in filler_ep)
+    elif len(complete_anime) == 3:
+        total_episodes = complete_anime[0].findAll("a")
+        mixed_ep = complete_anime[1].findAll("a")
+        filler_ep = complete_anime[2].findAll("a")
+        anime_canon_episodes = None
+        total_ep = "".join(f"{total_no.text}, " for total_no in total_episodes)
+        filler_episodes = "".join(f"{filler_no.text}, " for filler_no in filler_ep)
+        mixed_episodes = "".join(f"{miixed_no.text}, " for miixed_no in mixed_ep)
+    elif len(complete_anime) == 4:
+        total_episodes = complete_anime[0].findAll("a")
+        mixed_ep = complete_anime[1].findAll("a")
+        filler_ep = complete_anime[2].findAll("a")
+        animecanon_ep = complete_anime[3].findAll("a")
+        total_ep = "".join(f"{total_no.text}, " for total_no in total_episodes)
+        filler_episodes = "".join(f"{filler_no.text}, " for filler_no in filler_ep)
+        mixed_episodes = "".join(f"{miixed_no.text}, " for miixed_no in mixed_ep)
+        anime_canon_episodes = "".join(
+            f"{animecanon_no.text}, " for animecanon_no in animecanon_ep
+        )
+    return {
+        "filler_id": filler_id,
+        "total_ep": total_ep,
+        "mixed_ep": mixed_episodes,
+        "filler_episodes": filler_episodes,
+        "anime_canon_episodes": anime_canon_episodes,
+    }
