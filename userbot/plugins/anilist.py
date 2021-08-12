3ba16df0fd729436d9769b82000b8451b24039c8
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-08-12 22:12:57 +0530

    Update anilist.py

--- userbot/plugins/anilist.py
+++ userbot/plugins/anilist.py
@@ -475,8 +475,10 @@ async def aschedule_fetch(event):
     input_str = event.pattern_match.group(1) or datetime.now().weekday()
     if input_str in weekdays:
         input_str = weekdays[input_str]
-    print(input_str)
-    print(type(input_str))
+    try:
+        input_str = int(input_str)
+    except ValueError:
+        return await edit_delete(event, "`You have given and invalid weekday`", 7)
     if input_str not in [0, 1, 2, 3, 4, 5, 6]:
         return await edit_delete(event, "`You have given and invalid weekday`", 7)
     result = await get_anime_schedule(input_str)
