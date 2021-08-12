aab9b3aba7bc350ced830622237a925248d38b02
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-08-13 00:42:10 +0530

    Update jikan.py

--- userbot/helpers/functions/jikan.py
+++ userbot/helpers/functions/jikan.py
@@ -307,7 +307,7 @@ def getKitsu(mal):
     return requests.get(link).json()["data"]["id"]
 
 
-def getBannerLink(mal, kitsu_search=True):
+def getBannerLink(mal, kitsu_search=True,anilistid=0):
     # try getting kitsu backdrop
     if kitsu_search:
         kitsu = getKitsu(mal)
@@ -315,16 +315,22 @@ def getBannerLink(mal, kitsu_search=True):
         response = requests.get(image)
         if response.status_code == 200:
             return image
+    if anilistid !=0:
+        return f"https://img.anili.st/media/{anilistid}"
     # try getting anilist banner
-    print(mal)
-    query = """query($id: Int, $type: MediaType){Media(idMal: $id, type: $type){
-id
-siteUrl}}"""
-    data = {"query": query, "variables": {"idMal": int(mal), "type": "ANIME"}}
-    image = requests.post(anilisturl, json=data).json()["data"]["Media"]["id"]
-    print(image)
+    query = """
+    query ($idMal: Int){
+        Media(idMal: $idMal){
+            bannerImage
+        }
+    }
+    """
+    data = {"query": query, "variables": {"idMal": int(mal)}}
+    image = requests.post("https://graphql.anilist.co", json=data).json()["data"][
+        "Media"
+    ]["bannerImage"]
     if image:
-        return f"https://img.anili.st/media/{image}"
+        return image
     return getPosterLink(mal)
 
 
@@ -337,7 +343,6 @@ async def get_anime_manga(mal_id, search_type, _user_id):  # sourcery no-metrics
             TRAILER = f"<a href='{trailer}'>🎬 Trailer</a>"
         else:
             TRAILER = "🎬 <i>No Trailer Available</i>"
-        image = getBannerLink(mal_id, False)
         studio_string = ", ".join(
             studio_info["name"] for studio_info in result["studios"]
         )
@@ -396,11 +401,12 @@ async def get_anime_manga(mal_id, search_type, _user_id):  # sourcery no-metrics
         )
         coverImg = anime_data.get("coverImage")["extraLarge"]
         bannerImg = anime_data.get("bannerImage")
-        anime_data.get("siteUrl")
+        anilist = anime_data.get("siteUrl")
         title_img = coverImg or bannerImg
         romaji = anime_data["title"]["romaji"]
         native = anime_data["title"]["native"]
         english = anime_data["title"]["english"]
+        image = getBannerLink(mal_id, False,anime_data.get("id"))
         # Telegraph Post mejik
         html_pc = ""
         html_pc += f"<h1>{native}</h1>"
@@ -416,7 +422,7 @@ async def get_anime_manga(mal_id, search_type, _user_id):  # sourcery no-metrics
         html_pc += (
             f"<a href='https://myanimelist.net/anime/{anime_malid}'>View on MAL</a>"
         )
-        html_pc += f"<a href='{result['url']}'> View on anilist.co</a>"
+        html_pc += f"<a href='{anilisturl}'> View on anilist.co</a>"
         html_pc += f"<img src='{bannerImg}'/>"
         title_h = english or romaji
     if search_type == "anime_anime":
