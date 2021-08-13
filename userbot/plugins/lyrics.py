6f080e0204e8b8078bf6cc528be7bd6c09b506a2
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-08-13 14:29:32 +0000

    pylint: auto fixes

--- userbot/plugins/lyrics.py
+++ userbot/plugins/lyrics.py
@@ -1,9 +1,9 @@
 # credits to @mrconfused (@sandy1709)
-import os
 
 import lyricsgenius
 
 from userbot import catub
+
 from ..Config import Config
 from ..core.managers import edit_or_reply
 
@@ -11,19 +11,22 @@ plugin_category = "extra"
 
 GENIUS = Config.GENIUS_API_TOKEN
 
+
 @catub.cat_cmd(
     pattern="lyrics(?:\s|$)([\s\S]*)",
     command=("lyrics", plugin_category),
     info={
         "header": "Song lyrics searcher using genius api.",
-        "flags":{"-l":"to get list of search lists.",
-        "-g":"To get paticular song lyrics."},
+        "flags": {
+            "-l": "to get list of search lists.",
+            "-g": "To get paticular song lyrics.",
+        },
         "note": "For functioning of this command set the GENIUS_API_TOKEN in heroku. Get value from  https://genius.com/developers.",
         "usage": "{tr}lyrics <artist name> - <song name>",
         "examples": "{tr}lyrics Armaan Malik - butta bomma",
     },
 )
-async def lyrics(event):    # sourcery no-metrics
+async def lyrics(event):  # sourcery no-metrics
     "To fetch song lyrics"
     if GENIUS is None:
         return await edit_or_reply(
@@ -39,8 +42,11 @@ async def lyrics(event):    # sourcery no-metrics
         match = match.replace("-n" + songno, "")
     except IndexError:
         songno = 1
-    if songno <1 or songno >10:
-        return await edit_or_reply(event,"`song number must be in between 1 to 10 use -l flag to query results`")
+    if songno < 1 or songno > 10:
+        return await edit_or_reply(
+            event,
+            "`song number must be in between 1 to 10 use -l flag to query results`",
+        )
     match = match.replace("-l", "")
     listview = bool(listview)
     query = match.strip()
@@ -62,16 +68,21 @@ async def lyrics(event):    # sourcery no-metrics
     else:
         response = genius.search_songs(query)
         msg = f"**The songs found for the given query:** `{query}`\n\n"
-        if len(response["hits"])==0:
-            return await edit_or_reply(event,f"**I can't find lyrics for the given query: **`{query}`")
+        if len(response["hits"]) == 0:
+            return await edit_or_reply(
+                event, f"**I can't find lyrics for the given query: **`{query}`"
+            )
         for i, an in enumerate(reponse["hits"], start=1):
             msg += f"{i}. {an['result']['title']}\n"
         if listview:
             result = msg
         else:
             result = f"**The song found for the given query:** `{query}`\n\n"
-            if songno-1 >len(response["hits"]):
-                return await edit_or_reply(event,f"**Invalid song selection for the query select proper number**\n{msg}")
+            if songno - 1 > len(response["hits"]):
+                return await edit_or_reply(
+                    event,
+                    f"**Invalid song selection for the query select proper number**\n{msg}",
+                )
             songtitle = response["hits"][0]["result"]["title"]
             result += f"`{genius.search_song(songtitle).lyrics}`"
     await edit_or_reply(catevent, result)
