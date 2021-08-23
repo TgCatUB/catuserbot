0d4977c3eb026799584801a1ecc78dde50d0e9dd
Pianovillain <86278468+Pianovillain@users.noreply.github.com>
2021-08-23 13:15:59 +0530

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -106,13 +106,13 @@ async def bot_start(event):
             start_msg = f"Hey! 👤{mention},\
                         \nI am {my_mention}'s assistant bot.\
                         \nYou can contact to my master from here.\
-                        \n\nPowered by [Catuserbot](https://t.me/catuserbot)"
+                        \n\nPowered by [Team Pluginclouds](https://t.me/pluginclouds)"
         buttons = [
             (
-                Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
+                Button.url("Channel", "https://t.me/plugincloudsarchive"),
                 Button.url(
-                    "Deploy",
-                    "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack",
+                    "Group",
+                    "https://t.me/joinchat/QTngrvQcjXkyNmM9",
                 ),
             )
         ]
