58ee80a4588ba00ce703e20f156e0739d96d3414
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2022-02-11 19:25:09 +0530

    Update powertools.py

--- userbot/plugins/powertools.py
+++ userbot/plugins/powertools.py
@@ -35,19 +35,19 @@ async def _(event):
         event,
         "Restarted. `.ping` me or `.help` to check if I am online, actually it takes 1-2 min for restarting",
     )
+    # try:
+    #     ulist = get_collectionlist_items()
+    #     for i in ulist:
+    #         if i == "restart_update":
+    #             del_keyword_collectionlist("restart_update")
+    # except Exception as e:
+    #     LOGS.error(e)
+    # try:
+    #     add_to_collectionlist("restart_update", [sandy.chat_id, sandy.id])
+    # except Exception as e:
+    #     LOGS.error(e)
     try:
-        ulist = get_collectionlist_items()
-        for i in ulist:
-            if i == "restart_update":
-                del_keyword_collectionlist("restart_update")
-    except Exception as e:
-        LOGS.error(e)
-    try:
-        add_to_collectionlist("restart_update", [sandy.chat_id, sandy.id])
-    except Exception as e:
-        LOGS.error(e)
-    try:
-        delgvar("ipaddress")
+        # delgvar("ipaddress")
         await catub.disconnect()
     except CancelledError:
         pass
