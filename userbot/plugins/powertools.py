5152e3a3466e1f6311cc74d87366d667704bc405
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2022-02-11 22:45:56 +0530

    Update powertools.py

--- userbot/plugins/powertools.py
+++ userbot/plugins/powertools.py
@@ -3,7 +3,11 @@ from asyncio.exceptions import CancelledError
 from time import sleep
 
 from userbot import catub
-
+from ..sql_helper.global_collection import (
+    add_to_collectionlist,
+    del_keyword_collectionlist,
+    get_collectionlist_items,
+)
 from ..core.logger import logging
 from ..core.managers import edit_or_reply
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
@@ -30,17 +34,17 @@ async def _(event):
         event,
         "Restarted. `.ping` me or `.help` to check if I am online, actually it takes 1-2 min for restarting",
     )
-    # try:
-    #     ulist = get_collectionlist_items()
-    #     for i in ulist:
-    #         if i == "restart_update":
-    #             del_keyword_collectionlist("restart_update")
-    # except Exception as e:
-    #     LOGS.error(e)
-    # try:
-    #     add_to_collectionlist("restart_update", [sandy.chat_id, sandy.id])
-    # except Exception as e:
-    #     LOGS.error(e)
+    try:
+        ulist = get_collectionlist_items()
+        for i in ulist:
+            if i == "restart_update":
+                del_keyword_collectionlist("restart_update")
+    except Exception as e:
+        LOGS.error(e)
+    try:
+        add_to_collectionlist("restart_update", [sandy.chat_id, sandy.id])
+    except Exception as e:
+        LOGS.error(e)
     try:
         # delgvar("ipaddress")
         await catub.disconnect()
