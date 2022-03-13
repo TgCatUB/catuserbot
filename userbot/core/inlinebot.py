5cd25f8eeca91ea7de3ea8d7942bd56cf36b2c82
Jisan <66010486+Jisan09@users.noreply.github.com>
2022-03-14 00:10:38 +0530

    Revert "Update inlinebot.py"
    This reverts commit 1095f9399c312939f6d23f96839f0dbfe3113f13.

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -29,7 +29,7 @@ from .logger import logging
 
 LOGS = logging.getLogger(__name__)
 
-BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?).(:same)?\>)")
+BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
 MEDIA_PATH_REGEX = re.compile(r"(:?\<\bmedia:(:?(?:.*?)+)\>)")
 CATLOGO = "https://telegra.ph/file/493268c1f5ebedc967eba.jpg"
 tr = Config.COMMAND_HAND_LER
