dfe238101d46a5c6b226ee63b2922c5822057cd3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2022-04-03 23:49:47 +0530

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -86,9 +86,9 @@ async def bot_start(event):
     my_last = user.last_name
     my_fullname = f"{my_first} {my_last}" if my_last else my_first
     my_username = f"@{user.username}" if user.username else my_mention
+    custompic = gvarstatus("BOT_START_PIC") or None
     if chat.id != Config.OWNER_ID:
         customstrmsg = gvarstatus("START_TEXT") or None
-        custompic = gvarstatus("BOT_START_PIC") or None
         if customstrmsg is not None:
             start_msg = customstrmsg.format(
                 mention=mention,
