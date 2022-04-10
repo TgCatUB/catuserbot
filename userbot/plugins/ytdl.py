64df14ff6617d5bb6ea6d63bd2dfbb7355da92c8
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2022-04-10 11:37:08 +0000

    pylint: auto fixes

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -23,8 +23,6 @@ from yt_dlp.utils import (
     XAttrMetadataError,
 )
 
-from . import catub, BOTLOG, BOTLOG_CHATID
-
 from ..core import pool
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
@@ -32,6 +30,7 @@ from ..helpers import progress, reply_id
 from ..helpers.functions import delete_conv
 from ..helpers.functions.utube import _mp3Dl, get_yt_video_id, get_ytthumb, ytsearch
 from ..helpers.utils import _format
+from . import BOTLOG, BOTLOG_CHATID, catub
 
 BASE_YT_URL = "https://www.youtube.com/watch?v="
 extractor = URLExtract()
