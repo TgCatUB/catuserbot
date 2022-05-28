43048afd06e8341cd5ae697ffc7aac461765b197
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2022-05-28 19:04:25 +0530

    Update filesummary.py

--- userbot/plugins/filesummary.py
+++ userbot/plugins/filesummary.py
@@ -21,7 +21,7 @@ TYPES = [
     "Sticker",
     "Gif",
     "Voice",
-    "Round Video",
+    "Round",
 ]
 
 
@@ -117,7 +117,7 @@ async def _(event):  # sourcery no-metrics  # sourcery skip: low-code-quality
         str(round((weird_division((endtime - starttime), totalcount)) * 1000, 2))
         + " ms"
     )
-    totalstring = f"<code><b>Total files : </b>       | {totalcount}\\\x1f                  \nTotal file size :    | {humanbytes(totalsize)}\\\x1f                  \nAvg. file size :     | {avghubytes}\\\x1f                  \n</code>"
+    totalstring = f"<code><b>Total files : </b>       | {totalcount}\nTotal file size :    | {humanbytes(totalsize)}\nAvg. file size :     | {avghubytes}\n</code>"
 
     runtimestring = f"<code>Runtime :            | {runtime}\
                     \nRuntime per file :   | {avgruntime}\
@@ -244,7 +244,7 @@ async def _(event):  # sourcery no-metrics  # sourcery skip: low-code-quality
         str(round((weird_division((endtime - starttime), totalcount)) * 1000, 2))
         + " ms"
     )
-    totalstring = f"<code><b>Total files : </b>       | {totalcount}\\\x1f                  \nTotal file size :    | {humanbytes(totalsize)}\\\x1f                  \nAvg. file size :     | {avghubytes}\\\x1f                  \n</code>"
+    totalstring = f"<code><b>Total files : </b>       | {totalcount}\nTotal file size :    | {humanbytes(totalsize)}\nAvg. file size :     | {avghubytes}\n</code>"
 
     runtimestring = f"<code>Runtime :            | {runtime}\
                     \nRuntime per file :   | {avgruntime}\
