5c1752971b73857fa86535b9b05de7e7a9d3f805
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2022-06-15 19:45:36 +0530

    Update groupdata.py

--- userbot/plugins/groupdata.py
+++ userbot/plugins/groupdata.py
@@ -264,8 +264,8 @@ async def fetch_info(chat, event):  # sourcery no-metrics
     if hasattr(chat_obj_info, "verified"):
         caption += f"✅ <b>Verified by Telegram: </b><code>{verified}</code>\n"
     if grp_emoji:
-        caption += f"🙂 <b>Enabled Reactions: </b><code>{''.join(grp_emoji[:8])}</code>\
-                   \n                             <code>{''.join(grp_emoji[8:])}</code>\n"
+        caption += f"🙂 <b>Enabled Reactions: </b>{''.join(grp_emoji[:8])}\
+                   \n                         {''.join(grp_emoji[8:])}\n"
     else:
         caption += (
             f"🙂 <b>Enabled Reactions: </b><code>Reactions are not enabled.</code>\n"
