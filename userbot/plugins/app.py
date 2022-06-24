25656400c295dc403939fd6f3a2839cd1c906ae5
Jisan <66010486+Jisan09@users.noreply.github.com>
2022-06-24 10:01:25 +0530

    rewritten app

--- userbot/plugins/app.py
+++ userbot/plugins/app.py
@@ -1,15 +1,34 @@
-"""
-Fetch App Details from Playstore.
-.app <app_name> to fetch app details.
-  © [cHAuHaN](http://t.me/amnd33p)
-"""
+# \\ Created by-@Jisan7509 -- Github.com/Jisan09 //
+#  \\   https://github.com/TgCatUB/catuserbot   //
+#   \\       Plugin for @catuserbot            //
+#    ```````````````````````````````````````````
+
+import os
+import urllib.request
 
 import bs4
 import requests
+from PIL import Image, ImageDraw, ImageFont
 
-from . import ALIVE_NAME, catub, edit_or_reply
+from ..helpers.functions.functions import ellipse_create
+from . import catub, edit_delete, edit_or_reply
 
 plugin_category = "utils"
+
+
+bg_url_1 = "https://raw.githubusercontent.com/TgCatUB/CatUserbot-Resources/master/Resources/App/app-full.jpg"
+bg_url_2 = "https://raw.githubusercontent.com/TgCatUB/CatUserbot-Resources/master/Resources/App/app-suggest.jpg"
+
+
+def text_draw(font_name, font_size, img, text, width, hight, fill="white"):
+    font = ImageFont.truetype(font_name, font_size)
+    draw = ImageDraw.Draw(img)
+    draw.text(
+        (int(width), int(hight)),
+        text=text,
+        fill=fill,
+        font=font,
+    )
 
 
 @catub.cat_cmd(
@@ -23,69 +42,75 @@ plugin_category = "utils"
 )
 async def app_search(event):
     "To search any app in playstore."
-    app_name = event.pattern_match.group(1)
-    event = await edit_or_reply(event, "`Searching!..`")
+    query = event.pattern_match.group(1)
+    if not query:
+        return await edit_delete(
+            event, "`Enter an app name to get result from PlayStore..`"
+        )
+    await edit_or_reply(event, "`Searching!...`")
     try:
-        remove_space = app_name.split(" ")
-        final_name = "+".join(remove_space)
+        final_name = query.replace(" ", "+")
         page = requests.get(
             f"https://play.google.com/store/search?q={final_name}&c=apps"
         )
-
-        str(page.status_code)
-        soup = bs4.BeautifulSoup(page.content, "lxml", from_encoding="utf-8")
-        results = soup.findAll("div", "ZmHEEd")
-        app_name = (
-            results[0].findNext("div", "Vpfmgd").findNext("div", "WsMG1c nnK0zc").text
-        )
-        app_dev = results[0].findNext("div", "Vpfmgd").findNext("div", "KoLSrc").text
-        app_dev_link = (
-            "https://play.google.com"
-            + results[0].findNext("div", "Vpfmgd").findNext("a", "mnKHRc")["href"]
-        )
-        app_rating = (
-            results[0]
-            .findNext("div", "Vpfmgd")
-            .findNext("div", "pf5lIe")
-            .find("div")["aria-label"]
-        )
+        soup = bs4.BeautifulSoup(page.content, "lxml")
+        fullapp_name = app_name = (
+            soup.find("div", "vWM94c") or soup.find("span", "DdYX5")
+        ).text
+        dev_name = (soup.find("div", "LbQbAe") or soup.find("span", "wMUdtb")).text
+        rating = (
+            soup.find("div", "TT9eCd") or soup.find("span", "w2kbF")
+        ).text.replace("star", "")
+        app_icon = (
+            soup.find("img", "T75of bzqKMd") or soup.find("img", "T75of stzEZd")
+        )["src"].split("=s")[0]
         app_link = (
             "https://play.google.com"
-            + results[0]
-            .findNext("div", "Vpfmgd")
-            .findNext("div", "vU6FJ p63iDd")
-            .a["href"]
+            + (soup.find("a", "Qfxief") or soup.find("a", "Si6A0c Gy4nib"))["href"]
         )
-        app_icon = (
-            results[0]
-            .findNext("div", "Vpfmgd")
-            .findNext("div", "uzcko")
-            .img["data-src"]
+        dev_link = (
+            "https://play.google.com/store/apps/developer?id="
+            + dev_name.replace(" ", "+")
         )
-        app_details = "<a href='" + app_icon + "'>📲&#8203;</a>"
-        app_details += f" <b>{app_name}</b>"
-        app_details += (
-            "\n\n<code>Developer :</code> <a href='"
-            + app_dev_link
-            + "'>"
-            + app_dev
-            + "</a>"
+        review = soup.find("div", "g1rdde")
+        downloads = soup.findAll("div", "ClM7O")
+        info = soup.find("div", "omXQ6c")
+    except AttributeError:
+        return await edit_delete(
+            event, "No result found in search. Please enter **Valid app name**"
         )
-        app_details += "\n<code>Rating :</code> " + app_rating.replace(
-            "Rated ", "⭐ "
-        ).replace(" out of ", "/").replace(" stars", "", 1).replace(
-            " stars", "⭐ "
-        ).replace(
-            "five", "5"
-        )
-        app_details += (
-            "\n<code>Features :</code> <a href='"
-            + app_link
-            + "'>View in Play Store</a>"
-        )
-        app_details += f"\n\n===> {ALIVE_NAME} <==="
-        await event.edit(app_details, link_preview=True, parse_mode="HTML")
-    except IndexError:
-        await event.edit("No result found in search. Please enter **Valid app name**")
-    except Exception as err:
-        await event.edit(f"Exception Occured:- {str(err)}")
+    downloads = f"{downloads[1].text}  downloads" if downloads else None
+    info = info.text if info else None
+    review = review.text if review else None
+    bgurl = bg_url_1 if downloads and review else bg_url_2
+    if len(app_name) > 14:
+        app_name = f"{app_name[:14]}..."
+    pic_name = "playstore.png"
+    logo = "app_logo.png"
+    urllib.request.urlretrieve(app_icon, logo)
+    urllib.request.urlretrieve(bgurl, pic_name)
+    background = Image.open(pic_name).resize((1024, 512))
+
+    thumbmask = Image.new("RGBA", (1024, 512), 0)
+    thumbmask.paste(background, (0, 0))
+
+    thumb, x = ellipse_create(logo, 4, 0)
+    thumbmask.paste(thumb, (680, 53), thumb)
+
+    text_draw("temp/GoogleSans-Medium.ttf", 65, thumbmask, app_name, 50, 30)
+    text_draw("temp/GoogleSans-Medium.ttf", 30, thumbmask, dev_name, 60, 120, "red")
+    text_draw("temp/GoogleSans-Medium.ttf", 70, thumbmask, f"{rating} / 5", 190, 260)
+    if bgurl == bg_url_1:
+        text_draw("temp/GoogleSans-Medium.ttf", 35, thumbmask, review, 80, 420)
+        text_draw("temp/GoogleSans-Medium.ttf", 30, thumbmask, downloads, 415, 420)
+    thumbmask.save(pic_name)
+    os.remove(logo)
+    app_details = f" <b>{fullapp_name}</b>"
+    app_details += f"\n\n<b>Developer :</b> <a href= {dev_link}>{dev_name}</a>"
+    app_details += f"\n<b>Rating :</b> <code>{rating} ⭐ </code>"
+    app_details += f"\n\n<b>Features :</b> <a href= {app_link}>View in Play Store</a>"
+    await event.delete()
+    await event.client.send_file(
+        event.chat_id, pic_name, caption=app_details, parse_mode="HTML"
+    )
+    os.remove(pic_name)
