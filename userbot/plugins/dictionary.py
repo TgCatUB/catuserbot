85c969a379f13bb3a7e4d0477621b539ce96c5e2
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2022-06-27 17:45:46 +0000

    pylint: auto fixes

--- userbot/plugins/dictionary.py
+++ userbot/plugins/dictionary.py
@@ -32,7 +32,11 @@ async def _(event):
         result = f"**Text: {_format.replacetext(word)}**\n**Meaning:**\n`{_format.replacetext(definition)}`\n\n**Example:**\n`{_format.replacetext(example)}`"
         await edit_or_reply(event, result)
     except IndexError:
-        await edit_delete(event, text="`Sorry pal, we couldn't find meaning for the word you were looking for.`", time=10)
+        await edit_delete(
+            event,
+            text="`Sorry pal, we couldn't find meaning for the word you were looking for.`",
+            time=10,
+        )
     except Exception as e:
         await edit_delete(event, text="`The Urban Dictionary API could not be reached`")
         LOGS.info(e)
@@ -52,12 +56,12 @@ async def _(event):
     try:
         ft = f"<b>Search Query: </b><code>{word.title()}</code>\n\n"
         response = await AioHttp().get_json(
-                    f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}",
-                )
+            f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}",
+        )
         if "message" not in response:
             result = response[0]
             if "phonetic" in result:
-                phonetic = result['phonetic']
+                phonetic = result["phonetic"]
                 if phonetic:
                     ft += f"<b>Phonetic: </b>\n<code>{phonetic}</code>\n\n"
             meanings = result["meanings"]
@@ -65,12 +69,12 @@ async def _(event):
             antonyms = []
             for content in meanings:
                 ft += f"<u><b>Meaning ({content['partOfSpeech']}):</b></u>\n"
-                for count, text in enumerate(content['definitions'], 1):
+                for count, text in enumerate(content["definitions"], 1):
                     ft += f"<b>{count}.</b> <i>{text['definition']}</i>\n"
-                if content['synonyms']:
-                    synonyms.extend(content['synonyms'])
-                if content['antonyms']:
-                    antonyms.extend(content['antonyms'])
+                if content["synonyms"]:
+                    synonyms.extend(content["synonyms"])
+                if content["antonyms"]:
+                    antonyms.extend(content["antonyms"])
                 ft += "\n"
             if synonyms:
                 ft += f"<b>Synonyms: </b><code>{', '.join(synonyms)}</code>\n"
