028ace55422410f3ea30d01bb8d1a6748d3429c3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2022-06-27 21:10:39 +0530

    Update utils.py

--- userbot/helpers/utils/utils.py
+++ userbot/helpers/utils/utils.py
@@ -34,5 +34,5 @@ def run_async(loop, coro):
 
 def runasync(func: callable):
     """Run async functions with the right event loop."""
-    asyncio.get_event_loop()
+    loop = asyncio.get_event_loop()
     return loop.run_until_complete(func)
