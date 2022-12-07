8a9931fc15e6f3ca6c00f653a1d687216decf16e
Jisan <66010486+Jisan09@users.noreply.github.com>
2022-12-08 02:14:01 +0530

    .img fix

--- userbot/helpers/google_image_download.py
+++ userbot/helpers/google_image_download.py
@@ -22,6 +22,7 @@ from urllib.request import HTTPError, Request, URLError, urlopen
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 http.client._MAXHEADERS = 1000
 args_list = [
@@ -489,9 +490,9 @@ class googleimagesdownload:
         lines = data.split("\n")
         return json.loads(lines[3])[0][2]
 
-    def _image_objects_from_pack(self, data):
-        image_objects = json.loads(data)[31][-1][12][2]
-        image_objects = [x for x in image_objects if x[0] == 1]
+    def _image_objects_from_pack(self,data):
+        image_objects = json.loads(data)[56][-1][0][0][1][0]
+        image_objects = [x[0][0]['444383007'] for x in image_objects if x[0][0]['444383007'][0] == 1]
         return image_objects
 
     # Downloading entire Web Document (Raw Page Content)
@@ -570,7 +571,7 @@ class googleimagesdownload:
         time.sleep(1)
         print("Getting you a lot of images. This may take a few moments...")
 
-        element = browser.find_element_by_tag_name("body")
+        element = browser.find_element(By.TAG_NAME, "body")
         # Scroll down
         for _ in range(50):
             element.send_keys(Keys.PAGE_DOWN)
