68e4c7033a15559672859511c3eabff162731306
Jisan <66010486+Jisan09@users.noreply.github.com>
2023-04-26 02:10:28 +0530

    sanga

--- userbot/helpers/functions/functions.py
+++ userbot/helpers/functions/functions.py
@@ -74,18 +74,14 @@ def rand_key():
     return str(uuid4())[:8]
 
 
-async def sanga_seperator(sanga_list):
-    for i in sanga_list:
-        if i.startswith("🔗"):
-            sanga_list.remove(i)
-    s = 0
-    for i in sanga_list:
-        if i.startswith("Username History"):
-            break
-        s += 1
-    usernames = sanga_list[s:]
-    names = sanga_list[:s]
-    return names, usernames
+def sanga_seperator(sanga_list):
+    string = ""
+    for info in sanga_list:
+        string += info[info.find('\n')+1:]
+    string = re.sub(r'^$\n', '', string, flags=re.MULTILINE)
+    name, username = string.split("Usernames")
+    name = name.split("Names")[1]
+    return name, username
 
 
 # covid india data
