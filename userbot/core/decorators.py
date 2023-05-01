41b754106ee80a8b474be95e7b8aba54219f26c6
Jisan <66010486+Jisan09@users.noreply.github.com>
2023-05-01 11:11:57 +0530

    Update decorators.py

--- userbot/core/decorators.py
+++ userbot/core/decorators.py
@@ -17,27 +17,30 @@ from ..sql_helper.globals import gvarstatus
 from .data import _vcusers_list
 
 
-def check_owner(func, vc=False):
-    async def wrapper(c_q: CallbackQuery):
-        if c_q.query.user_id and (
-            c_q.query.user_id == Config.OWNER_ID
-            or c_q.query.user_id in Config.SUDO_USERS
-            or (vc and c_q.query.user_id in _vcusers_list)
-        ):
-            try:
-                await func(c_q)
-            except FloodWaitError as e:
-                await asyncio.sleep(e.seconds + 5)
-            except MessageNotModifiedError:
-                pass
-        else:
-            HELP_TEXT = (
-                gvarstatus("HELP_TEXT")
-                or "Only My Master can Access This !!\n\nDeploy your own Catuserbot."
-            )
-            await c_q.answer(
-                HELP_TEXT,
-                alert=True,
-            )
+def check_owner(vc=False):
+    def decorator(func):
+        async def wrapper(c_q: CallbackQuery):
+            if c_q.query.user_id and (
+                c_q.query.user_id == Config.OWNER_ID
+                or c_q.query.user_id in Config.SUDO_USERS
+                or (vc and c_q.query.user_id in _vcusers_list)
+            ):
+                try:
+                    await func(c_q)
+                except FloodWaitError as e:
+                    await asyncio.sleep(e.seconds + 5)
+                except MessageNotModifiedError:
+                    pass
+            else:
+                HELP_TEXT = (
+                    gvarstatus("HELP_TEXT")
+                    or "Only My Master can Access This !!\n\nDeploy your own Catuserbot."
+                )
+                await c_q.answer(
+                    HELP_TEXT,
+                    alert=True,
+                )
 
-    return wrapper
+        return wrapper
+
+    return decorator
