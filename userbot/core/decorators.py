c657a6a45e6c63abf45a688c1f9612a3a45059ca
Jisan <66010486+Jisan09@users.noreply.github.com>
2023-05-01 12:59:18 +0530

    Update decorators.py

--- userbot/core/decorators.py
+++ userbot/core/decorators.py
@@ -22,7 +22,7 @@ class check_owner:
         self.func = func
         self.vc = vc
 
-    def __call__(self, vc=False):
+    def __call__(self, query: CallbackQuery):
         async def wrapper(c_q: CallbackQuery):
             if c_q.query.user_id and (
                 c_q.query.user_id == Config.OWNER_ID
@@ -45,4 +45,4 @@ class check_owner:
                     alert=True,
                 )
 
-        return wrapper
+        return wrapper(query)
