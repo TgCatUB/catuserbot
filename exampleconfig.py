cf917eb0230f753dcbccaf4d65a85126a81f44c4
imstarboi <aminshahdi90@proton.me>
2023-09-17 00:12:51 +0330

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -6,9 +6,9 @@ class Development(Config):
     APP_ID = 9406309
     API_HASH = "38446d78262fab1a64662a3ae149403e"
     # the name to display in your alive message
-    ALIVE_NAME = "Amnhj"
+    ALIVE_NAME = "Aminbeee"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://ucjrevjy:moaXV2VNQSyNd-AwTWGzCoC4gPcdXLVX@berry.db.elephantsql.com/ucjrevjy"
+    DB_URI = "postgres://clsaebvp:YVzoWi6j44vLukvNNpPPNP0R9SEM95ND@bubble.db.elephantsql.com/clsaebvp"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
     STRING_SESSION = "1BJWap1sBu65XAhs5zNkzHqcT99PqoWwjWlnX4t6PDTIg1zD5RdOXnFjedhPFPabglZnxs-5oJ8chDLZd9sHkEYRC5LbNIFWcbWSssVtq61wndSZtkjrDobFcJk_qf8yAX4ny_9vd3HweFJenHdaFbtZs2lyw4GVWJFQgiBMPhYz9DNXym5bt49Up7Oab6heSysBPPt_2opa6sgJSlqSd2QnrcPfBgUKqtX8dQgnlW9zNXYZtE20s5_0uLKVGPmPBF6K0vCsLP3pg8ZvurBfY4VidjbJLIljU73HkaSP2iP8PE5omxv9txbhTcPMcmlDl74OZgpF45_qkKxUdUYaWxBtfiYByslI="
     # create a new bot in @botfather and fill the following vales with bottoken
